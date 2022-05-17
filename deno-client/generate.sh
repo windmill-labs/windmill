5de325ebf758d441336e6f3c0330e351736bb4f4
Ruben Fiszel <ruben@rubenfiszel.com>
2022-05-17 16:00:09 +0200

    test workflow

--- deno-client/generate.sh
+++ deno-client/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-#openapi-generator-cli generate -i ../backend/openapi.yaml -g typescript --additional-properties platform=deno -o windmill-api
+openapi-generator-cli generate -i ../backend/openapi.yaml -g typescript --additional-properties platform=deno -o windmill-api
 sed -i 's/this\.type = "Job";//' windmill-api/models/Job.ts
