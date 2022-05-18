b69ca7a8220ef7ce832ee651b534c59acc04bf11
Ruben Fiszel <ruben@rubenfiszel.com>
2022-05-18 10:09:41 +0200

    fix: clean exported deno-client api

--- deno-client/index.ts
+++ deno-client/index.ts
@@ -1,8 +1,16 @@
 import { ResourceApi, VariableApi, ServerConfiguration } from './windmill-api/index.ts'
 import { createConfiguration, type Configuration as Configuration } from './windmill-api/configuration.ts'
 
-export * from './windmill-api/index.ts'
+export {
+    AdminApi, AuditApi, FlowApi, GranularAclApi, GroupApi,
+    JobApi, ResourceApi, VariableApi, ScriptApi, ScheduleApi, SettingsApi,
+    UserApi, WorkspaceApi
+} from './windmill-api/index.ts'
 
+/**
+ * Create a client configuration from env variables
+ * @returns client configuration
+ */
 export function createConf(): Configuration & { workspace_id: string } {
     const token = Deno.env.get("WM_TOKEN") ?? 'no_token'
     const base_url = Deno.env.get("BASE_URL") ?? 'http://localhost:8000'
@@ -14,19 +22,29 @@ export function createConf(): Configuration & { workspace_id: string } {
     }
 }
 
+/**
+ * Get a resource value by path
+ * @param path path of the resource
+ * @returns resource value
+ */
 export async function getResource(path: string): Promise<any> {
     const conf = createConf()
     const resource = await new ResourceApi(conf).getResource(conf.workspace_id, path)
     return await transformLeaves(resource.value)
 }
 
+/**
+ * Get a variable by path
+ * @param path path of the variable
+ * @returns variable value
+ */
 export async function getVariable(path: string): Promise<string | undefined> {
     const conf = createConf()
     const variable = await new VariableApi(conf).getVariable(conf.workspace_id, path)
     return variable.value
 }
 
-export async function transformLeaves(d: { [key: string]: any }): Promise<{ [key: string]: any }> {
+async function transformLeaves(d: { [key: string]: any }): Promise<{ [key: string]: any }> {
     for (const k in d) {
         d[k] = await _transformLeaf(d[k])
     }
