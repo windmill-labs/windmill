58e2a5c1799c6ecbad09ec294e29a6e1d06b870d
Ruben Fiszel <ruben@rubenfiszel.com>
2022-05-24 14:24:15 +0200

    add cloudflare pages redirection

---
+++ functions/api/[[path]].ts
@@ -0,0 +1,17 @@
+//this is purely for cloudflare pages redirection purposes
+
+export async function onRequest(context: EventContext<unknown, string, unknown>) {
+    // Contents of context object
+    const {
+        request, // same as existing Worker API
+        env, // same as existing Worker API
+        params, // if filename includes [id] or [[path]]
+        waitUntil, // same as ctx.waitUntil in existing Worker API
+        next, // used for middleware or to fetch assets
+        data, // arbitrary space for passing data between middlewares
+    } = context
+
+    const url = new URL(request.url)
+    url.hostname = "app.windmill.dev"
+    return fetch(url.toString(), { method: request.method, body: request.body, headers: request.headers })
+}
