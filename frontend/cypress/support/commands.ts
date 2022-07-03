394546c797bfc0328b3aa6b978664c81afd40971
Ruben Fiszel <ruben@rubenfiszel.com>
2022-07-03 19:26:25 +0200

    baseUrl

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -1,5 +1,5 @@
 Cypress.Commands.add('login', (email: string, password: string) => {
-	cy.visit(`${Cypress.env('baseUrl')}/user/login`)
+	cy.visit(`/user/login`)
 	cy.get('#showPassword').click()
 	cy.get('#email').type('admin@windmill.dev')
 	cy.get('#password').type('changeme')
