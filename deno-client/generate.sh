5b89abe28283238a282da8920580a72f25e5a360
Ruben Fiszel <ruben@rubenfiszel.com>
2022-07-08 12:45:51 +0200

    fix(deno-client): fix stringify

--- deno-client/generate.sh
+++ deno-client/generate.sh
@@ -3,4 +3,4 @@ set -e
 
 /usr/local/bin/docker-entrypoint.sh generate -i ../backend/openapi.yaml -g typescript --additional-properties platform=deno -o windmill-api
 sed -i 's/this\.type = "Job";//' windmill-api/models/Job.ts
-sed -i '439 i \        if (mediaType === "text/plain") { return rawData }' windmill-api/models/ObjectSerializer.ts
+sed -i '440 i \        if (mediaType === "text/plain") { return rawData }' windmill-api/models/ObjectSerializer.ts
