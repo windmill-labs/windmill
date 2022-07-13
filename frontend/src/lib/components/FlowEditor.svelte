5502047474c5a896a835ba286ea893440ff3713f
Faton Ramadani <faton.ramadani14@gmail.com>
2022-07-13 11:29:43 +0200

    Refactor flow UI/UX + added fork and create script from inline script (#175)
    * Refactor flow UI/UX + added fork and create script from inline script
    
    * Prevent infinite loop when remove steps
    
    * Fix forking a script from the Hub
    
    * Fix viewing code of  a script from the Hub
    
    * Fix PR comments
    
    * Fix code highlight
    
    * Fix path
    
    * Find next available path
    
    * Fix copy first step schema

--- frontend/src/lib/components/FlowEditor.svelte
+++ frontend/src/lib/components/FlowEditor.svelte
@@ -1,73 +1,47 @@
 <script lang="ts">
-	import type { Schema } from '$lib/common'
-	import { FlowModuleValue, type Flow } from '$lib/gen'
-	import { loadSchema } from '$lib/scripts'
-	import { emptySchema } from '$lib/utils'
 	import { faPlus } from '@fortawesome/free-solid-svg-icons'
 	import Icon from 'svelte-awesome'
 	import FlowPreview from './FlowPreview.svelte'
+	import CopyFirstStepSchema from './flows/CopyFirstStepSchema.svelte'
+	import { addModule, flowStore } from './flows/flowStore'
 	import ModuleStep from './ModuleStep.svelte'
 	import SchemaEditor from './SchemaEditor.svelte'
-	import type SchemaForm from './SchemaForm.svelte'
-
-	export let flow: Flow
 
 	let args: Record<string, any> = {}
-	let schemas: Schema[] = []
-	let schemaForms: (SchemaForm | undefined)[] = []
-
-	function addModule() {
-		schemaForms.push(undefined)
-
-		let newModule = {
-			value: { type: FlowModuleValue.type.SCRIPT, path: '' },
-			input_transform: {}
-		}
-		flow.value.modules = flow.value.modules.concat(newModule)
-		schemas.push(emptySchema())
-	}
+	$: numberOfSteps = $flowStore?.value.modules.length - 1
 </script>
 
-<!-- <PageHeader title="Flow" /> -->
 <div class="flow-root bg-gray-50 rounded-xl border  border-gray-200">
 	<ul class="relative -mt-10">
 		<span class="absolute top-0 left-1/2  h-full w-1 bg-gray-400" aria-hidden="true" />
 		<div class="relative">
-			<li class="flex flex-row flex-shrink max-w-full  mx-auto mt-20">
+			<li class="flex flex-row flex-shrink max-w-full mx-auto mt-20">
 				<div class="bg-white border border-gray xl-rounded shadow-lg w-full mx-4 xl:mx-20">
 					<div
 						class="flex items-center justify-between flex-wra px-4 py-5 border-b border-gray-200 sm:px-6"
 					>
 						<h3 class="text-lg leading-6 font-medium text-gray-900">Flow Input</h3>
-						<button
-							class="text-xs default-button-secondary max-h-6 place-self-end"
-							disabled={flow.value.modules.length == 0 ||
-								flow.value.modules[0].value.path == undefined}
-							on:click={async () => {
-								flow.schema = await loadSchema(flow.value.modules[0].value.path ?? '')
-							}}
-							>Copy from step 1's schema
-						</button>
+						<CopyFirstStepSchema />
 					</div>
 					<div class="p-4">
-						<SchemaEditor bind:schema={flow.schema} />
+						<SchemaEditor bind:schema={$flowStore.schema} />
 						<div class="my-4" />
-						<FlowPreview {flow} i={flow.value.modules.length - 1} bind:args />
+						<FlowPreview bind:flow={$flowStore} i={numberOfSteps} bind:args />
 					</div>
 				</div>
 			</li>
-			{#each flow.value.modules as mod, i}
-				<ModuleStep bind:flow bind:mod bind:schemas bind:schemaForms bind:args {i} />
+			{#each $flowStore?.value.modules as mod, i}
+				<ModuleStep bind:mod bind:args {i} />
 			{/each}
 			<li class="relative m-20 ">
-				<div class="absolute inset-0 flex items-center" aria-hidden="true">
-					<div class="w-full border-t border-gray-300" />
-				</div>
 				<div class="relative flex justify-center">
 					<button
 						class="default-button h-10 w-10 shadow-blue-600/40  border-blue-600 shadow"
-						on:click={addModule}><Icon class="text-white mb-1" data={faPlus} scale={0.9} /></button
+						on:click={() => addModule()}
 					>
+						<Icon class="text-white mb-1" data={faPlus} />
+						Add step
+					</button>
 				</div>
 			</li>
 		</div>
---
+++ frontend/src/lib/components/flows/DynamicInputHelpBox.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+	import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+	import Icon from 'svelte-awesome'
+	import { slide } from 'svelte/transition'
+
+	$: opened = false
+</script>
+
+<div class="text-xs flex flex-row-reverse">
+	<span
+		class="underline mr-4"
+		on:click={() => {
+			opened = !opened
+		}}
+	>
+		Help
+		<Icon class="ml-2" data={opened ? faChevronUp : faChevronDown} scale={0.7} />
+	</span>
+</div>
+
+{#if opened}
+	<div
+		transition:slide
+		class="bg-gray-100 border-l-4 border-gray-600 text-gray-700 p-4 m-4"
+		role="alert"
+	>
+		<p class="font-bold">Dynamic arg help</p>
+		<p>
+			When a field is "dynamic", its value is computed dynamically as the evaluation of its
+			corresponding typescript snippet.
+		</p>
+		That snippet can be single line:
+		<pre><code>last_result.myarg</code></pre>
+		or multiline:
+		<pre><code
+				>let x = 5;
+x + 2</code
+			></pre>
+		<p>
+			If it is multiline, the last statement before the final expression<b
+				>MUST END WITH ; and a newline</b
+			>
+		</p>
+		The snippet can also be a string template:
+		<pre><code
+				>`Hello $&#123;params.name&#125;, all your base $&#123;previous_result.base_name&#125;
+belong to us`</code
+			></pre>
+		However, the last line must always be the final expression.
+		<p>
+			The snippet can use any typescript primitives, and the following flow specific objects and
+			functions:
+		</p>
+		<ul class="ml-4">
+			<li>
+				<b>previous_result</b>: the object containing the result of the previous step
+			</li>
+			<li><b>flow_input</b>: the object containing the flow input arguments</li>
+			<li><b>params</b>: the object containing the current step static values</li>
+			<li>
+				<b>step(n)</b>: the function returning the result of step number n. One can also use a
+				negative n. step(0) == flow_input , step(-1) == previous_result
+			</li>
+			<li>
+				<b>variable(path)</b>: the function returning the variable (including secrets) at given path
+				as a string
+			</li>
+			<li>
+				<b>resource(path)</b>: the function returning the resource at a given path as an object
+			</li>
+		</ul>
+	</div>
+{/if}
