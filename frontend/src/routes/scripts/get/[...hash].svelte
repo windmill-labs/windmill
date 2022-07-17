2b712e6ce5d6c829496e073722be76ecfa048cd6
Ruben Fiszel <ruben@rubenfiszel.com>
2022-07-18 00:44:58 +0200

    feat: add run_wait_result to mimic lambda ability

--- frontend/src/routes/scripts/get/[...hash].svelte
+++ frontend/src/routes/scripts/get/[...hash].svelte
@@ -275,7 +275,7 @@
 				<p class="text-gray-700">
 					<a href="/scripts/get/{script?.hash}">{script?.hash}</a>
 				</p>
-				<span>Webhook to run this script:</span>
+				<span>Webhook to run this script and get job's uuid as response:</span>
 				<Tooltip class="font-normal mx-1"
 					>Send a POST http request with a token as bearer token and the args respecting the
 					corresponding jsonschema as payload. To create a permanent token, go to your user setting
@@ -291,6 +291,19 @@
 				<pre><code
 						><a href="/api/w/{$workspaceStore}/jobs/run/p/{script?.path}"
 							>/api/w/{$workspaceStore}/jobs/run/p/{script?.path}</a
+						></code
+					></pre>
+				<span>Endpoint to run this script and get job's result as response:</span>
+				<Tooltip class="font-normal mx-1"
+					>Send a POST http request with a token as bearer token and the args respecting the
+					corresponding jsonschema as payload. To create a permanent token, go to your user setting
+					by clicking your username on the top-left. For more info about openapi, see <a
+						href="https://docs.windmill.dev/openapi/run-script-by-hash">openapi doc</a
+					></Tooltip
+				>
+				<pre><code
+						><a href="/api/w/{$workspaceStore}/jobs/run_wait_result/p/{script?.path}"
+							>/api/w/{$workspaceStore}/jobs/run_wait_result/p/{script?.path}</a
 						></code
 					></pre>
 			</div>
