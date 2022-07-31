803fb510c45aca7b1921cd31893fa8f6f56a4ecc
Ruben Fiszel <ruben@rubenfiszel.com>
2022-07-31 22:47:07 +0200

    update current job to in progress

--- backend/src/worker_flow.rs
+++ backend/src/worker_flow.rs
@@ -280,7 +280,7 @@ pub async fn update_flow_status_in_progress(
             SET flow_status = jsonb_set(flow_status, '{{modules, {step}, job}}', $1)
             WHERE id = $2 AND workspace_id = $3",
     ))
-    .bind(job_in_progress.to_string())
+    .bind(json!(job_in_progress.to_string()))
     .bind(flow)
     .bind(w_id)
     .execute(db)
