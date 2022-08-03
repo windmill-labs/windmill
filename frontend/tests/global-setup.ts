9aa4fba3691bd6b852b3ad1382ed1bfdb4c566e9
Faton Ramadani <faton.ramadani14@gmail.com>
2022-08-03 16:43:27 +0200

    Fix login test

--- frontend/tests/global-setup.ts
+++ frontend/tests/global-setup.ts
@@ -5,6 +5,11 @@ async function globalSetup(config: FullConfig) {
 	const browser = await chromium.launch()
 	const page = await browser.newPage()
 	await page.goto('http://localhost:8000/user/login')
+
+	if (!(await page.locator('#email').isVisible())) {
+		page.locator('text=login without third-party').click()
+	}
+
 	await page.locator('#email').fill('admin@windmill.dev')
 	await page.locator('input[type="password"]').fill('changeme')
 	await page.locator('text="Login"').click()
