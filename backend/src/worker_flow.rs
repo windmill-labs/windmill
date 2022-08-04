5cc1ae2c05bfd368749b357ceded86f2c174ebf2
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-05 00:46:10 +0200

    fix flowstatusviewer and updating to in_progress much sooner

--- backend/src/worker_flow.rs
+++ backend/src/worker_flow.rs
@@ -283,10 +283,11 @@ pub async fn update_flow_status_in_progress(
     let step = get_step_of_flow_status(db, flow).await?;
     sqlx::query(&format!(
         "UPDATE queue
-            SET flow_status = jsonb_set(flow_status, '{{modules, {step}, job}}', $1)
-            WHERE id = $2 AND workspace_id = $3",
+            SET flow_status = jsonb_set(jsonb_set(flow_status, '{{modules, {step}, job}}', $1), '{{modules, {step}, type}}', $2)
+            WHERE id = $3 AND workspace_id = $4",
     ))
     .bind(json!(job_in_progress.to_string()))
+    .bind(json!("InProgress"))
     .bind(flow)
     .bind(w_id)
     .execute(db)
