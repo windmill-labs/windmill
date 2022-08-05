80e28dbba3e77154c0017bd8e74d144e6aae13fb
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-05 17:13:05 +0200

    fix: improve flow viewer with retrieving hub script

--- frontend/src/lib/components/FlowViewer.svelte
+++ frontend/src/lib/components/FlowViewer.svelte
@@ -30,6 +30,7 @@
 
 	export let tab: 'ui' | 'json' | 'schema' = 'ui'
 	let open: { [id: number]: boolean } = {}
+	let hubCode: { [id: number]: string } = {}
 
 	function toAny(x: unknown): any {
 		return x as any
@@ -47,36 +48,40 @@
 	/>
 {/if}
 {#if tab == 'ui'}
-	<div class="flow-root w-full p-4">
-		<p class="font-black text-lg w-full ml-2">
-			<span>Inputs</span>
-		</p>
-		{#if flow.schema && flow.schema.properties && Object.keys(flow.schema.properties).length > 0 && flow.schema}
-			<ul class="my-4 ml-6">
-				{#each Object.entries(flow.schema.properties) as [inp, v]}
-					<li class="list-disc flex flex-row">
-						<FieldHeader
-							label={inp}
-							required={flow.schema.required?.includes(inp)}
-							type={toAny(v)?.type}
-							contentEncoding={toAny(v)?.contentEncoding}
-							format={toAny(v)?.format}
-							itemsType={toAny(v)?.itemsType}
-						/><span class="ml-4 mt-2 text-xs"
-							>{toAny(v)?.default != undefined
-								? 'default: ' + JSON.stringify(toAny(v)?.default)
-								: ''}</span
-						>
-					</li>
-				{/each}
-			</ul>
-		{:else}
-			<div class="text-gray-700 text-xs italic mb-4">
-				This script has no argument or is ill-defined
-			</div>
+	<div class="flow-root w-full pb-4">
+		{#if !embedded}
+			<h2 class="mt-4">{flow.summary}</h2>
+			<h3 class="mt-4">{flow.description}</h3>
+
+			<p class="font-black text-lg w-full my-4">
+				<span>Inputs</span>
+			</p>
+			{#if flow.schema && flow.schema.properties && Object.keys(flow.schema.properties).length > 0 && flow.schema}
+				<ul class="my-2">
+					{#each Object.entries(flow.schema.properties) as [inp, v]}
+						<li class="list-disc flex flex-row">
+							<FieldHeader
+								label={inp}
+								required={flow.schema.required?.includes(inp)}
+								type={toAny(v)?.type}
+								contentEncoding={toAny(v)?.contentEncoding}
+								format={toAny(v)?.format}
+								itemsType={toAny(v)?.itemsType}
+							/><span class="ml-4 mt-2 text-xs"
+								>{toAny(v)?.default != undefined
+									? 'default: ' + JSON.stringify(toAny(v)?.default)
+									: ''}</span
+							>
+						</li>
+					{/each}
+				</ul>
+			{:else}
+				<div class="text-gray-700 text-xs italic mb-4">No inputs</div>
+			{/if}
 		{/if}
-		<p class="font-black text-lg my-6 w-full ml-2">
-			<span>{flow?.value?.modules?.length} Steps </span>
+
+		<p class="font-black text-lg my-6 w-full">
+			<span>{flow?.value?.modules?.length} Step{flow?.value?.modules?.length > 1 ? 's' : ''} </span>
 			<span class="mt-4" />
 		</p>
 		<ul class="-mb-8 w-full">
@@ -105,6 +110,31 @@
 												href={scriptPathToHref(mod?.value?.path ?? '')}
 												class="font-medium text-gray-900">{mod?.value?.path}</a
 											>
+											{#if mod?.value?.path?.startsWith('hub/')}
+												<div>
+													<button
+														on:click={async () => {
+															open[i] = !open[i]
+														}}
+														class="mb-2 underline text-black"
+													>
+														View code {open[i] ? '(-)' : '(+)'}</button
+													>
+													{#if open[i]}
+														<div class="w-full h-full">
+															<iframe
+																style="height: 400px;"
+																class="w-full h-full  text-sm"
+																title="embedded script from hub"
+																frameborder="0"
+																src="https://hub.windmill.dev/embed/script/{mod?.value?.path?.substring(
+																	4
+																)}"
+															/>
+														</div>
+													{/if}
+												</div>
+											{/if}
 										{:else if mod?.value?.type == 'rawscript'}
 											<button
 												on:click={() => (open[i] = !open[i])}
@@ -124,7 +154,7 @@
 										{:else if mod?.value?.type == 'flow'}
 											Flow at path {mod?.value?.path}
 										{:else if mod?.value?.type == 'forloopflow'}
-											For loop over step {i}'s result':
+											For loop over all the elements of the list returned as a result of step {i}:
 											<svelte:self flow={mod?.value} embedded={true} />
 										{/if}
 									</p>
