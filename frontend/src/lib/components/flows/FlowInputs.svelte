07deaad202e431b4fe35567dd6aea7ceaa353c62
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-08 09:25:54 +0200

    New Postgres SQL query

--- frontend/src/lib/components/flows/FlowInputs.svelte
+++ frontend/src/lib/components/flows/FlowInputs.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { RawScript } from '$lib/gen'
 
-	import { faCode } from '@fortawesome/free-solid-svg-icons'
+	import { faCode, faRepeat } from '@fortawesome/free-solid-svg-icons'
 	import { createEventDispatcher } from 'svelte'
 	import FlowScriptPicker from './pickers/FlowScriptPicker.svelte'
 	import PickHubScript from './pickers/PickHubScript.svelte'
@@ -16,20 +16,34 @@
 	<PickScript {isTrigger} on:pick />
 	<PickHubScript {isTrigger} on:pick />
 	<FlowScriptPicker
-		label="New Typescript {isTrigger ? 'trigger ' : ''}script (Deno)"
+		label={`Create a for-loop here (coming soon)`}
+		disabled={true}
+		icon={faRepeat}
+		iconColor="text-blue-500"
+	/>
+
+	<FlowScriptPicker
+		label={`New Postgres SQL query`}
 		icon={faCode}
 		iconColor="text-blue-800"
-		on:click={() => dispatch('new', { language: RawScript.language.DENO })}
+		on:click={() => dispatch('pick', { path: 'hub/130/execute_custom_query_postgresql' })}
 	/>
 
 	<FlowScriptPicker
 		disabled={isTrigger}
 		label="New Python {isTrigger ? 'trigger ' : ''}script (3.10)"
 		icon={faCode}
-		iconColor="text-yellow-500"
+		iconColor="text-green-500"
 		on:click={() => dispatch('new', { language: RawScript.language.PYTHON3 })}
 		tooltip={isTrigger
 			? 'Python is not supported for trigger scripts yet but is supported for every other steps'
 			: undefined}
+	/>
+
+	<FlowScriptPicker
+		label="New Typescript {isTrigger ? 'trigger ' : ''}script (Deno)"
+		icon={faCode}
+		iconColor="text-blue-800"
+		on:click={() => dispatch('new', { language: RawScript.language.DENO })}
 	/>
 </div>
