807f4b325a885ae13771e1f069c0123e06c684b0
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-17 09:34:29 +0200

    fix styling

--- frontend/src/routes/flows.svelte
+++ frontend/src/routes/flows.svelte
@@ -184,18 +184,18 @@
 			<div class="shadow p-4 my-2">
 				{#if sectionTab == 'personal'}
 					<h2 class="">
-						My personal space ({`u/${$userStore?.username}`})
+						Personal <span class="text-sm">({`u/${$userStore?.username}`})</span>
 					</h2>
 					<p class="italic text-xs text-gray-600 mb-4">
 						All flows owned by you (and visible only to you if you do not explicitly share them)
 					</p>
 				{:else if sectionTab == 'groups'}
-					<h2 class="">Groups that I am member of</h2>
+					<h2 class="">Groups</h2>
 					<p class="italic text-xs text-gray-600">
 						All flows being owned by groups that you are member of
 					</p>
 				{:else if sectionTab == 'shared'}
-					<h2 class="">Shared with me</h2>
+					<h2 class="">Shared</h2>
 					<p class="italic text-xs text-gray-600">
 						All flows visible to you because they have been shared to you
 					</p>
@@ -238,7 +238,7 @@
 				{#each groupedFlows.filter((x) => tabFromPath(x[0]) == sectionTab) as [section, flows]}
 					{#if sectionTab != 'personal'}
 						<h3 class="mt-2 mb-2">
-							owner: {section}
+							{section}
 							{#if section == 'g/all'}
 								<Tooltip
 									>'g/all' is the namespace for the group all. Every user is a member of all.
@@ -248,10 +248,8 @@
 							{/if}
 						</h3>
 					{/if}
-					{#if flows.length == 0}
-						<p class="text-xs text-gray-600 font-black">
-							No flows for this owner space yet. To create one, click on the top right button.
-						</p>
+					{#if flows.length == 0 && sectionTab == 'personal'}
+						<p class="text-xs text-gray-600 italic">No flows yet</p>
 					{:else}
 						<div class="grid md:grid-cols-2 gap-4 sm:grid-cols-1 2xl:grid-cols-3">
 							{#each flows as { summary, path, extra_perms, canWrite }}
@@ -323,8 +321,8 @@
 													href="/flows/run/{path}"
 												>
 													<div class="inline-flex items-center justify-center">
-														<Icon data={faPlay} scale={0.5} />
-														<span class="pl-1">Run...</span>
+														<Icon data={faPlay} scale={0.6} />
+														<span class="pl-1">Run</span>
 													</div>
 												</a>
 											</div>
