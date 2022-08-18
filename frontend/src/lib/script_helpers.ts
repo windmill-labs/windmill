f9ea4eb475cc165d0ef454997aff05a0a61d9ddb
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-18 09:30:24 +0200

    change default typescript

--- frontend/src/lib/script_helpers.ts
+++ frontend/src/lib/script_helpers.ts
@@ -31,6 +31,7 @@ def main(name: str = "Nicolas Bourbaki",
 `
 export const DENO_INIT_CODE = `
 // reload the smart assistant on the top right if it dies to get autocompletion and syntax highlighting
+// (Ctrl+space to cache dependencies on imports hover).
 // to import most npm packages without deno.land, use esm:
 // import { toWords } from "https://esm.sh/number-to-words"
 // import * as wmill from 'https://deno.land/x/windmill@v${__pkg__.version}/mod.ts'
