72ce94b5dd5dba229e30a42207cbc591240dc4eb
Ruben Fiszel <ruben@rubenfiszel.com>
2022-08-31 22:15:00 +0200

    migrate

--- frontend/src/lib/components/flows/flowStore.ts
+++ frontend/src/lib/components/flows/flowStore.ts
@@ -1,4 +1,4 @@
-import type { Flow, FlowModule, ForloopFlow } from '$lib/gen'
+import type { Flow, FlowModule, ForloopFlow, InputTransform } from '$lib/gen'
 import { get, writable } from 'svelte/store'
 import { flowStateStore, initFlowState } from './flowState'
 
@@ -8,17 +8,27 @@ export const flowStore = writable<Flow>(undefined)
 
 export function initFlow(flow: Flow) {
 	for (const mod of flow.value.modules) {
+		migrateInitTransform(mod)
 		let val = mod.value
 		if (val.type == 'forloopflow') {
 			let flowVal = val as ForloopFlow & { value?: { modules?: FlowModule[] } }
 			if (flowVal.value && flowVal.value.modules) {
 				flowVal.modules = flowVal.value.modules
+				flowVal.modules.forEach(migrateInitTransform)
 				flowVal.value = undefined
 			}
 		}
 	}
 	flowStore.set(flow)
 	initFlowState(flow)
+
+	function migrateInitTransform(mod: FlowModule) {
+		let modVal = mod as FlowModule & { input_transform?: Record<string, InputTransform>} 
+		if (modVal.input_transform) {
+			modVal.input_transforms = modVal.input_transform
+			modVal.input_transform = undefined
+		}
+	}
 }
 
 export async function copyFirstStepSchema() {
