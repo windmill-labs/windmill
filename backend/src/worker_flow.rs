1f0c3742694c6f043c74d86cade89196021ecc5b
Ruben Fiszel <ruben@rubenfiszel.com>
2022-09-13 11:50:54 +0200

    v2

--- backend/src/worker_flow.rs
+++ backend/src/worker_flow.rs
@@ -633,15 +633,15 @@ async fn push_next_flow_job(
 
     let mut transform_context: Option<(String, Vec<String>)> = None;
     let mut args = if compute_input_transform {
-        let (token, steps) =
-            get_transform_context(&db, &flow_job, &status, &mut transform_context).await?;
+        transform_context = Some(get_transform_context(&db, &flow_job, &status).await?);
+        let (token, steps) = transform_context.as_ref().unwrap();
         transform_input(
             &flow_job.args,
             last_result.clone(),
             &module.input_transforms,
             &flow_job.workspace_id,
             &token,
-            steps,
+            steps.to_vec(),
         )
         .await?
     } else {
@@ -657,9 +657,12 @@ async fn push_next_flow_job(
     {
         match status_module {
             FlowStatusModule::WaitingForPriorSteps => {
+                let (token, steps) = if let Some(x) = transform_context {
+                    x
+                } else {
+                    get_transform_context(&db, &flow_job, &status).await?
+                };
                 /* Iterator is an InputTransform, evaluate it into an array. */
-                let (token, steps) =
-                    get_transform_context(&db, &flow_job, &status, &mut transform_context).await?;
                 let itered = iterator
                     .clone()
                     .evaluate_with(
@@ -865,30 +868,26 @@ async fn get_transform_context(
     db: &DB,
     flow_job: &QueuedJob,
     status: &FlowStatus,
-    transform_context: &mut Option<(String, Vec<String>)>,
 ) -> error::Result<(String, Vec<String>)> {
-    if transform_context.is_none() {
-        let new_token = create_token_for_owner(
-            db,
-            &flow_job.workspace_id,
-            &flow_job.permissioned_as,
-            "transform-input",
-            10,
-            &flow_job.created_by,
-        )
-        .await?;
-        let new_steps = status
-            .modules
-            .iter()
-            .map(|x| match x {
-                FlowStatusModule::Success { job, .. } => job.to_string(),
-                _ => "invalid step status".to_string(),
-            })
-            .collect();
+    let new_token = create_token_for_owner(
+        db,
+        &flow_job.workspace_id,
+        &flow_job.permissioned_as,
+        "transform-input",
+        10,
+        &flow_job.created_by,
+    )
+    .await?;
+    let new_steps = status
+        .modules
+        .iter()
+        .map(|x| match x {
+            FlowStatusModule::Success { job, .. } => job.to_string(),
+            _ => "invalid step status".to_string(),
+        })
+        .collect();
 
-        *transform_context = Some((new_token, new_steps));
-    }
-    Ok(transform_context.as_ref().unwrap().clone())
+    Ok((new_token, new_steps))
 }
 
 impl InputTransform {
