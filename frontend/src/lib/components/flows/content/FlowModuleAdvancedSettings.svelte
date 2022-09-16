381191442fecc6ee101e4f31694bdaaa01fc2ad6
Faton Ramadani <faton.ramadani14@gmail.com>
2022-09-16 14:00:02 +0200

    fix(frontend): Support of suspend & stop expression + restore import/export menu (#580)
    * feature(frontend): Various UI fix + restore removed features
    
    * feature(frontend): WIP Prop picker panel
    
    * feature(frontend): Advanced flow module tab
    
    * feature(frontend): Add import/export flow menu
    
    * feature(frontend): Remove summary edition + add a enable/disable button for suspend

---
+++ frontend/src/lib/components/flows/content/FlowModuleAdvancedSettings.svelte
@@ -0,0 +1,72 @@
+<script lang="ts">
+	import SimpleEditor from '$lib/components/SimpleEditor.svelte'
+
+	import Toggle from '$lib/components/Toggle.svelte'
+
+	import type { FlowModule } from '$lib/gen'
+
+	export let flowModule: FlowModule
+</script>
+
+<div class="space-y-4 ">
+	{#if flowModule.suspend}
+		<button
+			class="flex items-center  text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 focus:outline-none "
+			on:click={() => {
+				flowModule.suspend = undefined
+			}}
+		>
+			Disable suspend
+		</button>
+		<div class="font-bold text-sm">Suspend</div>
+
+		<input bind:value={flowModule.suspend} type="number" min="1" placeholder="0" />
+	{:else}
+		<button
+			class="flex items-center  text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 focus:outline-none "
+			on:click={() => {
+				flowModule.suspend = 1
+			}}
+		>
+			Enable suspend
+		</button>
+	{/if}
+	{#if flowModule.stop_after_if}
+		<button
+			class="flex items-center  text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 focus:outline-none "
+			on:click={() => {
+				flowModule.stop_after_if = undefined
+			}}
+		>
+			Disable stop expression
+		</button>
+		<div class="flex flex-col">
+			<div class="font-bold text-sm mb-2">Stop condition expression</div>
+
+			<SimpleEditor
+				lang="javascript"
+				bind:code={flowModule.stop_after_if.expr}
+				class="few-lines-editor border"
+			/>
+			<Toggle
+				bind:checked={flowModule.stop_after_if.skip_if_stopped}
+				options={{
+					right: 'Should skip if stopped'
+				}}
+				on:change
+			/>
+		</div>
+	{:else}
+		<button
+			class="flex items-center  text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-1 focus:outline-none "
+			on:click={() => {
+				flowModule.stop_after_if = {
+					skip_if_stopped: false,
+					expr: 'result == undefined'
+				}
+			}}
+		>
+			Enable stop expression
+		</button>
+	{/if}
+</div>
