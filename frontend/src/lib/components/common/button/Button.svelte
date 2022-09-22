983ceb5faffbcd90d865382ae4aa72be2e281476
Faton Ramadani <faton.ramadani14@gmail.com>
2022-09-22 11:05:04 +0200

    feature(frontend): Ajust style

--- frontend/src/lib/components/common/button/Button.svelte
+++ frontend/src/lib/components/common/button/Button.svelte
@@ -16,7 +16,7 @@
 	function getColorClasses() {
 		switch (color) {
 			case 'blue':
-				return 'bg-blue-500 hover:bg-blue-700 focus:ring-blue-300'
+				return 'bg-blue-500 hover:bg-blue-700 focus:ring-blue-300 text-white '
 			case 'light':
 				return 'text-gray-800 bg-white hover:bg-gray-100 focus:ring-gray-300'
 			default:
@@ -45,7 +45,7 @@
 		class: classNames(
 			getColorClasses(),
 			getSizeClasses(),
-			'text-white focus:ring-4 font-medium',
+			'focus:ring-4 font-medium',
 			'px-4 py-2',
 			'rounded-md',
 			'flex justify-center items-center',
