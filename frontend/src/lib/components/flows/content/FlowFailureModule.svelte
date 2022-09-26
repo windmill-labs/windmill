5e2f23c66f2c4a08791bc431841683405d8840f5
Faton Ramadani <faton.ramadani14@gmail.com>
2022-09-26 18:36:15 +0200

    feat(frontend): Add support for failure modules (#612)
    * feature(frontend): Failure module
    
    * feature(frontend): Fix wording + Remove advanced tab for failure modules + fix failure module test
    
    * feature(frontend): Fix wording + add toggle in the mini map + stick component at the bottom
    
    * feat(frontend): Add summary to failure module
    
    * feat(frontend): Add support for Failure module in the FlowModuleViewer
    
    * feat(frontend): Add support for FailureModule in the status viewer
    
    * feat(frontend): Fix building issues

---
+++ frontend/src/lib/components/flows/content/FlowFailureModule.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { getContext } from 'svelte'
+
+	import { flowStateStore } from '../flowState'
+	import { flowStore } from '../flowStore'
+	import type { FlowEditorContext } from '../types'
+
+	import FlowModule from './FlowModule.svelte'
+
+	const { previewArgs } = getContext<FlowEditorContext>('FlowEditorContext')
+</script>
+
+{#if $flowStore.value.failure_module}
+	<FlowModule
+		args={previewArgs}
+		bind:flowModule={$flowStore.value.failure_module}
+		bind:flowModuleState={$flowStateStore.failureModule}
+		on:delete={() => {
+			$flowStore.value.failure_module = undefined
+		}}
+	/>
+{/if}
--- frontend/src/lib/components/flows/content/FlowModule.svelte
+++ frontend/src/lib/components/flows/content/FlowModule.svelte
@@ -91,7 +91,9 @@
 		{#if shouldPick}
 			<FlowInputs
 				shouldDisableTriggerScripts={parentIndex != 0}
-				shouldDisableLoopCreation={childIndex !== undefined || parentIndex === 0}
+				shouldDisableLoopCreation={childIndex !== undefined ||
+					parentIndex === 0 ||
+					$selectedId.includes('failure')}
 				on:loop={() => {
 					applyCreateLoop()
 					select(['loop', $selectedId].join('-'))
@@ -139,7 +141,9 @@
 						<Tabs selected="inputs">
 							<Tab value="inputs">Inputs</Tab>
 							<Tab value="test">Test</Tab>
-							<Tab value="advanced">Advanced</Tab>
+							{#if !$selectedId.includes('failure')}
+								<Tab value="advanced">Advanced</Tab>
+							{/if}
 
 							<svelte:fragment slot="content">
 								<div class="overflow-hidden bg-white" style="height:calc(100% - 32px);">
