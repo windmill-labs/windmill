dc2bbca2695acd93c7147047adedfb0c31b9c31a
Ádám Kovács <43071496+adam-kov@users.noreply.github.com>
2022-09-26 17:18:17 +0200

    feat(frontend): Script page action row (#626)
    * feat(frontend): Update button component styles
    
    * feat(frontend): Use button component
    
    * fix(frontend): Re-export button types
    
    * feat(frontend): Use action row in script page
    
    * fix(frontend): Center the action row
    
    * fix(frontend): Revert to previous event forwarding
    
    * feature(frontend): Add action row to flow page
    
    * fix(frontend): Restore button red color

---
+++ frontend/src/lib/components/common/button/model.ts
@@ -0,0 +1,6 @@
+export namespace Button {
+	export type Size = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+	export type Color = 'blue' | 'red' | 'dark' | 'light'
+	export type Variant = 'contained' | 'border'
+	export type Target = '_self' | '_blank'
+}
