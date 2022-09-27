deb0b47a5f0f7b450b65ebd7003a2bdf9f81c798
Faton Ramadani <faton.ramadani14@gmail.com>
2022-09-27 17:52:02 +0200

    fix(frontend): Hide the editor panel when we are editing a PathScript (#631)
    * fix(frontend): Hide the editor panel when we are editing a PathScript
    
    * feat(front): Remove viewCode action and display the code in the Editor panel

---
+++ frontend/src/lib/components/flows/content/FlowModuleScript.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import HighlightCode from '$lib/components/HighlightCode.svelte'
+	import type { FlowModule } from '$lib/gen'
+	import { getScriptByPath } from '$lib/utils'
+
+	export let flowModule: FlowModule
+
+	let code: string
+	let language: 'deno' | 'python3' | 'go'
+
+	async function loadCode(module: FlowModule) {
+		if (module.value.type == 'script') {
+			const script = await getScriptByPath(module.value.path!)
+			code = script.content
+			language = script.language
+		} else {
+			throw Error('Not a script')
+		}
+	}
+
+	$: flowModule && loadCode(flowModule)
+</script>
+
+<div class="flex flex-col flex-1 h-full overflow-auto">
+	<HighlightCode {language} {code} />
+</div>
