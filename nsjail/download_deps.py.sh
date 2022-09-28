f651fbd48c0c8827674c116296176cc312ef3e81
Ruben Fiszel <ruben@rubenfiszel.com>
2022-09-28 08:16:53 +0200

    add private registries pip

--- nsjail/download_deps.py.sh
+++ nsjail/download_deps.py.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
+INDEX_URL_ARG=$([ -z "$INDEX_URL" ] && echo ""|| echo "-i $INDEX_URL" )
+EXTRA_INDEX_URL_ARG=$([ -z "$EXTRA_INDEX_URL" ] && echo ""|| echo "-i $EXTRA_INDEX_URL" )
+TRUSTED_HOST_ARG=$([ -z "$TRUSTED_HOST" ] &&  echo "" || echo "--trusted-host $TRUSTED_HOST")
+
 mkdir -p /tmp/dependencies
 touch /tmp/dependencies/_windmill
 /usr/local/bin/python3 -m pip install --cache-dir /tmp/.cache/pip -t /tmp/dependencies -r /user/requirements.txt\
-    --no-color --isolated --no-warn-conflicts --disable-pip-version-check
+    --no-color --isolated --no-warn-conflicts --disable-pip-version-check $INDEX_URL $EXTRA_INDEX_URL_ARG $TRUSTED_HOST_ARG
 
 mv /tmp/dependencies/* /out
