deba20b1b885fb3598441612b0dbcd5884acab42
Faton Ramadani <faton.ramadani14@gmail.com>
2022-09-28 09:20:04 +0200

    feat(front): Simplify styles

--- frontend/src/lib/components/common/confirmationModal/ConfirmationModal.svelte
+++ frontend/src/lib/components/common/confirmationModal/ConfirmationModal.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { classNames } from '$lib/utils'
+	import { faWarning } from '@fortawesome/free-solid-svg-icons'
 	import { createEventDispatcher } from 'svelte'
+	import Icon from 'svelte-awesome'
 
 	export let title: string
 	export let description: string
@@ -11,12 +13,7 @@
 	const dispatch = createEventDispatcher()
 </script>
 
-<div
-	class={classNames('relative', open ? 'z-50' : '-z-50')}
-	aria-labelledby="modal-title"
-	role="dialog"
-	aria-modal="true"
->
+<div class={classNames('relative', open ? 'z-50' : '-z-50')} role="dialog">
 	<div
 		class={classNames(
 			'fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity',
@@ -38,22 +35,7 @@
 					<div
 						class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
 					>
-						<!-- Heroicon name: outline/exclamation-triangle -->
-						<svg
-							class="h-6 w-6 text-red-600"
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M12 10.5v3.75m-9.303 3.376C1.83 19.126 2.914 21 4.645 21h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 4.88c-.866-1.501-3.032-1.501-3.898 0L2.697 17.626zM12 17.25h.007v.008H12v-.008z"
-							/>
-						</svg>
+						<Icon data={faWarning} class="text-red-500" />
 					</div>
 					<div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
 						<h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">
