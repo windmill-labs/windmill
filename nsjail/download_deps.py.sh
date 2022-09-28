ddfbd19b932405a4d483f59c97c5449664412ea5
Ruben Fiszel <ruben@rubenfiszel.com>
2022-09-28 08:47:09 +0200

    echo parameters

--- nsjail/download_deps.py.sh
+++ nsjail/download_deps.py.sh
@@ -4,9 +4,20 @@ INDEX_URL_ARG=$([ -z "$INDEX_URL" ] && echo ""|| echo "--index-url $INDEX_URL" )
 EXTRA_INDEX_URL_ARG=$([ -z "$EXTRA_INDEX_URL" ] && echo ""|| echo "--extra-index-url $EXTRA_INDEX_URL" )
 TRUSTED_HOST_ARG=$([ -z "$TRUSTED_HOST" ] &&  echo "" || echo "--trusted-host $TRUSTED_HOST")
 
-echo $INDEX_URL_ARG
-echo $EXTRA_INDEX_URL_ARG
-echo $TRUSTED_HOST_ARG
+if [ ! -z "$INDEX_URL" ]
+then
+      echo "\$INDEX_URL is set to $INDEX_URL"
+fi
+
+if [ ! -z "$EXTRA_INDEX_URL" ]
+then
+      echo "\$EXTRA_INDEX_URL is set to $EXTRA_INDEX_URL"
+fi
+
+if [ ! -z "$TRUSTED_HOST" ]
+then
+      echo "\$TRUSTED_HOST is set to $TRUSTED_HOST"
+fi
 
 mkdir -p /tmp/dependencies
 touch /tmp/dependencies/_windmill
