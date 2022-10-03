9d1d2aa157955a08400678c97c655e90037e9038
Faton Ramadani <faton.ramadani14@gmail.com>
2022-10-03 10:02:51 +0200

    feat(front): Init dirtyStore to true when creating a flow/script

--- frontend/src/routes/flows/add.svelte
+++ frontend/src/routes/flows/add.svelte
@@ -8,6 +8,7 @@
 
 <script lang="ts">
 	import { page } from '$app/stores'
+	import { dirtyStore } from '$lib/components/common/confirmationModal/dirtyStore'
 
 	import FlowBuilder from '$lib/components/FlowBuilder.svelte'
 	import { initFlow } from '$lib/components/flows/flowStore'
@@ -55,6 +56,8 @@
 	}
 
 	loadFlow()
+
+	$dirtyStore = true
 </script>
 
 <FlowBuilder />
