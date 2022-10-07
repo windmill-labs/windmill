d5c4d71027aec1dbec600ac32b475fb6ab47b0c4
Ádám Kovács <vartefol@gmail.com>
2022-10-07 08:51:32 +0200

    fix(frontend): Remove dark mode

--- frontend/src/lib/components/common/kbd/Kbd.svelte
+++ frontend/src/lib/components/common/kbd/Kbd.svelte
@@ -1,7 +1,6 @@
 <kbd
 	class="mx-1 px-2 py-1.5 text-xs font-semibold text-gray-800 
-	bg-gray-100 border border-gray-200 rounded-lg 
-	dark:bg-gray-600 dark:text-gray-100 dark:border-gray-500 {$$props.class}"
+	bg-gray-100 border border-gray-200 rounded-lg {$$props.class}"
 >
 	<slot />
 </kbd>
