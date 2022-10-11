9acee22b1fc0b4eb82a1b47bc62598fe5af076e1
Ádám Kovács <43071496+adam-kov@users.noreply.github.com>
2022-10-11 15:38:50 +0200

    feat(frontend): Loading placeholder (#707)
    * feat(frontend): Add skeleton component
    
    * feat(frontend): Add loading to script page
    
    * fix(frontend): Fix skeleton positioning
    
    * feat(frontend): Remove static content from loading
    
    * feat(frontend): Add loading to the main pages
    
    * fix(frontend): Use skeleton directive on leaf node
    
    * fix(frontend): Skeleton alignment
    
    * fix(front): Replace skeleton directive with class

---
+++ frontend/src/lib/components/common/skeleton/SkeletonElement.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { HEIGHT_UNIT, type ISkeletonElement } from './model'
+
+	export let element: ISkeletonElement
+</script>
+
+<div
+	class="animate-skeleton"
+	style="height: {element.h * HEIGHT_UNIT}px; width: {element.w}%; min-width: {element.minW}px;"
+/>
