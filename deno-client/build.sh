0b89ec79aa1a854f429b5eba1d2260b05a81a49e
Jakub Kołodziejczak <31549762+mrl5@users.noreply.github.com>
2022-10-22 14:01:50 +0000

    ci(deno-client): fix build.sh to have reproducible builds (#792)

--- deno-client/build.sh
+++ deno-client/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+npm ci --ignore-scripts
 npx --yes openapi-typescript-codegen --input ../backend/openapi.yaml \
  --output ./src --useOptions \
  && sed -i '213 i \\    request.referrerPolicy = \"no-referrer\"\n' src/core/request.ts
