45761f8e0cabdf41ba0f7c370c6888b84311a545
Ruben Fiszel <ruben@rubenfiszel.com>
2022-10-24 16:37:09 +0200

    approval flow with idendity of the approver

--- frontend/src/routes/approve/[workspace]/[job]/[resume]/[hmac]@none.svelte
+++ frontend/src/routes/approve/[workspace]/[job]/[resume]/[hmac]@none.svelte
@@ -7,18 +7,22 @@
 	import FlowMetadata from '$lib/components/FlowMetadata.svelte'
 	import FlowModulesViewer from '$lib/components/FlowModulesViewer.svelte'
 	import JobArgs from '$lib/components/JobArgs.svelte'
-	import { onMount } from 'svelte'
+	import { onDestroy, onMount } from 'svelte'
+	import Tooltip from '$lib/components/Tooltip.svelte'
 
 	let job: Job | undefined = undefined
-	let currentApprovers: string[] = []
+	let currentApprovers: { resume_id: number; approver: string }[] = []
 	let approver = $page.url.searchParams.get('approver') ?? undefined
 
 	let completed: boolean = false
 	$: completed = job?.type == 'CompletedJob'
 
+	let timeout: NodeJS.Timer | undefined = undefined
+
 	getJob()
 
 	onMount(() => {
+		timeout = setInterval(getJob, 1000)
 		window.onunhandledrejection = (event: PromiseRejectionEvent) => {
 			event.preventDefault()
 
@@ -34,6 +38,10 @@
 				console.log('Caught unhandled promise rejection without message', event)
 			}
 		}
+	})
+
+	onDestroy(() => {
+		timeout && clearInterval(timeout)
 	})
 
 	async function getJob() {
@@ -82,7 +90,17 @@
 				<h2 class="mt-4">Current approvers</h2>
 				<div class="my-4">
 					{#if currentApprovers.length > 0}
-						{currentApprovers.join(', ')}
+						<ul>
+							{#each currentApprovers as approver}
+								<li
+									><b
+										>{approver.approver}<Tooltip
+											>Unique id of approval: {approver.resume_id}</Tooltip
+										></b
+									></li
+								>
+							{/each}
+						</ul>
 					{:else}
 						<p class="text-sm"
 							>No current approvers for this step (approval steps can require more than one
@@ -106,19 +124,24 @@
 			{/if}
 		</div>
 		{#if completed}
-			<div class="my-2"><p><b>The flow is already completed</b></p></div>
+			<div class="my-2"
+				><p><b>The flow is not running anymore. You cannot cancel or resume it.</b></p></div
+			>
 		{/if}
 
 		<div class="w-max-md flex flex-row gap-x-4 gap-y-4 justify-between w-full flex-wrap mt-2">
 			<Button btnClasses="grow" color="red" on:click|once={cancel} size="md" disabled={completed}
 				>Disapprove/Cancel</Button
 			>
-			<Button btnClasses="grow" on:click|once={resume} size="md" disabled={completed}
+			<Button btnClasses="grow" color="green" on:click|once={resume} size="md" disabled={completed}
 				>Approve/Resume</Button
 			>
 		</div>
 
-		<div class="mt-4"><a href="https://windmill.dev">Learn more about Windmill</a></div>
+		<div class="mt-4 flex flex-row flex-wrap justify-between"
+			><a href="https://windmill.dev">Learn more about Windmill</a>
+			<a target="_blank" href="/run/{job?.id}">Flow run details (require auth)</a>
+		</div>
 		{#if job && job.raw_flow}
 			<h2 class="mt-10">Flow details</h2>
 			<FlowModulesViewer
