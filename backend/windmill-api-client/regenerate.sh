fb60c2da09ca4d277dbafe3c866e627fc6b909a0
Kai Jellinghaus <contact@kaij.tech>
2022-10-26 12:29:33 +0200

    Try generating more files

--- backend/windmill-api-client/regenerate.sh
+++ backend/windmill-api-client/regenerate.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-swagger-cli bundle ../windmill-api/openapi.yaml | openapi-generator generate -g rust -o ./ -i /dev/stdin --global-property apis --global-property models
+swagger-cli bundle ../windmill-api/openapi.yaml | openapi-generator generate -g rust -o ./ -i /dev/stdin --global-property apis --global-property models --global-property supportingFiles="mod.rs:lib.rs" --global-property modelDocs=false
