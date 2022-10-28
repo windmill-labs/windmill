f036af578849e0734882bf2c845312185ecc8912
Ruben Fiszel <ruben@rubenfiszel.com>
2022-10-28 11:20:12 +0200

    make going to source script/flow easier

--- frontend/src/lib/components/FlowMetadata.svelte
+++ frontend/src/lib/components/FlowMetadata.svelte
@@ -3,7 +3,14 @@
 	import JobStatus from '$lib/components/JobStatus.svelte'
 	import Icon from 'svelte-awesome'
 	import { displayDaysAgo } from '$lib/utils'
-	import { faCalendar, faClock, faRobot, faUser, faWind } from '@fortawesome/free-solid-svg-icons'
+	import {
+		faCalendar,
+		faClock,
+		faRobot,
+		faScroll,
+		faUser,
+		faWind
+	} from '@fortawesome/free-solid-svg-icons'
 
 	export let job: Job
 	const SMALL_ICON_SCALE = 0.7
@@ -43,6 +50,19 @@
 				></span
 			>
 		{/if}
+
+		{#if (job && job.job_kind == 'flow') || job?.job_kind == 'script'}
+			{@const stem = `/${job?.job_kind}s`}
+			{@const isScript = job?.job_kind === 'script'}
+			{@const viewHref = `${stem}/get/${isScript ? job?.script_hash : job?.script_path}`}
+			<div>
+				<Icon class="text-gray-700" data={faScroll} scale={SMALL_ICON_SCALE} /><span class="mx-2">
+					{job?.job_kind}:
+					<a href={viewHref}>{isScript ? job?.script_hash : job?.script_path}</a>
+				</span>
+			</div>
+		{/if}
+
 		<div>
 			<Icon class="text-gray-700" data={faUser} scale={SMALL_ICON_SCALE} /><span class="mx-2">
 				By {job.created_by}
