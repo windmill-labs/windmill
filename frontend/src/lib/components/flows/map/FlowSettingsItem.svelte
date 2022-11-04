41b77833c52cf6c597afb1a9d36465a704f43cb7
Ruben Fiszel <ruben@rubenfiszel.com>
2022-11-04 01:21:26 +0100

    UX improvements on schedule item

--- frontend/src/lib/components/flows/map/FlowSettingsItem.svelte
+++ frontend/src/lib/components/flows/map/FlowSettingsItem.svelte
@@ -4,6 +4,7 @@
 	import Icon from 'svelte-awesome'
 	import { faCalendarAlt, faSliders } from '@fortawesome/free-solid-svg-icons'
 	import { classNames } from '$lib/utils'
+	import { Badge } from '$lib/components/common'
 
 	const { select, selectedId, schedule } = getContext<FlowEditorContext>('FlowEditorContext')
 
@@ -16,14 +17,20 @@
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div on:click={() => select('settings')} class={settingsClass}>
 	<Icon data={faSliders} class="mr-2" />
-	<span class="text-xs font-bold flex flex-row justify-between w-full flex-wrap gap-2 items-center">
+	<span
+		class="text-xs font-bold flex flex-row justify-between w-full  gap-2 items-center flex-wrap"
+	>
 		Settings
 		<span
 			class={classNames('badge', $schedule?.enabled ? 'badge-on' : 'badge-off')}
 			on:click|stopPropagation={() => select('settings-schedule')}
 		>
-			{$schedule.cron}
-			<Icon class={$schedule.cron ? 'ml-2' : ''} data={faCalendarAlt} scale={0.8} />
+			{#if $schedule?.enabled}
+				<Badge color="gray"
+					>{$schedule.cron}
+					<Icon class={$schedule.cron ? 'ml-2' : ''} data={faCalendarAlt} scale={0.8} />
+				</Badge>
+			{/if}
 		</span>
 	</span>
 </div>
