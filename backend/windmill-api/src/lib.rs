e08ca0423cd6f4a11f64f7f19795661dea82b0e2
Jakub Kołodziejczak <31549762+mrl5@users.noreply.github.com>
2022-11-07 01:29:37 +0100

    refactor(backend): improve memory footprint related to csp (#866)

--- backend/windmill-api/src/lib.rs
+++ backend/windmill-api/src/lib.rs
@@ -47,7 +47,6 @@ const GIT_VERSION: &str = git_version!(args = ["--tag", "--always"], fallback = 
 struct BaseUrl(String);
 pub struct IsSecure(bool);
 pub struct CloudHosted(bool);
-#[derive(Clone)]
 pub struct ContentSecurityPolicy(String);
 
 pub use users::delete_expired_items_perdiodically;
@@ -90,9 +89,9 @@ pub async fn run_server(
         .layer(Extension(Arc::new(IsSecure(
             base_url.starts_with("https://"),
         ))))
-        .layer(Extension(ContentSecurityPolicy(
+        .layer(Extension(Arc::new(ContentSecurityPolicy(
             std::env::var("SERVE_CSP").unwrap_or("".to_owned()),
-        )))
+        ))))
         .layer(Extension(http_client))
         .layer(CookieManagerLayer::new());
     // build our application with a route
