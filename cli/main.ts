25750700829c1bbb8fd8dc832fc44294a1be2ea8
Kai Jellinghaus <contact@kaij.tech>
2022-11-07 16:02:49 +0100

    feat(cli): Tarball pull (#867)
    * Add tarball pull
    
    * Move command & prompt on conflict
    
    * Remove old file
    
    Co-authored-by: Ruben Fiszel <ruben@rubenfiszel.com>

--- cli/main.ts
+++ cli/main.ts
@@ -13,6 +13,7 @@ import user from "./user.ts";
 import setup from "./setup.ts";
 import variable from "./variable.ts";
 import push from "./push.ts";
+import pull from "./pull.ts";
 
 const VERSION = "v1.45.0";
 
@@ -71,6 +72,7 @@ await new Command()
   .command("setup", setup)
   .command("variable", variable)
   .command("push", push)
+  .command("pull", pull)
   .command(
     "upgrade",
     new UpgradeCommand({
