14854fec061ff4e976b4844f5b0b1943f0662930
Ruben Fiszel <ruben@rubenfiszel.com>
2022-11-10 14:35:51 +0100

    fix

--- backend/migrations/20221024225533_apps.up.sql
+++ backend/migrations/20221024225533_apps.up.sql
@@ -7,16 +7,9 @@ CREATE TABLE app (
 	path varchar(255) NOT NULL,
     summary VARCHAR(1000) NOT NULL DEFAULT '',
     policy JSONB NOT NULL,
-    versions BIGINT[] NOT NULL,
-    execution_mode EXECUTION_MODE NOT NULL DEFAULT 'publisher',
-    extra_perms JSONB NOT NULL DEFAULT '{}'
-);
-
-CREATE TABLE app_version(
-    id BIGSERIAL PRIMARY KEY,
-    flow_id BIGINT NOT NULL,
     value JSONB NOT NULL,
 	created_by VARCHAR(50) NOT NULL,
 	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    FOREIGN KEY (flow_id) REFERENCES app(id) ON DELETE CASCADE
+    execution_mode EXECUTION_MODE NOT NULL DEFAULT 'publisher',
+    extra_perms JSONB NOT NULL DEFAULT '{}'
 );
