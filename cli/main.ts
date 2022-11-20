3e01fe6eae0c827e2ba2ecd446f1e7678a985108
rubenfiszel <rubenfiszel@users.noreply.github.com>
2022-11-20 18:37:51 +0000

    Apply automatic changes

--- cli/main.ts
+++ cli/main.ts
@@ -15,7 +15,7 @@ import variable from "./variable.ts";
 import push from "./push.ts";
 import pull from "./pull.ts";
 
-const VERSION = "v1.49.0";
+const VERSION = "v1.49.1";
 
 await new Command()
   .name("wmill")
