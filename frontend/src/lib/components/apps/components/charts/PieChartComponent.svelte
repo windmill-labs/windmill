cb1d3f182d82490bf4718d8be49c233a94a92079
Faton Ramadani <faton.ramadani14@gmail.com>
2022-11-21 15:02:59 +0100

    fix(frontend): temporary fix

--- frontend/src/lib/components/apps/components/charts/PieChartComponent.svelte
+++ frontend/src/lib/components/apps/components/charts/PieChartComponent.svelte
@@ -40,25 +40,17 @@
 
 	function populateDataSet() {
 		if (dataSetValue) {
-			/*
-			Object.keys(dataSetValue).map((key) => {
-				// remove string properties
-				if (dataSetValue && typeof dataSetValue[key] === 'string') {
-					delete dataSetValue[key]
-				}
-			})
-			*/
-
 			data.datasets = [
 				{
-					data: Object.values(dataSetValue),
+					data: Object.values(dataSetValue).filter((val) => typeof val === 'number'),
 					backgroundColor: ['#F7464A', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360', '#AC64AD'],
 					hoverBackgroundColor: ['#FF5A5E', '#5AD3D1', '#FFC870', '#A8B3C5', '#616774', '#DA92DB']
 				}
 			]
-			data.labels = Object.keys(dataSetValue).map((s) =>
-				s.replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2')
-			)
+			data.labels = Object.keys(dataSetValue)
+				.filter((key) => typeof dataSetValue?.[key] === 'number')
+				.map((s) => s.replace(/([A-Z]+)*([A-Z][a-z])/g, '$1 $2'))
+
 			data = data
 		}
 	}
