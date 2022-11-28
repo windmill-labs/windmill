5e67e9040092c5a631bc770a847613c0e5737fba
Kai Jellinghaus <contact@kaij.tech>
2022-11-28 14:24:01 +0100

    Center & Size Loading Windmill Icon

--- frontend/src/routes/user/cli-success.svelte
+++ frontend/src/routes/user/cli-success.svelte
@@ -1,12 +1,30 @@
 <script lang="ts">
 	import { goto } from '$app/navigation'
+	import WindmillIcon from '$lib/components/icons/WindmillIcon.svelte'
 
 	import { sendUserToast } from '$lib/utils'
 	import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 	import Icon from 'svelte-awesome'
 
 	sendUserToast('Sucess. You can return to your terminal now.')
-	goto('/')
+	// goto('/')
 </script>
 
-<Icon data={faSpinner} class={`animate-spin`} />
+<div class="my-grid">
+	<div class="my-center">
+		<div class="animate-spin"><WindmillIcon width="100%" height="100%" /></div>
+	</div>
+</div>
+
+<style>
+	.my-grid {
+		display: grid;
+		grid-template-areas: 'a a a' 'b c d' 'e e e';
+		grid-auto-rows: 1fr;
+		grid-auto-columns: 1fr;
+		aspect-ratio: 1/1;
+	}
+	.my-center {
+		grid-area: c;
+	}
+</style>
