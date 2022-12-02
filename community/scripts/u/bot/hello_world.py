dee9a3bb4aac72233f9b972c7a94576701193f43
Ruben Fiszel <ruben@rubenfiszel.com>
2022-12-02 22:55:07 +0100

    ci: remove community/ and pull-hub ci

--- community/scripts/u/bot/hello_world.py
+++
@@ -1,6 +0,0 @@
-import wmill
-
-def main(name: str = "Nicolas Bourbaki"):
-	print(f"Hello World and a warm welcome especially to {name}")
-	print("The env variable at `g/all/pretty_secret`:", wmill.get_variable("g/all/pretty_secret"))
-	return {"len": len(name), "splitted": name.split() }
--- community/scripts/u/bot/http_request.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-def main(
-    my_value: str
-):
-    headers={'Authorization': "Bearer: {}".format(os.environ.get("WM_TOKEN"))}
-    r = requests.post('https://httpbin.org/post', data={'key': my_value})
-    return r.text
