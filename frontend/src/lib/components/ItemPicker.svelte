0ec465ca7ec1ce6a7af2195fa0d010895149aeeb
Ruben Fiszel <ruben@rubenfiszel.com>
2022-12-03 11:03:47 +0100

    skeletons for home search

--- frontend/src/lib/components/ItemPicker.svelte
+++ frontend/src/lib/components/ItemPicker.svelte
@@ -60,72 +60,70 @@
 				{#each new Array(6) as _}
 					<Skeleton layout={[[2], 0.7]} />
 				{/each}
-			{:else}
-				{#if !items?.length}
-					<div class="text-center text-sm text-gray-600 mt-2">
-						{@html noItemMessage}
-					</div>
-				{:else if filteredItems?.length}
-					<ul class="divide-y divide-gray-200">
-						{#each filteredItems as obj}
-							<li class="flex flex-row w-full">
-								<button
-									class="py-4 px-1 gap-1 flex flex-row grow border  border-gray-300 border-opacity-0 
+			{:else if !items?.length}
+				<div class="text-center text-sm text-gray-600 mt-2">
+					{@html noItemMessage}
+				</div>
+			{:else if filteredItems?.length}
+				<ul class="divide-y divide-gray-200">
+					{#each filteredItems as obj}
+						<li class="flex flex-row w-full">
+							<button
+								class="py-4 px-1 gap-1 flex flex-row grow border  border-gray-300 border-opacity-0 
 									hover:bg-white hover:border-opacity-100 text-black"
-									on:click={() => {
-										if (closeOnClick) {
-											drawer.closeDrawer()
-										}
-										pickCallback(obj['path'], obj[extraField])
-									}}
-								>
-									{#if `app` in obj}
-										<div class="mr-2 text-sm text-left truncate w-24">
-											<IconedResourceType after={true} silent={false} name={obj['app']} />
-										</div>
-									{/if}
-									{#if `resource_type` in obj}
-										<div class="mr-2 truncate text-left w-24 text-sm">
-											<IconedResourceType after={true} name={obj['resource_type']} />
-										</div>
-									{/if}
-									<div class="flex flex-col">
-										<div class="text-sm font-semibold flex flex-col">
-											<span class="mr-2 text-left">{obj[extraField] ?? ''}</span>
-											{#if extraField != 'path'}
-												<span class="font-normal text-xs text-left italic overflow-hidden"
-													>{obj['path'] ?? ''}</span
-												>
-											{/if}
-										</div>
-										{#if extraField != 'description'}
-											<div class="text-xs font-light italic text-left">{obj['description'] ?? ''}</div
+								on:click={() => {
+									if (closeOnClick) {
+										drawer.closeDrawer()
+									}
+									pickCallback(obj['path'], obj[extraField])
+								}}
+							>
+								{#if `app` in obj}
+									<div class="mr-2 text-sm text-left truncate w-24">
+										<IconedResourceType after={true} silent={false} name={obj['app']} />
+									</div>
+								{/if}
+								{#if `resource_type` in obj}
+									<div class="mr-2 truncate text-left w-24 text-sm">
+										<IconedResourceType after={true} name={obj['resource_type']} />
+									</div>
+								{/if}
+								<div class="flex flex-col">
+									<div class="text-sm font-semibold flex flex-col">
+										<span class="mr-2 text-left">{obj[extraField] ?? ''}</span>
+										{#if extraField != 'path'}
+											<span class="font-normal text-xs text-left italic overflow-hidden"
+												>{obj['path'] ?? ''}</span
 											>
 										{/if}
 									</div>
-								</button>
-								{#if buttons}
-									<div class="flex flex-row">
-										{#each Object.entries(buttons) as [name, button]}
-											<button
-												class="py-4 px-1 gap-1 flex flex-row grow  text-black"
-												on:click={() => {
-													button(obj['path'] ?? '')
-												}}
-											>
-												{name}
-											</button>
-										{/each}
-									</div>
-								{/if}
-							</li>
-						{/each}
-					</ul>
-				{:else}
-					<div class="text-center text-sm text-gray-600 mt-2">
-						{@html emptySearchMessage}
-					</div>
-				{/if}
+									{#if extraField != 'description'}
+										<div class="text-xs font-light italic text-left">{obj['description'] ?? ''}</div
+										>
+									{/if}
+								</div>
+							</button>
+							{#if buttons}
+								<div class="flex flex-row">
+									{#each Object.entries(buttons) as [name, button]}
+										<button
+											class="py-4 px-1 gap-1 flex flex-row grow  text-black"
+											on:click={() => {
+												button(obj['path'] ?? '')
+											}}
+										>
+											{name}
+										</button>
+									{/each}
+								</div>
+							{/if}
+						</li>
+					{/each}
+				</ul>
+			{:else}
+				<div class="text-center text-sm text-gray-600 mt-2">
+					{@html emptySearchMessage}
+				</div>
 			{/if}
 		</div>
 		<span slot="submission" class="mr-2">
