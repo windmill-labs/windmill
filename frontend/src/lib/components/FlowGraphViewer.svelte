c7c58ad8bc2fc7e50bd9f14f3c3cb7782004d42f
Ruben Fiszel <ruben@rubenfiszel.com>
2022-12-05 21:57:04 +0100

    improve flow viewer

--- frontend/src/lib/components/FlowGraphViewer.svelte
+++ frontend/src/lib/components/FlowGraphViewer.svelte
@@ -5,6 +5,11 @@
 	import IconedPath from './IconedPath.svelte'
 	import { scriptPathToHref } from '../utils'
 	import type { FlowModule, FlowValue } from '$lib/gen'
+	import { Drawer, DrawerContent } from './common'
+	import { Highlight } from 'svelte-highlight'
+	import ObjectViewer from './propertyPicker/ObjectViewer.svelte'
+	import typescript from 'svelte-highlight/languages/typescript'
+	import { cleanExpr } from './flows/utils'
 	export let flow: {
 		summary: string
 		description?: string
@@ -13,8 +18,60 @@
 	}
 	export let overflowAuto = false
 	let stepDetail: FlowModule | undefined = undefined
+	let codeViewer: Drawer
 </script>
 
+<Drawer bind:this={codeViewer} size="900px">
+	<DrawerContent title={'Expanded Code'} on:close={codeViewer.closeDrawer}>
+		{#if stepDetail}
+			{#if stepDetail.value.type == 'script'}
+				<div class="mb-4">
+					<a
+						rel="noreferrer"
+						target="_blank"
+						href={scriptPathToHref(stepDetail?.value?.path ?? '')}
+						class=""
+					>
+						<IconedPath path={stepDetail?.value?.path ?? ''} />
+					</a>
+				</div>
+				<div class="text-2xs mb-4">
+					<h3 class="mb-2">Step Inputs</h3>
+					<InputTransformsViewer
+						inputTransforms={stepDetail?.value?.input_transforms ??
+							stepDetail?.input_transforms ??
+							{}}
+					/>
+				</div>
+				{#if stepDetail.value.path.startsWith('hub/')}
+					<div class="mt-6">
+						<h3>Code</h3>
+						<iframe
+							class="w-full h-full  text-sm"
+							title="embedded script from hub"
+							frameborder="0"
+							src="https://hub.windmill.dev/embed/script/{stepDetail.value?.path?.substring(4)}"
+						/>
+					</div>
+				{/if}
+			{:else if stepDetail.value.type == 'rawscript'}
+				<div class="text-2xs mb-4">
+					<h3 class="mb-2">Step Inputs</h3>
+					<InputTransformsViewer
+						inputTransforms={stepDetail?.value?.input_transforms ??
+							stepDetail?.input_transforms ??
+							{}}
+					/>
+				</div>
+
+				<h3 class="mb-2">Code</h3>
+				<span class="!text-xs">
+					<HighlightCode language={stepDetail.value.language} code={stepDetail.value.content} />
+				</span>
+			{/if}
+		{/if}
+	</DrawerContent>
+</Drawer>
 <div class="grid grid-cols-3 w-full h-full">
 	<div class="h-full col-span-2 w-full border border-gray-700" class:overflow-auto={overflowAuto}>
 		<FlowGraph
@@ -45,8 +102,14 @@
 					/>
 				</div>
 
-				<h3 class="mb-2">Code</h3>
-				<HighlightCode language={stepDetail.value.language} code={stepDetail.value.content} />
+				<h3 class="mb-2"
+					>Code &nbsp; <button class="text-gray-600 text-xs" on:click={codeViewer.openDrawer}
+						>Expand</button
+					>
+				</h3>
+				<span class="!text-xs">
+					<HighlightCode language={stepDetail.value.language} code={stepDetail.value.content} />
+				</span>
 			{:else if stepDetail.value.type == 'script'}
 				<div class="mb-4">
 					<a
@@ -68,7 +131,11 @@
 				</div>
 				{#if stepDetail.value.path.startsWith('hub/')}
 					<div class="mt-6">
-						<h3>Code</h3>
+						<h3
+							>Code&nbsp; <button class="text-gray-600 text-xs" on:click={codeViewer.openDrawer}
+								>Expand</button
+							>
+						</h3>
 						<iframe
 							class="w-full h-full  text-sm"
 							title="embedded script from hub"
@@ -77,6 +144,24 @@
 						/>
 					</div>
 				{/if}
+			{:else if stepDetail.value.type == 'forloopflow'}
+				<p
+					>For loop with iterator: {#if stepDetail.value.iterator.type == 'static'}<ObjectViewer
+							json={stepDetail.value.iterator.value}
+						/>{:else}
+						<span class="text-xs">
+							<Highlight
+								offsetTop={0}
+								language={typescript}
+								code={cleanExpr(stepDetail.value.iterator.expr)}
+							/>
+						</span>
+					{/if}</p
+				>
+			{:else if stepDetail.value.type == 'branchall'}
+				<p>Run all branches</p>
+			{:else if stepDetail.value.type == 'branchone'}
+				<p>Run one branch based on a predicate</p>
 			{/if}
 		{/if}
 	</div>
