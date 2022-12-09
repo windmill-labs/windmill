103be87a8b1083f9bc6c8f99934c4c23b97baf4c
Ádám Kovács <vartefol@gmail.com>
2022-12-09 14:56:05 +0100

    fix(frontend): Remove runnable property from input

--- frontend/src/lib/components/apps/components/textInputs/AppTextInput.svelte
+++ frontend/src/lib/components/apps/components/textInputs/AppTextInput.svelte
@@ -1,43 +1,34 @@
 <script lang="ts">
+	import { getContext } from 'svelte'
 	import type { AppInput } from '../../inputType'
+	import type { Output } from '../../rx'
+	import type { AppEditorContext } from '../../types'
 	import DebouncedInput from '../helpers/DebouncedInput.svelte'
 	import InputValue from '../helpers/InputValue.svelte'
-	import type RunnableComponent from '../helpers/RunnableComponent.svelte'
-	import RunnableWrapper from '../helpers/RunnableWrapper.svelte'
 
 	export let id: string
-	export let componentInput: AppInput | undefined
 	export let configuration: Record<string, AppInput>
-	export let extraQueryParams: Record<string, any> = {}
 	export const staticOutputs: string[] = ['result']
+	console.log('');
 
+	const { worldStore } = getContext<AppEditorContext>('AppEditorContext')
 	let value: string
 	let labelValue: string = 'Title'
-	let runnableComponent: RunnableComponent
 
-	// $: if(value || !value) {
-	// 	if(componentInput) {
-	// 		(componentInput as any).value = value
-	// 	}
-	// 	runnableComponent?.runComponent()
-	// }
+	$: outputs = $worldStore?.outputsById[id] as {
+		result: Output<string>
+	}
+	$: if(value || !value) {
+		outputs?.result.set(value || '')
+	}
 </script>
 
 <InputValue input={configuration.label} bind:value={labelValue} />
 
-<RunnableWrapper
-	bind:runnableComponent
-	bind:componentInput
-	bind:result={value}
-	{id}
-	{extraQueryParams}
-	autoRefresh={true}
->
-	<!-- svelte-ignore a11y-label-has-associated-control -->
-	<label>
-		<div>
-			{labelValue}
-		</div>
-		<DebouncedInput bind:value={value} debounceDelay={300} type="text" placeholder="Type..." />
-	</label>
-</RunnableWrapper>
+<!-- svelte-ignore a11y-label-has-associated-control -->
+<label>
+	<div>
+		{labelValue}
+	</div>
+	<DebouncedInput bind:value={value} debounceDelay={300} type="text" placeholder="Type..." />
+</label>
