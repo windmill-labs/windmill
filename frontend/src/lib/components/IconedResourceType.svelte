df6049bdc53ba65aca5c11f8c4ffaccdcb083be7
Ruben Fiszel <ruben@rubenfiszel.com>
2022-12-10 13:05:48 +0100

    add all current resource types icons

--- frontend/src/lib/components/IconedResourceType.svelte
+++ frontend/src/lib/components/IconedResourceType.svelte
@@ -7,15 +7,17 @@
 	export let height = '24px'
 	export let width = '24px'
 	export let center = false
+
+	$: name_prefix = name.split('_')[0]
 </script>
 
 <div class=" truncate flex flex-row gap-2 {center ? 'justify-center items-center' : ''}  -pl-2">
 	{#if !silent && !after}
 		{name}
 	{/if}
-	{#if APP_TO_ICON_COMPONENT[name]}
+	{#if APP_TO_ICON_COMPONENT[name_prefix]}
 		<span class="text-gray-700">
-			<svelte:component this={APP_TO_ICON_COMPONENT[name]} {height} {width} />
+			<svelte:component this={APP_TO_ICON_COMPONENT[name_prefix]} {height} {width} />
 		</span>
 	{:else}
 		<span style="width: {width}" class="bg-gray-100 rounded-full" />
