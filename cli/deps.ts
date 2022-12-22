d99a62247fc1f9c374aafc04a3d71b07082abd27
Kai Jellinghaus <contact@kaij.tech>
2022-12-22 04:15:14 +0100

    Upgrade Versions

--- cli/deps.ts
+++ cli/deps.ts
@@ -1,25 +1,32 @@
-export { setClient } from "../deno-client/mod.ts";
-export * from "../deno-client/windmill-api/index.ts";
-export { Command } from "https://deno.land/x/cliffy@v0.25.4/command/command.ts";
-export { Table } from "https://deno.land/x/cliffy@v0.25.4/table/table.ts";
-export { colors } from "https://deno.land/x/cliffy@v0.25.4/ansi/colors.ts";
-export { Secret } from "https://deno.land/x/cliffy@v0.25.4/prompt/secret.ts";
-export { Select } from "https://deno.land/x/cliffy@v0.25.4/prompt/select.ts";
-export { getAvailablePort } from "https://deno.land/x/port@1.0.0/mod.ts";
-export { Untar } from "https://deno.land/std@0.162.0/archive/tar.ts";
+// windmill
+export { setClient } from "https://deno.land/x/windmill@v1.55.0/mod.ts";
+export * from "https://deno.land/x/windmill@v1.55.0/windmill-api/index.ts";
+
+// cliffy
+export { Command } from "https://deno.land/x/cliffy@v0.25.6/command/command.ts";
+export { Table } from "https://deno.land/x/cliffy@v0.25.6/table/table.ts";
+export { colors } from "https://deno.land/x/cliffy@v0.25.6/ansi/colors.ts";
+export { Secret } from "https://deno.land/x/cliffy@v0.25.6/prompt/secret.ts";
+export { Select } from "https://deno.land/x/cliffy@v0.25.6/prompt/select.ts";
+export { Confirm } from "https://deno.land/x/cliffy@v0.25.6/prompt/confirm.ts";
+export { Input } from "https://deno.land/x/cliffy@v0.25.6/prompt/input.ts";
+export {
+  DenoLandProvider,
+  UpgradeCommand,
+} from "https://deno.land/x/cliffy@v0.25.6/command/upgrade/mod.ts";
+
+// std
+export { Untar } from "https://deno.land/std@0.170.0/archive/tar.ts";
+export * as path from "https://deno.land/std@0.170.0/path/mod.ts";
+export { ensureDir } from "https://deno.land/std@0.170.0/fs/ensure_dir.ts";
 export {
   copy,
   readAll,
   readerFromStreamReader,
-} from "https://deno.land/std@0.162.0/streams/mod.ts";
-export * as path from "https://deno.land/std@0.162.0/path/mod.ts";
-export { ensureDir } from "https://deno.land/std@0.162.0/fs/ensure_dir.ts";
-export { Confirm } from "https://deno.land/x/cliffy@v0.25.4/prompt/confirm.ts";
-export {
-  DenoLandProvider,
-  UpgradeCommand,
-} from "https://deno.land/x/cliffy@v0.25.4/command/upgrade/mod.ts";
+} from "https://deno.land/std@0.170.0/streams/mod.ts";
+export { DelimiterStream } from "https://deno.land/std@0.170.0/streams/mod.ts";
+
+// other
+export { getAvailablePort } from "https://deno.land/x/port@1.0.0/mod.ts";
 export { default as dir } from "https://deno.land/x/dir@1.5.1/mod.ts";
 export { passwordGenerator } from "https://deno.land/x/password_generator@latest/mod.ts"; // TODO: I think the version is called latest, but it's still pinned.
-export { DelimiterStream } from "https://deno.land/std@0.165.0/streams/mod.ts";
-export { Input } from "https://deno.land/x/cliffy@v0.25.4/prompt/input.ts";
