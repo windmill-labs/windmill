955a0c773cd01cf3aef298557654eb8addb52b8e
Faton Ramadani <faton.ramadani14@gmail.com>
2022-12-23 15:02:24 +0100

    feat(frontend): Fix initial component dimensions + Select select + add spinner when buttons are clicked

--- frontend/src/lib/components/apps/editor/componentsPanel/ComponentList.svelte
+++ frontend/src/lib/components/apps/editor/componentsPanel/ComponentList.svelte
@@ -18,12 +18,18 @@
 	function getMinDimensionsByComponent(componentType: AppComponent['type'], column: number): Size {
 		// Dimensions key formula: <mobile width>:<mobile height>-<desktop width>:<desktop height>
 		const dimensions: Record<`${number}:${number}-${number}:${number}`, AppComponent['type'][]> = {
-			'1:2-3:2': ['buttoncomponent', 'textcomponent', 'checkboxcomponent'],
-			'1:2-2:2': ['textinputcomponent', 'numberinputcomponent', 'selectcomponent'],
-			'2:2-6:4': ['displaycomponent'],
-			'2:3-6:4': ['formcomponent'],
-			'2:4-6:4': ['barchartcomponent', 'piechartcomponent'],
-			'3:4-12:4': ['tablecomponent']
+			'1:2-2:2': [
+				'buttoncomponent',
+				'textcomponent',
+				'checkboxcomponent',
+				'textinputcomponent',
+				'numberinputcomponent',
+				'selectcomponent',
+				'passwordinputcomponent',
+				'dateinputcomponent'
+			],
+			'2:6-4:6': ['barchartcomponent', 'piechartcomponent', 'formcomponent', 'displaycomponent'],
+			'3:6-6:8': ['tablecomponent']
 		}
 		// Finds the key that is associated with the component type and extracts the dimensions from it
 		const [dimension] = Object.entries(dimensions).find(([_, value]) =>
