97f2eaa402d73a96eb084a49281dcee73004a329
Ádám Kovács <vartefol@gmail.com>
2023-01-12 12:11:13 +0100

    feat(frontend): Add more integration icons

---
+++ frontend/src/lib/components/icons/SupabaseIcon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let height = '24px'
+	export let width = '24px'
+</script>
+
+<svg {width} {height} viewBox="0 0 168 168" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M97.4434 164.242C93.2027 169.582 84.6042 166.656 84.502 159.837L83.0078 60.1013H150.07C162.217 60.1013 168.992 74.1309 161.439 83.644L97.4434 164.242Z" fill="url(#paint0_linear_210_201)"/>
+<path d="M97.4434 164.242C93.2027 169.582 84.6042 166.656 84.502 159.837L83.0078 60.1013H150.07C162.217 60.1013 168.992 74.1309 161.439 83.644L97.4434 164.242Z" fill="url(#paint1_linear_210_201)" fill-opacity="0.2"/>
+<path d="M70.1702 3.75823C74.411 -1.58278 83.0096 1.3437 83.1118 8.1628L83.7666 107.899H17.5434C5.39617 107.899 -1.37853 93.8692 6.17495 84.3561L70.1702 3.75823Z" fill="#3ECF8E"/>
+<defs>
+<linearGradient id="paint0_linear_210_201" x1="83.0078" y1="82.2153" x2="142.611" y2="107.213" gradientUnits="userSpaceOnUse">
+<stop stop-color="#249361"/>
+<stop offset="1" stop-color="#3ECF8E"/>
+</linearGradient>
+<linearGradient id="paint1_linear_210_201" x1="56.5831" y1="46.0351" x2="83.765" y2="97.2037" gradientUnits="userSpaceOnUse">
+<stop/>
+<stop offset="1" stop-opacity="0"/>
+</linearGradient>
+</defs>
+</svg>
