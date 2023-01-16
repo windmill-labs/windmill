9d17b9d12cc7f40c0d9f12d9ea28d338957ee6d6
Ádám Kovács <vartefol@gmail.com>
2023-01-16 16:14:19 +0100

    fix(frontend): Playwright

--- frontend/tests/playwright.config.ts
+++ frontend/tests/playwright.config.ts
@@ -3,8 +3,8 @@ import type { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
     globalSetup: './global-setup',
     use: {
-        // Tell all tests to load signed-in state from 'storageState.json'.
-        storageState: 'storageState.json'
+			// Tell all tests to load signed-in state from 'storageState.json'.
+			storageState: 'storageState.json'
     }
 }
 export default config
