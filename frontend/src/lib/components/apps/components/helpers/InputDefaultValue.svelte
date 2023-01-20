f14b103edeafd5849e38f541e06d00b876ad7a9f
Faton Ramadani <faton.ramadani14@gmail.com>
2023-01-20 13:17:34 +0100

    fix(frontend): Add default value for text, number and date input + fix issues with number input + add date input in the settings panel

---
+++ frontend/src/lib/components/apps/components/helpers/InputDefaultValue.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	type InputType = string | number | boolean
+	export let input: HTMLInputElement | undefined = undefined
+	export let defaultValue: InputType | undefined = undefined
+
+	function setInputValueToDefaultValue() {
+		if (defaultValue !== undefined && input) {
+			input.value = String(defaultValue)
+		}
+	}
+
+	function clearInputValue() {
+		if (input) {
+			input.value = ''
+		}
+	}
+
+	$: input && defaultValue && setInputValueToDefaultValue()
+	$: input &&
+		(defaultValue === '' || defaultValue === undefined || defaultValue === null) &&
+		clearInputValue()
+</script>
