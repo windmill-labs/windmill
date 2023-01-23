22b8fed9d904a37aae66f6d957f4987f6ca9955c
Faton Ramadani <faton.ramadani14@gmail.com>
2023-01-23 18:03:31 +0100

    feat(frontend): Display error as an icon in order to avoid clutter wh… (#1143)
    * feat(frontend): Display error as an icon in order to avoid clutter when an error occures
    
    * feat(frontend): Fix build
    
    * feat(frontend): Fix error icon position

--- frontend/src/lib/components/apps/editor/AppPreview.svelte
+++ frontend/src/lib/components/apps/editor/AppPreview.svelte
@@ -59,7 +59,8 @@
 		isEditor,
 		jobs: writable([]),
 		staticExporter: writable({}),
-		noBackend
+		noBackend,
+		errorByComponent: writable({})
 	})
 
 	let mounted = false
@@ -74,8 +75,10 @@
 </script>
 
 <div class="relative">
-	<div class="{$$props.class} {lockedClasses} h-full max-h-[calc(100%-41px)] overflow-auto 
-	w-full {app.fullscreen ? '' : 'max-w-6xl'} mx-auto">
+	<div
+		class="{$$props.class} {lockedClasses} h-full max-h-[calc(100%-41px)] overflow-auto 
+	w-full {app.fullscreen ? '' : 'max-w-6xl'} mx-auto"
+	>
 		{#if $appStore.grid}
 			<div class={classNames('mx-auto pb-4', width)}>
 				<GridEditor {policy} />
@@ -86,10 +89,10 @@
 		<!-- svelte-ignore a11y-click-events-have-key-events -->
 		<div
 			transition:fade|local={{ duration: 200, easing: cubicOut }}
-			on:click={() => isLocked = false}
+			on:click={() => (isLocked = false)}
 			class="absolute inset-0 center-center bg-black/20 z-50 backdrop-blur-[1px] cursor-pointer"
 		>
-			<Button on:click={() => isLocked = false}>
+			<Button on:click={() => (isLocked = false)}>
 				Unlock preview
 				<Unlock size={18} class="ml-1" strokeWidth={2.5} />
 			</Button>
