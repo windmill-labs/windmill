51a710ffa0ac284c04145af87567544336f31d3a
Ruben Fiszel <ruben@rubenfiszel.com>
2023-01-26 03:20:01 +0100

    do not copy necessarily for the flow object viewer

--- frontend/src/lib/components/propertyPicker/PropPickerResult.svelte
+++ frontend/src/lib/components/propertyPicker/PropPickerResult.svelte
@@ -7,6 +7,6 @@
 <div class="w-full px-2">
 	<span class="font-bold text-sm">Result</span>
 	<div class="overflow-y-auto mb-2 w-full">
-		<ObjectViewer json={{ result }} on:select />
+		<ObjectViewer allowCopy={false} json={{ result }} on:select />
 	</div>
 </div>
