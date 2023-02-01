3ecab28b91f6498d4ba1a97e275218a38df51058
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-01 23:10:43 +0100

    add privacy policy and terms of service links

--- frontend/src/lib/logout.ts
+++ frontend/src/lib/logout.ts
@@ -6,7 +6,7 @@ import { sendUserToast } from './utils.js'
 export async function logoutWithRedirect(rd?: string): Promise<void> {
 
 	await clearUser()
-	if (rd && rd?.split('?')[0] != '/user/login') {
+	if (rd && rd != "/" && rd?.split('?')[0] != '/user/login') {
 		const error = document.cookie.includes('token')
 			? `error=${encodeURIComponent('You have been logged out because your session has expired.')}&`
 			: ''
