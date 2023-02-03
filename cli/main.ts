1a5a5b4704b543eb1e3824f02b680a81ac80dc44
rubenfiszel <rubenfiszel@users.noreply.github.com>
2023-02-03 18:52:42 +0000

    Apply automatic changes

--- cli/main.ts
+++ cli/main.ts
@@ -13,7 +13,7 @@ import sync from "./sync.ts";
 import { tryResolveVersion } from "./context.ts";
 import { GlobalOptions } from "./types.ts";
 
-const VERSION = "v1.61.1";
+const VERSION = "v1.62.0";
 
 let command: any = new Command()
   .name("wmill")
