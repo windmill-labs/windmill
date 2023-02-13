dabceae2ea892478964b6c97ae1149369c927eed
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-13 15:10:48 +0100

    fix linguo

--- frontend/src/routes/(root)/(logged)/user/(user)/first-time/+page.svelte
+++ frontend/src/routes/(root)/(logged)/user/(user)/first-time/+page.svelte
@@ -16,19 +16,20 @@
 	}
 </script>
 
-<CenteredModal title="Welcome to Windmill!">
-	<p class="text-center text-lg">
-		Looks like this is a brand new instance. We've prepared a script for you to get a proper account
-		and get you setup with the Hub. Should I take you there now?
+<CenteredModal title="Welcome to Windmill">
+	<p class="text-center text-lg mt-4">
+		This is a brand new instance. In the superadmins workspace, there is an app to setup an account
+		and configure syncing the resource types of this instance with the hub periodically. Go there?
+		You can always go back to this app by going to the superadmins workspace.
 	</p>
 	<div class="flex flex-row justify-between pt-4 gap-x-1">
-		<Button variant="border" size="sm" href="/" on:click={decline}>Decline</Button>
+		<Button variant="border" size="sm" href="/" on:click={decline}>Skip setup</Button>
 		<button
 			class="place-items-end bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border rounded"
 			type="button"
 			on:click={startSetup}
 		>
-			Accept
+			Setup
 		</button>
 	</div>
 </CenteredModal>
