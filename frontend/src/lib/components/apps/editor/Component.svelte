5b7ce394960a8f38d3b10c58c49b8c2f50a8c0fe
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-13 22:46:42 +0100

    implement double ended slider

--- frontend/src/lib/components/apps/editor/Component.svelte
+++ frontend/src/lib/components/apps/editor/Component.svelte
@@ -39,6 +39,7 @@
 	export type DateInputComponent = BaseComponent<'dateinputcomponent'>
 	export type NumberInputComponent = BaseComponent<'numberinputcomponent'>
 	export type SliderComponent = BaseComponent<'slidercomponent'>
+	export type RangeComponent = BaseComponent<'rangecomponent'>
 	export type HtmlComponent = BaseComponent<'htmlcomponent'>
 	export type VegaLiteComponent = BaseComponent<'vegalitecomponent'>
 	export type PlotlyComponent = BaseComponent<'plotlycomponent'>
@@ -74,6 +75,7 @@
 			| DateInputComponent
 			| NumberInputComponent
 			| SliderComponent
+			| RangeComponent
 			| BarChartComponent
 			| TimeseriesComponent
 			| HtmlComponent
@@ -700,7 +702,7 @@
 		slidercomponent: {
 			name: 'Slider',
 			icon: SlidersHorizontal,
-			dims: '2:1-2:1',
+			dims: '4:1-4:1',
 			data: {
 				softWrap: true,
 				verticalAlignment: 'center',
@@ -717,6 +719,63 @@
 					max: {
 						type: 'static',
 						value: 42,
+						fieldType: 'number',
+						onlyStatic: true
+					},
+					defaultValue: {
+						type: 'static',
+						value: 20,
+						fieldType: 'number',
+						onlyStatic: true
+					},
+					step: {
+						type: 'static',
+						value: 1,
+						fieldType: 'number',
+						onlyStatic: true
+					}
+				},
+				card: false
+			}
+		},
+		rangecomponent: {
+			name: 'Range',
+			icon: SlidersHorizontal,
+			dims: '4:2-4:2',
+			data: {
+				softWrap: true,
+				verticalAlignment: 'center',
+				id: '',
+				type: 'rangecomponent',
+				componentInput: undefined,
+				configuration: {
+					min: {
+						type: 'static',
+						value: 0,
+						fieldType: 'number',
+						onlyStatic: true
+					},
+					max: {
+						type: 'static',
+						value: 42,
+						fieldType: 'number',
+						onlyStatic: true
+					},
+					defaultLow: {
+						type: 'static',
+						value: 10,
+						fieldType: 'number',
+						onlyStatic: true
+					},
+					defaultHigh: {
+						type: 'static',
+						value: 20,
+						fieldType: 'number',
+						onlyStatic: true
+					},
+					step: {
+						type: 'static',
+						value: 1,
 						fieldType: 'number',
 						onlyStatic: true
 					}
@@ -784,6 +843,7 @@
 			'passwordinputcomponent',
 			'numberinputcomponent',
 			'slidercomponent',
+			'rangecomponent',
 			'dateinputcomponent',
 			'checkboxcomponent',
 			'selectcomponent'
@@ -1168,6 +1228,7 @@
 	import VegaLiteHtml from '../components/dataDisplay/VegaLiteHtml.svelte'
 	import PlotlyHtml from '../components/dataDisplay/PlotlyHtml.svelte'
 	import { defaultAlignement } from './componentsPanel/componentDefaultProps'
+	import AppRangeInput from '../components/numberInputs/AppRangeInput.svelte'
 
 	export let component: AppComponent
 	export let selected: boolean
@@ -1318,6 +1379,8 @@
 			<AppNumberInput {...component} bind:staticOutputs={$staticOutputs[component.id]} />
 		{:else if component.type === 'slidercomponent'}
 			<AppSliderInputs {...component} bind:staticOutputs={$staticOutputs[component.id]} />
+		{:else if component.type === 'rangecomponent'}
+			<AppRangeInput {...component} bind:staticOutputs={$staticOutputs[component.id]} />
 		{/if}
 	</div>
 </div>
