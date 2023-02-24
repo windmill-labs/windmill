bc3fd01237f888139ecf1835cb711b1f272064a4
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-24 14:48:28 +0100

    subgrids editor panel iter 1

---
+++ frontend/src/lib/components/apps/editor/SettingsPanelList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { GridItem } from '../types'
+
+	import SettingsPanelRec from './SettingsPanelRec.svelte'
+
+	export let subgrids: GridItem[][]
+
+	let elems = subgrids
+	$: subgrids = elems
+</script>
+
+{#if subgrids}
+	{#each elems as subGrid, i (i)}
+		<SettingsPanelRec bind:gridItems={subGrid} />
+	{/each}
+{/if}
---
+++ frontend/src/lib/components/apps/editor/inlineScriptsPanel/InlineScriptEditorList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { GridItem } from '../../types'
+	import InlineScriptEditorRec from './InlineScriptEditorRec.svelte'
+
+	export let subgrids: GridItem[][]
+	export let selectedScriptComponentId: string | undefined = undefined
+
+	let elems = subgrids
+	$: subgrids = elems
+</script>
+
+{#if subgrids}
+	{#each elems as subGrid, i (i)}
+		<InlineScriptEditorRec {selectedScriptComponentId} bind:gridItems={subGrid} />
+	{/each}
+{/if}
