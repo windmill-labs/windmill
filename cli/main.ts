64bbd7fb6f902ed1d359097d2fc814d7970213a3
Ruben Fiszel <ruben@rubenfiszel.com>
2023-02-28 22:51:57 +0100

    chore(main): release 1.71.0 (#1242)
    * chore(main): release 1.71.0
    
    * Apply automatic changes
    
    ---------
    
    Co-authored-by: rubenfiszel <rubenfiszel@users.noreply.github.com>

--- cli/main.ts
+++ cli/main.ts
@@ -13,7 +13,7 @@ import sync from "./sync.ts";
 import { tryResolveVersion } from "./context.ts";
 import { GlobalOptions } from "./types.ts";
 
-const VERSION = "v1.70.1";
+const VERSION = "v1.71.0";
 
 let command: any = new Command()
   .name("wmill")
