3bafda399b80d10ba177171fa1e3484106993ac2
Ádám Kovács <vartefol@gmail.com>
2023-03-01 23:28:31 +0100

    fix(frontend): Customize map controls

--- frontend/src/lib/components/apps/components/display/AppMap.svelte
+++ frontend/src/lib/components/apps/components/display/AppMap.svelte
@@ -10,6 +10,7 @@
 	import { OSM, Vector as VectorSource } from 'ol/source'
 	import { Vector as VectorLayer, Tile as TileLayer } from 'ol/layer'
 	import { Point } from 'ol/geom'
+	import { defaults as defaultControls } from 'ol/control'
 
 	interface Marker {
 		lon: number
@@ -127,6 +128,9 @@
 			view: new View({
 				center: [longitude ?? 0, latitude ?? 0],
 				zoom: zoom ?? 2
+			}),
+			controls: defaultControls({
+				attribution: false
 			})
 		})
 		map.on('moveend', () => {
@@ -159,3 +163,14 @@
 	class={twMerge(`w-full h-full`, css?.map?.class ?? '')}
 	style={css?.map?.style ?? ''}
 />
+
+<style global lang="postcss">
+	.ol-overlaycontainer-stopevent {
+		@apply flex flex-col justify-start items-end;
+	}
+	.ol-control button {
+		@apply w-7 h-7 center-center bg-white border border-gray-300 text-gray-700 
+		rounded mt-1 mr-1 shadow duration-200 hover:bg-gray-100 focus:bg-gray-100 
+		hover:border-gray-500 focus:border-gray-500;
+	}
+</style>
