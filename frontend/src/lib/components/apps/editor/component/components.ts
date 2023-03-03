9fda7e4eda8f86fb97465eca4d28246a8eb1ff39
Ruben Fiszel <ruben@rubenfiszel.com>
2023-03-03 01:06:36 +0100

    feat(frontend): add creatable select

--- frontend/src/lib/components/apps/editor/component/components.ts
+++ frontend/src/lib/components/apps/editor/component/components.ts
@@ -72,6 +72,7 @@ export type DisplayComponent = BaseComponent<'displaycomponent'>
 export type ImageComponent = BaseComponent<'imagecomponent'>
 export type InputComponent = BaseComponent<'inputcomponent'>
 export type SelectComponent = BaseComponent<'selectcomponent'>
+export type MultiSelectComponent = BaseComponent<'multiselectcomponent'>
 export type CheckboxComponent = BaseComponent<'checkboxcomponent'>
 export type RadioComponent = BaseComponent<'radiocomponent'>
 export type IconComponent = BaseComponent<'iconcomponent'>
@@ -110,6 +111,7 @@ export type AppComponent = BaseAppComponent &
 		| PieChartComponent
 		| ScatterChartComponent
 		| SelectComponent
+		| MultiSelectComponent
 		| CheckboxComponent
 		| FormComponent
 		| FormButtonComponent
@@ -871,7 +873,7 @@ Hello \${ctx.username}
 						{ value: 'bar', label: 'Bar' }
 					]
 				},
-				multiple: {
+				create: {
 					type: 'static',
 					fieldType: 'boolean',
 					value: false,
@@ -887,6 +889,38 @@ Hello \${ctx.username}
 					type: 'static',
 					value: undefined,
 					fieldType: 'object'
+				}
+			},
+			customCss: {
+				input: { style: '' }
+			} as const,
+			card: false,
+			softWrap: true
+		}
+	},
+	multiselectcomponent: {
+		name: 'MultiSelect',
+		icon: List,
+		dims: '2:1-3:1',
+		data: {
+			verticalAlignment: 'center',
+			id: '',
+			type: 'multiselectcomponent',
+			componentInput: undefined,
+			configuration: {
+				items: {
+					type: 'static',
+					fieldType: 'array',
+					subFieldType: 'text',
+					value: [
+						"Foo", "Bar"
+					]
+				},
+				placeholder: {
+					type: 'static',
+					fieldType: 'text',
+					value: 'Select items',
+					onlyStatic: true
 				}
 			},
 			customCss: {
