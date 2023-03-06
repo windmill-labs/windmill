d36948c02824e04c209cae855621fba0a40b48f8
Faton Ramadani <faton.ramadani14@gmail.com>
2023-03-06 08:54:14 +0100

    fix(frontend): fix branch deletion

--- frontend/src/lib/components/flows/map/FlowModuleSchemaMap.svelte
+++ frontend/src/lib/components/flows/map/FlowModuleSchemaMap.svelte
@@ -147,12 +147,14 @@
 
 	function removeBranch(module: FlowModule, index: number) {
 		if (module.value.type === 'branchone' || module.value.type === 'branchall') {
-			if (module.value.branches[index]?.modules) {
-				const leaves = dfs(module.value.branches[index].modules, (mod) => mod.id)
+			const offset = module.value.type === 'branchone' ? 1 : 0
+
+			if (module.value.branches[index - offset]?.modules) {
+				const leaves = dfs(module.value.branches[index - offset].modules, (mod) => mod.id)
 				leaves.forEach((leafId: string) => deleteFlowStateById(leafId))
 			}
 
-			module.value.branches.splice(index - 1, 1)
+			module.value.branches.splice(index - offset, 1)
 		}
 	}
 </script>
