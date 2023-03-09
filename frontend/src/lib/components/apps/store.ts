ed29d51c368d9c582e4222f73bda336350acd570
Ruben Fiszel <ruben@rubenfiszel.com>
2023-03-09 01:16:57 +0100

    fix app json import

--- frontend/src/lib/components/apps/store.ts
+++ frontend/src/lib/components/apps/store.ts
@@ -1,7 +1,8 @@
+import type { Policy } from "$lib/gen";
 import { writable } from "svelte/store";
 import type {
     App,
 
 } from './types'
 
-export const importStore = writable<App | undefined>(undefined)
+export const importStore = writable<{ summary: string, value: App, policy: Policy } | undefined>(undefined)
