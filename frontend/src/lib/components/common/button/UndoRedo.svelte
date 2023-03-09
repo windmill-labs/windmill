4210a2e8dcfed4c0be1fee1eb23184d76ea3ee51
Ádám Kovács <vartefol@gmail.com>
2023-03-09 09:27:25 +0100

    fix(frontend): Use undo-redo component in flows

---
+++ frontend/src/lib/components/common/button/UndoRedo.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Redo, Undo } from 'lucide-svelte'
+	import { createEventDispatcher } from 'svelte'
+	import { Button } from '..'
+
+	export let undoProps: Record<string, any> = {}
+	export let redoProps: Record<string, any> = {}
+	const dispatch = createEventDispatcher()
+</script>
+
+<div class="flex">
+	<Button
+		title="Undo"
+		variant="border"
+		color="light"
+		size="xs"
+		btnClasses="!min-h-[30px] !rounded-r-none"
+		on:click={() => dispatch('undo')}
+		{...undoProps}
+	>
+		<Undo size={14} />
+	</Button>
+	<Button
+		title="Redo"
+		variant="border"
+		color="light"
+		size="xs"
+		btnClasses="!min-h-[30px] !rounded-l-none !border-l-0"
+		on:click={() => dispatch('redo')}
+		{...redoProps}
+	>
+		<Redo size={14} />
+	</Button>
+</div>
