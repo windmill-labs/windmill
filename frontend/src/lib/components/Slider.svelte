d3296c958ea8cf46f7f988ee8a38271681c8aaf1
Ruben Fiszel <ruben@rubenfiszel.com>
2023-03-11 15:30:46 +0100

    feat: add filter jobs by args or result

--- frontend/src/lib/components/Slider.svelte
+++ frontend/src/lib/components/Slider.svelte
@@ -2,13 +2,18 @@
 	import { ChevronDown, ChevronUp } from 'lucide-svelte'
 	import { slide } from 'svelte/transition'
 	import Button from './common/button/Button.svelte'
+	import Tooltip from './Tooltip.svelte'
 
 	export let text: string
+	export let tooltip: string | undefined = undefined
 	export let view = false
 </script>
 
 <Button color="light" on:click={() => (view = !view)} variant="border"
 	>{text}
+	{#if tooltip}
+		<Tooltip wrapperClass="mx-1">{tooltip}</Tooltip>
+	{/if}
 	{#if !view}<ChevronDown />{:else}<ChevronUp />{/if}</Button
 >
 {#if view}
