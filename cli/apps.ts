1a7dc0a3bd6e1f3456140733e8d8201a4965720a
Ruben Fiszel <ruben@rubenfiszel.com>
2023-03-11 20:53:34 +0100

    more cli fixes

--- cli/apps.ts
+++ cli/apps.ts
@@ -1,6 +1,7 @@
 import { Any, model, property } from "./decoverto.ts";
 import {
   AppService,
+  AppWithLastVersion,
   colors,
   microdiff,
   Policy,
@@ -27,14 +28,18 @@ export class AppFile implements Resource, PushDiffs {
     remotePath: string,
     diffs: Difference[],
   ): Promise<void> {
-    if (await AppService.existsApp({ workspace, path: remotePath })) {
+    let app: AppWithLastVersion | undefined = undefined;
+    try {
+      app = await AppService.getAppByPath({ workspace, path: remotePath });
+    } catch (e) {}
+
+    if (app) {
       console.log(
         colors.bold.yellow(
           `Applying ${diffs.length} diffs to existing app... ${remotePath}`,
         ),
       );
       const changeset: {
-        path?: string | undefined;
         summary?: string | undefined;
         value?: any;
         policy?: Policy | undefined;
@@ -45,7 +50,7 @@ export class AppFile implements Resource, PushDiffs {
           (
             diff.path[0] !== "value" && diff.path[0] !== "policy" && (
               diff.path.length !== 1 ||
-              !["path", "summary"].includes(
+              !["summary"].includes(
                 diff.path[0] as string,
               )
             )
@@ -58,6 +63,13 @@ export class AppFile implements Resource, PushDiffs {
         } else if (diff.type === "REMOVE") {
           setValueByPath(changeset, diff.path, null);
         }
+      }
+
+      if ((!changeset?.policy || JSON.stringify(changeset?.policy) == JSON.stringify(app.policy)) 
+        && (!changeset?.value || JSON.stringify(changeset?.value) == JSON.stringify(app.value))
+        && (!changeset?.summary || changeset.summary == app.summary)) {
+        console.log(colors.yellow(`No changes to push for app ${remotePath}, skipping`))
+        return;
       }
 
       const hasChanges = Object.values(changeset).some((v) =>
