601ed1ed47be45be62d8c4782f1bbae2842516e4
Faton Ramadani <faton.ramadani14@gmail.com>
2023-03-14 16:53:54 +0100

    fix(frontend): app controls

--- frontend/src/lib/components/apps/editor/ComponentHeader.svelte
+++ frontend/src/lib/components/apps/editor/ComponentHeader.svelte
@@ -26,61 +26,62 @@
 	}
 </script>
 
-<span
-	title={`Id: ${component.id}`}
-	class={classNames(
-		'px-2 text-2xs font-bold w-fit absolute shadow  -top-1 -left-2 border z-50',
-		selected
-			? 'bg-indigo-500/90 border-blue-500 text-white'
-			: 'bg-gray-200/90 border-gray-300  text-gray-500'
-	)}
-	style="padding-top: 1px; padding-bottom: 1px;"
->
-	{component.id}
-</span>
-
 {#if pointerdown || selected || hover}
-	<button
-		title="Lock Position"
+	<span
+		title={`Id: ${component.id}`}
 		class={classNames(
-			'text-gray-800 px-1 text-2xs py-0.5 font-bold w-fit shadow border border-gray-300 absolute  -top-1  right-[2.5rem] z-50 cursor-pointer',
-			' hover:bg-gray-300',
-			selected ? 'bg-gray-200/80' : 'bg-gray-200/80'
+			'px-2 text-2xs font-bold w-fit absolute shadow top-[-19px] left-[1px] border z-50',
+			selected
+				? 'bg-indigo-500/90 border-blue-500 text-white'
+				: 'bg-gray-200/90 border-gray-300  text-gray-500'
 		)}
-		on:click={() => dispatch('lock')}
-		on:pointerdown|stopPropagation
+		style="padding-top: 1px; padding-bottom: 1px;"
 	>
-		{#if locked}
-			<Anchor aria-label="Unlock position" size={14} class="text-orange-500" />
-		{:else}
-			<Anchor aria-label="Lock position" size={14} />
-		{/if}
-	</button>
-	<button
-		title="Expand"
-		class={classNames(
-			'text-gray-800 px-1 text-2xs py-0.5 font-bold w-fit shadow border border-gray-300 absolute  -top-1  right-[4.5rem] z-50 cursor-pointer',
-			' hover:bg-gray-300',
-			selected ? 'bg-gray-200/80' : 'bg-gray-200/80'
-		)}
-		on:click={() => dispatch('expand')}
-		on:pointerdown|stopPropagation
-	>
-		<Expand aria-label="Expand position" size={14} />
-	</button>
+		{component.id}
+	</span>
 {/if}
 
-{#if selected || hover}
-	<span
-		title="Move"
-		on:mousedown|stopPropagation|capture
-		class={classNames(
-			'text-gray-600 px-1 text-2xs py-0.5 font-bold rounded-t-sm w-fit absolute border border-gray-300 -top-1 shadow right-[0.5rem] z-50 cursor-move',
-			'bg-gray-200/80'
-		)}
-	>
-		<Move size={14} />
-	</span>
+{#if selected}
+	<div class="top-[-18px] right-[1px] flex flex-row absolute">
+		<button
+			title="Expand"
+			class={classNames(
+				'text-indigo-800 px-1 text-2xs py-0.5 font-bold w-fit shadow border-t border-l border-indigo-300  z-50 cursor-pointer',
+				' hover:bg-indigo-300',
+				selected ? 'bg-indigo-200/80' : 'bg-indigo-200/80'
+			)}
+			on:click={() => dispatch('expand')}
+			on:pointerdown|stopPropagation
+		>
+			<Expand aria-label="Expand position" size={14} />
+		</button>
+		<button
+			title="Lock Position"
+			class={classNames(
+				'text-indigo-800 px-1 text-2xs py-0.5 font-bold w-fit shadow border-t border-l border-indigo-300  z-50 cursor-pointer',
+				' hover:bg-indigo-300',
+				selected ? 'bg-indigo-200/80' : 'bg-indigo-200/80'
+			)}
+			on:click={() => dispatch('lock')}
+			on:pointerdown|stopPropagation
+		>
+			{#if locked}
+				<Anchor aria-label="Unlock position" size={14} class="text-orange-500" />
+			{:else}
+				<Anchor aria-label="Lock position" size={14} />
+			{/if}
+		</button>
+		<span
+			title="Move"
+			on:mousedown|stopPropagation|capture
+			class={classNames(
+				'text-indigo-600 px-1 text-2xs py-0.5 font-bold w-fit  border-t border-x border-indigo-300 -top-5 shadow z-50 cursor-move',
+				'bg-indigo-200/80'
+			)}
+		>
+			<Move size={14} />
+		</span>
+	</div>
 {/if}
 
 {#if error}
--- frontend/src/lib/components/apps/editor/contextPanel/components/BackgroundScriptsOutput.svelte
+++ frontend/src/lib/components/apps/editor/contextPanel/components/BackgroundScriptsOutput.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import type { AppViewerContext } from '$lib/components/apps/types'
 	import { getContext } from 'svelte'
-	import OutputHeader from './OutputHeader.svelte'
 	import BackgroundScriptOutput from './BackgroundScriptOutput.svelte'
 
 	const { app } = getContext<AppViewerContext>('AppViewerContext')
