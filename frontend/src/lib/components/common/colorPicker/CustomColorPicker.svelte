1662b6999e5baf26c3223a53a426e9e08fe30351
Ádám Kovács <vartefol@gmail.com>
2023-03-20 15:08:57 +0100

    refactor(frontend): Separate color picker

---
+++ frontend/src/lib/components/common/colorPicker/CustomColorPicker.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import ColorPicker, { ChromeVariant } from 'svelte-awesome-color-picker'
+
+	export let hex: string = '#fff'
+	export let isOpen = false
+</script>
+
+<ColorPicker
+	bind:isOpen
+	bind:hex
+	label={hex}
+	canChangeMode={false}
+	components={ChromeVariant}
+	toRight
+	--focus-color="#e0e7ff"
+/>
+
+<style global>
+	.color-picker-input span > label.svelte-rogbpz {
+		display: none;
+	}
+
+	.color-picker-input span > div.wrapper {
+		box-shadow: 0 10px 40px -5px rgba(0, 0, 0, 0.25) !important;
+		border-style: none !important;
+		border-radius: 0.375rem !important;
+	}
+
+	.color-picker-input .slider-wrapper {
+		overflow: visible !important;
+	}
+
+	.color-picker-input .slider-indicator {
+		width: 14px !important;
+		height: 14px !important;
+		border-radius: 7px !important;
+		margin-top: 0 !important;
+		background-color: #fafafa !important;
+		border: 1px solid #2e2e2eaf !important;
+		top: -2px !important;
+		box-shadow: 0 2px 6px -2px rgba(0, 0, 0, 0.5) !important;
+	}
+</style>
