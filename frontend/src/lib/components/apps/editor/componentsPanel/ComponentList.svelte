f214d5f96b6ac26cd3ef90a6ab696a6dfe02b3f0
Faton Ramadani <faton.ramadani14@gmail.com>
2023-04-06 16:07:34 +0200

    feat(frontend): Tone down text + display whole text (#1366)

--- frontend/src/lib/components/apps/editor/componentsPanel/ComponentList.svelte
+++ frontend/src/lib/components/apps/editor/componentsPanel/ComponentList.svelte
@@ -96,20 +96,20 @@
 				{#if components.length}
 					<div transition:slide|local={{ duration: 100 }}>
 						<ListItem title={`${title} (${components.length})`}>
-							<div class="flex flex-wrap gap-2 py-2">
+							<div class="flex flex-wrap gap-3 py-2">
 								{#each components as item (item)}
-									<button
-										animate:flip={{ duration: 100 }}
-										on:click={() => addComponent(item)}
-										title={componentsRecord[item].name}
-										class="border w-24 shadow-sm h-16 p-2 flex flex-col gap-2 items-center
-										justify-center bg-white rounded-md hover:bg-gray-100 duration-200"
-									>
-										<svelte:component this={componentsRecord[item].icon} />
-										<div class="text-xs w-full text-center ellipsize">
+									<div animate:flip={{ duration: 100 }} class="w-20">
+										<button
+											on:click={() => addComponent(item)}
+											title={componentsRecord[item].name}
+											class="transition-all border w-20 shadow-sm h-16 p-2 flex flex-col gap-2 items-center justify-center bg-white rounded-md hover:bg-gray-100 duration-200 hover:border-blue-500"
+										>
+											<svelte:component this={componentsRecord[item].icon} class="text-gray-600" />
+										</button>
+										<div class="text-xs text-center flex-wrap text-gray-600 mt-1">
 											{componentsRecord[item].name}
 										</div>
-									</button>
+									</div>
 								{/each}
 							</div>
 						</ListItem>
