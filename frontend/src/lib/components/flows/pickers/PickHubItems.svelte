8adea0d71e5e74dd45ae4c63b445eaf1cd200f64
Faton Ramadani <faton.ramadani14@gmail.com>
2023-04-13 14:16:30 +0200

    feat(frontend): Merge Hub items into one tab

---
+++ frontend/src/lib/components/flows/pickers/PickHubItems.svelte
@@ -0,0 +1,121 @@
+<!-- PickHubItems.svelte -->
+<script lang="ts">
+	import { createEventDispatcher, onMount } from 'svelte'
+	import { Skeleton, ToggleButton, ToggleButtonGroup } from '$lib/components/common'
+	import SearchItems from '$lib/components/SearchItems.svelte'
+	import { loadHubScripts, loadHubFlows, loadHubApps } from '$lib/utils'
+	import ListFilters from '$lib/components/home/ListFilters.svelte'
+	import NoItemFound from '$lib/components/home/NoItemFound.svelte'
+	import { hubScripts, hubFlows, hubApps, type HubItem } from '$lib/stores'
+	import PickHubScript from './PickHubScript.svelte'
+	import PickHubFlow from './PickHubFlow.svelte'
+	import PickHubApp from './PickHubApp.svelte'
+	import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
+	import { Code2, LayoutDashboard } from 'lucide-svelte'
+	import { Icon } from 'svelte-awesome'
+
+	export let filter = ''
+
+	let itemKind: 'script' | 'flow' | 'app' | 'all' = 'all'
+	const dispatch = createEventDispatcher()
+	let filteredItems: (HubItem & { marked?: string })[] = []
+	let appFilter: string | undefined = undefined
+
+	$: items = [
+		...($hubScripts?.map((s) => ({ itemType: 'script', data: s })) ?? []),
+		...($hubFlows?.map((s) => ({ itemType: 'flow', data: s })) ?? []),
+		...($hubApps?.map((s) => ({ itemType: 'app', data: s })) ?? [])
+	] as HubItem[]
+
+	$: apps = Array.from(
+		new Set(
+			filteredItems
+				?.map((x) => {
+					if (x.itemType === 'script') {
+						return x.data.app
+					} else if (x.itemType === 'flow') {
+						return x.data.apps
+					} else if (x.itemType === 'app') {
+						return x.data.apps
+					} else {
+						return undefined
+					}
+				})
+				.filter(Boolean)
+				.flat() ?? []
+		)
+	).sort() as string[]
+
+	$: prefilteredItems = appFilter
+		? (items ?? []).filter((i) => {
+				if (i.itemType === 'script' && itemKind === 'script') {
+					return i.data.app == appFilter
+				} else if (i.itemType === 'flow' && appFilter && itemKind === 'flow') {
+					return i.data.apps.includes(appFilter)
+				} else if (i.itemType === 'app' && appFilter && itemKind === 'app') {
+					return i.data.apps.includes(appFilter)
+				}
+				return false
+		  })
+		: items ?? []
+
+	onMount(async () => {
+		await loadHubScripts()
+		$hubFlows = await loadHubFlows()
+		$hubApps = await loadHubApps()
+	})
+</script>
+
+<SearchItems {filter} items={prefilteredItems} bind:filteredItems f={(x) => x.summary} />
+<div class="w-full flex mt-1 items-center gap-2">
+	<ToggleButtonGroup bind:selected={itemKind}>
+		<ToggleButton light position="left" value="all" size="sm">All</ToggleButton>
+		<ToggleButton light position="center" value="script" size="sm">
+			<div class="flex gap-1 items-center">
+				<Code2 size={16} />
+				Scripts
+			</div>
+		</ToggleButton>
+		<ToggleButton light position="center" value="flow" size="sm">
+			<div class="flex gap-1 items-center">
+				<Icon data={faBarsStaggered} scale={0.8} class="mr-1" />
+				Flows
+			</div>
+		</ToggleButton>
+		<ToggleButton light position="right" value="app" size="sm">
+			<div class="flex gap-1 items-center">
+				<LayoutDashboard size={16} />
+				Apps
+			</div>
+		</ToggleButton>
+	</ToggleButtonGroup>
+	<slot />
+	<input type="text" placeholder="Search Hub Items" bind:value={filter} class="text-2xl grow" />
+</div>
+
+<ListFilters filters={apps} bind:selectedFilter={appFilter} resourceType />
+
+{#if prefilteredItems.length}
+	{#if filteredItems.length == 0}
+		<NoItemFound />
+	{:else}
+		<ul class="divide-y divide-gray-200 border rounded-md">
+			{#each filteredItems as item, index (index)}
+				<li class="flex flex-row w-full">
+					{#if item.itemType === 'script'}
+						<PickHubScript item={item.data} on:pick={() => dispatch('pickScript', item.data)} />
+					{:else if item.itemType === 'flow'}
+						<PickHubFlow item={item.data} on:pick={() => dispatch('pickFlow', item.data)} />
+					{:else if item.itemType === 'app'}
+						<PickHubApp item={item.data} on:pick={() => dispatch('pickApp', item.data)} />
+					{/if}
+				</li>
+			{/each}
+		</ul>
+	{/if}
+{:else}
+	<div class="my-2" />
+	{#each Array(10).fill(0) as _}
+		<Skeleton layout={[[4], 0.5]} />
+	{/each}
+{/if}
