eda8e1221f04540be12e6d657fba24555eaec170
Ádám Kovács <vartefol@gmail.com>
2023-04-14 10:49:29 +0200

    handle running jobs

--- frontend/src/lib/components/jobs/JobPreview.svelte
+++ frontend/src/lib/components/jobs/JobPreview.svelte
@@ -5,26 +5,28 @@
 <script lang="ts">
 	import { tick } from 'svelte'
 	import { fade } from 'svelte/transition'
-	import type { Job } from '../../gen'
+	import { Job } from '../../gen'
 	import TestJobLoader from '../TestJobLoader.svelte'
 	import DisplayResult from '../DisplayResult.svelte'
 	import JobArgs from '../JobArgs.svelte'
 	import { writable } from 'svelte/store'
+	import LogViewer from '../LogViewer.svelte'
 
-	export let job: Job
+	export let job: Job | undefined
 	let timeout: NodeJS.Timeout | undefined
 	let watchJob: (id: string) => Promise<void>
-	let result: any, args: Job['args']
+	let args = job?.args
+	let result: any
+	let loaded = false
 
-	$: open = $openStore === job.id
-	$: loaded = result && args
-
-	function setValues(event: { detail: Job }) {
-		result = event.detail['result']
-		args = event.detail.args
-	}
+	$: open = $openStore === job?.id
+	$: completed = job?.type === Job.type.COMPLETED_JOB
+	$: logs = job?.logs || logs
 
 	async function instantOpen() {
+		if (!job) {
+			return
+		}
 		openStore.set(job.id)
 		if (!loaded) {
 			await tick()
@@ -51,10 +53,16 @@
 			openStore.set('')
 		}
 	}
+
+	function onDone(event: { detail: Job }) {
+		job = event.detail
+		result = job['result']
+		loaded = true
+	}
 </script>
 
-<TestJobLoader {job} bind:watchJob on:done={setValues} />
 <svelte:window on:keydown={({ key }) => ['Escape', 'Esc'].includes(key) && close()} />
+<TestJobLoader bind:job bind:watchJob on:done={onDone} />
 
 <div
 	on:mouseenter={staggeredOpen}
@@ -64,7 +72,7 @@
 	class="relative"
 >
 	<slot {open} />
-	{#if open && loaded}
+	{#if open}
 		<div
 			transition:fade={{ duration: 200 }}
 			class="absolute z-50 bottom-[71px] left-0 bg-white rounded-lg rounded-b-none border-x border-t
@@ -74,7 +82,12 @@
 				<JobArgs {args} tableClass="!pt-0 !min-w-0 !block" />
 			</div>
 			<div class="w-1/2 h-full overflow-auto p-2">
-				<DisplayResult {result} />
+				{#if completed}
+					<DisplayResult {result} />
+				{:else}
+					<div class="text-sm font-semibold text-gray-600 mb-1"> Job is still running </div>
+					<LogViewer content={logs} isLoading wrapperClass="!h-[calc(100%-24px)]" />
+				{/if}
 			</div>
 		</div>
 	{/if}
