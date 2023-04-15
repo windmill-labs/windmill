7c58c3cd0c3092caed4d86280e41f638014db7b7
Ruben Fiszel <ruben@rubenfiszel.com>
2023-04-16 00:40:54 +0200

    fix refreshFlow loop

--- frontend/src/lib/components/apps/editor/inlineScriptsPanel/InlineScriptEditorPanel.svelte
+++ frontend/src/lib/components/apps/editor/inlineScriptsPanel/InlineScriptEditorPanel.svelte
@@ -67,12 +67,15 @@
 	}
 
 	async function refreshFlow(x: RunnableByPath) {
-		const schema = (await loadSchema($workspaceStore ?? '', x.path, 'flow')) ?? emptySchema()
-		x.schema = schema
 		if (componentInput?.type == 'runnable') {
-			componentInput.fields = computeFields(schema, false, componentInput.fields)
+			const schema = (await loadSchema($workspaceStore ?? '', x.path, 'flow')) ?? emptySchema()
+			x.schema = schema
+			const nfields = computeFields(schema, false, componentInput.fields)
+			if (!deepEqual(componentInput.fields, nfields)) {
+				componentInput.fields = nfields
+				componentInput = componentInput
+			}
 		}
-		componentInput = componentInput
 	}
 	$: if (
 		componentInput &&
