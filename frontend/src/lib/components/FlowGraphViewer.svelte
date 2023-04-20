40747106c56dfac922fcbb4b031a94f42f0faf90
Ádám Kovács <vartefol@gmail.com>
2023-04-20 15:51:59 +0200

    fix(frontend): Update flow viewer styling

--- frontend/src/lib/components/FlowGraphViewer.svelte
+++ frontend/src/lib/components/FlowGraphViewer.svelte
@@ -5,7 +5,7 @@
 	import IconedPath from './IconedPath.svelte'
 	import { scriptPathToHref } from '../utils'
 	import type { FlowModule, FlowValue } from '$lib/gen'
-	import { Drawer, DrawerContent } from './common'
+	import { Badge, Button, Drawer, DrawerContent } from './common'
 	import { Highlight } from 'svelte-highlight'
 	import ObjectViewer from './propertyPicker/ObjectViewer.svelte'
 	import typescript from 'svelte-highlight/languages/typescript'
@@ -46,7 +46,7 @@
 				</div>
 				{#if stepDetail.value.path.startsWith('hub/')}
 					<div class="mt-6">
-						<h3>Code</h3>
+						<h3 class="mb-2">Code</h3>
 						<iframe
 							class="w-full h-full text-sm"
 							title="embedded script from hub"
@@ -69,11 +69,11 @@
 		{/if}
 	</DrawerContent>
 </Drawer>
-<div class="grid grid-cols-3 w-full">
+<div class="grid grid-cols-3 w-full h-full">
 	<div
 		class="{noSide
 			? 'col-span-3'
-			: 'sm:col-span-2 col-span-3'} w-full border border-gray-400 max-h-screen"
+			: 'sm:col-span-2 col-span-3'} w-full border border-gray-400 max-h-full"
 		class:overflow-auto={overflowAuto}
 	>
 		<FlowGraph
@@ -86,41 +86,69 @@
 	</div>
 	{#if !noSide}
 		<div
-			class="w-full border-r border-b border-t border-gray-400 min-h-[150px] p-2 overflow-auto hidden sm:block"
+			class="relative w-full h-full min-h-[150px] max-h-[90vh] border-r border-b border-t border-gray-400
+			p-2 pt-0 overflow-auto hidden sm:flex flex-col gap-4"
 		>
 			{#if stepDetail == undefined}
-				<SchemaViewer schema={flow?.schema} />
-
-				<span class="font-black text-lg w-full my-4 mt-14">
-					<span>Click on a step to see its details</span>
-				</span>
+				<div>
+					<p class="font-medium text-gray-600 text-center pt-4 pb-8">
+						Click on a step to see its details
+					</p>
+					<h3 class="mb-2 font-semibold">Flow Inputs</h3>
+					<SchemaViewer schema={flow?.schema} />
+				</div>
 			{:else if stepDetail == 'Input'}
 				<SchemaViewer schema={flow?.schema} />
 			{:else if stepDetail == 'Result'}
-				End of the flow
+				<p class="font-medium text-gray-600 text-center pt-4 pb-8"> End of the flow </p>
 			{:else if typeof stepDetail != 'string' && stepDetail.value}
-				<div class="font-black text-lg w-full mb-6"
-					>Step {stepDetail.id ?? ''}<span class="ml-2 font-normal">{stepDetail.summary || ''}</span
-					></div
-				>
+				<div class="sticky top-0 bg-white w-full flex items-center py-2">
+					{#if stepDetail.id}
+						<Badge color="indigo">
+							{stepDetail.id}
+						</Badge>
+					{/if}
+					<span class="ml-2 font-medium text-lg">
+						{#if stepDetail.summary}
+							{stepDetail.summary}
+						{:else if stepDetail.value.type == 'identity'}
+							Identity
+						{:else if stepDetail.value.type == 'forloopflow'}
+							For loop
+						{:else if stepDetail.value.type == 'branchall'}
+							Run all branches
+						{:else if stepDetail.value.type == 'branchone'}
+							Run one branch
+						{:else if stepDetail.value.type == 'flow'}
+							Inner flow
+						{:else}
+							Anonymous step
+						{/if}
+					</span>
+				</div>
 				{#if stepDetail.value.type == 'identity'}
-					<div> An identity step return as output its input </div>
+					<p class="font-medium text-gray-600 text-center pt-4 pb-8">
+						An identity step returns its inputs as outputs
+					</p>
 				{:else if stepDetail.value.type == 'rawscript'}
-					<div class="text-2xs mb-4">
-						<h3 class="mb-2">Step Inputs</h3>
+					<div class="text-xs">
+						<h3 class="mb-2 font-semibold">Step Inputs</h3>
 						<InputTransformsViewer inputTransforms={stepDetail?.value?.input_transforms ?? {}} />
 					</div>
 
-					<h3 class="mb-2"
-						>Code &nbsp; <button class="text-gray-600 text-xs" on:click={codeViewer.openDrawer}
-							>Expand</button
-						>
-					</h3>
-					<span class="!text-xs">
-						<HighlightCode language={stepDetail.value.language} code={stepDetail.value.content} />
-					</span>
+					<div>
+						<div class="mb-2 flex justify-between items-center">
+							<h3 class="font-semibold">Code</h3>
+							<Button size="xs2" color="light" variant="contained" on:click={codeViewer.openDrawer}>
+								Expand
+							</Button>
+						</div>
+						<div class="w-full overflow-auto">
+							<HighlightCode language={stepDetail.value.language} code={stepDetail.value.content} />
+						</div>
+					</div>
 				{:else if stepDetail.value.type == 'script'}
-					<div class="mb-4">
+					<div>
 						<a
 							rel="noreferrer"
 							target="_blank"
@@ -130,19 +158,25 @@
 							<IconedPath path={stepDetail?.value?.path ?? ''} />
 						</a>
 					</div>
-					<div class="text-2xs mb-4">
-						<h3 class="mb-2">Step Inputs</h3>
+					<div class="text-2xs">
+						<h3 class="mb-2 font-semibold">Step Inputs</h3>
 						<InputTransformsViewer inputTransforms={stepDetail?.value?.input_transforms ?? {}} />
 					</div>
 					{#if stepDetail.value.path.startsWith('hub/')}
-						<div class="mt-6">
-							<h3
-								>Code&nbsp; <button class="text-gray-600 text-xs" on:click={codeViewer.openDrawer}
-									>Expand</button
+						<div class="flex flex-col grow">
+							<div class="mb-2 flex justify-between items-center">
+								<h3 class="font-semibold">Code</h3>
+								<Button
+									size="xs2"
+									color="light"
+									variant="contained"
+									on:click={codeViewer.openDrawer}
 								>
-							</h3>
+									Expand
+								</Button>
+							</div>
 							<iframe
-								class="w-full h-full text-sm"
+								class="w-full grow text-sm"
 								title="embedded script from hub"
 								frameborder="0"
 								src="https://hub.windmill.dev/embed/script/{stepDetail.value?.path?.substring(4)}"
@@ -150,19 +184,24 @@
 						</div>
 					{/if}
 				{:else if stepDetail.value.type == 'forloopflow'}
-					<p
-						>For loop with iterator: {#if stepDetail.value.iterator.type == 'static'}<ObjectViewer
-								json={stepDetail.value.iterator.value}
-							/>{:else}
+					<div>
+						<p class="font-medium text-gray-600 pb-2"> Iterator expression: </p>
+						{#if stepDetail.value.iterator.type == 'static'}
+							<ObjectViewer json={stepDetail.value.iterator.value} />
+						{:else}
 							<span class="text-xs">
 								<Highlight language={typescript} code={cleanExpr(stepDetail.value.iterator.expr)} />
 							</span>
-						{/if}</p
-					>
+						{/if}
+					</div>
 				{:else if stepDetail.value.type == 'branchall'}
-					<p>Run all branches</p>
+					<p class="font-medium text-gray-600 text-center pt-4 pb-8">
+						All branches will run, regardless of the inputs
+					</p>
 				{:else if stepDetail.value.type == 'branchone'}
-					<p>Run one branch based on a predicate</p>
+					<p class="font-medium text-gray-600 text-center pt-4 pb-8">
+						Only one branch will run based on a predicate
+					</p>
 				{:else if stepDetail.value.type == 'flow'}
 					<FlowPathViewer noSide path={stepDetail.value.path} />
 				{/if}
