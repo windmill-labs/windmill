3d7f91a5ecf2407d82d2ae7cebd03c5e571a1939
Ádám Kovács <43071496+adam-kov@users.noreply.github.com>
2023-04-22 09:56:16 +0200

    fix(frontend): App color picker overflow issue (#1449)

--- frontend/src/lib/components/apps/editor/settingsPanel/inputEditor/ColorInput.svelte
+++ frontend/src/lib/components/apps/editor/settingsPanel/inputEditor/ColorInput.svelte
@@ -2,26 +2,43 @@
 	import { createEventDispatcher } from 'svelte'
 	import ColorPicker, { ChromeVariant } from 'svelte-awesome-color-picker'
 	import { ClearableInput } from '../../../../common'
+	import { createPopperActions } from 'svelte-popperjs'
+	import { fade } from 'svelte/transition'
 
 	export let value: string = '#fff'
 	const dispatch = createEventDispatcher()
+	const [popperRef, popperContent] = createPopperActions()
 	let isOpen = false
+	let width: number
 
 	$: dispatch('change', value)
+
+	function open() {
+		isOpen = true
+	}
 </script>
 
-<div class="color-picker-input grow">
-	<ClearableInput readonly bind:value on:focus={() => (isOpen = true)} />
-	<ColorPicker
-		bind:isOpen
-		bind:hex={value}
-		label={value}
-		canChangeMode={false}
-		components={ChromeVariant}
-		toRight
-		--focus-color="#e0e7ff"
-	/>
+<div use:popperRef bind:clientWidth={width} class="grow">
+	<ClearableInput readonly bind:value on:focus={open} />
 </div>
+{#if isOpen}
+	<div
+		transition:fade={{ duration: 150 }}
+		use:popperContent={{ placement: 'bottom', strategy: 'fixed' }}
+		class="color-picker-input z-[1002]"
+		style="width: {width > 280 ? width : 280}px;"
+	>
+		<ColorPicker
+			bind:isOpen
+			bind:hex={value}
+			label={value}
+			canChangeMode={false}
+			components={ChromeVariant}
+			toRight
+			--focus-color="#e0e7ff"
+		/>
+	</div>
+{/if}
 
 <style global>
 	.color-picker-input span > label {
@@ -29,10 +46,11 @@
 	}
 
 	.color-picker-input span > div.wrapper {
-		box-shadow: 0 10px 40px -5px rgba(0, 0, 0, 0.25) !important;
+		box-shadow: 0 5px 30px -5px rgba(0, 0, 0, 0.25) !important;
 		border-style: none !important;
 		border-radius: 0.375rem !important;
-		z-index: 30;
+		top: 6px !important;
+		z-index: 30 !important;
 	}
 
 	.color-picker-input .slider-wrapper {
@@ -48,5 +66,11 @@
 		border: 1px solid #2e2e2eaf !important;
 		top: -2px !important;
 		box-shadow: 0 2px 6px -2px rgba(0, 0, 0, 0.5) !important;
+	}
+
+	.color-picker-input .input-container > input {
+		background-color: #ffffff;
+		border: 1px solid #d1d5db;
+		border-radius: 6px;
 	}
 </style>
