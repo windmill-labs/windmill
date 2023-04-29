ff963bcaea3e7f4d376397256866e57ee9b76836
Ruben Fiszel <ruben@rubenfiszel.com>
2023-04-29 09:45:03 +0200

    chore(main): release 1.90.0 (#1455)
    * chore(main): release 1.90.0
    
    * Apply automatic changes
    
    * Apply automatic changes
    
    * update api client
    
    ---------
    
    Co-authored-by: rubenfiszel <rubenfiszel@users.noreply.github.com>

--- backend/windmill-api-client/build.rs
+++ backend/windmill-api-client/build.rs
@@ -13,7 +13,9 @@ fn main() {
     let spec = serde_json::from_reader(file).unwrap();
     let mut generator = progenitor::Generator::default();
 
-    let content = generator.generate_text(&spec).unwrap();
+    let tokens = generator.generate_tokens(&spec).unwrap();
+    let ast = syn::parse2(tokens).unwrap();
+    let content = prettyplease::unparse(&ast);
 
     let mut out_file = Path::new(&env::var("OUT_DIR").unwrap()).to_path_buf();
     out_file.push("codegen.rs");
