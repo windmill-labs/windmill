254774ce71ef043e87e288bc3edd4fd246d987ed
Ruben Fiszel <ruben@rubenfiszel.com>
2023-05-06 14:36:36 +0200

    add react support

---
+++ frontend/src/routes/(root)/(logged)/apps/get_raw/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { Skeleton } from '$lib/components/common'
+	import { onMount } from 'svelte'
+
+	let loaded = false
+
+	onMount(async () => {
+		//@ts-ignore
+
+		await import('http://localhost/app.iife.js')
+		loaded = false
+	})
+</script>
+
+<div id="root" />
+
+{#if !loaded}
+	<Skeleton layout={[10]} />
+{/if}
