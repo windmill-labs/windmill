83ea50bb26eb9882c8fe71ddecd85eae2c070ac7
Ruben Fiszel <ruben@rubenfiszel.com>
2023-05-09 20:36:50 +0200

    change drawer truncated for long title

--- frontend/src/lib/components/common/drawer/DrawerContent.svelte
+++ frontend/src/lib/components/common/drawer/DrawerContent.svelte
@@ -9,18 +9,19 @@
 	export let forceOverflowVisible = false
 	export let tooltip: string = ''
 	export let documentationLink: string | undefined = undefined
-
 </script>
 
 <div class="flex flex-col divide-y h-screen max-h-screen">
-	<div class="flex justify-between w-wull items-center px-2 py-2">
-		<div class="flex items-center gap-2">
+	<div class="flex justify-between w-full items-center px-2 py-2 gap-2">
+		<div class="flex items-center gap-2 min-w-0">
 			<CloseButton on:close />
 
-			<span class="font-semibold truncate text-gray-800">{title ?? ''} 
+			<span class="font-semibold truncate text-gray-800"
+				>{title ?? ''}
 				{#if tooltip != '' || documentationLink}
 					<Tooltip {documentationLink} scale={0.9}>{tooltip}</Tooltip>
-				{/if}</span>
+				{/if}</span
+			>
 		</div>
 		{#if $$slots.actions}
 			<div class="flex gap-2 items-center justify-end">
