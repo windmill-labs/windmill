490dbcd2fe10876621484efb422e3283b8e5dfcf
Ruben Fiszel <ruben@rubenfiszel.com>
2023-05-10 18:55:37 +0200

    feat(backend): run endpoints also support support x-www-form-urlencoded encoded payloads

--- frontend/src/routes/(root)/(logged)/flows/get/[...path]/+page.svelte
+++ frontend/src/routes/(root)/(logged)/flows/get/[...path]/+page.svelte
@@ -316,7 +316,8 @@
 				<h2 bind:this={webhook} class="mt-10 text-gray-700 pb-1 mb-3 border-b"
 					>Webhoosk<Tooltip>
 						Pass the input as a json payload, the token as a Bearer token or as query arg
-						`?token=XXX` and pass as header: 'Content-Type: application/json <a
+						`?token=XXX` and pass as header: 'Content-Type: application/json'. The webhook also
+						support 'x-www-form-urlencoded' encoded payloads <a
 							href="https://docs.windmill.dev/docs/core_concepts/webhooks">See docs</a
 						></Tooltip
 					></h2
