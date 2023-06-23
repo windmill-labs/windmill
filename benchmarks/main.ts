0eab904185482733938d50d361b0d854f867894e
Ruben Fiszel <ruben@windmill.dev>
2023-06-23 15:11:07 +0200

    chore(main): release 1.122.0 (#1761)
    * chore(main): release 1.122.0
    
    * Apply automatic changes
    
    ---------
    
    Co-authored-by: rubenfiszel <rubenfiszel@users.noreply.github.com>

--- benchmarks/main.ts
+++ benchmarks/main.ts
@@ -21,7 +21,7 @@ async function login(email: string, password: string): Promise<string> {
   });
 }
 
-export const VERSION = "v1.121.0";
+export const VERSION = "v1.122.0";
 
 await new Command()
   .name("wmillbench")
