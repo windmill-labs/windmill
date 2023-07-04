0b372acb3ba2b52c38594899707a8a285d2ae788
HugoCasa <hugo@casademont.ch>
2023-07-04 11:34:57 +0200

    feat(backend): add OpenAI key workspace settings

--- backend/windmill-api/src/workspaces.rs
+++ backend/windmill-api/src/workspaces.rs
@@ -64,7 +64,8 @@ pub fn workspaced_service() -> Router {
         .route("/edit_auto_invite", post(edit_auto_invite))
         .route("/edit_deploy_to", post(edit_deploy_to))
         .route("/tarball", get(tarball_workspace))
-        .route("/premium_info", get(premium_info));
+        .route("/premium_info", get(premium_info))
+        .route("/edit_openai_key", post(edit_openai_key));
 
     #[cfg(feature = "enterprise")]
     tracing::info!("stripe enabled");
@@ -111,6 +112,7 @@ pub struct WorkspaceSettings {
     pub plan: Option<String>,
     pub webhook: Option<String>,
     pub deploy_to: Option<String>,
+    pub openai_key: Option<String>,
 }
 
 #[derive(FromRow, Serialize, Debug)]
@@ -148,6 +150,11 @@ struct EditAutoInvite {
 #[derive(Deserialize)]
 struct EditWebhook {
     webhook: Option<String>,
+}
+
+#[derive(Deserialize)]
+struct EditOpenAIKey {
+    openai_key: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -642,6 +649,48 @@ async fn edit_webhook(
     tx.commit().await?;
 
     Ok(format!("Edit webhook for workspace {}", &w_id))
+}
+
+async fn edit_openai_key(
+    authed: Authed,
+    Extension(db): Extension<DB>,
+    Path(w_id): Path<String>,
+    Authed { is_admin, username, .. }: Authed,
+    Json(ew): Json<EditOpenAIKey>,
+) -> Result<String> {
+    require_admin(is_admin, &username)?;
+
+    let mut tx = db.begin().await?;
+
+    if let Some(openai_key) = &ew.openai_key {
+        sqlx::query!(
+            "UPDATE workspace_settings SET openai_key = $1 WHERE workspace_id = $2",
+            openai_key,
+            &w_id
+        )
+        .execute(&mut tx)
+        .await?;
+    } else {
+        sqlx::query!(
+            "UPDATE workspace_settings SET openai_key = NULL WHERE workspace_id = $1",
+            &w_id,
+        )
+        .execute(&mut tx)
+        .await?;
+    }
+    audit_log(
+        &mut tx,
+        &authed.username,
+        "workspaces.edit_openai_key",
+        ActionKind::Update,
+        &w_id,
+        Some(&authed.email),
+        Some([("openai_key", &format!("{:?}", ew.openai_key)[..])].into()),
+    )
+    .await?;
+    tx.commit().await?;
+
+    Ok(format!("Edit openai_key for workspace {}", &w_id))
 }
 
 async fn list_workspaces_as_super_admin(
