c1f55cc0a6c4dadb16d09c8f2f85a60bca21f267
Faton Ramadani <faton.ramadani14@gmail.com>
2023-07-10 10:30:07 +0200

    fix(frontend): Fix debug runs zIndex

--- frontend/src/lib/components/common/drawer/Drawer.svelte
+++ frontend/src/lib/components/common/drawer/Drawer.svelte
@@ -106,7 +106,7 @@
 	.drawer.open {
 		height: 100%;
 		width: 100%;
-		z-index: 1002;
+		z-index: var(--zIndex, 1002);
 		transition: z-index var(--duration) step-start;
 		pointer-events: auto;
 	}
