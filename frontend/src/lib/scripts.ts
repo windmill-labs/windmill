198515db39bc365613c788454250b954542aa6be
Ruben Fiszel <ruben@rubenfiszel.com>
2023-07-10 08:37:22 +0200

    fix build

--- frontend/src/lib/scripts.ts
+++ frontend/src/lib/scripts.ts
@@ -12,7 +12,11 @@ export function scriptLangToEditorLang(lang: Script.language) {
 		return 'typescript'
 	} else if (lang == 'nativets') {
 		return 'typescript'
+	} else if (lang == 'graphql') {
+		return 'typescript'
 	} else if (lang == 'postgresql') {
+		return 'sql'
+	} else if (lang == 'mysql') {
 		return 'sql'
 	} else if (lang == 'python3') {
 		return 'python'
