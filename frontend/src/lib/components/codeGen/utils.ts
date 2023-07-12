df3a0beb89ea738e935e001f6573995f9e421619
HugoCasa <hugo@casademont.ch>
2023-07-12 20:05:16 +0200

    feat: use typed dict for AI gen

--- frontend/src/lib/components/codeGen/utils.ts
+++ frontend/src/lib/components/codeGen/utils.ts
@@ -6,7 +6,7 @@ import { capitalize, toCamel } from '$lib/utils'
 
 function compile(schema: Schema) {
 	function rec(x: { [name: string]: SchemaProperty }, root = false) {
-		let res = '{ '
+		let res = '{\n'
 		const entries = Object.entries(x)
 		if (entries.length == 0) {
 			return 'any'
@@ -14,33 +14,66 @@ function compile(schema: Schema) {
 		let i = 0
 		for (let [name, prop] of entries) {
 			if (prop.type == 'object') {
-				res += `${name}: ${rec(prop.properties ?? {})}`
+				res += `  ${name}: ${rec(prop.properties ?? {})}`
 			} else if (prop.type == 'array') {
-				res += `${name}: ${prop?.items?.type ?? 'any'}[]`
+				res += `  ${name}: ${prop?.items?.type ?? 'any'}[]`
 			} else {
 				let typ = prop?.type ?? 'any'
 				if (typ == 'integer') {
 					typ = 'number'
 				}
-				res += `${name}: ${typ}`
+				res += `  ${name}: ${typ}`
 			}
 			i++
 			if (i < entries.length) {
-				res += ', '
+				res += ',\n'
 			}
 		}
-		res += ' }'
+		res += '\n}'
 		return res
 	}
 	return rec(schema.properties, true)
 }
 
-export function formatResourceTypes(resourceTypes: ResourceType[], camel = false) {
-	const result = resourceTypes.map((resourceType) => {
-		return `${camel ? toCamel(capitalize(resourceType.name)) : resourceType.name} ${compile(
-			resourceType.schema
-		)}`
-	})
+export function pythonCompile(schema: Schema) {
+	let res = ''
+	const entries = Object.entries(schema.properties)
+	if (entries.length === 0) {
+		return 'dict'
+	}
+	let i = 0
+	for (let [name, prop] of entries) {
+		let typ = 'dict'
+		if (prop.type === 'array') {
+			typ = 'list'
+		} else if (prop.type === 'string') {
+			typ = 'str'
+		} else if (prop.type === 'number') {
+			typ = 'float'
+		} else if (prop.type === 'integer') {
+			typ = 'int'
+		} else if (prop.type === 'boolean') {
+			typ = 'bool'
+		}
+		res += `    ${name}: ${typ}`
+		i++
+		if (i < entries.length) {
+			res += '\n'
+		}
+	}
+	return res
+}
 
-	return result.join(', ')
+export function formatResourceTypes(resourceTypes: ResourceType[], lang: 'python3' | 'typescript') {
+	if (lang === 'python3') {
+		const result = resourceTypes.map((resourceType) => {
+			return `class ${resourceType.name}(TypedDict):\n${pythonCompile(resourceType.schema)}`
+		})
+		return '\n' + result.join('\n\n')
+	} else {
+		const result = resourceTypes.map((resourceType) => {
+			return `type ${toCamel(capitalize(resourceType.name))} = ${compile(resourceType.schema)}`
+		})
+		return '\n' + result.join('\n\n')
+	}
 }
