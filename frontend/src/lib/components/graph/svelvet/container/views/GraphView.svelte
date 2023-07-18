37054b7bacdc20a4396c88d9954965a727f60c5a
Faton Ramadani <faton.ramadani14@gmail.com>
2023-07-18 10:22:33 +0200

    Fix build

--- frontend/src/lib/components/graph/svelvet/container/views/GraphView.svelte
+++ frontend/src/lib/components/graph/svelvet/container/views/GraphView.svelte
@@ -90,6 +90,7 @@
 
 		d3.select(`#zoom_in_${canvasId}`).on('click', function () {
 			try {
+				// @ts-ignore
 				d3Zoom.scaleBy(nodes.transition().duration(250), 1.4)
 			} catch (e) {
 				console.log('error', e)
@@ -97,6 +98,7 @@
 		})
 		d3.select(`#zoom_out_${canvasId}`).on('click', function () {
 			try {
+				// @ts-ignore
 				d3Zoom.scaleBy(nodes.transition().duration(250), 0.714)
 			} catch (e) {
 				console.log('error', e)
