508760329cbfe701cf263a30090b0309e1e9221b
Faton Ramadani <faton.ramadani14@gmail.com>
2023-07-28 08:34:59 +0200

    feat(frontend): app carousel

---
+++ frontend/src/lib/components/apps/components/display/AppCarousel.svelte
@@ -0,0 +1,123 @@
+<script lang="ts">
+	import Carousel from 'svelte-carousel'
+
+	import { getContext } from 'svelte'
+	import { initConfig, initOutput } from '../../editor/appUtils'
+	import { components } from '../../editor/component'
+	import SubGridEditor from '../../editor/SubGridEditor.svelte'
+	import type {
+		AppViewerContext,
+		ComponentCustomCSS,
+		RichConfiguration,
+		RichConfigurations
+	} from '../../types'
+	import { concatCustomCss } from '../../utils'
+	import InputValue from '../helpers/InputValue.svelte'
+	import InitializeComponent from '../helpers/InitializeComponent.svelte'
+
+	export let id: string
+	export let configuration: RichConfigurations
+	export let componentContainerHeight: number
+	export let tabs: string[]
+	export let customCss: ComponentCustomCSS<'tabscomponent'> | undefined = undefined
+	export let render: boolean
+	export let disabledTabs: RichConfiguration[]
+
+	let resolvedConfig = initConfig(
+		components['tabscomponent'].initialData.configuration,
+		configuration
+	)
+
+	const {
+		app,
+		worldStore,
+		focusedGrid,
+		selectedComponent,
+		mode,
+		componentControl,
+		connectingInput
+	} = getContext<AppViewerContext>('AppViewerContext')
+
+	let selected: string = tabs[0]
+	let tabHeight: number = 0
+
+	let outputs = initOutput($worldStore, id, {
+		selectedTabIndex: 0
+	})
+
+	function handleTabSelection() {
+		selectedIndex = tabs?.indexOf(selected)
+		outputs?.selectedTabIndex.set(selectedIndex)
+
+		if ($focusedGrid?.parentComponentId != id || $focusedGrid?.subGridIndex != selectedIndex) {
+			$focusedGrid = {
+				parentComponentId: id,
+				subGridIndex: selectedIndex
+			}
+		}
+	}
+
+	$componentControl[id] = {
+		left: () => {
+			const index = tabs.indexOf(selected)
+			if (index > 0) {
+				selected = tabs[index - 1]
+				return true
+			}
+			return false
+		},
+		right: () => {
+			const index = tabs.indexOf(selected)
+			if (index < tabs.length - 1) {
+				selected = tabs[index + 1]
+				return true
+			}
+			return false
+		},
+		setTab: (tab: number) => {
+			selected = tabs[tab]
+			handleTabSelection()
+		}
+	}
+
+	$: selected != undefined && handleTabSelection()
+	let selectedIndex = tabs?.indexOf(selected) ?? -1
+	$: css = concatCustomCss($app.css?.tabscomponent, customCss)
+
+	let resolvedDisabledTabs: boolean[] = []
+</script>
+
+<InputValue {id} input={configuration.tabsKind} bind:value={resolvedConfig.tabsKind} />
+
+<InitializeComponent {id} />
+
+{#each disabledTabs ?? [] as disableTab, index}
+	<InputValue {id} input={disableTab} bind:value={resolvedDisabledTabs[index]} />
+{/each}
+
+<div class={resolvedConfig.tabsKind == 'sidebar' ? 'flex gap-4 w-full' : 'w-full'}>
+	<div class="w-full">
+		<Carousel>
+			{#if $app.subgrids}
+				{#each tabs ?? [] as _res, i}
+					<SubGridEditor
+						{id}
+						visible={render && i === selectedIndex}
+						subGridId={`${id}-${i}`}
+						class={css?.container?.class}
+						style={css?.container?.style}
+						containerHeight={resolvedConfig.tabsKind !== 'sidebar' && $mode !== 'preview'
+							? componentContainerHeight - tabHeight
+							: componentContainerHeight}
+						on:focus={() => {
+							if (!$connectingInput.opened) {
+								$selectedComponent = [id]
+								handleTabSelection()
+							}
+						}}
+					/>
+				{/each}
+			{/if}
+		</Carousel>
+	</div>
+</div>
