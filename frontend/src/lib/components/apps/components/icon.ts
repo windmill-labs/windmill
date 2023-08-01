edd98c61492dec186a21e58a2f588b0e7192ff66
Faton Ramadani <faton.ramadani14@gmail.com>
2023-08-01 12:55:45 +0200

    fix(frontend): Fix app icons (#1977)

--- frontend/src/lib/components/apps/components/icon.ts
+++ frontend/src/lib/components/apps/components/icon.ts
@@ -1,4 +1,10 @@
-import { toKebabCase } from '../utils'
+function toKebabCase(str: string): string {
+	return str
+		.replace(/([a-z0-9])([A-Z])/g, '$1-$2') // Convert camel case to kebab case
+		.replace(/([A-Z0-9])([A-Z])/g, '$1-$2') // Separate consecutive uppercase letters
+		.replace(/([a-z])([0-9])/g, '$1-$2') // Separate letters from digits
+		.toLowerCase()
+}
 
 export async function loadIcon(name: string): Promise<any> {
 	let iconComponent: any
