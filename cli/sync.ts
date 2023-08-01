0f07c14f08a05967f508d17f6259bfe49e7e8536
Ruben Fiszel <ruben@rubenfiszel.com>
2023-08-01 10:55:19 +0200

    fix(cli): add support for inlining native ts

--- cli/sync.ts
+++ cli/sync.ts
@@ -137,6 +137,7 @@ function ZipFSElement(zip: JSZip, useYaml: boolean): DynFSElement {
       else if (language == "mysql") ext = "my.sql";
       else if (language == "bigquery") ext = "bq.sql";
       else if (language == "bun") ext = "bun.ts";
+      else if (language == "nativets") ext = "native.ts";
 
       return `${name}.inline_script.${ext}`;
     }
