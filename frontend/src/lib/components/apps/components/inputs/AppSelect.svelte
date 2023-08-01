ddcf73b85c5005f8e8bf524ea9e6ba9411b39dfc
Ruben Fiszel <ruben@rubenfiszel.com>
2023-08-01 10:31:25 +0200

    improve error for app select

--- frontend/src/lib/components/apps/components/inputs/AppSelect.svelte
+++ frontend/src/lib/components/apps/components/inputs/AppSelect.svelte
@@ -16,6 +16,10 @@
 	import InitializeComponent from '../helpers/InitializeComponent.svelte'
 	import ResolveConfig from '../helpers/ResolveConfig.svelte'
 	import { components } from '../../editor/component'
+	import { Alert } from '$lib/components/common'
+	import { classNames } from '$lib/utils'
+	import { Bug } from 'lucide-svelte'
+	import Popover from '$lib/components/Popover.svelte'
 
 	export let id: string
 	export let configuration: RichConfigurations
@@ -180,34 +184,56 @@
 			}
 		}}
 	>
-		<Select
-			--border-radius="0"
-			--border-color="#999"
-			bind:filterText
-			on:filter={handleFilter}
-			on:clear={onClear}
-			on:change={onChange}
-			inAppEditor={true}
-			items={listItems}
-			listAutoWidth={resolvedConfig.fullWidth}
-			inputStyles={SELECT_INPUT_DEFAULT_STYLE.inputStyles}
-			containerStyles={'border-color: #999;' +
-				($darkMode
-					? SELECT_INPUT_DEFAULT_STYLE.containerStylesDark
-					: SELECT_INPUT_DEFAULT_STYLE.containerStyles) +
-				css?.input?.style}
-			{value}
-			placeholder={resolvedConfig.placeholder}
-			on:focus={() => {
-				if (!$connectingInput.opened) {
-					$selectedComponent = [id]
-				}
-			}}
-		>
-			<svelte:fragment slot="item" let:item
-				>{#if resolvedConfig.create}{item.created ? 'Add new: ' : ''}{/if}{item.label}
-			</svelte:fragment>
-		</Select>
+		{#if Array.isArray(listItems) && listItems.every((x) => typeof x == 'object' && typeof x['label'] == 'string' && `value` in x)}
+			<Select
+				--border-radius="0"
+				--border-color="#999"
+				bind:filterText
+				on:filter={handleFilter}
+				on:clear={onClear}
+				on:change={onChange}
+				inAppEditor={true}
+				items={listItems}
+				listAutoWidth={resolvedConfig.fullWidth}
+				inputStyles={SELECT_INPUT_DEFAULT_STYLE.inputStyles}
+				containerStyles={'border-color: #999;' +
+					($darkMode
+						? SELECT_INPUT_DEFAULT_STYLE.containerStylesDark
+						: SELECT_INPUT_DEFAULT_STYLE.containerStyles) +
+					css?.input?.style}
+				{value}
+				placeholder={resolvedConfig.placeholder}
+				on:focus={() => {
+					if (!$connectingInput.opened) {
+						$selectedComponent = [id]
+					}
+				}}
+			>
+				<svelte:fragment slot="item" let:item
+					>{#if resolvedConfig.create}{item.created ? 'Add new: ' : ''}{/if}{item.label}
+				</svelte:fragment>
+			</Select>
+		{:else}
+			<Popover notClickable placement="bottom" popupClass="!bg-surface border w-96">
+				<div
+					class={classNames(
+						'bg-red-100 w-full h-full flex items-center justify-center text-red-500'
+					)}
+				>
+					<Bug size={14} />
+				</div>
+				<span slot="text">
+					<div class="bg-surface">
+						<Alert title="Incorrect options" type="error" size="xs" class="h-full w-full ">
+							The selectable items should be an array of {'{"value": any, "label":}'}. Found:
+							<pre class="w-full bg-surface p-2 rounded-md whitespace-pre-wrap"
+								>{JSON.stringify(listItems, null, 4)}</pre
+							>
+						</Alert>
+					</div>
+				</span>
+			</Popover>
+		{/if}
 	</div>
 </AlignWrapper>
 
