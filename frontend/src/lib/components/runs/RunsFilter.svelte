8ab843428bb6c8b98ce0c1691d85794a9840c7fc
Faton Ramadani <faton.ramadani14@gmail.com>
2023-08-10 11:50:34 +0200

    feat(frontend): rework the filters

--- frontend/src/lib/components/runs/RunsFilter.svelte
+++ frontend/src/lib/components/runs/RunsFilter.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Button } from '../common'
+	import { Button, Popup } from '../common'
 	import { page } from '$app/stores'
 	import { setQuery } from '$lib/navigation'
 	import ToggleButton from '../common/toggleButton-v2/ToggleButton.svelte'
@@ -8,14 +8,17 @@
 	import { goto } from '$app/navigation'
 	import { createEventDispatcher } from 'svelte'
 	import AutoComplete from 'simple-svelte-autocomplete'
-	import Toggle from '../Toggle.svelte'
+	import { Filter } from 'lucide-svelte'
+	import JsonEditor from '../apps/editor/settingsPanel/inputEditor/JsonEditor.svelte'
 
 	export let paths: string[] = []
 	export let selectedPath: string | undefined = undefined
 	export let success: boolean | undefined = undefined
 	export let isSkipped: boolean | undefined = undefined
-	export let autoRefresh: boolean = false
-
+	export let argFilter: string
+	export let argError: string
+	export let resultFilter: string
+	export let resultError: string
 	export let jobKindsCat: string
 
 	const dispatch = createEventDispatcher()
@@ -138,6 +141,43 @@
 		</div>
 	</div>
 
+	<Popup
+		floatingConfig={{ strategy: 'absolute', placement: 'bottom-end' }}
+		containerClasses="border rounded-lg shadow-lg p-4 bg-surface"
+		let:close
+	>
+		<svelte:fragment slot="button">
+			<Button color="light" size="xs" nonCaptureEvent={true} variant="border">
+				<div class="flex flex-row gap-1 items-center">
+					<Filter size={16} />
+					Filter by arguments/result
+				</div>
+			</Button>
+		</svelte:fragment>
+		<div class="flex flex-col w-72 p-2 gap-2">
+			<span class="text-sm eading-6 font-semibold">Filters</span>
+			<span class="text-xs leading-6">
+				{`Filter by a json being a subset of the args. Try '\{"foo": "bar"\}'`}
+			</span>
+			<span class="text-xs eading-6 font-semibold">Filter by arguments</span>
+			<JsonEditor on:change bind:error={argError} bind:code={argFilter} />
+			<span class="text-xs eading-6 font-semibold">Filter by result</span>
+			<JsonEditor on:change bind:error={resultError} bind:code={resultFilter} />
+
+			<Button
+				size="xs"
+				color="dark"
+				on:click={() => {
+					close(null)
+					argFilter = ''
+					resultFilter = ''
+				}}
+			>
+				Clear filter
+			</Button>
+		</div>
+	</Popup>
+
 	<Button
 		color="light"
 		variant="border"
@@ -149,10 +189,4 @@
 	>
 		Clear filters
 	</Button>
-	<Toggle
-		size="xs"
-		bind:checked={autoRefresh}
-		options={{ right: 'Auto-refresh' }}
-		textClass="whitespace-nowrap"
-	/>
 </div>
--- frontend/src/routes/(root)/(logged)/runs/[...path]/+page.svelte
+++ frontend/src/routes/(root)/(logged)/runs/[...path]/+page.svelte
@@ -6,13 +6,13 @@
 	import { page } from '$app/stores'
 	import { sendUserToast } from '$lib/toast'
 	import { workspaceStore } from '$lib/stores'
-	import { Button, Drawer, DrawerContent, Popup } from '$lib/components/common'
+	import { Button, Drawer, DrawerContent } from '$lib/components/common'
 	import { goto } from '$app/navigation'
 	import RunChart from '$lib/components/RunChart.svelte'
 
 	import JobPreview from '$lib/components/runs/JobPreview.svelte'
 	import Tooltip from '$lib/components/Tooltip.svelte'
-	import { Filter, RefreshCcw } from 'lucide-svelte'
+	import { RefreshCw } from 'lucide-svelte'
 	import CalendarPicker from '$lib/components/common/calendarPicker/CalendarPicker.svelte'
 
 	import RunsTable from '$lib/components/runs/RunsTable.svelte'
@@ -20,7 +20,7 @@
 	import { Pane, Splitpanes } from 'svelte-splitpanes'
 	import RunsFilter from '$lib/components/runs/RunsFilter.svelte'
 	import MobileFilters from '$lib/components/runs/MobileFilters.svelte'
-	import JsonEditor from '$lib/components/apps/editor/settingsPanel/inputEditor/JsonEditor.svelte'
+	import Toggle from '$lib/components/Toggle.svelte'
 
 	let jobs: Job[] | undefined
 	let intervalId: NodeJS.Timer | undefined
@@ -33,7 +33,7 @@
 	let isSkipped: boolean | undefined =
 		$page.url.searchParams.get('is_skipped') != undefined
 			? $page.url.searchParams.get('is_skipped') == 'true'
-			: undefined
+			: false
 
 	let argFilter: any = $page.url.searchParams.get('arg') ?? undefined
 	let resultFilter: any = $page.url.searchParams.get('result') ?? undefined
@@ -84,7 +84,9 @@
 		})
 	}
 
+	let loading: boolean = false
 	async function loadJobs(): Promise<void> {
+		loading = true
 		jobs = undefined
 		try {
 			const newJobs = await fetchJobs(maxTs, minTs)
@@ -93,6 +95,7 @@
 			sendUserToast(`There was a problem fetching jobs: ${err}`, true)
 			console.error(JSON.stringify(err))
 		}
+		loading = false
 	}
 
 	async function syncer() {
@@ -235,6 +238,20 @@
 				minTs = new Date(new Date().getTime() - 24 * 60 * 60 * 1000).toISOString()
 				maxTs = new Date().toISOString()
 			}
+		},
+		{
+			label: 'Last 7 days',
+			setMinMax: () => {
+				minTs = new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000).toISOString()
+				maxTs = new Date().toISOString()
+			}
+		},
+		{
+			label: 'Last month',
+			setMinMax: () => {
+				minTs = new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000).toISOString()
+				maxTs = new Date().toISOString()
+			}
 		}
 	]
 
@@ -268,7 +285,10 @@
 					{jobKindsCat}
 					bind:selectedPath={searchPath}
 					bind:success
-					bind:autoRefresh
+					bind:argFilter
+					bind:resultFilter
+					bind:argError
+					bind:resultError
 					on:change={reloadLogsWithoutFilterError}
 					on:clearFilters={() => {
 						minTs = undefined
@@ -287,7 +307,10 @@
 							{jobKindsCat}
 							bind:selectedPath={searchPath}
 							bind:success
-							bind:autoRefresh
+							bind:argFilter
+							bind:resultFilter
+							bind:argError
+							bind:resultError
 							on:change={reloadLogsWithoutFilterError}
 							on:clearFilters={() => {
 								minTs = undefined
@@ -365,75 +388,17 @@
 				]}
 			>
 				<div class="flex flex-row items-center gap-2">
-					<RefreshCcw size={14} />
+					<RefreshCw size={14} class={loading ? 'animate-spin' : ''} />
 					{manualDates[selectedManualDate].label}
 				</div>
 			</Button>
 
-			<Popup
-				floatingConfig={{ strategy: 'absolute', placement: 'bottom-end' }}
-				containerClasses="border rounded-lg shadow-lg p-4 bg-surface"
-				let:close
-			>
-				<svelte:fragment slot="button">
-					<Button color="light" size="xs" nonCaptureEvent={true}>
-						<div class="flex flex-row gap-1 items-center">
-							<Filter size={16} />
-							Filter by arguments
-						</div>
-					</Button>
-				</svelte:fragment>
-				<div class="flex flex-col w-72 p-2 gap-2">
-					<span class="text-sm eading-6 font-semibold">Filter by arguments</span>
-					<span class="text-xs leading-6">
-						{`Filter by a json being a subset of the args. Try '\{"foo": "bar"\}'`}</span
-					>
-
-					<JsonEditor on:change bind:error={argError} bind:code={argFilter} />
-					<Button
-						size="xs"
-						color="dark"
-						on:click={() => {
-							close(null)
-							argFilter = ''
-						}}
-					>
-						Clear filter
-					</Button>
-				</div>
-			</Popup>
-			<Popup
-				floatingConfig={{ strategy: 'absolute', placement: 'bottom-end' }}
-				containerClasses="border rounded-lg shadow-lg p-4 bg-surface"
-				let:close
-			>
-				<svelte:fragment slot="button">
-					<Button color="light" size="xs" nonCaptureEvent={true}>
-						<div class="flex flex-row gap-1 items-center">
-							<Filter size={16} />
-							Filter by result
-						</div>
-					</Button>
-				</svelte:fragment>
-				<div class="flex flex-col w-72 p-2 gap-2">
-					<span class="text-sm leading-6 font-semibold">Filter by result</span>
-					<span class="text-xs leading-6">
-						{`Filter by a json being a subset of the args. Try '\{"foo": "bar"\}'`}
-					</span>
-
-					<JsonEditor on:change bind:error={resultError} bind:code={resultFilter} />
-					<Button
-						size="xs"
-						color="dark"
-						on:click={async () => {
-							close(null)
-							resultFilter = ''
-						}}
-					>
-						Clear filter
-					</Button>
-				</div>
-			</Popup>
+			<Toggle
+				size="xs"
+				bind:checked={autoRefresh}
+				options={{ right: 'Auto-refresh' }}
+				textClass="whitespace-nowrap"
+			/>
 		</div>
 	</div>
 
