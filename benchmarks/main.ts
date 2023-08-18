f15ea7b644c8d944746668da79e32622342dec78
rubenfiszel <rubenfiszel@users.noreply.github.com>
2023-08-18 12:58:40 +0000

    Apply automatic changes

--- benchmarks/main.ts
+++ benchmarks/main.ts
@@ -21,7 +21,7 @@ async function login(email: string, password: string): Promise<string> {
   });
 }
 
-export const VERSION = "v1.151.0";
+export const VERSION = "v1.151.1";
 
 await new Command()
   .name("wmillbench")
--- cli/main.ts
+++ cli/main.ts
@@ -31,7 +31,7 @@ addEventListener("error", (event) => {
   }
 });
 
-export const VERSION = "v1.151.0";
+export const VERSION = "v1.151.1";
 
 let command: any = new Command()
   .name("wmill")
