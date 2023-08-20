befef02cd95067ad1bb027e27d6a605e26c09063
Ruben Fiszel <ruben@rubenfiszel.com>
2023-08-20 11:34:32 +0200

    fix drift usage

--- frontend/src/lib/forLater.ts
+++ frontend/src/lib/forLater.ts
@@ -14,7 +14,7 @@ function adjustDate(drift: number): Date {
 export async function computeDrift() {
 	const now = Date.now()
 	const dbClock = await JobService.getDbClock()
-	dbClockDrift.set(now - dbClock)
+	dbClockDrift.set(dbClock - now)
 }
 
 export function forLater(scheduledString: string): boolean {
