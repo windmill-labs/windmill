7a140d1a076dd5c71ef7ef8c9b1d2f8dcba666b6
Ruben Fiszel <ruben@rubenfiszel.com>
2023-08-25 19:39:42 +0200

    v0

--- frontend/src/lib/components/apps/components/display/AppCustomComponent.svelte
+++ frontend/src/lib/components/apps/components/display/AppCustomComponent.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-	import { getContext } from 'svelte'
+	import { getContext, onMount } from 'svelte'
 	import { initOutput } from '../../editor/appUtils'
 	import type { AppViewerContext, RichConfigurations } from '../../types'
 
-	type CCProps<Init, Output> = {
-		initState: Init | undefined
+	type CCProps<Input> = {
 		id: string
-		saveInitState: (state: Init) => void
-		setOutput: (output: Output) => void
+		render: boolean
+		passSetters: (setter: Setter<Input>) => void
+		setOutput: (output: any) => void
+	}
+
+	interface Setter<Input> {
+		onRender(bool: boolean): void
+		onInput(input: Input): void
 	}
 
 	import InitializeComponent from '../helpers/InitializeComponent.svelte'
@@ -20,31 +25,35 @@
 	export let render: boolean
 	export let componentInput: AppInput | undefined
 
-	let divId = 'custom-component-{id}'
+	const COMPONENT_NAME = 'mycomponent'
+
+	let divId = `custom-component-${id}`
 	const { worldStore } = getContext<AppViewerContext>('AppViewerContext')
 
-	let divEl: HTMLDivElement
 	const outputs = initOutput($worldStore, id, {
 		result: undefined,
 		ccOutput: undefined,
 		loading: false
 	})
 
-	let ccProps: CCProps<any, any> = {
-		initState: undefined,
+	let setInput
+	let setRender
+	let ccProps: CCProps<any> = {
+		render,
 		id: divId,
-		saveInitState: (state) => {
-			console.log('saveInitState', state)
+		passSetters: (setter) => {
+			setInput = setter.onInput
+			setRender = setter.onRender
 		},
 		setOutput: (output) => {
 			outputs.ccOutput.set(output)
 			console.log('setOutput', output)
 		}
 	}
+	let loaded = false
 
 	// $: renderer && divEl && renderer(ccProps)
-	$: render && load()
-	async function load() {
+	onMount(async () => {
 		// //@ts-ignore
 		// await import('http://localhost:3000/app.iife.js')
 		/* @vite-ignore */
@@ -56,28 +65,34 @@
 		await import('https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js')
 		//@ts-ignore
 		await import('http://localhost:3000/cc.iife.js')
-
+		loaded = true
 		try {
-			let renderer: ((props: CCProps<any, any>) => void) | undefined = globalThis.windmill['foo']
+			let renderer: (props: CCProps<number>) => void = globalThis.windmill[COMPONENT_NAME]
 			renderer?.(ccProps)
 		} catch (e) {
 			sendUserToast('Custom Component seem to be ill-defined', true)
 			console.error(e)
 		}
+	})
+	let result
+
+	$: render != undefined && handleRender()
+	function handleRender() {
+		setRender?.(render)
 	}
+	$: result != undefined && handleResult()
+	function handleResult() {
+		setInput?.(result)
+	}
+	// $: result && setInput && setInput(result)
 </script>
 
 <InitializeComponent {id} />
 
 {#if render}
-	<RunnableWrapper
-		{outputs}
-		{render}
-		autoRefresh
-		{componentInput}
-		{id}
-		bind:result={ccProps.initState}
-	>
-		<div bind:this={divEl} id={divId} />
-	</RunnableWrapper>
+	<div class="w-full h-full overflow-auto {COMPONENT_NAME}">
+		<RunnableWrapper {outputs} {render} autoRefresh {componentInput} {id} bind:result>
+			<div id={divId} />
+		</RunnableWrapper>
+	</div>
 {/if}
