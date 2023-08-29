7f7a97f009a849a79b843341f2aea7b941d51c9c
Ruben Fiszel <ruben@rubenfiszel.com>
2023-08-29 21:41:26 +0200

    fix premium info computation

--- backend/windmill-api/src/workspaces.rs
+++ backend/windmill-api/src/workspaces.rs
@@ -252,7 +252,7 @@ async fn premium_info(
     require_admin(authed.is_admin, &authed.username)?;
     let mut tx = db.begin().await?;
     let row = sqlx::query_as::<_, PremiumWorkspaceInfo>(
-        "SELECT premium, usage.usage FROM workspace LEFT JOIN usage ON usage.id = $1 AND usage.is_workspace IS true WHERE workspace.id = $1",
+        "SELECT premium, usage.usage FROM workspace LEFT JOIN usage ON usage.id = $1 AND month_ = EXTRACT(YEAR FROM current_date) * 12 + EXTRACT(MONTH FROM current_date) AND usage.is_workspace IS true WHERE workspace.id = $1",
     )
     .bind(w_id)
     .fetch_one(&mut *tx)
