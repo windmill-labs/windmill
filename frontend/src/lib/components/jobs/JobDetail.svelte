d311d76557432a72a5d6d7ab010aeb1fe0e599de
Ruben Fiszel <ruben@rubenfiszel.com>
2023-08-31 12:55:28 +0200

    fix: uniformize that all job links specify the workspace

--- frontend/src/lib/components/jobs/JobDetail.svelte
+++ frontend/src/lib/components/jobs/JobDetail.svelte
@@ -23,6 +23,7 @@
 	import JobPreview from './JobPreview.svelte'
 	import TimeAgo from '../TimeAgo.svelte'
 	import { forLater } from '$lib/forLater'
+	import { workspaceStore } from '$lib/stores'
 
 	const SMALL_ICON_SCALE = 0.7
 
@@ -116,7 +117,7 @@
 							</div>
 						</JobPreview>
 						<div class="whitespace-nowrap">
-							{#if 'job_kind' in job}<a href="/run/{job.id}"
+							{#if 'job_kind' in job}<a href="/run/{job.id}?workspace={job.workspace_id}"
 									><Badge color="blue">{job.job_kind}</Badge></a
 								>
 							{/if}
@@ -191,14 +192,15 @@
 							<Icon class="text-secondary" data={faBarsStaggered} scale={SMALL_ICON_SCALE} /><span
 								class="mx-1"
 							>
-								Step of flow <a href={`/run/${job.parent_job}`}>{truncateRev(job.parent_job, 6)}</a
+								Step of flow <a href={`/run/${job.parent_job}?workspace=${job.workspace_id}`}
+									>{truncateRev(job.parent_job, 6)}</a
 								></span
 							>
 						{:else}
 							<Icon class="text-secondary" data={faRobot} scale={SMALL_ICON_SCALE} /><span
 								class="mx-1"
 							>
-								Parent <a href={`/run/${job.parent_job}`}>{job.parent_job}</a></span
+								Parent <a href={`/run/${job.parent_job}?workspace=${job.workspace_id}`}>{job.parent_job}</a></span
 							>
 						{/if}
 					{:else if job && job.schedule_path}
