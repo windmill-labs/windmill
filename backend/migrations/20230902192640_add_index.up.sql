9bfb97d2c5a3a893bd081951fe131c4b32df5a4f
Ruben Fiszel <ruben@rubenfiszel.com>
2023-09-03 01:19:02 +0200

    all

---
+++ backend/migrations/20230902192640_add_index.up.sql
@@ -0,0 +1,8 @@
+-- Add up migration script here
+DROP INDEX IF EXISTS queue_sort;
+CREATE INDEX IF NOT EXISTS queue_sort ON queue (scheduled_for, created_at, tag) WHERE running = false;
+
+CREATE TABLE IF NOT EXISTS concurrency_counter (
+  concurrency_id VARCHAR(1000) PRIMARY KEY,
+  counter INTEGER NOT NULL
+);
