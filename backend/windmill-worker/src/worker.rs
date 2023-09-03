12ef348b7ff77b22dabf145a1e8798636cb24b9b
Ruben Fiszel <ruben@rubenfiszel.com>
2023-09-03 18:05:23 +0200

    fix initialization

--- backend/windmill-worker/src/worker.rs
+++ backend/windmill-worker/src/worker.rs
@@ -560,7 +560,7 @@ pub async fn run_worker<R: rsmq_async::RsmqConnection + Send + Sync + Clone + 's
 
     let vacuum_shift = rand::thread_rng().gen_range(0..VACUUM_PERIOD);
 
-    IS_READY.store(true, Ordering::Relaxed);
+    // IS_READY.store(true, Ordering::Relaxed);
     tracing::info!(worker = %worker_name, "listening for jobs");
 
     loop {
@@ -732,7 +732,7 @@ pub async fn run_worker<R: rsmq_async::RsmqConnection + Send + Sync + Clone + 's
             }
         };
 
-        if !first_run {
+        if first_run {
             first_run = false;
         }
 
