5054a8ceb8e71cf723e9f83836f2985e43ef2805
Daniel Englert <d.englert@elo.com>
2023-09-28 11:41:24 +0200

    feat(backend): parse expires_in from string in TokenResponse

--- backend/windmill-common/src/more_serde.rs
+++ backend/windmill-common/src/more_serde.rs
@@ -9,6 +9,8 @@
 //! helpers for serde + serde derive attributes
 
 use crate::utils::rd_string;
+use serde::{Deserialize, Deserializer};
+use std::{fmt::Display, str::FromStr};
 
 pub fn default_true() -> bool {
     true
@@ -32,4 +34,33 @@ pub fn default_id() -> String {
 
 pub fn is_default<T: Default + std::cmp::PartialEq>(t: &T) -> bool {
     &T::default() == t
+}
+
+pub fn maybe_number_opt<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: FromStr + serde::Deserialize<'de>,
+    <T as FromStr>::Err: Display,
+{
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    enum NumericOrNull<'a, T> {
+        String(String),
+        Str(&'a str),
+        FromStr(T),
+        Null,
+    }
+
+    match NumericOrNull::<T>::deserialize(deserializer)? {
+        NumericOrNull::String(s) => match s.as_str() {
+            "" => Ok(None),
+            _ => T::from_str(&s).map(Some).map_err(serde::de::Error::custom),
+        },
+        NumericOrNull::Str(s) => match s {
+            "" => Ok(None),
+            _ => T::from_str(s).map(Some).map_err(serde::de::Error::custom),
+        },
+        NumericOrNull::FromStr(i) => Ok(Some(i)),
+        NumericOrNull::Null => Ok(None),
+    }
 }
