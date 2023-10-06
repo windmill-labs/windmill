07913ae08fa8e3b784b2f7e861bbe8f4d9ac6d73
Faton Ramadani <faton.ramadani14@gmail.com>
2023-10-06 18:04:18 +0200

    feat(frontend): add all options

--- frontend/src/lib/components/wizards/AgGridWizard.svelte
+++ frontend/src/lib/components/wizards/AgGridWizard.svelte
@@ -7,6 +7,15 @@
 	export let value: {
 		width: number
 		hide: boolean
+		flex: number
+		sort: 'asc' | 'desc'
+		sortIndex: number
+		aggFunc: string
+		pivot: boolean
+		pivotIndex: number
+		pinned: 'left' | 'right' | boolean
+		rowGroup: boolean
+		rowGroupIndex: number
 		valueFormatter: string
 		valueParser: string
 		field: string
@@ -47,5 +56,49 @@
 
 		<span class="text-xs mb-1 leading-6">Value Parser</span>
 		<SimpleEditor autoHeight lang="javascript" bind:code={value.valueParser} />
+
+		<!-- Flex -->
+		<span class="text-xs mb-1 leading-6">Flex</span>
+		<input type="number" placeholder="flex" bind:value={value.flex} />
+
+		<!-- Sort -->
+		<span class="text-xs mb-1 leading-6">Sort</span>
+		<select bind:value={value.sort}>
+			<option value={null}>None</option>
+			<option value="asc">Ascending</option>
+			<option value="desc">Descending</option>
+		</select>
+
+		<!-- Sort Index -->
+		<span class="text-xs mb-1 leading-6">Sort Index</span>
+		<input type="number" placeholder="sort index" bind:value={value.sortIndex} />
+
+		<!-- Aggregation Function -->
+		<span class="text-xs mb-1 leading-6">Aggregation Function</span>
+		<SimpleEditor autoHeight lang="javascript" bind:code={value.aggFunc} />
+
+		<!-- Pivot -->
+		<span class="text-xs mb-1 leading-6">Pivot</span>
+		<Toggle bind:checked={value.pivot} size="xs" />
+
+		<!-- Pivot Index -->
+		<span class="text-xs mb-1 leading-6">Pivot Index</span>
+		<input type="number" placeholder="pivot index" bind:value={value.pivotIndex} />
+
+		<!-- Pinned -->
+		<span class="text-xs mb-1 leading-6">Pinned</span>
+		<select bind:value={value.pinned}>
+			<option value={null}>None</option>
+			<option value="left">Left</option>
+			<option value="right">Right</option>
+		</select>
+
+		<!-- Row Group -->
+		<span class="text-xs mb-1 leading-6">Row Group</span>
+		<Toggle bind:checked={value.rowGroup} size="xs" />
+
+		<!-- Row Group Index -->
+		<span class="text-xs mb-1 leading-6">Row Group Index</span>
+		<input type="number" placeholder="row group index" bind:value={value.rowGroupIndex} />
 	</div>
 </Popup>
