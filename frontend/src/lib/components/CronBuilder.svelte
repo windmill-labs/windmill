11e0bc76c4bc80339e43590f5becf6b2442a2227
Faton Ramadani <faton.ramadani14@gmail.com>
2023-10-08 23:31:53 +0200

    feat(frontend): App polish (#2397)
    * fix(frontend): app polish
    
    * wip
    
    * fix(frontend): rework schedule drawer
    
    * fix(frontend): add Label wrapper for consistency
    
    * fix(frontend): fix tab inconstiencies
    
    * fix(frontend): make kbd modifier visible + fix spacings
    
    * fix(frontend): add spinning animation when assistant is loading
    
    * wip
    
    * fix(frontend): add inner border
    
    * feat(frontend): reorganised metadata fields
    
    * feat(frontend): fix merge
    
    * feat(frontend): fix build
    
    * feat(frontend): fix build
    
    * Update CenteredPage.svelte
    
    * feat(frontend): fix inner-border dark
    
    * wip
    
    * feat(frontend): Fix buttons
    
    ---------
    
    Co-authored-by: Ruben Fiszel <ruben@windmill.dev>

---
+++ frontend/src/lib/components/CronBuilder.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Section from './Section.svelte'
+	import { Button, Popup } from './common'
+
+	import { Clock } from 'lucide-svelte'
+</script>
+
+<Popup
+	floatingConfig={{ strategy: 'absolute', placement: 'bottom-end' }}
+	containerClasses="border rounded-lg shadow-lg p-4 bg-surface"
+	let:close
+>
+	<svelte:fragment slot="button">
+		<Button color="dark" size="xs" nonCaptureEvent={true}>
+			<div class="flex flex-row gap-1 items-center">
+				<Clock size={16} />
+				Use simplified builder
+			</div>
+		</Button>
+	</svelte:fragment>
+	<Section label="CRON Builder">
+		<div class="flex flex-col w-72">
+			<slot {close} />
+		</div>
+	</Section>
+</Popup>
