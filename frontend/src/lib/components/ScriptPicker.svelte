3f3e039abb3ea2c343b0e56df93a50cf4fa196ee
gbouv <guillaume@windmill.dev>
2023-10-13 18:45:29 +0200

    filter on backend if only one kind

--- frontend/src/lib/components/ScriptPicker.svelte
+++ frontend/src/lib/components/ScriptPicker.svelte
@@ -48,14 +48,23 @@
 				label: `${flow.path}${flow.summary ? ` | ${truncate(flow.summary, 20)}` : ''}`
 			}))
 		} else if (itemKind == 'script') {
-			items = (await ScriptService.listScripts({ workspace: $workspaceStore! })).filter(
-				(script) => kinds.includes(script.kind)
-			).map(
-				(script) => ({
-					value: script.path,
-					label: `${script.path}${script.summary ? ` | ${truncate(script.summary, 20)}` : ''}`
-				})
-			)
+			if (kinds.length === 1) {
+				items = (await ScriptService.listScripts({ workspace: $workspaceStore!, kind: kinds[0]})).map(
+					(script) => ({
+						value: script.path,
+						label: `${script.path}${script.summary ? ` | ${truncate(script.summary, 20)}` : ''}`
+					})
+				)
+			} else {
+				items = (await ScriptService.listScripts({ workspace: $workspaceStore! })).filter(
+					(script) => kinds.includes(script.kind)
+				).map(
+					(script) => ({
+						value: script.path,
+						label: `${script.path}${script.summary ? ` | ${truncate(script.summary, 20)}` : ''}`
+					})
+				)
+			}
 		} else {
 			items =
 				$hubScripts?.map((x) => ({
