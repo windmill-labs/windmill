6a34f47e8db67b953f048ac90d7b944ef1520771
Ruben Fiszel <ruben@rubenfiszel.com>
2023-10-16 10:41:05 +0200

    add bigquery

---
+++ backend/migrations/20231016084043_addback_bigquery.up.sql
@@ -0,0 +1,3 @@
+-- Add up migration script here
+UPDATE config set config = '{"worker_tags": ["nativets", "postgresq", "mysql", "graphql", "snowflake", "bigquery"]}'::jsonb where name = 'worker__native' and config = '{"worker_tags": ["nativets", "postgresql", "mysql", "graphql"
+, "snowflake"]}'::jsonb ;
