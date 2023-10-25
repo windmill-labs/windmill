8efe0cadacaae894cf93a3a569e3d0b8e79c7d14
Ruben Fiszel <ruben@rubenfiszel.com>
2023-10-25 09:40:51 +0200

    fix: fix global instance dynamic css

--- frontend/src/lib/components/apps/components/helpers/ResolveStyle.svelte
+++ frontend/src/lib/components/apps/components/helpers/ResolveStyle.svelte
@@ -6,22 +6,27 @@
 	export let css: ComponentCssProperty
 	export let id: string
 	export let key: string
+	export let extraKey: string = ''
 
 	export let customCss: Record<string, ComponentCssProperty> | undefined = undefined
 	export let componentStyle: Record<string, ComponentCssProperty> | undefined = undefined
 
 	let evalClassValue: string | undefined = undefined
+	let evalClassValueGlobal: string | undefined = undefined
 
 	function updateCss(
 		componentStyle: Record<string, ComponentCssProperty> | undefined,
 		customCss: Record<string, ComponentCssProperty> | undefined,
-		evalClassValue: string | undefined
+		evalClassValue: string | undefined,
+		evalClassValueGlobal: string | undefined
 	) {
 		const { class: componentClass, style: componentStyleValue } = componentStyle?.[key] ?? {}
 		const { class: customClass, style: customStyleValue } = customCss?.[key] ?? {}
 
 		const newCss = {
-			class: [componentClass, customClass, evalClassValue].filter(Boolean).join(' '),
+			class: [componentClass, customClass, evalClassValue ?? evalClassValueGlobal]
+				.filter(Boolean)
+				.join(' '),
 			style: [componentStyleValue, customStyleValue].filter(Boolean).join(';')
 		}
 
@@ -31,17 +36,39 @@
 	}
 
 	// When any of the values change, update the css
-	$: updateCss(componentStyle, customCss, evalClassValue)
+	$: updateCss(componentStyle, customCss, evalClassValue, evalClassValueGlobal)
 
 	// We need to clear the evalClassValue if the user has disabled the evalClass
 	$: if (customCss?.[key].evalClass === undefined && evalClassValue !== undefined) {
 		evalClassValue = undefined
+	}
+
+	// We need to clear the evalClassValue if the user has disabled the evalClass
+	$: if (componentStyle?.[key].evalClass === undefined && evalClassValueGlobal !== undefined) {
+		evalClassValueGlobal = undefined
 	}
 </script>
 
 {#if customCss}
 	{@const property = customCss[key]}
 	{#if property.evalClass}
-		<InputValue {id} bind:value={evalClassValue} input={property.evalClass} />
+		<InputValue
+			key={key + extraKey + 'css'}
+			{id}
+			bind:value={evalClassValue}
+			input={property.evalClass}
+		/>
+	{/if}
+{/if}
+
+{#if componentStyle}
+	{@const property = componentStyle[key]}
+	{#if property.evalClass}
+		<InputValue
+			key={key + extraKey + 'cssGlobal'}
+			{id}
+			bind:value={evalClassValueGlobal}
+			input={property.evalClass}
+		/>
 	{/if}
 {/if}
