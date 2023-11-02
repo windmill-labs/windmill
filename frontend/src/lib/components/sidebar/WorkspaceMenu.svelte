89b7cab9777e8aa2636635bc6b84905f635ab823
Faton Ramadani <faton.ramadani14@gmail.com>
2023-11-02 12:58:29 +0100

    Refactor sidebar (#2539)
    * feat(frontend): wip
    
    * feat(frontend): wip
    
    * feat(frontend): menu v2
    
    * feat(frontend): menu v2
    
    * feat(frontend): done
    
    * feat(frontend): fix mobile view
    
    * feat(frontend): fix build
    
    * feat(frontend): fix interactions
    
    * feat(frontend): fix small ui issues
    
    * feat(frontend): fix close issue
    
    * feat(frontend): fix close issue

--- frontend/src/lib/components/sidebar/WorkspaceMenu.svelte
+++ frontend/src/lib/components/sidebar/WorkspaceMenu.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	import { userWorkspaces, workspaceStore } from '$lib/stores'
-	import { classNames } from '$lib/utils'
 	import { Building } from 'lucide-svelte'
 
-	import Menu from '../common/menu/Menu.svelte'
+	import Menu from '../common/menu/MenuV2.svelte'
 	import { faCog, faPlus } from '@fortawesome/free-solid-svg-icons'
 	import { Icon } from 'svelte-awesome'
 	import { goto } from '$app/navigation'
 	import { dirtyStore } from '../common/confirmationModal/dirtyStore'
 	import { page } from '$app/stores'
 	import { switchWorkspace } from '$lib/storeUtils'
+	import MultiplayerMenu from './MultiplayerMenu.svelte'
+	import { enterpriseLicense } from '$lib/stores'
+	import MenuButton from './MenuButton.svelte'
+	import { MenuItem } from '@rgossiaux/svelte-headlessui'
+
 	export let isCollapsed: boolean = false
 
 	function waitForNextUpdate(store) {
@@ -52,42 +56,30 @@
 	}
 </script>
 
-<Menu placement="bottom-start" let:close>
-	<button
-		title="Workspace Menu"
-		slot="trigger"
-		type="button"
-		class={classNames(
-			'group w-full flex items-center text-white hover:bg-gray-50 hover:text-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 px-2 py-2 text-sm font-medium rounded-md h-8 '
-		)}
-	>
-		<div class="center-center mr-2">
-			<Building size={16} />
-		</div>
-
-		{#if !isCollapsed}
-			<span class={classNames('whitespace-pre truncate')}> {$workspaceStore} </span>
-		{/if}
-	</button>
+<Menu>
+	<div slot="trigger">
+		<MenuButton class="!text-xs" icon={Building} label={$workspaceStore ?? ''} {isCollapsed} />
+	</div>
 
 	<div class="divide-y" role="none">
 		<div class="py-1">
 			{#each $userWorkspaces as workspace}
-				<button
-					class="text-xs min-w-0 w-full overflow-hidden flex flex-col py-1.5
+				<MenuItem>
+					<button
+						class="text-xs min-w-0 w-full overflow-hidden flex flex-col py-1.5
 						{$workspaceStore === workspace.id
-						? 'cursor-default bg-surface-selected'
-						: 'cursor-pointer hover:bg-surface-hover'}"
-					on:click={async () => {
-						await toggleSwitchWorkspace(workspace.id)
-						close()
-					}}
-				>
-					<div class="text-primary pl-4 truncate text-left text-[1.2em]">{workspace.name}</div>
-					<div class="text-tertiary font-mono pl-4 text-2xs whitespace-nowrap truncate text-left">
-						{workspace.id}
-					</div>
-				</button>
+							? 'cursor-default bg-surface-selected'
+							: 'cursor-pointer hover:bg-surface-hover'}"
+						on:click={async () => {
+							await toggleSwitchWorkspace(workspace.id)
+						}}
+					>
+						<div class="text-primary pl-4 truncate text-left text-[1.2em]">{workspace.name}</div>
+						<div class="text-tertiary font-mono pl-4 text-2xs whitespace-nowrap truncate text-left">
+							{workspace.id}
+						</div>
+					</button>
+				</MenuItem>
 			{/each}
 		</div>
 		<div class="py-1" role="none">
@@ -114,14 +106,19 @@
 			</a>
 		</div>
 		<div class="py-1" role="none">
-			<a
-				href="/workspace_settings"
-				class="text-secondary block px-4 py-2 text-xs hover:bg-surface-hover hover:text-primary"
-				role="menuitem"
-				tabindex="-1"
-			>
-				<Icon class="pr-0.5" data={faCog} /> Workspace Settings
-			</a>
+			<MenuItem>
+				<a
+					href="/workspace_settings"
+					class="text-secondary block px-4 py-2 text-xs hover:bg-surface-hover hover:text-primary"
+					role="menuitem"
+					tabindex="-1"
+				>
+					<Icon class="pr-0.5" data={faCog} /> Workspace Settings
+				</a>
+			</MenuItem>
 		</div>
 	</div>
+	{#if $enterpriseLicense}
+		<MultiplayerMenu />
+	{/if}
 </Menu>
