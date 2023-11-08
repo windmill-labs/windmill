33bcd799c6b396a55ffa3a046c9154708eede481
Faton Ramadani <faton.ramadani14@gmail.com>
2023-11-08 17:12:31 +0100

    feat(frontend): Migrate flow advanced settings to new layout (#2589)
    * feat(frontend): wip
    
    * feat(frontend): migrate flow advanced settings to new layout
    
    ---------
    
    Co-authored-by: Ruben Fiszel <ruben@windmill.dev>

--- frontend/src/lib/components/flows/content/FlowModuleCache.svelte
+++ frontend/src/lib/components/flows/content/FlowModuleCache.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Label from '$lib/components/Label.svelte'
+	import Section from '$lib/components/Section.svelte'
 	import Toggle from '$lib/components/Toggle.svelte'
 	import Tooltip from '$lib/components/Tooltip.svelte'
 
@@ -10,40 +12,40 @@
 	$: isCacheEnabled = Boolean(flowModule.cache_ttl)
 </script>
 
-<h2 class="pb-4">
-	Cache
-	<Tooltip documentationLink="https://www.windmill.dev/docs/flows/cache">
-		If defined, the result of the step will be cached for the number of seconds defined such that if
-		this step were to be re-triggered with the same input it would retrieve and return its cached
-		value instead of recomputing it.
-	</Tooltip>
-</h2>
-{#if flowModule.value.type != 'rawscript'}
-	<p
-		>The cache settings need to be set in the referenced script/flow settings directly. Cache for
-		hub scripts is not available yet.</p
-	>
-{:else}
-	<Toggle
-		checked={isCacheEnabled}
-		on:change={() => {
-			if (isCacheEnabled && flowModule.cache_ttl != undefined) {
-				flowModule.cache_ttl = undefined
-			} else {
-				flowModule.cache_ttl = 600
-			}
-		}}
-		options={{
-			right: 'Cache the results for each possible inputs'
-		}}
-	/>
-	<div class="mb-4">
-		<span class="text-xs font-bold">How long to keep cache valid</span>
+<Section label="Cache" class="flex flex-col gap-4">
+	<svelte:fragment slot="header">
+		<Tooltip documentationLink="https://www.windmill.dev/docs/flows/cache">
+			If defined, the result of the step will be cached for the number of seconds defined such that
+			if this step were to be re-triggered with the same input it would retrieve and return its
+			cached value instead of recomputing it.
+		</Tooltip>
+	</svelte:fragment>
 
-		{#if flowModule.cache_ttl}
-			<SecondsInput bind:seconds={flowModule.cache_ttl} />
-		{:else}
-			<SecondsInput disabled />
-		{/if}
-	</div>
-{/if}
+	{#if flowModule.value.type != 'rawscript'}
+		<p
+			>The cache settings need to be set in the referenced script/flow settings directly. Cache for
+			hub scripts is not available yet.</p
+		>
+	{:else}
+		<Toggle
+			checked={isCacheEnabled}
+			on:change={() => {
+				if (isCacheEnabled && flowModule.cache_ttl != undefined) {
+					flowModule.cache_ttl = undefined
+				} else {
+					flowModule.cache_ttl = 600
+				}
+			}}
+			options={{
+				right: 'Cache the results for each possible inputs'
+			}}
+		/>
+		<Label label="How long to keep cache valid">
+			{#if flowModule.cache_ttl}
+				<SecondsInput bind:seconds={flowModule.cache_ttl} />
+			{:else}
+				<SecondsInput disabled />
+			{/if}
+		</Label>
+	{/if}
+</Section>
