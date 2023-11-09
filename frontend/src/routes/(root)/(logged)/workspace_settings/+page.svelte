a05c4be64a4a3b0d5fd760ce6c5769b27af24b92
gbouv <guillaume@windmill.dev>
2023-11-09 10:54:43 +0100

    6512

--- frontend/src/routes/(root)/(logged)/workspace_settings/+page.svelte
+++ frontend/src/routes/(root)/(logged)/workspace_settings/+page.svelte
@@ -33,7 +33,7 @@
 	import Toggle from '$lib/components/Toggle.svelte'
 	import TestOpenaiKey from '$lib/components/copilot/TestOpenaiKey.svelte'
 
-	const slackErrorHandler = 'hub/6511/workspace-or-schedule-error-handler-slack'
+	const slackErrorHandler = 'hub/6512/workspace-or-schedule-error-handler-slack'
 
 	let initialPath: string
 	let scriptPath: string
