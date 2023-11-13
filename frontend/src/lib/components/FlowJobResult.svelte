f185eba49cc84fc0bcfcfd00716999c11212db44
HugoCasa <hugo@casademont.ch>
2023-11-13 19:14:30 +0100

    fix: false not undefined in result (#2620)

--- frontend/src/lib/components/FlowJobResult.svelte
+++ frontend/src/lib/components/FlowJobResult.svelte
@@ -19,7 +19,7 @@
 >
 	<div class="bg-surface {col ? '' : 'max-h-80'} h-full p-1 overflow-auto relative">
 		<span class="text-tertiary">Result</span>
-		{#if result}
+		{#if result !== undefined}
 			<DisplayResult {workspaceId} {jobId} {filename} {result} />
 		{:else if loading}
 			<Loader2 class="animate-spin" />
