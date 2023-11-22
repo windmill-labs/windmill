98bc928fda00b006139d2a3f7613704fd0624fed
Faton Ramadani <faton.ramadani14@gmail.com>
2023-11-22 14:29:40 +0100

    fix(frontend): improve tutorial ux

---
+++ frontend/src/lib/components/tutorials/SkipTutorials.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+	import Button from '../common/button/Button.svelte'
+	import { CheckCircle } from 'lucide-svelte'
+
+	const dispatch = createEventDispatcher()
+</script>
+
+<div class="flex flex-row gap-2 justify-end w-full pt-2">
+	<Button
+		size="xs"
+		color="light"
+		startIcon={{ icon: CheckCircle }}
+		variant="border"
+		on:click={() => {
+			dispatch('skipThis')
+		}}
+	>
+		Mark this tutorial as completed
+	</Button>
+	<Button
+		size="xs"
+		color="light"
+		startIcon={{ icon: CheckCircle }}
+		variant="border"
+		on:click={() => {
+			dispatch('skipThis')
+		}}
+	>
+		Mark all tutorials as completed
+	</Button>
+</div>
