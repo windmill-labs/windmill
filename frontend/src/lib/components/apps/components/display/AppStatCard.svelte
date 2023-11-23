a9309402f616f6868e0dfd2bbf542abb240960e8
Faton Ramadani <faton.ramadani14@gmail.com>
2023-11-23 15:28:42 +0100

    feat(frontend): fix layout

--- frontend/src/lib/components/apps/components/display/AppStatCard.svelte
+++ frontend/src/lib/components/apps/components/display/AppStatCard.svelte
@@ -57,25 +57,23 @@
 {/each}
 
 {#if render}
-	<div class="p-6 border rounded-md h-full flex flex-row gap-4 w-full items-center">
+	<div class="p-6 border rounded-md h-full flex flex-row gap-6 w-full items-center">
 		{#if resolvedConfig.icon && iconComponent}
-			<div class="bg-indigo-500 rounded-md p-2">
+			<div class="bg-blue-500 rounded-md p-2">
 				<svelte:component this={iconComponent} size={24} color="white" />
 			</div>
 		{/if}
 		<div class="w-full">
-			<dt class="text-base font-normal text-primary">{resolvedConfig.title}</dt>
-			<dd class="mt-1 flex items-baseline justify-between md:block lg:flex">
-				<div
-					class="flex items-baseline text-2xl font-semibold text-indigo-600 dark:text-indigo-200"
-				>
+			<div class="text-base font-normal text-primary">{resolvedConfig.title}</div>
+			<div class="mt-1 flex items-baseline justify-between">
+				<div class="flex items-baseline text-2xl font-semibold text-blue-600 dark:text-blue-200">
 					{resolvedConfig?.value}
 				</div>
 
-				{#if resolvedConfig?.progress !== undefined}
+				{#if resolvedConfig?.progress !== undefined && resolvedConfig?.progress !== null && resolvedConfig?.progress !== 0}
 					<div
 						class={twMerge(
-							'flex items-center flex-row gap-2 rounded-full px-2.5 py-0.5 text-sm font-medium  md:mt-2 lg:mt-0',
+							'flex items-center flex-row gap-2 rounded-full px-2.5 py-0.5 text-sm font-medium',
 							resolvedConfig?.progress > 0
 								? 'bg-green-100 text-green-800'
 								: 'bg-red-100 text-red-800'
@@ -85,11 +83,10 @@
 							size={16}
 							class={resolvedConfig?.progress > 0 ? 'transform rotate-180' : 'transform rotate-0'}
 						/>
-						<span class="sr-only"> Increased by </span>
 						{resolvedConfig?.progress}%
 					</div>
 				{/if}
-			</dd>
+			</div>
 		</div>
 	</div>
 {/if}
