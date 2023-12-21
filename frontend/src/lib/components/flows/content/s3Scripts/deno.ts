92f9d850457b51963a9abcc840505d599d2aaf10
gbouv <guillaume@windmill.dev>
2023-12-21 20:56:23 +0100

    use S3Object type from SDK

--- frontend/src/lib/components/flows/content/s3Scripts/deno.ts
+++ frontend/src/lib/components/flows/content/s3Scripts/deno.ts
@@ -1,10 +1,9 @@
 const deno = {
-  s3_client: `import * as wmill from "npm:windmill-client@${__pkg__.version}";
+  s3_client: `import type { S3Object } from "npm:windmill-client@${__pkg__.version}";
+import * as wmill from "npm:windmill-client@${__pkg__.version}";
 import { S3Client } from "https://deno.land/x/s3_lite_client@0.2.0/mod.ts";
 
-type s3object = object;
-
-export async function main(inputFile: s3object) {
+export async function main(inputFile: S3Object) {
   // this will default to the workspace s3 resource
   let args = await wmill.denoS3LightClientSettings();
   // this will use the designated resource
@@ -14,7 +13,7 @@ export async function main(inputFile: s3object) {
   const outputFile = "output/hello.txt"
 
   // read object from S3
-  const getObjectResponse = await s3Client.getObject(inputFile["s3"]);
+  const getObjectResponse = await s3Client.getObject(inputFile.s3);
   const inputObjContent = await getObjectResponse.text();
   console.log(inputObjContent);
 
@@ -26,9 +25,10 @@ export async function main(inputFile: s3object) {
     console.log(obj.key);
   }
 
-  return {
-    "s3": outputFile,
+  const result: S3Object = {
+    s3: outputFile,
   };
+  return result;
 }
 `
 }
