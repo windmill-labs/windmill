876b2266d36643c128755e605a5deb94b6118611
Faton Ramadani <faton.ramadani14@gmail.com>
2023-12-29 12:53:32 +0100

    feat(frontend): wip

---
+++ frontend/src/lib/components/apps/components/display/AppAgChart.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import { Chart } from 'svelte-chartjs'
+	import { Chart as ChartJS, registerables, type ChartOptions } from 'chart.js'
+	import RunnableWrapper from '../helpers/RunnableWrapper.svelte'
+	import type { AppInput } from '../../inputType'
+	import type { AppViewerContext, ComponentCustomCSS, RichConfigurations } from '../../types'
+	import { initCss } from '../../utils'
+	import { getContext } from 'svelte'
+	import { initConfig, initOutput } from '../../editor/appUtils'
+	import { components } from '../../editor/component'
+	import ResolveConfig from '../helpers/ResolveConfig.svelte'
+	import { twMerge } from 'tailwind-merge'
+	import ResolveStyle from '../helpers/ResolveStyle.svelte'
+
+	export let id: string
+	export let componentInput: AppInput | undefined
+	export let configuration: RichConfigurations
+	export let initializing: boolean | undefined = undefined
+	export let customCss: ComponentCustomCSS<'piechartcomponent'> | undefined = undefined
+	export let render: boolean
+
+	const { app, worldStore } = getContext<AppViewerContext>('AppViewerContext')
+
+	const outputs = initOutput($worldStore, id, {
+		result: undefined,
+		loading: false
+	})
+
+	ChartJS.register(...registerables)
+
+	let result: undefined = undefined
+
+	const resolvedConfig = initConfig(
+		components['chartjscomponent'].initialData.configuration,
+		configuration
+	)
+	$: options = {
+		responsive: true,
+		animation: false,
+		maintainAspectRatio: false,
+		...(resolvedConfig.options ?? {})
+	} as ChartOptions
+
+	let css = initCss($app.css?.chartjscomponent, customCss)
+</script>
+
+{#each Object.keys(components['chartjscomponent'].initialData.configuration) as key (key)}
+	<ResolveConfig
+		{id}
+		{key}
+		bind:resolvedConfig={resolvedConfig[key]}
+		configuration={configuration[key]}
+	/>
+{/each}
+
+{#each Object.keys(css ?? {}) as key (key)}
+	<ResolveStyle
+		{id}
+		{customCss}
+		{key}
+		bind:css={css[key]}
+		componentStyle={$app.css?.chartjscomponent}
+	/>
+{/each}
+
+<RunnableWrapper {outputs} {render} autoRefresh {componentInput} {id} bind:initializing bind:result>
+	<div
+		class={twMerge('w-full h-full', css?.container?.class, 'wm-chartjs')}
+		style={css?.container?.style ?? ''}
+	>
+		{#if result && resolvedConfig.type}
+			{#key resolvedConfig.type}
+				{#key options}
+					<Chart type={resolvedConfig.type} data={result} {options} />
+				{/key}
+			{/key}
+		{/if}
+	</div>
+</RunnableWrapper>
