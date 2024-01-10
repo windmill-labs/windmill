d341bc2ee0269e0fdc3d00c0268fd2b3d320ae60
Faton Ramadani <faton.ramadani14@gmail.com>
2024-01-10 18:44:13 +0100

    feat(frontend): update s3 upload logic

--- frontend/src/lib/components/apps/components/inputs/AppS3FileInput.svelte
+++ frontend/src/lib/components/apps/components/inputs/AppS3FileInput.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { getContext, tick } from 'svelte'
+	import { getContext } from 'svelte'
 	import { twMerge } from 'tailwind-merge'
 	import { FileInput } from '../../../common'
 	import { initConfig, initOutput } from '../../editor/appUtils'
@@ -13,7 +13,8 @@
 	import { RunnableComponent, RunnableWrapper } from '../helpers'
 	import type { AppInput } from '../../inputType'
 	import { sendUserToast } from '$lib/toast'
-	import { createS3FileUpload } from './s3Utils'
+	import { workspaceStore } from '$lib/stores'
+	import { HelpersService, type UploadFilePart } from '$lib/gen'
 
 	export let id: string
 	export let configuration: RichConfigurations
@@ -39,42 +40,107 @@
 	})
 
 	// Receives Base64 encoded strings from the input component
-	async function handleChange(files: { name: string; data: string }[] | undefined) {
+	async function handleChange(files: File[] | undefined) {
 		for (const file of files ?? []) {
-			input = createS3FileUpload(
-				resolvedConfig.resource,
-				file.name,
-				file.data,
-				resolvedConfig.accessLevel
-			)
-
-			await tick()
-
-			if (runnableComponent) {
-				await runnableComponent?.runComponent(
-					undefined,
-					undefined,
-					undefined,
-					{},
-					{
-						done: (x) => {
-							sendUserToast(`File ${file.name} uploaded!`, false)
-						},
-						cancel: () => {
-							sendUserToast(`Error uploading file ${file.name}`, true)
-						},
-						error: () => {
-							sendUserToast(`Error uploading file ${file.name}`, true)
-						}
-					}
-				)
-			}
+			uploadFileToS3(file, file.name)
 		}
-		outputs?.result.set(files)
+		//outputs?.result.set(files)
 	}
 
 	let css = initCss($app.css?.fileinputcomponent, customCss)
 	let done: boolean = false
+
+	let uploadModalOpen = false
+	let fileUploadProgress: number | undefined = undefined
+	let fileUploadCancelled: boolean = false
+	let fileUploadErrorMsg: string | undefined = undefined
+
+	let allFilesByKey: Record<
+		string,
+		{
+			type: 'folder' | 'leaf'
+			full_key: string
+			display_name: string
+			collapsed: boolean
+			parentPath: string | undefined
+			nestingLevel: number
+		}
+	> = {}
+
+	async function uploadFileToS3(fileToUpload: File, fileToUploadKey: string) {
+		fileUploadErrorMsg = undefined
+		if (fileToUpload === undefined || fileToUploadKey === undefined) {
+			return
+		}
+		if (allFilesByKey[fileToUploadKey] !== undefined) {
+			fileUploadErrorMsg =
+				'A file with this name already exists in the S3 bucket. If you want to replace it, delete it first.'
+			return
+		}
+
+		let upload_id: string | undefined = undefined
+		let parts: UploadFilePart[] = []
+
+		let reader = fileToUpload?.stream().getReader()
+		let { value: chunk, done: readerDone } = await reader.read()
+		if (chunk === undefined || readerDone) {
+			sendUserToast('Error reading file, no data read', true)
+			return
+		}
+
+		fileUploadProgress = 0
+		while (true) {
+			let { value: chunk_2, done: readerDone } = await reader.read()
+			if (!readerDone && chunk_2 !== undefined && chunk.length <= 5 * 1024 * 1024) {
+				// AWS enforces part to be bigger than 5MB, so we accumulate bytes until we reach that limit before triggering the request to the BE
+				chunk = new Uint8Array([...chunk, ...chunk_2])
+				continue
+			}
+			fileUploadProgress += (chunk.length * 100) / fileToUpload.size
+			let response = await HelpersService.multipartFileUpload({
+				workspace: $workspaceStore!,
+				requestBody: {
+					file_key: fileToUploadKey,
+					part_content: Array.from(chunk),
+					upload_id: upload_id,
+					parts: parts,
+					is_final: readerDone,
+					cancel_upload: fileUploadCancelled
+				}
+			})
+			upload_id = response.upload_id
+			parts = response.parts
+			if (response.is_done) {
+				if (fileUploadCancelled) {
+					sendUserToast('File upload cancelled!')
+				} else {
+					sendUserToast('File upload finished!')
+				}
+				break
+			}
+			if (chunk_2 === undefined) {
+				sendUserToast(
+					'File upload is not finished, yet there is no more data to stream. This is unexpected',
+					true
+				)
+				return
+			}
+			chunk = chunk_2
+		}
+		uploadModalOpen = false
+
+		if (!fileUploadCancelled) {
+			/*
+			selectedFileKey = { s3: fileToUploadKey }
+			await loadFiles()
+			await loadFileMetadataPlusPreviewAsync(selectedFileKey['s3'])
+			*/
+		}
+
+		fileUploadProgress = undefined
+		fileUploadCancelled = false
+		fileUploadErrorMsg = undefined
+	}
 </script>
 
 {#each Object.keys(css ?? {}) as key (key)}
@@ -99,6 +165,10 @@
 
 {#if render}
 	<div class="w-full h-full p-1">
+		<div class="flex w-full bg-gray-200 rounded-full h-4 overflow-hidden">
+			<div class="h-full bg-blue-400" style="width: {fileUploadProgress ?? 0}%" />
+		</div>
+
 		{#if done}
 			<div class="flex items-center justify-center h-full flex-col gap-2">
 				{resolvedConfig.submittedFileText}
@@ -118,7 +188,6 @@
 					? resolvedConfig.acceptedFileTypes?.join(', ')
 					: undefined}
 				multiple={resolvedConfig.allowMultiple}
-				convertTo="base64"
 				returnFileNames
 				includeMimeType
 				on:change={({ detail }) => {
