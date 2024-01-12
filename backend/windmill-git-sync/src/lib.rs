fda0e28db31fba9f71be6db7280696c3949fd7be
Guillaume Bouvignies <guillaume@windmill.dev>
2024-01-12 16:19:28 +0100

    feat: GIt sync to multiple repo (#2996)

--- backend/windmill-git-sync/src/lib.rs
+++ backend/windmill-git-sync/src/lib.rs
@@ -9,6 +9,7 @@
 use std::collections::HashMap;
 
 use sqlx::{Pool, Postgres};
+use uuid::Uuid;
 use windmill_common::users::SUPERADMIN_SYNC_EMAIL;
 use windmill_common::workspaces::WorkspaceGitRepo;
 
@@ -45,7 +46,7 @@ impl DeployedObject {
     }
 }
 
-pub async fn handle_deployment_metadata<'c, R: rsmq_async::RsmqConnection + Send + 'c>(
+pub async fn handle_deployment_metadata<'c, R: rsmq_async::RsmqConnection + Send + Clone + 'c>(
     email: &str,
     created_by: &str,
     db: &DB,
@@ -94,122 +95,119 @@ pub async fn handle_deployment_metadata<'c, R: rsmq_async::RsmqConnection + Send
         ));
     }
 
-    let workspace_git_repo = workspace_git_repo_setting
+    let workspace_git_repos = workspace_git_repo_setting
         .unwrap()
-        .map(|conf| serde_json::from_value::<WorkspaceGitRepo>(conf).ok())
-        .flatten();
+        .map(|conf| serde_json::from_value::<Vec<WorkspaceGitRepo>>(conf).ok())
+        .flatten()
+        .unwrap_or_default();
 
-    let git_sync_job_uuid = if !skip_git_sync && workspace_git_repo.is_some() {
-        let workspace_git_repo = workspace_git_repo.unwrap();
+    let mut git_sync_job_uuids: Vec<Uuid> = vec![];
+    if !skip_git_sync {
+        for workspace_git_repo in workspace_git_repos {
+            let mut args: HashMap<String, serde_json::Value> = HashMap::new();
+            args.insert(
+                "repo_url_resource_path".to_string(),
+                json!(workspace_git_repo
+                    .git_repo_resource_path
+                    .strip_prefix("$res:")),
+            );
 
-        let mut args: HashMap<String, serde_json::Value> = HashMap::new();
-        args.insert(
-            "repo_url_resource_path".to_string(),
-            json!(workspace_git_repo
-                .git_repo_resource_path
-                .strip_prefix("$res:")),
-        );
+            if let Some(path) = obj_path {
+                args.insert("path".to_string(), json!(path));
+            }
+            if let Some(parent_path) = obj_parent_path.clone() {
+                args.insert("parent_path".to_string(), json!(parent_path));
+            }
 
-        if let Some(path) = obj_path {
-            args.insert("path".to_string(), json!(path));
+            let message = match obj.clone() {
+                DeployedObject::Script { path, .. } => {
+                    if deployment_message.as_ref().is_none()
+                        || deployment_message.as_ref().is_some_and(|x| x.is_empty())
+                    {
+                        format!("Script '{}' deployed", path)
+                    } else {
+                        deployment_message.clone().unwrap()
+                    }
+                }
+                DeployedObject::Flow { path, .. } => {
+                    if deployment_message.as_ref().is_none()
+                        || deployment_message.as_ref().is_some_and(|x| x.is_empty())
+                    {
+                        format!("Flow '{}' deployed", path)
+                    } else {
+                        deployment_message.clone().unwrap()
+                    }
+                }
+                DeployedObject::App { path, .. } => {
+                    if deployment_message.as_ref().is_none()
+                        || deployment_message.as_ref().is_some_and(|x| x.is_empty())
+                    {
+                        format!("App '{}' deployed", path)
+                    } else {
+                        deployment_message.clone().unwrap()
+                    }
+                }
+            };
+
+            args.insert("commit_msg".to_string(), json!(message));
+            args.insert(
+                "use_individual_branch".to_string(),
+                json!(workspace_git_repo.use_individual_branch.unwrap_or(false)),
+            );
+
+            let tx: PushIsolationLevel<'_, R> =
+                PushIsolationLevel::IsolatedRoot(db.clone(), rsmq.clone());
+
+            let (job_uuid, new_tx) = windmill_queue::push(
+                db,
+                tx,
+                &w_id,
+                JobPayload::DeploymentCallback { path: workspace_git_repo.script_path.clone() },
+                args,
+                created_by,
+                &email,
+                SUPERADMIN_SYNC_EMAIL.to_string(),
+                None,
+                None,
+                None,
+                None,
+                None,
+                false,
+                false,
+                None,
+                true,
+                None,
+                None,
+                None,
+                None,
+            )
+            .await?;
+            new_tx.commit().await?;
+
+            git_sync_job_uuids.push(job_uuid);
         }
-        if let Some(parent_path) = obj_parent_path {
-            args.insert("parent_path".to_string(), json!(parent_path));
-        }
-
-        let message = match obj.clone() {
-            DeployedObject::Script { path, .. } => {
-                if deployment_message.as_ref().is_none()
-                    || deployment_message.as_ref().is_some_and(|x| x.is_empty())
-                {
-                    format!("Script '{}' deployed", path)
-                } else {
-                    deployment_message.clone().unwrap()
-                }
-            }
-            DeployedObject::Flow { path, .. } => {
-                if deployment_message.as_ref().is_none()
-                    || deployment_message.as_ref().is_some_and(|x| x.is_empty())
-                {
-                    format!("Flow '{}' deployed", path)
-                } else {
-                    deployment_message.clone().unwrap()
-                }
-            }
-            DeployedObject::App { path, .. } => {
-                if deployment_message.as_ref().is_none()
-                    || deployment_message.as_ref().is_some_and(|x| x.is_empty())
-                {
-                    format!("App '{}' deployed", path)
-                } else {
-                    deployment_message.clone().unwrap()
-                }
-            }
-        };
-
-        args.insert("commit_msg".to_string(), json!(message));
-        args.insert(
-            "use_individual_branch".to_string(),
-            json!(workspace_git_repo.use_individual_branch.unwrap_or(false)),
-        );
-
-        let tx: PushIsolationLevel<'_, R> = PushIsolationLevel::IsolatedRoot(db.clone(), rsmq);
-
-        let (job_uuid, new_tx) = windmill_queue::push(
-            db,
-            tx,
-            &w_id,
-            JobPayload::DeploymentCallback { path: workspace_git_repo.script_path.clone() },
-            args,
-            created_by,
-            &email,
-            SUPERADMIN_SYNC_EMAIL.to_string(),
-            None,
-            None,
-            None,
-            None,
-            None,
-            false,
-            false,
-            None,
-            true,
-            None,
-            None,
-            None,
-            None,
-        )
-        .await?;
-        new_tx.commit().await?;
-        Some(job_uuid)
-    } else {
-        None
-    };
+    }
 
     // We're not persisting the default commit msg as it's pretty useless. We will persist the ones manually set by users
-    let job_uuids = if git_sync_job_uuid.is_some() {
-        vec![git_sync_job_uuid.unwrap()]
-    } else {
-        vec![]
-    };
-    if !skip_db_insert && (deployment_message.is_some() || job_uuids.len() > 0) {
+    if !skip_db_insert && (deployment_message.is_some() || git_sync_job_uuids.len() > 0) {
         // if the git sync job hasn't been triggered, and there is not custom deployment message, there's not point adding an entry to the table
         match obj.clone() {
              DeployedObject::Script { path, hash, .. } => {
                  sqlx::query!(
                      "INSERT INTO deployment_metadata (workspace_id, path, script_hash, callback_job_ids, deployment_msg) VALUES ($1, $2, $3, $4, $5)",
-                     w_id, path, hash.0, &job_uuids, deployment_message,
+                     w_id, path, hash.0, &git_sync_job_uuids, deployment_message,
                  )
              },
              DeployedObject::Flow { path, .. } => {
                  sqlx::query!(
                      "INSERT INTO deployment_metadata (workspace_id, path, callback_job_ids, deployment_msg) VALUES ($1, $2, $3, $4) ON CONFLICT (workspace_id, path) WHERE script_hash IS NULL AND app_version IS NULL DO UPDATE SET callback_job_ids = $3, deployment_msg = $4",
-                     w_id, path, &job_uuids, deployment_message,
+                     w_id, path, &git_sync_job_uuids, deployment_message,
                  )
              }
              DeployedObject::App { path, version, .. } => {
                  sqlx::query!(
                      "INSERT INTO deployment_metadata (workspace_id, path, app_version, callback_job_ids, deployment_msg) VALUES ($1, $2, $3, $4, $5)",
-                     w_id, path, version, &job_uuids, deployment_message,
+                     w_id, path, version, &git_sync_job_uuids, deployment_message,
                  )
              }
          }.execute(db)
