58cad70363310eb107a3e4c8c7b9428630e5deb3
Ruben Fiszel <ruben@rubenfiszel.com>
2024-01-18 10:47:56 +0100

    fix: cli sync improvement

--- cli/sync.ts
+++ cli/sync.ts
@@ -387,7 +387,7 @@ async function compareDynFSElement(
     } else if (
       m2[k] != v &&
       (!k.endsWith(".json") || !deepEqual(JSON.parse(v), JSON.parse(m2[k]))) &&
-      (!k.endsWith(".yaml") || !deepEqual(parseYaml(k, v), parseYaml(k, v)))
+      (!k.endsWith(".yaml") || !deepEqual(parseYaml(k, v), parseYaml(k, m2[k])))
     ) {
       changes.push({ name: "edited", path: k, after: v, before: m2[k] });
     }
