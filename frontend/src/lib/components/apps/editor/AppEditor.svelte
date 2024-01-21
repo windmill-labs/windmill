07c38eed79f7b83d22c4387e3cb08f7775cee25f
Ruben Fiszel <ruben@rubenfiszel.com>
2024-01-21 18:48:35 +0100

    make appeditor & preview resilient to no css

--- frontend/src/lib/components/apps/editor/AppEditor.svelte
+++ frontend/src/lib/components/apps/editor/AppEditor.svelte
@@ -412,7 +412,9 @@
 				css = currentAppStore.theme.css
 			} else if (currentAppStore.theme.type === 'path' && currentAppStore.theme?.path) {
 				let loadedCss = await getTheme($workspaceStore!, currentAppStore.theme.path)
-				css = loadedCss.value
+				if (loadedCss) {
+					css = loadedCss.value
+				}
 			}
 			lastTheme = JSON.stringify(currentAppStore.theme)
 		}
