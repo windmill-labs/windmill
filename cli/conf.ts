b8defbcfc68181397ae50daba6616df26e4383fd
Ruben Fiszel <ruben@rubenfiszel.com>
2024-01-22 15:23:18 +0100

    feat: deprecate .wmillignore in favor of wmill.yaml/includes

---
+++ cli/conf.ts
@@ -0,0 +1,16 @@
+import { yamlParse } from "./deps.ts";
+
+export interface WmillConfiguration {
+  includes?: string[];
+}
+export async function readConfigFile(): Promise<WmillConfiguration> {
+  try {
+    const conf = yamlParse(
+      await Deno.readTextFile("wmill.yaml")
+    ) as WmillConfiguration;
+
+    return typeof conf == "object" ? conf : ({} as WmillConfiguration);
+  } catch (e) {
+    return {};
+  }
+}
