a352d85b0ba4756226b98032e0879a9f91bb20e4
Faton Ramadani <faton.ramadani14@gmail.com>
2024-02-06 18:34:35 +0100

    fix(frontend): fix toggles margins (#3165)

--- frontend/src/lib/components/apps/editor/settingsPanel/inputEditor/StaticInputEditor.svelte
+++ frontend/src/lib/components/apps/editor/settingsPanel/inputEditor/StaticInputEditor.svelte
@@ -43,7 +43,7 @@
 		{:else if fieldType === 'date'}
 			<input on:keydown|stopPropagation type="date" bind:value={componentInput.value} />
 		{:else if fieldType === 'boolean'}
-			<Toggle bind:checked={componentInput.value} size="xs" />
+			<Toggle bind:checked={componentInput.value} size="xs" class="mt-2" />
 		{:else if fieldType === 'select' && selectOptions}
 			<select on:keydown|stopPropagation bind:value={componentInput.value}>
 				{#each selectOptions ?? [] as option}
