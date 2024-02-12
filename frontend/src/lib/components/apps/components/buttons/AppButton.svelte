bebed53d54df1e528dfde8eed952d38c236332d8
Faton Ramadani <faton.ramadani14@gmail.com>
2024-02-12 10:53:49 +0100

    feat(frontend): add confirmation modal to the app button (#3199)
    * feat(frontend): add confirmation modal to the app button
    
    * feat(frontend): add missing await
    
    * feat(frontend): select the component immediatly

--- frontend/src/lib/components/apps/components/buttons/AppButton.svelte
+++ frontend/src/lib/components/apps/components/buttons/AppButton.svelte
@@ -19,6 +19,8 @@
 	import ResolveConfig from '../helpers/ResolveConfig.svelte'
 	import ResolveStyle from '../helpers/ResolveStyle.svelte'
 	import { initCss } from '../../utils'
+	import ConfirmationModal from '$lib/components/common/confirmationModal/ConfirmationModal.svelte'
+	import Portal from 'svelte-portal'
 
 	export let id: string
 	export let componentInput: AppInput | undefined
@@ -81,6 +83,8 @@
 	$: resolvedConfig.beforeIcon && handleBeforeIcon()
 	$: resolvedConfig.afterIcon && handleAfterIcon()
 
+	let confirmedCallback: (() => void) | undefined = undefined
+
 	async function handleBeforeIcon() {
 		if (resolvedConfig.beforeIcon) {
 			beforeIconComponent = await loadIcon(resolvedConfig.beforeIcon)
@@ -109,21 +113,29 @@
 		event?.preventDefault()
 
 		$selectedComponent = [id]
-		const inputOutput = { result: outputs.result.peak(), loading: true }
-		if (rowContext && rowInputs) {
-			rowInputs.set(id, inputOutput)
-		}
-		if (iterContext && listInputs) {
-			listInputs.set(id, inputOutput)
-		}
-		if (preclickAction) {
-			await preclickAction()
+		const action = async () => {
+			const inputOutput = { result: outputs.result.peak(), loading: true }
+			if (rowContext && rowInputs) {
+				rowInputs.set(id, inputOutput)
+			}
+			if (iterContext && listInputs) {
+				listInputs.set(id, inputOutput)
+			}
+			if (preclickAction) {
+				await preclickAction()
+			}
+
+			if (!runnableComponent) {
+				runnableWrapper?.handleSideEffect(true)
+			} else {
+				await runnableComponent?.runComponent()
+			}
 		}
 
-		if (!runnableComponent) {
-			runnableWrapper?.handleSideEffect(true)
+		if (resolvedConfig?.confirmationModal?.selected === 'confirmationModal') {
+			confirmedCallback = action
 		} else {
-			await runnableComponent?.runComponent()
+			await action()
 		}
 	}
 	let loading = false
@@ -225,3 +237,29 @@
 		{/key}
 	</AlignWrapper>
 </RunnableWrapper>
+
+{#if resolvedConfig?.confirmationModal?.selected === 'confirmationModal'}
+	<Portal target="#app-editor-top-level-drawer">
+		<ConfirmationModal
+			open={Boolean(confirmedCallback)}
+			title={resolvedConfig?.confirmationModal?.configuration?.confirmationModal?.title ?? ''}
+			confirmationText={resolvedConfig?.confirmationModal?.configuration?.confirmationModal
+				?.confirmationText ?? ''}
+			on:canceled={() => {
+				confirmedCallback = undefined
+			}}
+			on:confirmed={() => {
+				if (confirmedCallback) {
+					confirmedCallback()
+				}
+				confirmedCallback = undefined
+			}}
+		>
+			<div class="flex flex-col w-full space-y-4">
+				<span>
+					{resolvedConfig?.confirmationModal?.configuration?.confirmationModal?.description ?? ''}
+				</span>
+			</div>
+		</ConfirmationModal>
+	</Portal>
+{/if}
