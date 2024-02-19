4c5d6139ce590eac0a94bb9c8bf9dbceb0dac385
Ruben Fiszel <ruben@rubenfiszel.com>
2024-02-19 17:29:57 +0100

    fix: improve date picker across app

--- frontend/src/lib/components/DateTimeInput.svelte
+++ frontend/src/lib/components/DateTimeInput.svelte
@@ -1,27 +1,94 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+	import { Button } from './common'
+
 	export let value: string | undefined = undefined
 
 	export let autofocus: boolean = false
+	export let useDropdown: boolean = false
 
-	let dateFromValue: Date | undefined = value ? new Date(value) : undefined
+	let date: string | undefined = undefined
+	let time: string | undefined = undefined
+	function parseValue(value: string | undefined = undefined) {
+		let dateFromValue: Date | undefined = value ? new Date(value) : undefined
 
-	let date = isValidDate(dateFromValue) ? dateFromValue!.toISOString().split('T')[0] : undefined
-	let time = isValidDate(dateFromValue) ? dateFromValue!.toISOString().split('T')[1] : '00:00'
+		date = isValidDate(dateFromValue) ? dateFromValue!.toISOString().split('T')[0] : undefined
+		time = isValidDate(dateFromValue)
+			? `${dateFromValue!.getHours().toString().padStart(2, '0')}:${dateFromValue!
+					.getMinutes()
+					.toString()
+					.padStart(2, '0')}`
+			: undefined
+	}
 
-	$: {
-		if (date && time) {
+	$: parseValue(value)
+
+	let initialDate = date
+	let initialTime = time
+
+	function parseDateAndTime(date: string | undefined, time: string | undefined) {
+		if (date && time && (initialDate != date || initialTime != time)) {
 			let newDate = new Date(`${date}T${time}`)
 			value = newDate.toISOString()
+			dispatch('change', value)
 		}
 	}
+
+	$: parseDateAndTime(date, time)
 
 	function isValidDate(d: Date | undefined): boolean {
 		return d instanceof Date && !isNaN(d as any)
 	}
+
+	const dispatch = createEventDispatcher()
+
+	function setTimeLater(mins: number) {
+		let newDate = new Date()
+		newDate.setMinutes(newDate.getMinutes() + mins)
+		value = newDate.toISOString()
+		dispatch('change', value)
+	}
 </script>
 
-<div class="flex flex-row gap-1 items-center w-full">
+<div class="flex flex-row gap-1 items-center w-full" id="droptarget">
 	<!-- svelte-ignore a11y-autofocus -->
 	<input type="date" bind:value={date} {autofocus} class="!w-3/4" />
-	<input type="time" bind:value={time} class="!w-1/4" />
+	<input type="time" bind:value={time} class="!w-1/4 min-w-[100px]" />
+	<Button
+		variant="border"
+		color="light"
+		size="xs"
+		portalTarget="#droptarget"
+		dropdownItems={useDropdown
+			? [
+					{
+						label: 'In 15 minutes',
+						onClick: () => {
+							setTimeLater(15)
+						}
+					},
+					{
+						label: 'In 1 hour',
+						onClick: () => {
+							setTimeLater(60)
+						}
+					},
+					{
+						label: 'Tomorrow',
+						onClick: () => {
+							setTimeLater(60 * 24)
+						}
+					},
+					{
+						label: 'In 1 week',
+						onClick: () => {
+							setTimeLater(7 * 60 * 24)
+						}
+					}
+			  ]
+			: undefined}
+		on:click={() => {
+			setTimeLater(0)
+		}}>now</Button
+	>
 </div>
