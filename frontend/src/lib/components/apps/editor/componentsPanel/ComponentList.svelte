247396d04d10aa5fcff04a35615335649e09672f
Faton Ramadani <faton.ramadani14@gmail.com>
2024-02-20 23:50:29 +0100

    feat(frontend): App debug mode (#3252)
    * feat(frontend): App debug mode
    
    * feat(frontend): App debug mode
    
    * feat(frontend): App debug mode
    
    * feat(frontend): App debug mode
    
    * feat(frontend): App debug mode done
    
    * feat(frontend): App debug mode done

--- frontend/src/lib/components/apps/editor/componentsPanel/ComponentList.svelte
+++ frontend/src/lib/components/apps/editor/componentsPanel/ComponentList.svelte
@@ -175,7 +175,7 @@
 		title="Custom Components"
 		on:close={ccDrawer.closeDrawer}
 		documentationLink="https://www.windmill.dev/docs/apps/react_components"
-		>
+	>
 		<ComponentsList on:reload={fetchCustomComponents} />
 	</DrawerContent>
 </Drawer>
