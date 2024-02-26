968726cc0911a33ea5ed049f3f1b554f40903ae9
hcourdent <henri@windmill.dev>
2024-02-26 16:54:10 +0100

    How to upgrade message on cloud

--- frontend/src/lib/components/Uptodate.svelte
+++ frontend/src/lib/components/Uptodate.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { SettingsService } from '$lib/gen'
+	import { isCloudHosted } from '$lib/cloud'
 	import Tooltip from './Tooltip.svelte'
 
 	let uptodate: string | undefined = undefined
+	let tooltipContent: string = "How to update?<br />- docker: `docker compose up -d`<br />- <a href='https://github.com/windmill-labs/windmill-helm-charts#install'>helm</a>"
 
 	loadVersion()
 
@@ -11,6 +13,9 @@
 			const res = await SettingsService.backendUptodate()
 			if (res != 'yes') {
 				uptodate = res
+				if (isCloudHosted()) {
+					tooltipContent = "The cloud version is updated daily."
+				}
 			}
 		} catch (e) {
 			console.warn('Could not fetch latest version', e)
@@ -19,11 +24,5 @@
 </script>
 
 {#if uptodate}
-	<span class="text-blue-400"
-		>{uptodate} &nbsp;<Tooltip>
-			How to update?<br />
-			- docker: `docker compose up -d`<br />-
-			<a href="https://github.com/windmill-labs/windmill-helm-charts#install">helm</a>
-		</Tooltip>
-	</span>
+	<span class="text-blue-400">{uptodate} &nbsp;<Tooltip>{@html tooltipContent}</Tooltip></span>
 {/if}
