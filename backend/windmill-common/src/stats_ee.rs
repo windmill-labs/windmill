4203b7f7725f094e3ff04074d0ad2ee1c55d6f52
HugoCasa <hugo@casademont.ch>
2024-02-26 18:43:41 +0100

    fix: add emptied stats

---
+++ backend/windmill-common/src/stats_ee.rs
@@ -0,0 +1,45 @@
+use std::str::FromStr;
+
+use crate::{
+    error::{to_anyhow, Result},
+    global_settings::{BASE_URL_SETTING, DISABLE_STATS_SETTING},
+    scripts::ScriptLang,
+    utils::{get_uid, Mode, GIT_VERSION},
+    DB,
+};
+use chrono::Utc;
+use cron::Schedule;
+
+pub async fn get_disable_stats_setting(_db: &DB) -> bool {
+    // stats details are closed source
+
+    false
+}
+
+pub async fn schedule_stats(
+    _instance_name: String,
+    _mode: Mode,
+    _db: &DB,
+    _http_client: &reqwest::Client,
+    _is_enterprise: bool,
+) -> () {
+    // stats details are closed source
+}
+
+#[derive(Debug, sqlx::FromRow, serde::Serialize)]
+struct JobsUsage {
+    language: Option<ScriptLang>,
+    total_duration: i64,
+    count: i64,
+}
+
+pub async fn send_stats(
+    _instance_name: &String,
+    _mode: &Mode,
+    _http_client: &reqwest::Client,
+    _db: &DB,
+    _is_enterprise: bool,
+) -> Result<()> {
+    // stats details are closed source
+    Ok(())
+}
