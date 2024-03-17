f48767163cbc399a02944f5bdeaa5dc3bca3dff0
Ruben Fiszel <ruben@rubenfiszel.com>
2024-03-17 13:51:33 +0100

    chore: openidconnect is a feature flag

--- backend/windmill-git-sync/src/git_sync_ee.rs
+++ backend/windmill-git-sync/src/git_sync_ee.rs
@@ -1,17 +1,1 @@
-use windmill_common::error::Result;
-
-use crate::{DeployedObject, DB};
-
-pub async fn handle_deployment_metadata<'c, R: rsmq_async::RsmqConnection + Send + Clone + 'c>(
-    _email: &str,
-    _created_by: &str,
-    _db: &DB,
-    _w_id: &str,
-    _obj: DeployedObject,
-    _deployment_message: Option<String>,
-    _rsmq: Option<R>,
-    _skip_db_insert: bool,
-) -> Result<()> {
-    // Git sync is an enterprise feature and not part of the open-source version
-    return Ok(());
-}
+/git/windmill/../windmill-ee-private/windmill-git-sync/src/git_sync_ee.rs
