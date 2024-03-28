e3a636ab178ea6d1d6a49040325081ebb23fa3e7
HugoCasa <hugo@casademont.ch>
2024-03-28 15:50:18 +0100

    fix: show start to finish time for flows instead of cumulative (#3486)
    * fix: show start to finish time for flows instead of cumulative
    
    * fix: build

--- frontend/src/lib/components/DurationMs.svelte
+++ frontend/src/lib/components/DurationMs.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
 	import { msToSec } from '$lib/utils'
 	import { Badge } from './common'
-	import Tooltip from './Tooltip.svelte'
 	import { Hourglass } from 'lucide-svelte'
 
 	export let duration_ms: number
-	export let flow: boolean
 </script>
 
 <Badge large icon={{ icon: Hourglass, position: 'left' }}>
-	Ran in {msToSec(duration_ms)}s {#if flow}(sum){/if}
-	{#if flow}
-		<Tooltip>
-			Cumulative time of the execution of each steps. Suspend/sleep/transition times are not
-			accounted for, and each step's duration in a parallel branch would be added. Hence the time
-			here can differ from the time it took for the flow to have a result from start.
-		</Tooltip>
-	{/if}
+	Ran in {msToSec(duration_ms)}s
 </Badge>
--- frontend/src/lib/components/jobs/JobPreview.svelte
+++ frontend/src/lib/components/jobs/JobPreview.svelte
@@ -108,10 +108,7 @@
 					Mem: {job?.['mem_peak'] ? `${(job['mem_peak'] / 1024).toPrecision(4)}MB` : 'N/A'}
 				</Badge>
 				{#if job?.['duration_ms']}
-					<DurationMs
-						flow={job.job_kind == 'flow' || job?.job_kind == 'flowpreview'}
-						duration_ms={job?.['duration_ms']}
-					/>
+					<DurationMs duration_ms={job?.['duration_ms']} />
 				{/if}
 			</div>
 			<div class="w-1/2 h-full overflow-auto">
