538103f5dc8eef06e6f31b7684d014ee5445ea8e
lfanew <tyler.bowers@outlook.com>
2024-03-31 19:11:08 -0500

    Added more functionality

--- powershell-client/client.psm1
+++ powershell-client/client.psm1
@@ -48,6 +48,57 @@ function Get-WindmillResource {
     return $global:WindmillConnection.GetResource($Path)
 }
 
+function Set-WindmillResource {
+    param(
+        [string] $Path,
+        [Hashtable] $Value,
+        [string] $ResourceType = $null
+    )
+
+    if (-not $global:WindmillConnection) {
+        throw "Windmill connection not established. Run Connect-Windmill first."
+    }
+
+    $global:WindmillConnection.SetResource($Path, $Value, $ResourceType)
+}
+
+function Get-WindmillResult {
+    param(
+        [string] $JobId,
+        [switch] $AssertResultIsNotNull
+    )
+
+    if (-not $global:WindmillConnection) {
+        throw "Windmill connection not established. Run Connect-Windmill first."
+    }
+
+    return $global:WindmillConnection.GetResult($JobId, $AssertResultIsNotNull)
+}
+
+function Get-WindmillVersion {
+    if (-not $global:WindmillConnection) {
+        throw "Windmill connection not established. Run Connect-Windmill first."
+    }
+
+    return $global:WindmillConnection.Version()
+}
+
+function Get-WindmillWorkspace {
+    if (-not $global:WindmillConnection) {
+        throw "Windmill connection not established. Run Connect-Windmill first."
+    }
+
+    return $global:WindmillConnection.Workspace
+}
+
+function Get-WindmillUser {
+    if (-not $global:WindmillConnection) {
+        throw "Windmill connection not established. Run Connect-Windmill first."
+    }
+
+    return $global:WindmillConnection.Whoami()
+}
+
 class Windmill {
     [string] $BaseUrl
     [string] $Token
@@ -109,8 +160,37 @@ class Windmill {
             }
     }
 
-    [PSCustomObject] GetResource([string] $Path) {
-        $response = $this.Get("/w/$($this.Workspace)/resources/get/$Path", $true)
-        return $response.Content | ConvertFrom-Json
+    [PSCustomObject] GetResult([string] $JobId, [boolean] $AssertResultIsNotNull = $true) {
+        $response = $this.Get("/w/$($this.Workspace)/jobs_u/completed/get_result/$JobId", $true)
+        $result = $response.Content | ConvertFrom-Json
+        if ($AssertResultIsNotNull -and -not $result) {
+            throw "result is null for job $JobId"
+        }
+        
+        return $result
+    }
+
+
+
+    [void] SetResource([string] $Path, [Hashtable] $Value, [string] $ResourceType) {
+        $response = $this.Get("/w/$($this.Workspace)/resources/get/$Path", $false)
+
+        if ($response.StatusCode -eq 404) {
+            $this.Post("/w/$($this.Workspace)/resources/create", @{ "path" = $Path; "value" = $Value; "resource_type" = $ResourceType }, $true)
+            return
+        } else {
+            $this.Post("/w/$($this.Workspace)/resources/update_value/$Path", @{ "value" = $Value }, $true)
+        }
+    }
+
+    [string] Version() {
+        $response = $this.Get("/version", $true)
+        return $response.Content
+    }
+    
+    [PSCustomObject] Whoami() {
+        $response = $this.Get("/users/whoami", $true)
+        $result = $response.Content | ConvertFrom-Json
+        return $result
     }
 }
