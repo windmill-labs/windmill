7139dc467e775b519323b1beed08d30dc7ab7e91
Faton Ramadani <faton.ramadani14@gmail.com>
2024-04-17 16:39:46 +0200

    feat(frontend): wip

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -839,3 +839,16 @@ export function getLocalSetting(name: string) {
 		return undefined
 	}
 }
+
+export const zIndexes = {
+	appEditorComponents: 1000,
+	secondaryMenu: 1001,
+	splitPanelHandle: 1001,
+	colorInput: 1002,
+	modals: 1100,
+	svelteSelectOptions: 5000,
+	contextMenu: 6000,
+	draggingComponent: 10000,
+	monacoEditor: 10000,
+	monacoEditorSuggestions: 10001
+}
