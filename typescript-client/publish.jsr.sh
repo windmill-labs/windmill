0a8ba4bbbc62836cff3b44e32967de5db5db26a2
wendrul <dethomassin.etienne@gmail.com>
2024-04-25 17:31:32 +0200

    Add publish to jsr scripts

---
+++ typescript-client/publish.jsr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eou pipefail
+script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+args=${1:-}
+
+rm -rf "${script_dirpath}/dist"
+
+${script_dirpath}/build.jsr.sh
+rm "${script_dirpath}/client.ts"
+rm "${script_dirpath}/s3Types.ts"
+npm install
+tsc
+cp "${script_dirpath}/src/client.ts" ${script_dirpath}
+cp "${script_dirpath}/src/s3Types.ts" ${script_dirpath}
+npx jsr publish ${args}
