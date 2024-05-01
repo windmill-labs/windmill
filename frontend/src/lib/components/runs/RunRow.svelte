44ddb9ebeee8f8a469e14e5c81816abc59aadea3
Ruben Fiszel <ruben@rubenfiszel.com>
2024-05-02 00:48:15 +0200

    improve runs nit UX

--- frontend/src/lib/components/runs/RunRow.svelte
+++ frontend/src/lib/components/runs/RunRow.svelte
@@ -165,10 +165,9 @@
 				</div>
 			{:else}
 				<div class="flex flex-row gap-1 items-center">
-					<Bot size={12} class="text-secondary" />
-					<span class="mx-1 text-xs">
-						Parent <a href={`/run/${job.parent_job}?workspace=${job.workspace_id}`}>
-							{job.parent_job}
+					<span class="text-2xs text-tertiary truncate">
+						parent <a href={`/run/${job.parent_job}?workspace=${job.workspace_id}`}>
+							{truncateRev(job.parent_job, 10)}
 						</a>
 					</span>
 				</div>
