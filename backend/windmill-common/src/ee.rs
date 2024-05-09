20dfbda69e643753b1ff17137ce18ed23ff45dd1
Ruben Fiszel <ruben@rubenfiszel.com>
2024-05-10 01:24:46 +0200

    fix: fix build

--- backend/windmill-common/src/ee.rs
+++ backend/windmill-common/src/ee.rs
@@ -1,1 +1,29 @@
-/git/windmill/../windmill-ee-private/windmill-common/src/ee.rs
+use crate::ee::LicensePlan::Community;
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+lazy_static::lazy_static! {
+  pub static ref LICENSE_KEY_VALID: Arc<RwLock<bool>> = Arc::new(RwLock::new(true));
+  pub static ref LICENSE_KEY_ID: Arc<RwLock<String>> = Arc::new(RwLock::new("".to_string()));
+  pub static ref LICENSE_KEY: Arc<RwLock<String>> = Arc::new(RwLock::new("".to_string()));
+}
+
+pub enum LicensePlan {
+    Community,
+    Pro,
+    Enterprise,
+}
+
+pub async fn get_license_plan() -> LicensePlan {
+    // Implementation is not open source
+    return Community;
+}
+
+pub async fn trigger_critical_error_channels(_x: String) {
+    //not open-source
+    ()
+}
+
+#[derive(Serialize, Deserialize)]
+pub enum CriticalErrorChannel {}
