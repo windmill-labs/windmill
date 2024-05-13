3aebae7770f90dc56f382a7a84deba0b2d16da94
Ruben Fiszel <ruben@rubenfiszel.com>
2024-05-13 08:14:11 +0200

    all

--- typescript-client/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { ApiError } from './core/ApiError';
-export { CancelablePromise, CancelError } from './core/CancelablePromise';
-export { OpenAPI, type OpenAPIConfig } from './core/OpenAPI';
-export * from './schemas.gen';
-export * from './services.gen';
-export * from './types.gen';
-export type { S3Object, DenoS3LightClientSettings } from "./s3Types";
-export { type Base64, setClient, getVariable, setVariable, getResource, setResource, getResumeUrls, setState, getState, getIdToken, denoS3LightClientSettings, loadS3FileStream, loadS3File, writeS3File, task, runScript, runScriptAsync, waitJob, getRootJobId, setFlowUserState, getFlowUserState } from "./client";
