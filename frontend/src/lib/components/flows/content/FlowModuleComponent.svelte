417e9e2c9ff2e1fad6d43dd406a525c515d096ce
wendrul <dethomassin.etienne@gmail.com>
2024-05-23 14:30:53 +0200

    Add concurrency key for inline scripts in flows

--- frontend/src/lib/components/flows/content/FlowModuleComponent.svelte
+++ frontend/src/lib/components/flows/content/FlowModuleComponent.svelte
@@ -444,11 +444,21 @@
 														bind:seconds={flowModule.value.concurrency_time_window_s}
 													/>
 												</Label>
-												<Label label="Custom concurrency key">
-													<div class="text-tertiary text-xs"
-														>Custom concurrency keys can only be set as the setting of a workspace
-														script</div
-													>
+												<Label label="Custom concurrency key (optional)">
+													<svelte:fragment slot="header">
+														<Tooltip>
+															Concurrency keys are global, you can have them be workspace specific
+															using the variable `$workspace`. You can also use an argument's value
+															using `$args[name_of_arg]`</Tooltip
+														>
+													</svelte:fragment>
+													<input
+														type="text"
+														autofocus
+														disabled={!$enterpriseLicense}
+														bind:value={flowModule.value.custom_concurrency_key}
+														placeholder={`$workspace/script/${$pathStore}-$args[foo]`}
+													/>
 												</Label>
 											{:else}
 												<Alert type="warning" title="Limitation" size="xs">
