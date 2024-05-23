51551a51eaf397f91b9b7a68f0917ff007a21e99
Faton Ramadani <faton.ramadani14@gmail.com>
2024-05-23 10:37:09 +0200

    fix(frontend): Add missing loading state for non-runnable components + fix select and aggrid styling

--- frontend/src/lib/components/apps/components/helpers/NonRunnableComponent.svelte
+++ frontend/src/lib/components/apps/components/helpers/NonRunnableComponent.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import { getContext } from 'svelte'
-	import type { AppInput } from '../../inputType'
+	import type { AppInput, InputConnectionEval } from '../../inputType'
 	import type { Output } from '../../rx'
 	import type { AppViewerContext } from '../../types'
 	import InputValue from './InputValue.svelte'
 	import InitializeComponent from './InitializeComponent.svelte'
+	import RefreshIndicator from './RefreshIndicator.svelte'
 
 	export let componentInput: AppInput
 	export let id: string
@@ -25,6 +26,48 @@
 		outputs?.result?.set(v, true)
 	}
 
+	let loading: boolean = false
+	let loadingStates: { [key: string]: boolean } = {}
+	let subscriptions: Array<() => void> = []
+
+	function updateGlobalLoading() {
+		loading = Object.values(loadingStates).some((state) => state === true)
+	}
+
+	function builtSubscriptions(connections: InputConnectionEval[]) {
+		console.log('builtSubscriptions', connections)
+
+		// If we are rebuilding the subscriptions, we need to unsubscribe from the previous ones
+		if (subscriptions.length > 0) {
+			subscriptions.forEach((unsubscribe) => unsubscribe?.())
+			subscriptions = []
+			loadingStates = {}
+			loading = false
+		}
+
+		connections.forEach((connection) => {
+			const output = $worldStore.outputsById[connection.componentId]
+
+			if (output) {
+				const unsubscribe = output.loading.subscribe(
+					{
+						id: `loading-${connection.componentId}`,
+						next: (isConnectionLoading: boolean) => {
+							loadingStates[connection.componentId] = isConnectionLoading
+							updateGlobalLoading()
+						}
+					},
+					loadingStates[connection.componentId]
+				)
+				subscriptions.push(unsubscribe)
+			}
+		})
+	}
+
+	$: componentInput.type === 'evalv2' &&
+		componentInput.connections &&
+		builtSubscriptions(componentInput.connections)
+
 	$: result != undefined && outputs && setOutput(result)
 </script>
 
@@ -39,6 +82,9 @@
 {#if render || hasChildrens}
 	<div class={render ? 'h-full w-full' : 'invisible h-0 overflow-hidden'}>
 		<slot />
+		<div class="flex absolute top-1 right-1 z-50 app-component-refresh-btn">
+			<RefreshIndicator {loading} />
+		</div>
 	</div>
 {:else}
 	<div class="w-full h-full" />
---
+++ frontend/src/lib/components/apps/components/helpers/RefreshIndicator.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { LoaderIcon } from 'lucide-svelte'
+	import Popover from '$lib/components/Popover.svelte'
+
+	export let loading: boolean
+</script>
+
+{#if loading}
+	<Popover>
+		<div class={'bg-blue-100 dark:bg-blue-400 transition-all p-1 rounded-component'}>
+			<LoaderIcon size={14} class="animate-spin text-blue-800" />
+		</div>
+		<svelte:fragment slot="text">
+			{#if loading}
+				Refreshing...
+			{:else}
+				Refresh
+			{/if}
+		</svelte:fragment>
+	</Popover>
+{/if}
