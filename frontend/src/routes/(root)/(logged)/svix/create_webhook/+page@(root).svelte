b7715c3624b59ea01451c848c95948ec50888ebf
wendrul <dethomassin.etienne@gmail.com>
2024-05-24 17:22:16 +0200

    Make small fixes

--- frontend/src/routes/(root)/(logged)/svix/create_webhook/+page@(root).svelte
+++ frontend/src/routes/(root)/(logged)/svix/create_webhook/+page@(root).svelte
@@ -2,8 +2,6 @@
 	import PageHeader from '$lib/components/PageHeader.svelte'
 	import ScriptPicker from '$lib/components/ScriptPicker.svelte'
 	import CenteredPage from '$lib/components/CenteredPage.svelte'
-	import { ScriptService, FlowService } from '$lib/gen'
-	import FlowScriptPicker from '$lib/components/flows/pickers/FlowScriptPicker.svelte'
 	import { Button } from '$lib/components/common'
 	import { page } from '$app/stores'
 	import { workspaceStore } from '$lib/stores'
@@ -16,17 +14,18 @@
 
 	async function createWebhook(path: string, kind: 'script' | 'flow' | 'app') {
 		try {
+			let webhook
 			switch (kind) {
 				case 'script':
-					const webhook = `${$page.url.origin}/api/w/${$workspaceStore}/jobs/run/p/${path}`
+					webhook = `${$page.url.origin}/api/w/${$workspaceStore}/jobs/run/p/${path}`
 					redirectSuccess(webhook)
 					break
 				case 'flow':
-					const webhook = `${$page.url.origin}/api/w/${$workspaceStore}/jobs/run/f/${path}`
+					webhook = `${$page.url.origin}/api/w/${$workspaceStore}/jobs/run/f/${path}`
 					redirectSuccess(webhook)
 					break
 				default:
-					sendUserToast("Invalid item type", true)
+					sendUserToast('Invalid item type', true)
 					break
 			}
 		} catch (error) {
@@ -47,17 +46,11 @@
 	}
 </script>
 
-{#if redirectURI == undefined}
-	<CenteredPage>404</CenteredPage>
-{:else}
+{#if redirectURI != null}
 	<CenteredPage>
 		<div class="flex items-center justify-center h-screen">
 			<div class="flex flex-col items-center gap-5">
-				{#if clientName != undefined}
-					<PageHeader title={`${clientName} wants to create a webhook`} />
-				{:else}
-					<PageHeader title="Svix wants to create a webhook" />
-				{/if}
+				<PageHeader title={`${clientName} wants to create a webhook`} />
 				<h2>Select the script or flow to be triggered by the webhook</h2>
 				<ScriptPicker
 					allowEdit={false}
@@ -68,10 +61,10 @@
 				/>
 				<div class="flex flex-row gap-2">
 					<Button
-						disabled={scriptPath === undefined}
-						on:click={() => createWebhook(scriptPath, itemKind)}>Select script</Button
+						disabled={scriptPath == undefined}
+						on:click={() => scriptPath && createWebhook(scriptPath, itemKind)}>Select script</Button
 					>
-					<Button on:click={() => redirectCancel("user_canceled")}>Cancel</Button>
+					<Button on:click={() => redirectCancel('user_canceled')}>Cancel</Button>
 				</div>
 				<h2>or</h2>
 				<div class="flex flex-row gap-2">
