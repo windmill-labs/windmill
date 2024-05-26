9d2e4df920ce46d5b37e6d72b74f3384a0508d23
Ruben Fiszel <ruben@rubenfiszel.com>
2024-05-26 05:04:42 +0200

    reschedule to at least 1secs in future

--- backend/windmill-queue/src/jobs.rs
+++ backend/windmill-queue/src/jobs.rs
@@ -1798,7 +1798,8 @@ pub async fn pull<R: rsmq_async::RsmqConnection + Send + Clone>(
                 .unwrap_or_default();
 
         let now = min_started_at.now.unwrap();
-        let min_started_p_inc = min_started_at.min_started_at.unwrap_or(now) + inc;
+        let min_started_p_inc = (min_started_at.min_started_at.unwrap_or(now) + inc)
+            .max(now + Duration::try_seconds(1).unwrap_or_default());
 
         let mut estimated_next_schedule_timestamp = min_started_p_inc;
         loop {
