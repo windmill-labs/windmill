5e3979ffacad91db467594bc986d38f998a27c4b
Ruben Fiszel <ruben@rubenfiszel.com>
2024-05-31 10:31:54 +0200

    rm symlinks

--- backend/windmill-common/src/ee.rs
+++ backend/windmill-common/src/ee.rs
@@ -1,1 +1,38 @@
-/git/windmill/../windmill-ee-private/windmill-common/src/ee.rs
+use crate::ee::LicensePlan::Community;
+use serde::Deserialize;
+use std::sync::Arc;
+use tokio::sync::RwLock;
+
+lazy_static::lazy_static! {
+  pub static ref LICENSE_KEY_VALID: Arc<RwLock<bool>> = Arc::new(RwLock::new(true));
+  pub static ref LICENSE_KEY_ID: Arc<RwLock<String>> = Arc::new(RwLock::new("".to_string()));
+  pub static ref LICENSE_KEY: Arc<RwLock<String>> = Arc::new(RwLock::new("".to_string()));
+}
+
+pub enum LicensePlan {
+    Community,
+    Pro,
+    Enterprise,
+}
+
+pub async fn get_license_plan() -> LicensePlan {
+    // Implementation is not open source
+    return Community;
+}
+
+#[derive(Deserialize)]
+#[serde(untagged)]
+pub enum CriticalErrorChannel {}
+
+pub async fn trigger_critical_error_channels(_error_message: String) {}
+
+#[cfg(feature = "enterprise")]
+pub async fn schedule_key_renewal(_http_client: &reqwest::Client, _db: &crate::db::DB) -> () {
+    // Implementation is not open source
+}
+
+#[cfg(feature = "enterprise")]
+pub async fn renew_license_key(_http_client: &reqwest::Client, _db: &crate::db::DB) -> String {
+    // Implementation is not open source
+    "".to_string()
+}
