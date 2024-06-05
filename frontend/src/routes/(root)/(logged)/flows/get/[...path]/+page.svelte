837ca42484db897cba437b8da78ab03c57c63c28
Marcel Klehr <mklehr@gmx.net>
2024-06-05 14:35:26 +0200

    fix: Allow depolying frontend in a sub directory
    Signed-off-by: Marcel Klehr <mklehr@gmx.net>

--- frontend/src/routes/(root)/(logged)/flows/get/[...path]/+page.svelte
+++ frontend/src/routes/(root)/(logged)/flows/get/[...path]/+page.svelte
@@ -5,7 +5,7 @@
 
 	import DetailPageLayout from '$lib/components/details/DetailPageLayout.svelte'
 	import { goto } from '$lib/navigation'
-	import { base } from '$app/base'
+	import { base } from '$app/paths'
 	import { Alert, Button, Badge as HeaderBadge, Skeleton } from '$lib/components/common'
 	import MoveDrawer from '$lib/components/MoveDrawer.svelte'
 	import RunForm from '$lib/components/RunForm.svelte'
