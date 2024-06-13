26f35107e5b40cd2456c949c10d3c1fdf526ad08
Faton Ramadani <faton.ramadani14@gmail.com>
2024-06-13 15:34:32 +0200

    feat(frontend): fix layout

--- frontend/src/lib/components/apps/editor/settingsPanel/EventHandlerItem.svelte
+++ frontend/src/lib/components/apps/editor/settingsPanel/EventHandlerItem.svelte
@@ -2,28 +2,33 @@
 	import Tooltip from '$lib/components/Tooltip.svelte'
 
 	import MultiSelect from '$lib/components/multiselect/MultiSelectWrapper.svelte'
+	import { twMerge } from 'tailwind-merge'
 
 	export let items: string[]
 	export let value: string[] | undefined = undefined
 	export let title: string
 	export let tooltip: string
+
+	$: width = 0
+	const inputWidth = 280
 </script>
 
-<div class="grid grid-cols-12 w-full gap-8">
-	<div class="col-span-3 flex items-center text-xs whitespace-nowrap h-8">
+<div
+	class={twMerge(
+		'flex flex-col justify-between w-full',
+		width < inputWidth ? 'flex-col gap-2' : 'flex-row gap-16 '
+	)}
+	bind:clientWidth={width}
+>
+	<div class="flex items-center text-xs h-8 whitespace-nowrap">
 		{title}
 		<Tooltip light small>{tooltip}</Tooltip>
 	</div>
 
-	<div class="col-span-1" />
-
-	<div class="col-span-8">
+	<div class="w-full">
 		{#if items.length === 0}
-			<div
-				class="text-xs text-secondary w-full bg-gray-100 border rounded-md flex items-center px-2 h-8 dark:bg-gray-900 dark:border-gray-600"
-			>
+			<div class="text-xs text-secondary w-full flex items-center justify-end h-8">
 				No components to recompute.
-				<Tooltip light small>Create one and select it here.</Tooltip>
 			</div>
 		{:else}
 			<MultiSelect {items} bind:value />
