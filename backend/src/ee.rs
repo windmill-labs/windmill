a7250bfae7007b39e5a42f5000c56e8bdcea372c
Ruben Fiszel <ruben@rubenfiszel.com>
2024-06-18 20:51:52 +0200

    update datafusion

--- backend/src/ee.rs
+++ backend/src/ee.rs
@@ -1,1 +1,23 @@
-/git/windmill/../windmill-ee-private/src/ee.rs
+use anyhow::anyhow;
+#[cfg(feature = "enterprise")]
+use windmill_common::error::{Error, Result};
+
+pub async fn set_license_key(_license_key: String) -> anyhow::Result<()> {
+    // Implementation is not open source
+    Err(anyhow!("License cannot be set in Windmill CE"))
+}
+
+#[cfg(feature = "enterprise")]
+pub async fn verify_license_key() -> Result<()> {
+    // Implementation is not open source
+    Err(Error::InternalErr(
+        "License always invalid in Windmill CE".to_string(),
+    ))
+}
+
+#[cfg(feature = "enterprise")]
+pub async fn jwt_ext_auth(_w_id: Option<&String>, _token: &str) -> Option<(ApiAuthed, usize)> {
+    // Implementation is not open source
+
+    None
+}
