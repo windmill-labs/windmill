7cdb58439f4664bc5269c2630eca106371a82553
Ruben Fiszel <ruben@rubenfiszel.com>
2024-06-18 22:57:51 +0200

    fix tests

--- backend/windmill-api/src/ee.rs
+++ backend/windmill-api/src/ee.rs
@@ -4,3 +4,13 @@ pub async fn validate_license_key(_license_key: String) -> anyhow::Result<String
     // Implementation is not open source
     Err(anyhow!("License can't be validated in Windmill CE"))
 }
+
+#[cfg(feature = "enterprise")]
+pub async fn jwt_ext_auth(
+    _w_id: Option<&String>,
+    _token: &str,
+) -> Option<(crate::db::ApiAuthed, usize)> {
+    // Implementation is not open source
+
+    None
+}
