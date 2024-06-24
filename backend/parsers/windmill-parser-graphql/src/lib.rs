093d24349cd165c77989fc6cb5dbbc42db1c6f91
dommyrock <dominik.polzer@hotmail.com>
2024-06-24 12:35:00 +0200

    update fn parse_graphql_file as per code scan iput

--- backend/parsers/windmill-parser-graphql/src/lib.rs
+++ backend/parsers/windmill-parser-graphql/src/lib.rs
@@ -7,13 +7,14 @@ use serde_json::json;
 use windmill_parser::{Arg, MainArgSignature, Typ};
 
 pub fn parse_graphql_sig(code: &str) -> anyhow::Result<MainArgSignature> {
-    let parsed = parse_graphql_file(&code);
-    if let Some(x) = parsed {
-        let args = x;
-        Ok(MainArgSignature { star_args: false, star_kwargs: false, args, no_main_func: None })
-    } else {
-        Err(anyhow!("Error parsing sql".to_string()))
-    }
+    parse_graphql_file(&code)
+        .map(|args| MainArgSignature {
+            star_args: false,
+            star_kwargs: false,
+            args,
+            no_main_func: None,
+        })
+        .ok_or_else(|| anyhow!("Error parsing sql".to_string()))
 }
 
 lazy_static::lazy_static! {
@@ -23,36 +24,38 @@ lazy_static::lazy_static! {
 fn parse_graphql_file(code: &str) -> Option<Vec<Arg>> {
     RE_ARG_GRAPHQL
         .captures_iter(code)
-        .fold(Some(Vec::new()), |acc, cap| {
-            acc.and_then(|mut vec| {
-                let mut typ = cap.get(2).map(|x| x.as_str().to_string());
-                let parsed_typ = match typ {
-                    Some(ref t) => parse_graphql_typ(t),
-                    None => {
-                        typ = cap.get(3).map(|x| format!("[{}]", x.as_str()));
-                        Typ::List(Box::new(parse_graphql_typ(typ.as_ref().unwrap())))
-                    }
-                };
+        .filter_map(|cap| Some(parse_arg_cap(cap)))
+        .collect()
+}
+fn parse_arg_cap(cap: regex::Captures) -> Option<Arg> {
+    let mut typ = cap.get(2).map(|x| x.as_str().to_string());
+    let parsed_typ = match typ {
+        Some(ref t) => parse_graphql_typ(t),
+        None => {
+            typ = cap.get(3).map(|x| format!("[{}]", x.as_str()));
+            Typ::List(Box::new(parse_graphql_typ(typ.as_ref().unwrap())))
+        }
+    };
 
-                let default = cap.get(4).map(|x| x.as_str().to_string());
-                let has_default = default.is_some();
+    let default = cap.get(4).map(|x| x.as_str().to_string());
+    let has_default = default.is_some();
 
-                let parsed_default = default.and_then(|x| match parsed_typ {
-                    Typ::Int => x.parse::<i64>().ok().map(|x| json!(x)),
-                    Typ::Float => x.parse::<f64>().ok().map(|x| json!(x)),
-                    _ => Some(json!(x)),
-                });
-                vec.push(Arg {
-                    name: cap.get(1).map(|x| x.as_str().to_string()).unwrap(),
-                    typ: parsed_typ,
-                    default: parsed_default,
-                    otyp: typ,
-                    has_default,
-                });
+    let parsed_default = default.and_then(|x| match parsed_typ {
+        Typ::Int => x.parse::<i64>().ok().map(|x| json!(x)),
+        Typ::Float => x.parse::<f64>().ok().map(|x| json!(x)),
+        _ => Some(json!(x)),
+    });
 
-                Some(vec)
-            })
-        })
+    Some(Arg {
+        name: cap
+            .get(1)
+            .map(|x| x.as_str().to_string())
+            .expect("Failed to capture 'name'"),
+        typ: parsed_typ,
+        default: parsed_default,
+        otyp: typ,
+        has_default,
+    })
 }
 
 pub fn parse_graphql_typ(typ: &str) -> Typ {
