06377e1f34a0ef37c7cbbf8760965affa2e3bfc3
Faton Ramadani <faton.ramadani14@gmail.com>
2024-06-25 13:30:06 +0200

    feat(frontend): fix theme

--- frontend/src/lib/components/apps/editor/settingsPanel/DocLink.svelte
+++ frontend/src/lib/components/apps/editor/settingsPanel/DocLink.svelte
@@ -2,7 +2,7 @@
 	import Popover from '$lib/components/Popover.svelte'
 	import { ButtonType } from '$lib/components/common'
 	import Button from '$lib/components/common/button/Button.svelte'
-	import { BookText } from 'lucide-svelte'
+	import { BookText, ExternalLink } from 'lucide-svelte'
 	import { twMerge } from 'tailwind-merge'
 
 	export let docLink: string
@@ -11,14 +11,23 @@
 </script>
 
 <Popover>
-	<svelte:fragment slot="text">See documentation</svelte:fragment>
+	<svelte:fragment slot="text">
+		<div class="flex flex-row gap-1">
+			Open documentation in a new tab
+
+			<ExternalLink size={16} />
+		</div>
+	</svelte:fragment>
 	<Button
 		iconOnly
 		startIcon={{
 			icon: BookText
 		}}
 		{size}
-		btnClasses={twMerge('p-1 text-gray-300 hover:text-gray-600 bg-transparent', btnClasses)}
+		btnClasses={twMerge(
+			'p-1 text-gray-300 hover:!text-gray-600 dark:text-gray-500 dark:hover:!text-gray-200 bg-transparent',
+			btnClasses
+		)}
 		href={docLink}
 		target="_blank"
 		color="light"
