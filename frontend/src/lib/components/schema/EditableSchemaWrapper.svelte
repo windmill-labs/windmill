2ec8ce5b7da76d7b5429120b749c66ffb833ff68
Ruben Fiszel <ruben@rubenfiszel.com>
2024-07-14 13:30:39 +0200

    fix: improve schema editor

--- frontend/src/lib/components/schema/EditableSchemaWrapper.svelte
+++ frontend/src/lib/components/schema/EditableSchemaWrapper.svelte
@@ -3,7 +3,6 @@
 	import { twMerge } from 'tailwind-merge'
 	import EditableSchemaForm from '../EditableSchemaForm.svelte'
 	import AddProperty from './AddProperty.svelte'
-	import { createEventDispatcher } from 'svelte'
 
 	export let schema: Schema | undefined | any
 	export let offset: number = 0
@@ -11,21 +10,17 @@
 	export let noPreview: boolean = false
 	export let fullHeight: boolean = true
 	export let lightweightMode: boolean = false
-	export let watchChanges: boolean = false
-
-	const dispatch = createEventDispatcher()
-
-	$: watchChanges && dispatch('change', { schema })
 
 	let addProperty: AddProperty | undefined = undefined
 </script>
 
 <div class={twMerge(fullHeight ? 'h-full' : 'h-80', 'border overflow-y-auto rounded-md')}>
 	<div class="p-4 border-b">
-		<AddProperty bind:schema bind:this={addProperty} />
+		<AddProperty on:change bind:schema bind:this={addProperty} />
 	</div>
 	<EditableSchemaForm
 		bind:schema
+		on:change
 		isFlowInput
 		on:edit={(e) => {
 			addProperty?.openDrawer(e.detail)
---
+++ frontend/src/routes/test_dev_page/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import EditableSchemaWrapper from '$lib/components/schema/EditableSchemaWrapper.svelte'
+
+	let schema = {
+		type: 'object',
+		properties: {
+			foo: {
+				type: 'string'
+			}
+		}
+	}
+</script>
+
+<EditableSchemaWrapper on:change={() => console.log('FOO')} {schema} />
