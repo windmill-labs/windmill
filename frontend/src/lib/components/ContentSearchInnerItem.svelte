fddea2e802c56c4d902dc4caa843541a36526ecd
Faton Ramadani <faton.ramadani14@gmail.com>
2024-07-17 17:17:10 +0200

    fix(frontend): close the content search modal when clicking on an item (#4098)

--- frontend/src/lib/components/ContentSearchInnerItem.svelte
+++ frontend/src/lib/components/ContentSearchInnerItem.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+
 	export let title: string
 	export let href: string
+
+	const dispatch = createEventDispatcher()
 </script>
 
 <div class="flex flex-col gap-2">
 	<div class="flex flex-row items-center justify-between">
-		<a class="text-sm text-primary truncate" {href}>
+		<a
+			class="text-sm text-blue-500 truncate hover:underline"
+			{href}
+			on:click={() => {
+				dispatch('close')
+			}}
+		>
 			{title}
 		</a>
 
