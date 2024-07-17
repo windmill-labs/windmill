cbd8e7efb322127f5a8d38b13735134de397e1a0
Faton Ramadani <faton.ramadani14@gmail.com>
2024-07-17 14:29:41 +0200

    fix(frontend): improve search modal (#4088)
    * feat(frontend): wip
    
    * feat(frontend): wip
    
    * feat(frontend): wip
    
    * feat(frontend): improve search modal style
    
    * feat(frontend): fix content search height
    
    * feat(frontend): fix overflow
    
    * feat(frontend): revert unrelated changes

---
+++ frontend/src/lib/components/ContentSearchInnerItem.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let title: string
+	export let href: string
+</script>
+
+<div class="flex flex-col gap-2">
+	<div class="flex flex-row items-center justify-between">
+		<a class="text-sm text-primary truncate" {href}>
+			{title}
+		</a>
+
+		<div class="flex flex-row gap-2 items-center">
+			<slot name="actions" />
+		</div>
+	</div>
+	<slot />
+</div>
