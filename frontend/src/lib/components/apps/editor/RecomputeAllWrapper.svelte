b7d740c6e1f8a6b2b80054d6e1c620271a662c2c
Faton Ramadani <faton.ramadani14@gmail.com>
2024-07-31 17:15:34 +0200

    fix(frontend): fix how the onClick callback is called

--- frontend/src/lib/components/apps/editor/RecomputeAllWrapper.svelte
+++ frontend/src/lib/components/apps/editor/RecomputeAllWrapper.svelte
@@ -50,7 +50,7 @@
 			<RecomputeAllButton
 				interval={$recomputeAllContext.interval}
 				componentNumber={$recomputeAllContext.componentNumber ?? 0}
-				on:click={$recomputeAllContext.onClick}
+				on:click={() => $recomputeAllContext.onClick?.()}
 				on:setInter={(e) => {
 					$recomputeAllContext.setInter?.(e.detail)
 				}}
