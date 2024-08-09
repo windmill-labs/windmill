07fc1eee2977715793ad6f7daff0912b86f2e9ec
Ruben Fiszel <ruben@rubenfiszel.com>
2024-08-09 09:57:43 +0200

    feat: windmill embed

---
+++ frontend/src/routes/user/close/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import CenteredModal from '$lib/components/CenteredModal.svelte'
+	import { Button } from '$lib/components/common'
+</script>
+
+<CenteredModal>
+	There was an error during login
+
+	<Button on:click={() => window.close()}>close</Button>
+</CenteredModal>
