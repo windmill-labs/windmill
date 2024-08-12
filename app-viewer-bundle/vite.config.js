e7d08359fcdba659e55383259a800093ff7e88f7
Ruben Fiszel <ruben@rubenfiszel.com>
2024-08-12 10:36:37 +0200

    fix compile

--- app-viewer-bundle/vite.config.js
+++ app-viewer-bundle/vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), cssInjectedByJsPlugin()],
   build: {
     lib: { entry: "src/main.js", formats: ["iife"], name: "app" },
   },
