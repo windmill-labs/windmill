0e35ab098aab59c1c05ecd00a897b0d03a45f232
Ruben Fiszel <ruben@rubenfiszel.com>
2024-08-14 17:35:30 +0200

    fix tests

--- backend/tests/worker.rs
+++ backend/tests/worker.rs
@@ -372,7 +372,7 @@ mod suspend_resume {
         server.close().await.unwrap();
 
         assert_eq!(
-            json!( {"error": {"name": "SuspendedDisapproved", "message": "Disapproved by Some(\"ruben\")"}}),
+            json!( {"error": {"name": "SuspendedDisapproved", "message": "Disapproved by ruben"}}),
             result
         );
     }
@@ -433,7 +433,7 @@ mod suspend_resume {
         let result = completed_job(flow, &db).await.json_result().unwrap();
 
         assert_eq!(
-            json!( {"error": {"name": "SuspendedDisapproved", "message": "Disapproved by None"}}),
+            json!( {"error": {"name": "SuspendedDisapproved", "message": "Disapproved by unknown"}}),
             result
         );
     }
