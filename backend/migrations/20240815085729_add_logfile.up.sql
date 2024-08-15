3aed1c86b483a767d7d8ee150781e592473a7a28
Ruben Fiszel <ruben@rubenfiszel.com>
2024-08-15 11:08:49 +0200

    log

--- backend/migrations/20240815085729_add_logfile.up.sql
+++ backend/migrations/20240815085729_add_logfile.up.sql
@@ -1,11 +1,11 @@
 -- Add up migration script here
+-- Add up migration script here
 CREATE TYPE LOG_MODE AS ENUM ('standalone', 'server', 'worker', 'agent');
 CREATE TABLE log_file (
     hostname VARCHAR(255) NOT NULL,
+     TIMESTAMP,
     mode LOG_MODE NOT NULL,
-    file_tss TIMESTAMP[],
-    file_paths VARCHAR(510)[],
-    last_updated TIMESTAMP,
+    file_path VARCHAR(510),
     PRIMARY KEY (hostname, mode)
 );
 
