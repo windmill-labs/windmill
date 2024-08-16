5c5c9136729e53888d649296f54e3cafff3e7633
wendrul <dethomassin.etienne@gmail.com>
2024-08-16 17:33:34 +0200

    Add rust as a tag on worker__default

--- backend/migrations/20240807114128_add_rust_lang.up.sql
+++ backend/migrations/20240807114128_add_rust_lang.up.sql
@@ -1,2 +1,3 @@
 -- Add up migration script here
 ALTER TYPE SCRIPT_LANG ADD VALUE IF NOT EXISTS 'rust';
+UPDATE config set config = '{"worker_tags": ["deno", "python3", "go", "bash", "powershell", "dependency", "flow", "hub", "other", "bun", "php", "rust"]}'::jsonb where name = 'worker__default' and config @> '{"worker_tags": ["deno", "python3", "go", "bash", "powershell", "dependency", "flow", "hub", "other", "bun", "php"]}'::jsonb;
