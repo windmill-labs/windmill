bcde2e62d7846822cfe7f8ef89a9484938829903
HugoCasa <hugo@casademont.ch>
2024-08-21 00:47:34 +0200

    feat: improve early stop (#4257)

--- frontend/src/lib/components/propertyPicker/PropPickerResult.svelte
+++ frontend/src/lib/components/propertyPicker/PropPickerResult.svelte
@@ -3,6 +3,7 @@
 	import ObjectViewer from './ObjectViewer.svelte'
 
 	export let result: any
+	export let extraResults: any = undefined
 	export let flow_input: any = undefined
 
 	const dispatch = createEventDispatcher()
@@ -11,7 +12,11 @@
 <div class="w-full px-2">
 	<span class="font-bold text-sm">Result</span>
 	<div class="overflow-y-auto mb-2 w-full">
-		<ObjectViewer allowCopy={false} json={{ result }} on:select />
+		<ObjectViewer
+			allowCopy={false}
+			json={{ result, ...(extraResults ? extraResults : {}) }}
+			on:select
+		/>
 	</div>
 	{#if flow_input}
 		<span class="font-bold text-sm">Flow Input</span>
