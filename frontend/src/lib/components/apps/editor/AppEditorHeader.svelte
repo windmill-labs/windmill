3b14d566236a11cdb7e6985683105a42346ddea5
Faton Ramadani <faton.ramadani14@gmail.com>
2024-08-22 15:38:57 +0200

    feat(frontend): Add a toggle to disable breakpoints in the App editor (#4274)
    * feat(frontend): Add a toggle to disable breakpoints in the App editor
    
    * feat(frontend): improve UI
    
    * feat(frontend): fix build
    
    * feat(frontend): fix build
    
    * feat(frontend): fix how mobileViewOnSmallerScreens works for older apps
    
    * feat(frontend): fix build

--- frontend/src/lib/components/apps/editor/AppEditorHeader.svelte
+++ frontend/src/lib/components/apps/editor/AppEditorHeader.svelte
@@ -83,6 +83,7 @@
 	import { getDeleteInput } from '../components/display/dbtable/queries/delete'
 	import { collectOneOfFields } from './appUtils'
 	import Summary from '$lib/components/Summary.svelte'
+	import ToggleEnable from '$lib/components/common/toggleButton-v2/ToggleEnable.svelte'
 
 	async function hash(message) {
 		try {
@@ -1293,20 +1294,29 @@
 					/>
 				</ToggleButtonGroup>
 			{/if}
-			<div>
+			<div class="flex flex-row gap-2">
 				<ToggleButtonGroup class="h-[30px]" bind:selected={$breakpoint}>
-					<ToggleButton
-						tooltip="Mobile View"
-						icon={Smartphone}
-						value="sm"
-						iconProps={{ size: 16 }}
-					/>
 					<ToggleButton
 						tooltip="Computer View"
 						icon={Laptop2}
-						value="lg"
+						value={'lg'}
 						iconProps={{ size: 16 }}
 					/>
+					<ToggleButton
+						tooltip="Mobile View"
+						icon={Smartphone}
+						value={'sm'}
+						iconProps={{ size: 16 }}
+					/>
+					{#if $breakpoint === 'sm'}
+						<ToggleEnable
+							tooltip="Desktop view is enabled by default. Enable this to customize the layout of the components for the mobile view"
+							label="Enable mobile view for smaller screens"
+							bind:checked={$app.mobileViewOnSmallerScreens}
+							iconProps={{ size: 16 }}
+							iconOnly={false}
+						/>
+					{/if}
 				</ToggleButtonGroup>
 			</div>
 		</div>
