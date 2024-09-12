7c16f2cef4943a5d02e2bc6163761d80618aeae4
Ruben Fiszel <ruben@rubenfiszel.com>
2024-09-12 14:50:56 +0200

    fix: tighten number input validity if min or max is set

--- frontend/src/lib/components/ArgInput.svelte
+++ frontend/src/lib/components/ArgInput.svelte
@@ -214,7 +214,20 @@
 			error = 'Required'
 			valid && (valid = false)
 		} else {
-			if (pattern && !testRegex(pattern, v)) {
+			if (inputCat == 'number' && typeof v === 'number') {
+				let min = extra['min']
+				let max = extra['max']
+				if (min != undefined && typeof min == 'number' && v < min) {
+					error = `Should be greater than or equal to ${min}`
+					valid && (valid = false)
+				} else if (max != undefined && typeof max == 'number' && v > max) {
+					error = `Should be less than or equal to ${max}`
+					valid && (valid = false)
+				} else {
+					error = ''
+					!valid && (valid = true)
+				}
+			} else if (pattern && !testRegex(pattern, v)) {
 				if (!emptyString(customErrorMessage)) {
 					error = customErrorMessage ?? ''
 				} else if (format == 'email') {
