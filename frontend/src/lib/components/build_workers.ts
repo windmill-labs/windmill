e10f307800514ece68386611ee6690a131951603
Ruben Fiszel <ruben@rubenfiszel.com>
2024-09-13 10:39:39 +0200

    update 2

--- frontend/src/lib/components/build_workers.ts
+++ frontend/src/lib/components/build_workers.ts
@@ -1,42 +1,41 @@
 import { useWorkerFactory } from 'monaco-editor-wrapper/workerFactory'
 
-import tsWorker from 'monaco-editor-wrapper/workers/module/ts?worker&url'
-import cssWorker from 'monaco-editor-wrapper/workers/module/css?worker&url'
-import htmlWorker from 'monaco-editor-wrapper/workers/module/html?worker&url'
-import jsonWorker from 'monaco-editor-wrapper/workers/module/json?worker&url'
-import editorWorker from 'monaco-editor-wrapper/workers/module/editor?worker&url'
+// import cssWorker from 'monaco-editor-wrapper/workers/module/css?worker&url'
+// import htmlWorker from 'monaco-editor-wrapper/workers/module/html?worker&url'
+// import jsonWorker from 'monaco-editor-wrapper/workers/module/json?worker&url'
+// import editorWorker from 'monaco-editor-wrapper/workers/module/editor?worker&url'
 
 export function buildWorkerDefinition(workerPath: string, basePath: string, ...args: any[]) {
 	useWorkerFactory({
 		ignoreMapping: true,
 		workerLoaders: {
 			editorWorkerService: () => {
-				return new Worker(new URL(editorWorker, import.meta.url), {
+				return new Worker(new URL('monaco-editor-wrapper/workers/module/editor', import.meta.url), {
 					type: 'module'
 				})
 			},
 			javascript: () => {
-				return new Worker(new URL(tsWorker, import.meta.url), {
+				return new Worker(new URL('monaco-editor-wrapper/workers/module/ts', import.meta.url), {
 					type: 'module'
 				})
 			},
 			typescript: () => {
-				return new Worker(new URL(tsWorker, import.meta.url), {
+				return new Worker(new URL('monaco-editor-wrapper/workers/module/ts', import.meta.url), {
 					type: 'module'
 				})
 			},
 			json: () => {
-				return new Worker(new URL(jsonWorker, import.meta.url), {
+				return new Worker(new URL('monaco-editor-wrapper/workers/module/json', import.meta.url), {
 					type: 'module'
 				})
 			},
 			html: () => {
-				return new Worker(new URL(htmlWorker, import.meta.url), {
+				return new Worker(new URL('monaco-editor-wrapper/workers/module/html', import.meta.url), {
 					type: 'module'
 				})
 			},
 			css: () => {
-				return new Worker(new URL(cssWorker, import.meta.url), {
+				return new Worker(new URL('monaco-editor-wrapper/workers/module/css', import.meta.url), {
 					type: 'module'
 				})
 			},
