c418e0ace1913f2d7d2e9d1205c62ca2d00b6a20
Guilhem Le Mouel <guilhem.le-mouel.ext@altran.com>
2024-09-20 19:23:14 +0200

    (frontend) add quick access menu in flow editor

---
+++ frontend/src/lib/components/copilot/StepGenQuick.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import { workspaceStore } from '$lib/stores'
+	import type { FlowCopilotModule } from './flow'
+	import { ScriptService, type Script } from '$lib/gen'
+
+	import { Wand2, Loader2 } from 'lucide-svelte'
+	import SearchItems from '../SearchItems.svelte'
+	import { emptyString } from '$lib/utils'
+
+	export let funcDesc: string
+	export let trigger = false
+	export let loading = false
+
+	// state
+	export let hubCompletions: FlowCopilotModule['hubCompletions'] = []
+
+	let scripts: Script[] | undefined = undefined
+	export let filteredItems: (Script & { marked?: string })[] | (Item & { marked?: string })[] = []
+	$: prefilteredItems = scripts ?? []
+
+	async function loadScripts(): Promise<void> {
+		const loadedScripts = await ScriptService.listScripts({
+			workspace: $workspaceStore!,
+			perPage: 300,
+			kinds: trigger ? 'trigger' : 'script'
+		})
+
+		scripts = loadedScripts
+	}
+
+	$: scripts == undefined && funcDesc?.length > 1 && loadScripts()
+
+	let doneTs = 0
+	async function getHubCompletions(text: string) {
+		try {
+			// make sure we display the results of the last request last
+			const ts = Date.now()
+			const scripts = (
+				await ScriptService.queryHubScripts({
+					text: `${text}`,
+					limit: 3,
+					kind: trigger ? 'trigger' : 'script'
+				})
+			).map((s) => ({
+				...s,
+				path: `hub/${s.version_id}/${s.app}/${s.summary.toLowerCase().replaceAll(/\s+/g, '_')}`
+			}))
+			if (ts < doneTs) return
+			doneTs = ts
+
+			hubCompletions = scripts as FlowCopilotModule['hubCompletions']
+		} catch (err) {
+			if (err.name !== 'CancelError') throw err
+		}
+	}
+</script>
+
+<SearchItems
+	filter={funcDesc}
+	items={prefilteredItems}
+	bind:filteredItems
+	f={(x) => (emptyString(x.summary) ? x.path : x.summary + ' (' + x.path + ')')}
+/>
+
+<div class="text-primary transition-all flex-grow">
+	<div>
+		<div class="flex py-2 relative items-center">
+			<input
+				type="text"
+				bind:value={funcDesc}
+				on:input={() => {
+					if (funcDesc.length > 2) {
+						getHubCompletions(funcDesc)
+					} else {
+						hubCompletions = []
+					}
+				}}
+				placeholder="Search {trigger ? 'triggers' : 'scripts'} or AI gen"
+			/>
+			{#if loading}
+				<Loader2 class="animate-spin text-gray-400 absolute right-3" />
+			{/if}
+			{#if funcDesc.length === 0 && !loading}
+				<Wand2
+					size={14}
+					class="absolute right-4 top-1/2 -translate-y-1/2 fill-current opacity-70 text-violet-800 dark:text-violet-400"
+				/>
+			{/if}
+		</div>
+	</div>
+</div>
---
+++ frontend/src/lib/components/flows/pickers/PickHubScriptQuick.svelte
@@ -0,0 +1,156 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+	import { Alert, Skeleton } from '$lib/components/common'
+	import { classNames } from '$lib/utils'
+	import NoItemFound from '$lib/components/home/NoItemFound.svelte'
+	import { APP_TO_ICON_COMPONENT } from '$lib/components/icons'
+	import { IntegrationService, ScriptService, type HubScriptKind } from '$lib/gen'
+
+	export let kind: HubScriptKind & string = 'script'
+	export let filter = ''
+
+	export let loading = false
+	let hubNotAvailable = false
+
+	const dispatch = createEventDispatcher()
+
+	export let appFilter: string | undefined = undefined
+	let items: {
+		path: string
+		summary: string
+		id: number
+		version_id: number
+		ask_id: number
+		app: string
+		kind: HubScriptKind
+	}[] = []
+
+	let allApps: string[] = []
+	export let apps: string[] = []
+
+	$: apps = filter.length > 0 ? Array.from(new Set(items?.map((x) => x.app) ?? [])).sort() : allApps
+
+	$: applyFilter(filter, kind, appFilter)
+	$: getAllApps(kind)
+
+	async function getAllApps(filterKind: typeof kind) {
+		try {
+			hubNotAvailable = false
+			allApps = (
+				await IntegrationService.listHubIntegrations({
+					kind: filterKind
+				})
+			).map((x) => x.name)
+		} catch (err) {
+			console.error('Hub is not available')
+			allApps = []
+			hubNotAvailable = true
+		}
+	}
+
+	let startTs = 0
+	async function applyFilter(
+		filter: string,
+		filterKind: typeof kind,
+		appFilter: string | undefined
+	) {
+		try {
+			loading = true
+			hubNotAvailable = false
+			const ts = Date.now()
+			startTs = ts
+			await new Promise((r) => setTimeout(r, 100))
+			if (ts < startTs) return
+			const scripts =
+				filter.length > 0
+					? await ScriptService.queryHubScripts({
+							text: `${filter}`,
+							limit: 40,
+							kind: filterKind,
+							app: appFilter
+					  })
+					: (
+							await ScriptService.getTopHubScripts({
+								limit: 40,
+								app: appFilter,
+								kind: filterKind
+							})
+					  ).asks ?? []
+			if (ts === startTs) {
+				loading = false
+			}
+			items = scripts.map(
+				(x: {
+					summary: string
+					version_id: number
+					id: number
+					ask_id: number
+					app: string
+					kind: HubScriptKind
+				}) => ({
+					...x,
+					path: `hub/${x.version_id}/${x.app}/${x.summary.toLowerCase().replaceAll(/\s+/g, '_')}`,
+					summary: `${x.summary} (${x.app})`
+				})
+			)
+			hubNotAvailable = false
+		} catch (err) {
+			hubNotAvailable = true
+			console.error('Hub not available')
+			loading = false
+		}
+	}
+</script>
+
+{#if hubNotAvailable}
+	<div class="mt-2" />
+	<Alert type="error" title="Hub not available" />
+{:else if items.length > 0 && apps.length > 0}
+	{#if items.length == 0}
+		<NoItemFound />
+	{:else}
+		<ul>
+			{#each items as item (item.path)}
+				<li class="flex flex-row w-full">
+					<button
+						class="p-1 gap-1 flex flex-row grow hover:bg-surface-hover bg-surface transition-all items-center"
+						on:click={() => dispatch('pickScript', item)}
+					>
+						<div class="flex items-center gap-4">
+							<div
+								class={classNames(
+									'rounded-md p-1 flex justify-center items-center border',
+									'bg-surface border'
+								)}
+							>
+								{#if item['app'] in APP_TO_ICON_COMPONENT}
+									<svelte:component
+										this={APP_TO_ICON_COMPONENT[item['app']]}
+										height={14}
+										width={14}
+									/>
+								{/if}
+							</div>
+
+							<div
+								class="w-full max-w-60 text-left text-xs text-primary font-semibold mb-1 truncate"
+							>
+								{item.summary ?? ''}
+							</div>
+						</div>
+					</button>
+				</li>
+			{/each}
+		</ul>
+	{/if}
+	{#if items.length == 40}
+		<div class="text-tertiary text-xs py-4 px-2">
+			There are more items than being displayed. Refine your search.
+		</div>
+	{/if}
+{:else}
+	<div class="my-2" />
+	{#each Array(10).fill(0) as _}
+		<Skeleton layout={[0.5, [4]]} />
+	{/each}
+{/if}
