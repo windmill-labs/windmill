74c2cabd9f894f8aad8541d35fee00cd654d7f44
pyranota <92104930+pyranota@users.noreply.github.com>
2024-09-24 17:13:06 +0000

    Add Explicit Progress Hint (#4433)
    * Add new component `ExecutionDuration`
    
    Reusable component helps with tracking execution time of job
    It is not using job.started_at, but instead uses it's own tracking mechanism
    
    The key difference that it can give insights during execution of job and measures pure execution time
    Accuracy is configurable with `updateResolution`
    
    Can also detect if job is `longRunning` e.g. runs more than X-seconds
    
    * Implement Hint for Explicit Progress
    It uses ExecutionDuration component introduced in previous commit
    and device's local storage to handle `Dont show again`
    
    * Remove dublication of `FlowProgressBar` in `run` page
    
    * Change Hint styling
    
    * Change values in `ExecutionDuration` to match required
    
    `longDefinition`: 3 -> 30
    `updateResolution`: 2 -> 10
    
    Meaning jobs running more than 30s counts as a `Long Running Job`
    
    * Fix broken link
    
    * Scope to langs: `python3`, `bun` and `deno` jobKinds: `script`
    
    * Simplify ExecutionDuration for new scope
    
    * Bring `preview` job kind into the scope
    
    We need this to show this tip in preview pages.
    e.g. By clicking on subjob details of flow (this subjob is preview)

---
+++ frontend/src/lib/components/ExecutionDuration.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { type Job } from '$lib/gen'
+	import { onDestroy } from 'svelte'
+
+	export let job: Job | undefined = undefined
+	/** Execution duration of current active job (in ms) */
+	export let executionDuration: number = 0
+	/** Is current job running more than specified value in `longDefinition` seconds */
+	export let longRunning: boolean = false
+	/** What do we count as "long" (in ms)*/
+	export let longDefinition: number = 30_000
+	/** How often component updates execution duration (in ms)
+	 *   Higher value -> more efficient component is, less accuracy it has
+	 *   Lower value -> less efficient component is, more accuracy it has
+	 */
+	export let updateResolution: number = 5_000
+
+	let startedAt: number | undefined = undefined
+	let busy: boolean = false
+	let interval: NodeJS.Timeout | undefined
+	// Detect when execution of job started
+	$: if (
+		!busy &&
+		job &&
+		'running' in job &&
+		(job.job_kind == 'script' || job?.job_kind == 'preview')
+	)
+		start(job)
+
+	function start(job: Job) {
+		busy = true
+		startedAt = new Date(job?.started_at ?? '').getTime()
+		interval = setInterval(updateDuration, updateResolution)
+	}
+
+	function updateDuration() {
+		if (job?.type == 'CompletedJob') {
+			clearInterval(interval)
+			return
+		}
+
+		if (startedAt) executionDuration = Date.now() - startedAt
+
+		// Detect long running
+		if (executionDuration >= longDefinition) longRunning = true
+	}
+
+	onDestroy(() => {
+		// Clear the interval when the component is destroyed
+		clearInterval(interval)
+	})
+</script>
