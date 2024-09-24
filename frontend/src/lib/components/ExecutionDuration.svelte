0f805b33de3e2f389ea98d734bd668bf719caa23
pyranota <pyra@duck.com>
2024-09-24 16:43:40 +0000

    Simplify ExecutionDuration for new scope

--- frontend/src/lib/components/ExecutionDuration.svelte
+++ frontend/src/lib/components/ExecutionDuration.svelte
@@ -1,82 +1,30 @@
 <script lang="ts">
-	// Efficient way to track execution time of jobs
 	import { type Job } from '$lib/gen'
 	import { onDestroy } from 'svelte'
 
 	export let job: Job | undefined = undefined
-	/** Pure execution duration of current active job
-	 *  If job is flow, variable will represent duration of currently active subjob
-	 *  It will count only level-0 subjobs, meaning subjobs of subflows will be ignored
-	 *  executionDuration is guranteed to be started when job starts execution
-	 */
+	/** Execution duration of current active job (in ms) */
 	export let executionDuration: number = 0
 	/** Is current job running more than specified value in `longDefinition` seconds */
 	export let longRunning: boolean = false
-	/** What do we count as "long" (in seconds)*/
-	export let longDefinition: number = 30
-	/** How often component updates execution duration (in seconds)
+	/** What do we count as "long" (in ms)*/
+	export let longDefinition: number = 30_000
+	/** How often component updates execution duration (in ms)
 	 *   Higher value -> more efficient component is, less accuracy it has
 	 *   Lower value -> less efficient component is, more accuracy it has
 	 */
-	export let updateResolution: number = 10
+	export let updateResolution: number = 5_000
 
-	// Use this internally and write results to exported executionDuration
-	let _executionDuration: number = 0
-	// Track job
-	let trackedJob: string | undefined = undefined
-	// In case of flows (flow_status.step)
-	let trackedIndex: number | undefined = undefined
-
+	let startedAt: number | undefined = undefined
+	let busy: boolean = false
 	let interval: NodeJS.Timeout | undefined
 	// Detect when execution of job started
-	$: if (
-		(trackedIndex == undefined || trackedIndex != job?.flow_status?.step) &&
-		!trackedJob &&
-		job
-	)
-		tryStart(job)
+	$: if (!busy && job && 'running' in job && job.job_kind == "script") start(job)
 
-	// We want to start timer just right after job execution started
-	function tryStart(job: Job) {
-		console.log('Update in job')
-		// Handle individual jobs
-		if (job.job_kind == 'script' || job.job_kind == 'preview') {
-			// It is possible that this function is invoked multiple times on single job
-			if (trackedJob != job.id && job['running']) {
-				trackedJob = job.id
-				start()
-			}
-		}
-
-		// Handle flows
-		else if (
-			job.flow_status &&
-			['flow', 'flowpreview', 'singlescriptflow'].includes(job.job_kind)
-		) {
-			const status = job.flow_status
-			let module = status.modules[status?.step]
-
-			if (trackedIndex != status.step && module.type == 'InProgress' ) {
-				_executionDuration = 0
-				trackedIndex = status.step
-				longRunning = false
-				// We want to track every subjob right after beginning of their execution
-				start()
-			}
-		}
-		// Other job kinds are not supported
-		else
-			console.warn(
-				"JobKind of '",
-				job.job_kind,
-				"' is not supported by ExecutionDuration component"
-			)
-	}
-
-	function start() {
-		// If there is any running intervals we want to make sure we stop them
-		clearInterval(interval)
-		interval = setInterval(updateDuration, updateResolution * 1_000)
+	function start(job: Job) {
+		busy = true
+		startedAt = new Date(job?.started_at ?? '').getTime()
+		interval = setInterval(updateDuration, updateResolution)
 	}
 
 	function updateDuration() {
@@ -85,13 +33,10 @@
 			return
 		}
 
-		_executionDuration += updateResolution
+		if (startedAt) executionDuration = Date.now() - startedAt
 
 		// Detect long running
-		if (_executionDuration >= longDefinition) longRunning = true
-
-		// Update exposed executionDuration
-		executionDuration = _executionDuration
+		if (executionDuration >= longDefinition) longRunning = true
 	}
 
 	onDestroy(() => {
