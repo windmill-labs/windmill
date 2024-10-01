1006e533be9fbb8611ef45c9cf26ae1e4231ca24
Ruben Fiszel <ruben@rubenfiszel.com>
2024-10-01 15:47:38 +0200

    all

---
+++ frontend/src/lib/components/flows/pickers/FlowToplevelNode.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte'
+	import TopLevelNode from './TopLevelNode.svelte'
+
+	export let label: string
+	export let selected = false
+	const dispatch = createEventDispatcher()
+	function handleKeydown(event: KeyboardEvent & { currentTarget: EventTarget & Window }) {
+		if (selected && event.key === 'Enter') {
+			click()
+		}
+	}
+
+	function click() {
+		dispatch('click')
+	}
+</script>
+
+<svelte:window on:keydown={handleKeydown} />
+
+<TopLevelNode {label} {selected} returnIcon on:select={click} />
