99814c7ab08cd9dcffd996a140931404efbc9291
Ruben Fiszel <ruben@rubenfiszel.com>
2024-10-16 19:19:24 +0200

    nits schedule UX

--- frontend/src/lib/components/toast.ts
+++ frontend/src/lib/components/toast.ts
@@ -1,5 +1,5 @@
 //regex that match path starting with u/ or f/ and with at least 2 /
-const pathRegex = /\b(u|f)\/[^\/]+\/[^\/]+\b/g
+const pathRegex = /\b(u|f)\/[^\/\s]+\/[^\/\s]+\b/g
 
 export function processMessage(message: string): string {
 	return message.replaceAll(pathRegex, (path) => {
