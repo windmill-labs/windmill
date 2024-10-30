01b9a352e71c31aaa3a4bf33f4f582abfbc14a6c
pyranota <pyra@duck.com>
2024-10-30 14:19:01 +0000

    Fix EE build error error[E0599]: no method named `iter` found for tuple `(PyVersion, std::vec::Vec<std::string::String>)` in the current scope

--- backend/windmill-worker/src/python_executor.rs
+++ backend/windmill-worker/src/python_executor.rs
@@ -1895,7 +1895,7 @@ pub async fn start_worker(
     .to_vec();
 
     let context_envs = build_envs_map(context).await;
-    let additional_python_paths = handle_python_deps(
+    let (_, additional_python_paths) = handle_python_deps(
         job_dir,
         requirements_o,
         inner_content,
