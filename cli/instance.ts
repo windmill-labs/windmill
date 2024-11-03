57d59bc90a867b41d374b2b70951527cc3fbe6b8
Ruben Fiszel <ruben@windmill.dev>
2024-11-03 02:37:19 +0100

    feat(cli): instance yaml files inside the instance prefix folders (#4627)
    * feat(cli): instance yaml files inside the instance folders
    
    * fix: add opt-in for save instance yamls inside prefixed folders
    
    * chore: change baseUrl not needed to opts.baseUrl
    
    * do not reassign global const path
    
    * set prefix from instance
    
    ---------
    
    Co-authored-by: Wítallo <15958770+witalloliveira@users.noreply.github.com>

--- cli/instance.ts
+++ cli/instance.ts
@@ -184,6 +184,7 @@ export type InstanceSyncOptions = {
   folderPerInstance?: boolean;
   yes?: boolean;
   prefix?: string;
+  prefixSettings?: boolean;
 };
 
 export async function pickInstance(
@@ -265,21 +266,22 @@ async function instancePull(opts: GlobalOptions & InstanceSyncOptions) {
   log.info("Pulling instance-level changes");
   log.info(`remote (${instance.name}) -> local`);
 
+  opts.prefix = opts.prefix ?? instance.prefix;
   let uChanges = 0;
   if (!opts.skipUsers) {
-    uChanges = (await pullInstanceUsers(true)) as number;
+    uChanges = (await pullInstanceUsers(opts, true)) as number;
   }
   let sChanges = 0;
   if (!opts.skipSettings) {
-    sChanges = (await pullInstanceSettings(true)) as number;
+    sChanges = (await pullInstanceSettings(opts, true)) as number;
   }
   let cChanges = 0;
   if (!opts.skipConfigs) {
-    cChanges = (await pullInstanceConfigs(true)) as number;
+    cChanges = (await pullInstanceConfigs(opts, true)) as number;
   }
   let gChanges = 0;
   if (!opts.skipGroups) {
-    gChanges = (await pullInstanceGroups(true)) as number;
+    gChanges = (await pullInstanceGroups(opts, true)) as number;
   }
 
   const totalChanges = uChanges + sChanges + cChanges + gChanges;
@@ -295,16 +297,16 @@ async function instancePull(opts: GlobalOptions & InstanceSyncOptions) {
 
     if (confirm) {
       if (!opts.skipUsers && uChanges > 0) {
-        await pullInstanceUsers();
+        await pullInstanceUsers(opts);
       }
       if (!opts.skipSettings && sChanges > 0) {
-        await pullInstanceSettings();
+        await pullInstanceSettings(opts);
       }
       if (!opts.skipConfigs && cChanges > 0) {
-        await pullInstanceConfigs();
+        await pullInstanceConfigs(opts);
       }
       if (!opts.skipGroups && gChanges > 0) {
-        await pullInstanceGroups();
+        await pullInstanceGroups(opts);
       }
     }
   } else {
@@ -400,19 +402,19 @@ async function instancePush(opts: GlobalOptions & InstanceSyncOptions) {
 
   let uChanges = 0;
   if (!opts.skipUsers) {
-    uChanges = (await pushInstanceUsers(true)) as number;
+    uChanges = (await pushInstanceUsers(opts, true)) as number;
   }
   let sChanges = 0;
   if (!opts.skipSettings) {
-    sChanges = (await pushInstanceSettings(true, opts.baseUrl)) as number;
+    sChanges = (await pushInstanceSettings(opts, true)) as number;
   }
   let cChanges = 0;
   if (!opts.skipConfigs) {
-    cChanges = (await pushInstanceConfigs(true)) as number;
+    cChanges = (await pushInstanceConfigs(opts, true)) as number;
   }
   let gChanges = 0;
   if (!opts.skipGroups) {
-    gChanges = (await pushInstanceGroups(true)) as number;
+    gChanges = (await pushInstanceGroups(opts, true)) as number;
   }
 
   const totalChanges = uChanges + sChanges + cChanges + gChanges;
@@ -428,16 +430,16 @@ async function instancePush(opts: GlobalOptions & InstanceSyncOptions) {
 
     if (confirm) {
       if (!opts.skipUsers && uChanges > 0) {
-        await pushInstanceUsers();
+        await pushInstanceUsers(opts);
       }
       if (!opts.skipSettings && sChanges > 0) {
-        await pushInstanceSettings(false, opts.baseUrl);
+        await pushInstanceSettings(opts);
       }
       if (!opts.skipConfigs && cChanges > 0) {
-        await pushInstanceConfigs();
+        await pushInstanceConfigs(opts);
       }
       if (!opts.skipGroups && gChanges > 0) {
-        await pushInstanceGroups();
+        await pushInstanceGroups(opts);
       }
     }
   } else {
@@ -709,7 +711,10 @@ const command = new Command()
     "--prefix <prefix:string>",
     "Prefix of the local workspaces to pull, used to create the folders when using --include-workspaces"
   )
-
+  .option(
+    "--prefix-settings",
+    "Store instance yamls inside prefixed folders when using --prefix and --folder-per-instance"
+  )
   .action(instancePull as any)
   .command("push")
   .description(
@@ -729,6 +734,10 @@ const command = new Command()
   .option(
     "--prefix <prefix:string>",
     "Prefix of the local workspaces folders to push"
+  )
+  .option(
+    "--prefix-settings",
+    "Store instance yamls inside prefixed folders when using --prefix and --folder-per-instance"
   )
   .action(instancePush as any)
   .command("whoami")
