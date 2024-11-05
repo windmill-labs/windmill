4b6f749c82ac4753f9b988c806d7983bf154480e
dieriba <t.dieriba@gmail.com>
2024-11-05 20:32:15 +0100

    fixing migration script

--- backend/migrations/20241105133531_alter_openai_ressource_path_column_in_workspace_settings.down.sql
+++ backend/migrations/20241105133531_alter_openai_ressource_path_column_in_workspace_settings.down.sql
@@ -1,4 +1,7 @@
 -- Add down migration script here
 ALTER TABLE workspace_settings
-ALTER COLUMN ai_resource_path TYPE text
-USING ai_resource_path->>'path';
+ALTER COLUMN ai_resource TYPE text
+USING ai_resource->>'path';
+
+ALTER TABLE workspace_settings
+RENAME COLUMN ai_resource to openai_resource_path;
--- backend/migrations/20241105133531_alter_openai_ressource_path_column_in_workspace_settings.up.sql
+++ backend/migrations/20241105133531_alter_openai_ressource_path_column_in_workspace_settings.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 ALTER TABLE workspace_settings
-ALTER COLUMN ai_resource_path TYPE jsonb
-USING jsonb_build_object('resource_type', 'openai', 'path', ai_resource_path);
+ALTER COLUMN openai_resource_path TYPE jsonb
+USING jsonb_build_object('resource_type', 'openai', 'path', openai_resource_path);
 
 ALTER TABLE workspace_settings
-RENAME COLUMN ai_resource_path TO ai_resource;
+RENAME COLUMN openai_resource_path TO ai_resource;
