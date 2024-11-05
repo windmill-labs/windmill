a196851998f9d8ca159c37168ace0e61cb817c57
Guilhem <guilhemlemouel@gmail.com>
2024-11-05 09:14:45 +0100

    hide suggested results when filtering input

--- frontend/src/lib/components/propertyPicker/PropPicker.svelte
+++ frontend/src/lib/components/propertyPicker/PropPicker.svelte
@@ -247,7 +247,7 @@
 				</div>
 			{/if}
 			{#if Object.keys(pickableProperties.priorIds).length > 0}
-				{#if suggestedPropsFiltered && Object.keys(suggestedPropsFiltered).length > 0}
+				{#if !$inputMatches && suggestedPropsFiltered && Object.keys(suggestedPropsFiltered).length > 0}
 					<span class="font-normal text-sm text-secondary">Suggested Results</span>
 					<div class="overflow-y-auto mb-2">
 						<ObjectViewer
