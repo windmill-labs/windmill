7d01b87b21de61253cd78db1c003d727017d3d2c
Guilhem <guilhemlemouel@gmail.com>
2024-11-05 18:06:38 +0100

    revert scrollbar on hover

--- frontend/src/lib/components/flows/propPicker/PropPickerWrapper.svelte
+++ frontend/src/lib/components/flows/propPicker/PropPickerWrapper.svelte
@@ -125,13 +125,4 @@
 		width: 0 !important;
 		border: none !important;
 	}
-
-	:global(.splitpanes__pane) {
-		overflow-y: auto;
-		scrollbar-width: none;
-	}
-
-	:global(.splitpanes__pane:hover) {
-		scrollbar-width: thin;
-	}
 </style>
