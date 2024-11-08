ef36b0f5c9069548aa7bed4a5767ecb3db998ab7
Ruben Fiszel <ruben@rubenfiszel.com>
2024-11-08 19:35:21 +0100

    nit right panel flow props

--- frontend/src/lib/components/flows/content/FlowModuleComponent.svelte
+++ frontend/src/lib/components/flows/content/FlowModuleComponent.svelte
@@ -378,7 +378,7 @@
 							class={advancedSelected === 'runtime' ? 'h-[calc(100%-68px)]' : 'h-[calc(100%-34px)]'}
 						>
 							{#if selected === 'inputs' && (flowModule.value.type == 'rawscript' || flowModule.value.type == 'script' || flowModule.value.type == 'flow')}
-								<div class="h-full overflow-auto px-2 bg-surface" id="flow-editor-step-input">
+								<div class="h-full overflow-auto bg-surface" id="flow-editor-step-input">
 									<PropPickerWrapper
 										pickableProperties={stepPropPicker.pickableProperties}
 										error={failureModule}
