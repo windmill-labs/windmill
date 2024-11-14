cec82a9cd242d950c3c8ebe971a5fb800631655d
pyranota <pyra@duck.com>
2024-11-14 11:30:02 +0000

    Dont pin python to specific version

--- backend/windmill-worker/nsjail/download_deps.py.sh
+++ backend/windmill-worker/nsjail/download_deps.py.sh
@@ -27,7 +27,6 @@ CMD="/usr/local/bin/uv pip install
 --no-color 
 --no-deps 
 --link-mode=copy
--p 3.11
 $INDEX_URL_ARG $EXTRA_INDEX_URL_ARG $TRUSTED_HOST_ARG
 --index-strategy unsafe-best-match
 --system
