732f34fb814356f12d947366965eb0c990abf336
HugoCasa <hugo@casademont.ch>
2024-11-15 17:36:46 +0100

    unbreak ::text for json in pg

--- backend/windmill-worker/src/pg_executor.rs
+++ backend/windmill-worker/src/pg_executor.rs
@@ -583,6 +583,9 @@ fn convert_val(
                 .unwrap_or(vec![]);
             Ok(Box::new(bytes))
         }
+        Value::Object(_) if arg_t == "text" || arg_t == "varchar" => {
+            Ok(Box::new(serde_json::to_string(value).unwrap_or_default()))
+        }
         Value::Object(_) => Ok(Box::new(value.clone())),
         Value::String(s) => Ok(Box::new(s.clone())),
         _ => Err(Error::ExecutionErr(format!(
