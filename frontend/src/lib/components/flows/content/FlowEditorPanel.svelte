793b6b76a7c63634a385140e8df0b0e6141ef4a2
Henri Courdent <122811744+hcourdent@users.noreply.github.com>
2024-11-15 16:55:53 +0100

    Result node placeholder (#4721)

--- frontend/src/lib/components/flows/content/FlowEditorPanel.svelte
+++ frontend/src/lib/components/flows/content/FlowEditorPanel.svelte
@@ -62,7 +62,7 @@
 {:else if $selectedId === 'Input'}
 	<FlowInput {noEditor} disabled={disabledFlowInputs} />
 {:else if $selectedId === 'Result'}
-	<p class="p-4 text-secondary">Nothing to show about the result node. Happy flow building!</p>
+	<p class="p-4 text-secondary">The result of the flow will be the result of the last node.</p>
 {:else if $selectedId === 'constants'}
 	<FlowConstants {noEditor} />
 {:else if $selectedId === 'failure'}
