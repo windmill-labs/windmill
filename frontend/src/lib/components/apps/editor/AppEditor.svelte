538cdb9f2af594cdb7f0332414c2b2833a166c26
Guilhem <guilhemlemouel@gmail.com>
2024-11-18 18:11:42 +0100

    fix minor issue

--- frontend/src/lib/components/apps/editor/AppEditor.svelte
+++ frontend/src/lib/components/apps/editor/AppEditor.svelte
@@ -761,7 +761,7 @@
 			{/if}
 
 			{#if $connectingInput.opened}
-				<div class="absolute z-50 inset-0 w-min whitespace-nowrap mx-auto pt-0.5">
+				<div class="absolute z-50 inset-0 w-min h-min whitespace-nowrap mx-auto pt-0.5">
 					<Alert title="Press Esc to exit connection mode." size="xs" class="h-10 py-1" />
 				</div>
 			{/if}
