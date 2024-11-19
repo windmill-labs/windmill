78d08d1f90de49ed96a39c3af4c7285cbb8ff36a
HugoCasa <hugo@casademont.ch>
2024-11-19 19:58:55 +0100

    app pdf nits (#4756)

--- frontend/src/lib/components/apps/components/display/AppPdf.svelte
+++ frontend/src/lib/components/apps/components/display/AppPdf.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { getContext } from 'svelte'
-	import { findGridItem, initOutput } from '../../editor/appUtils'
+	import { initOutput } from '../../editor/appUtils'
 	import type { AppViewerContext, ComponentCustomCSS, RichConfigurations } from '../../types'
 	import { initCss } from '../../utils'
 	import InputValue from '../helpers/InputValue.svelte'
@@ -13,8 +13,7 @@
 	export let customCss: ComponentCustomCSS<'pdfcomponent'> | undefined = undefined
 	export let render: boolean
 
-	const { app, mode, worldStore, selectedComponent } =
-		getContext<AppViewerContext>('AppViewerContext')
+	const { app, worldStore } = getContext<AppViewerContext>('AppViewerContext')
 
 	const outputs = initOutput($worldStore, id, {
 		loading: false
@@ -22,17 +21,6 @@
 
 	let source: string | ArrayBuffer | undefined = undefined
 	let zoom: number | undefined = undefined
-	function syncZoomValue() {
-		const gridItem = findGridItem($app, id)
-
-		//@ts-ignore
-		if (gridItem && gridItem.data.configuration.zoom.value !== zoom) {
-			//@ts-ignore
-			gridItem.data.configuration.zoom.value = zoom
-		}
-
-		$app = $app
-	}
 
 	let css = initCss($app.css?.pdfcomponent, customCss)
 </script>
@@ -65,18 +53,6 @@
 			on:loaded={() => {
 				outputs.loading.set(false)
 			}}
-		>
-			<svelte:fragment slot="extra-button">
-				{#if $mode !== 'preview' && $selectedComponent?.includes(id)}
-					<button
-						class="fixed z-10 bottom-0 left-0 px-2 py-0.5 bg-indigo-500/90
-			hover:bg-indigo-500 focus:bg-indigo-500 duration-200 text-white text-2xs"
-						on:click={() => syncZoomValue()}
-					>
-						Sync zoom value
-					</button>
-				{/if}
-			</svelte:fragment>
-		</PdfViewer>
+		/>
 	</div>
 {/if}
