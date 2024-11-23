6a335b021dbd318941756f18bb00ad542ccb2487
Alexander Petric <petric.al@gmail.com>
2024-11-22 19:27:20 -0500

    add documentation link for cron input field

--- frontend/src/lib/components/CronInput.svelte
+++ frontend/src/lib/components/CronInput.svelte
@@ -210,6 +210,12 @@
 					<div class="text-red-600 text-xs"> Invalid cron syntax </div>
 				{/if}
 			</svelte:fragment>
+			<div class="flex flex-row-reverse text-2xs text-tertiary -mt-1">
+				<a
+					class="text-tertiary"
+					href="https://www.windmill.dev/docs/core_concepts/scheduling#cron-syntax">Croner</a
+				>
+			</div>
 			<input
 				class="inline-block"
 				type="text"
