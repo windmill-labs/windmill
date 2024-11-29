088c666a7f9c0f074cdd312e39b79418045dc7db
wendrul <53628737+wendrul@users.noreply.github.com>
2024-11-29 19:14:05 +0100

    feat: indexer extra settings + parallel downloads + many improvements (#4822)
    * Nits on the service logs page
    
    * Show all the hosts returned by query + sumOtherDocCount warning
    
    * Remove from index endpoint
    
    * Fix tests
    
    * Prepare sqlx

--- backend/windmill-indexer/src/lib.rs
+++ backend/windmill-indexer/src/lib.rs
@@ -1,6 +1,3 @@
-#[cfg(all(feature = "enterprise", feature = "parquet"))]
 pub mod completed_runs_ee;
-#[cfg(all(feature = "enterprise", feature = "parquet"))]
 pub mod service_logs_ee;
-#[cfg(all(feature = "enterprise", feature = "parquet"))]
 pub mod indexer_ee;
