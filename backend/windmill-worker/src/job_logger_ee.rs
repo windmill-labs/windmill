61d31cc9a74c4d2a143233cc33ebd56b7bd03253
Ruben Fiszel <ruben@rubenfiszel.com>
2024-12-03 23:10:21 +0100

    improve draft only delete security

--- backend/windmill-worker/src/job_logger_ee.rs
+++ backend/windmill-worker/src/job_logger_ee.rs
@@ -8,7 +8,14 @@ use windmill_common::DB;
 use crate::job_logger::CompactLogs;
 
 #[cfg(all(feature = "enterprise", feature = "parquet"))]
-pub(crate) async fn s3_storage(_job_id: Uuid, _w_id: &String, _db: &sqlx::Pool<sqlx::Postgres>, _logs: &String, _total_size: &Arc<AtomicU32>, _worker_name: &String) {
+pub(crate) async fn s3_storage(
+    _job_id: Uuid,
+    _w_id: &String,
+    _db: &sqlx::Pool<sqlx::Postgres>,
+    _logs: &String,
+    _total_size: &Arc<AtomicU32>,
+    _worker_name: &String,
+) {
     tracing::info!("Logs length of {job_id} has exceeded a threshold. Implementation to store excess on s3 in not OSS");
 }
 
@@ -24,7 +31,9 @@ pub(crate) async fn default_disk_log_storage(
     tracing::info!("Logs length of {job_id} has exceeded a threshold. Implementation to store excess on disk in not OSS");
 }
 
-
-pub(crate) fn process_streaming_log_lines(r: Result<Option<String>, io::Error>, _stderr: bool) -> Option<Result<String, io::Error>> {
+pub(crate) fn process_streaming_log_lines(
+    r: Result<Option<String>, io::Error>,
+    _stderr: bool,
+) -> Option<Result<String, io::Error>> {
     r.transpose()
 }
