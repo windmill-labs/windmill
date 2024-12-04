09010782edcde81a2e2e27303bbf23578f5c6b74
Guilhem <guilhemlemouel@gmail.com>
2024-12-04 17:57:03 +0000

    remove capture tab in input

--- frontend/src/lib/components/flows/content/FlowInput.svelte
+++ frontend/src/lib/components/flows/content/FlowInput.svelte
@@ -12,17 +12,13 @@
 	import EditableSchemaForm from '$lib/components/EditableSchemaForm.svelte'
 	import AddProperty from '$lib/components/schema/AddProperty.svelte'
 	import FlowInputViewer from '$lib/components/FlowInputViewer.svelte'
-	import Tabs from '$lib/components/common/tabs/Tabs.svelte'
-	import Tab from '$lib/components/common/tabs/Tab.svelte'
-	import CapturePanel from '$lib/components/triggers/CapturePanel.svelte'
-	import { insertNewPreprocessorModule } from '../flowStateUtils'
 	import Popover from '$lib/components/meltComponents/Popover.svelte'
 	import InputSchemaPicker from '$lib/components/flows/pickers/InputSchemaPicker.svelte'
 
 	export let noEditor: boolean
 	export let disabled: boolean
 
-	const { flowStore, flowStateStore, previewArgs, initialPath, pathStore, selectedId } =
+	const { flowStore, flowStateStore, previewArgs, initialPath, pathStore } =
 		getContext<FlowEditorContext>('FlowEditorContext')
 
 	let inputLibraryDrawer: Drawer
@@ -43,103 +39,70 @@
 		jsonPayload.closeDrawer()
 	}
 	const yOffset = 191
-
-	let tabSelected = 'input'
 </script>
 
 <FlowCard {noEditor} title="Flow Input">
 	{#if !disabled}
-		<Tabs bind:selected={tabSelected}>
-			<Tab value="input">Input form</Tab>
-			<Tab value="capture">Capture</Tab>
-		</Tabs>
-
-		{#if tabSelected === 'input'}
-			<div class="flex flex-row items-center gap-2 px-4 py-2 border-b">
-				<div class="text-sm">Copy input's schema from</div>
-				<Popover closeButton={false}>
-					<svelte:fragment slot="trigger">
-						<Button color="dark" size="xs" nonCaptureEvent>Captures</Button>
-					</svelte:fragment>
-					<svelte:fragment slot="content">
-						<InputSchemaPicker isFlow={true} path={$pathStore} on:openTriggers />
-					</svelte:fragment>
-				</Popover>
-				<Button
-					color="dark"
-					size="xs"
-					on:click={() => {
-						jsonPayload.openDrawer()
-					}}
-				>
-					A JSON
-				</Button>
-				<Button
-					color="dark"
-					size="xs"
-					on:click={() => {
-						inputLibraryDrawer.openDrawer()
-					}}
-				>
-					Past Runs/Input library
-				</Button>
-				<Button
-					color="dark"
-					size="xs"
-					disabled={$flowStore.value.modules.length === 0 ||
-						$flowStore.value.modules[0].value.type == 'identity'}
-					on:click={() => copyFirstStepSchema($flowStateStore, flowStore)}
-				>
-					First step's inputs
-				</Button>
-			</div>
-			<div class="p-4 border-b">
-				<AddProperty
-					bind:schema={$flowStore.schema}
-					bind:this={addProperty}
-					on:change={() => {
-						$flowStore = $flowStore
-					}}
-				/>
-			</div>
-
-			<EditableSchemaForm
+		<div class="flex flex-row items-center gap-2 px-4 py-2 border-b">
+			<div class="text-sm">Copy input's schema from</div>
+			<Popover closeButton={false}>
+				<svelte:fragment slot="trigger">
+					<Button color="dark" size="xs" nonCaptureEvent>Capture</Button>
+				</svelte:fragment>
+				<svelte:fragment slot="content">
+					<InputSchemaPicker isFlow={true} path={$pathStore} on:openTriggers />
+				</svelte:fragment>
+			</Popover>
+			<Button
+				color="dark"
+				size="xs"
+				on:click={() => {
+					jsonPayload.openDrawer()
+				}}
+			>
+				A JSON
+			</Button>
+			<Button
+				color="dark"
+				size="xs"
+				on:click={() => {
+					inputLibraryDrawer.openDrawer()
+				}}
+			>
+				Past Runs/Input library
+			</Button>
+			<Button
+				color="dark"
+				size="xs"
+				disabled={$flowStore.value.modules.length === 0 ||
+					$flowStore.value.modules[0].value.type == 'identity'}
+				on:click={() => copyFirstStepSchema($flowStateStore, flowStore)}
+			>
+				First step's inputs
+			</Button>
+		</div>
+		<div class="p-4 border-b">
+			<AddProperty
 				bind:schema={$flowStore.schema}
-				isFlowInput
-				on:edit={(e) => {
-					addProperty?.openDrawer(e.detail)
-				}}
-				on:delete={(e) => {
-					addProperty?.handleDeleteArgument([e.detail])
-				}}
-				offset={yOffset}
-				displayWebhookWarning
-			/>
-		{:else}
-			<CapturePanel
-				isFlow
-				path={$pathStore}
-				hasPreprocessor={!!$flowStore.value.preprocessor_module}
-				canHavePreprocessor
-				newItem={initialPath === ''}
-				on:openTriggers
-				on:applyArgs
-				on:addPreprocessor={async () => {
-					await insertNewPreprocessorModule(flowStore, flowStateStore, {
-						language: 'bun',
-						subkind: 'preprocessor'
-					})
-					$selectedId = 'preprocessor'
-				}}
-				on:updateSchema={(e) => {
-					const { schema, redirect } = e.detail
-					$flowStore.schema = schema
-					if (redirect) {
-						tabSelected = 'input'
-					}
+				bind:this={addProperty}
+				on:change={() => {
+					$flowStore = $flowStore
 				}}
 			/>
-		{/if}
+		</div>
+
+		<EditableSchemaForm
+			bind:schema={$flowStore.schema}
+			isFlowInput
+			on:edit={(e) => {
+				addProperty?.openDrawer(e.detail)
+			}}
+			on:delete={(e) => {
+				addProperty?.handleDeleteArgument([e.detail])
+			}}
+			offset={yOffset}
+			displayWebhookWarning
+		/>
 	{:else}
 		<div class="p-4 border-b">
 			<FlowInputViewer schema={$flowStore.schema} />
