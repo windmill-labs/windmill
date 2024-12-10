b458c239277be27e1deab1e44a870e60cfe17f9b
Ruben Fiszel <ruben@rubenfiszel.com>
2024-12-10 15:31:09 +0100

    all

---
+++ frontend/src/routes/(root)/(logged)/apps_raw/add/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import 'sandpack-lit/dist/presets/sandpack'
+	import 'sandpack-lit/dist/themes/odyssey.css'
+</script>
+
+<main>
+	<div class="container">
+		<!-- <sandpack-preset
+			options={{
+				closableTabs: false,
+				files: {
+					/** files go here */
+				},
+				customSetup: {
+					dependencies: {
+						lit: '2.6.1'
+					}
+				}
+			}}
+		/> -->
+		<sandpack-provider>
+			<sandpack-layout>
+				<sandpack-preview initMode="" />
+			</sandpack-layout>
+		</sandpack-provider>
+		<!-- <sandpack-preview options={{ template: 'vite', initMode: 'lazy' }} /> -->
+	</div>
+</main>
+
+<style>
+	.container {
+		width: 900px;
+	}
+</style>
