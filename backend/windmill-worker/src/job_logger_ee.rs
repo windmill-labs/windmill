1b922f3ba94b9d8004246fe193ceb7155cfb4aff
Lucas Abel <22837557+uael@users.noreply.github.com>
2024-12-11 14:39:45 +0100

    fix `s3_storage` decl not being the same as in `ee` (#4901)

--- backend/windmill-worker/src/job_logger_ee.rs
+++ backend/windmill-worker/src/job_logger_ee.rs
@@ -12,8 +12,8 @@ pub(crate) async fn s3_storage(
     _job_id: Uuid,
     _w_id: &String,
     _db: &sqlx::Pool<sqlx::Postgres>,
-    _logs: &String,
-    _total_size: &Arc<AtomicU32>,
+    _logs: String,
+    _total_size: Arc<AtomicU32>,
     _worker_name: &String,
 ) {
     tracing::info!("Logs length of {_job_id} has exceeded a threshold. Implementation to store excess on s3 in not OSS");
