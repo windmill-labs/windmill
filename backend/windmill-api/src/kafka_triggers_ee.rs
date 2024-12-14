690f0fd46a9669ff9fc6029ffa34e1323e490512
Ruben Fiszel <ruben@rubenfiszel.com>
2024-12-14 16:42:55 +0100

    all

--- backend/windmill-api/src/kafka_triggers_ee.rs
+++ backend/windmill-api/src/kafka_triggers_ee.rs
@@ -1,1 +1,13 @@
-/git/windmill/../windmill-ee-private/windmill-api/src/kafka_triggers_ee.rs
+use crate::db::DB;
+use axum::Router;
+
+pub fn workspaced_service() -> Router {
+    Router::new()
+}
+
+pub async fn start_kafka_consumers(
+    _db: DB,
+    mut _killpill_rx: tokio::sync::broadcast::Receiver<()>,
+) -> () {
+    // implementation is not open source
+}
--- backend/windmill-api/src/smtp_server_ee.rs
+++ backend/windmill-api/src/smtp_server_ee.rs
@@ -1,1 +1,16 @@
-/git/windmill/../windmill-ee-private/windmill-api/src/smtp_server_ee.rs
+use crate::{db::DB, users::AuthCache};
+use std::{net::SocketAddr, sync::Arc};
+use windmill_common::db::UserDB;
+
+pub struct SmtpServer {
+    pub auth_cache: Arc<AuthCache>,
+    pub db: DB,
+    pub user_db: UserDB,
+    pub base_internal_url: String,
+}
+
+impl SmtpServer {
+    pub async fn start_listener_thread(self: Arc<Self>, _addr: SocketAddr) -> anyhow::Result<()> {
+        Err(anyhow::anyhow!("Implementation not open source"))
+    }
+}
