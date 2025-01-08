2371994bc1dd3837d495003b33f2f074fc85c85d
Alexander Petric <petric.al@gmail.com>
2025-01-08 16:45:51 -0500

    feat(frontend): allow workspace admin to set workspace color

--- frontend/src/lib/components/sidebar/MenuButton.svelte
+++ frontend/src/lib/components/sidebar/MenuButton.svelte
@@ -11,7 +11,7 @@
 	export let stopPropagationOnClick: boolean = false
 	export let shortcut: string = ''
 	export let notificationsCount: number = 0
-
+	export let color: string | null = null
 	let dispatch = createEventDispatcher()
 </script>
 
@@ -26,10 +26,12 @@
 				'group flex items-center px-2 py-2 font-light rounded-md h-8 gap-3 w-full',
 				lightMode
 					? 'text-primary hover:bg-surface-hover '
-					: '  hover:bg-[#2A3648] text-primary-inverse dark:text-primary',
+					: 'hover:bg-[#2A3648] text-primary-inverse dark:text-primary',
+				color ? 'border-2' : '',
 				'transition-all',
 				$$props.class
 			)}
+			style={color ? `border-color: ${color}; padding: 0 calc(0.5rem - 2px);` : ''}
 			title={label}
 		>
 			{#if icon}
@@ -50,7 +52,7 @@
 				<span
 					class={twMerge(
 						'whitespace-pre truncate',
-						lightMode ? 'text-primary' : 'text-primary-inverse  dark:text-primary',
+						lightMode ? 'text-primary' : 'text-primary-inverse dark:text-primary',
 						'transition-all',
 						$$props.class
 					)}
