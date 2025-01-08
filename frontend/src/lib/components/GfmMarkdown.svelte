f1c4d2dc7817e0ce1c7dab676c678b6980f2c1e5
Alexander Petric <petric.al@gmail.com>
2025-01-08 10:47:06 -0500

    support html in github markdown plugin

--- frontend/src/lib/components/GfmMarkdown.svelte
+++ frontend/src/lib/components/GfmMarkdown.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import Markdown from 'svelte-exmarkdown'
+	import {Markdown, type Plugin} from 'svelte-exmarkdown'
 	import { gfmPlugin } from 'svelte-exmarkdown/gfm'
+	import rehypeRaw from 'rehype-raw';
 
 	export let md: string
+	const plugins: Plugin[] = [gfmPlugin(),{ rehypePlugin: [rehypeRaw] }];
 </script>
 
 <div class="!prose-xs pgap">
-	<Markdown {md} plugins={[gfmPlugin()]} />
+	<Markdown {md} plugins={plugins} />
 </div>
 
 <style global>
