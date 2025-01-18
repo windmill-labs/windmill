f2a57442b18d5e4563d755229c229b46b3b118fc
Ruben Fiszel <ruben@rubenfiszel.com>
2025-01-19 00:47:28 +0100

    all

--- backend/windmill-api-client/build.rs
+++ backend/windmill-api-client/build.rs
@@ -5,12 +5,28 @@ use std::{
     process::Command,
 };
 
+use openapiv3::{OpenAPI, Operation};
+
 fn main() {
     let src = "../windmill-api/openapi.yaml";
     println!("cargo:rerun-if-changed={}", src);
     Command::new("sh").args(&["bundle.sh"]).status().unwrap();
     let file = File::open("./bundled.json").unwrap();
-    let spec = serde_json::from_reader(file).unwrap();
+    let mut spec: OpenAPI = serde_json::from_reader(file).unwrap();
+    // Remove all multipart/form-data endpoints
+    for (_, value) in spec.paths.paths.iter_mut() {
+        if let openapiv3::ReferenceOr::Item(pv) = value {
+            if let Some(post) = pv.post.as_mut() {
+                if post.responses.responses.iter().any(|(_, v)| {
+                    v.as_item()
+                        .is_some_and(|x| x.content.contains_key("multipart/form-data"))
+                }) {
+                    *post = Operation::default();
+                }
+            }
+        }
+    }
+
     let mut generator = progenitor::Generator::default();
 
     let tokens = generator.generate_tokens(&spec).unwrap();
