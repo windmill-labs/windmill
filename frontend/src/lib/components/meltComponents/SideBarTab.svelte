59bcce7a969ba57506b435d13f078c4e8c2f0465
Guilhem <guilhemlemouel@gmail.com>
2025-01-22 12:51:14 +0100

    feat (frontend): improve capture UI (#5051)
    * feat (frontend): improve capture UI
    
    * show schema diff
    
    * add accept reject form
    
    * add diff for first node input
    
    * move update schema button
    
    * change modified properties display
    
    * propagate change through nested components
    
    * allow modification of nested object in schema
    
    * reject nested changes
    
    * clean logic
    
    * clean code
    
    * clean
    
    * nit
    
    * fix first strep input
    
    * fix argument preview
    
    * clean
    
    * restore indentation for nested objects
    
    * ignore undefined field in diff computation
    
    * fix run button disabled update
    
    * nit
    
    * fix update JSON
    
    * fix deletion of nested components
    
    * dark mode
    
    * clean
    
    * replace dropdown with sidebar
    
    * only check type for compatibility
    
    * add shadow
    
    * disbale dnd on edit
    
    * auto-scroll within schema
    
    * fix nested not dnd object diff viewer
    
    * fix captures drawer
    
    * open edit tab on add new arg
    
    * change button label when schema is the same
    
    * ajust padding
    
    * fix arg update
    
    * fix oneof display
    
    * propagate change event through nested schema
    
    * handle oneOf
    
    * fix preview arg sync
    
    * handle s3 object
    
    * fix arg sync
    
    * update schema input compatibility
    
    * clean compatible
    
    * accept empty array items
    
    * clean
    
    * increase of schema args gap
    
    * fix nested schema update
    
    * allow number and int compatibility
    
    * reset args when modifying schema
    
    * open fields on add with addPropertyV2
    
    ---------
    
    Co-authored-by: Ruben Fiszel <ruben@windmill.dev>

---
+++ frontend/src/lib/components/meltComponents/SideBarTab.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	export let dropdownItems: Array<{
+		label: string
+		onClick: () => void
+		icon?: any
+		selected?: boolean
+	}> = []
+
+	export let fullMenu: boolean = false
+
+	let open = false
+	let timeout: NodeJS.Timeout | null = null
+
+	function handleMouseEnter() {
+		if (!fullMenu) return
+		timeout = setTimeout(() => {
+			open = true
+		}, 500)
+	}
+
+	function handleMouseLeave() {
+		if (timeout) {
+			clearTimeout(timeout)
+			timeout = null
+		}
+		open = false
+	}
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<div
+	class="flex flex-col relative !overflow-visible"
+	on:mouseenter={handleMouseEnter}
+	on:mouseleave={handleMouseLeave}
+>
+	<slot name="close button" />
+
+	{#if fullMenu}
+		<div
+			class="absolute flex-col top-[30px] left-0 z-50 bg-surface border-l border-b {open
+				? 'rounded-md rounded-tl-none overflow-hidden shadow-md'
+				: 'rounded-bl-md'}"
+		>
+			{#each dropdownItems as item}
+				<button
+					class="hover:bg-surface-hover p-2 transition-colors duration-150 w-full {item.selected
+						? 'bg-surface-selected'
+						: 'text-secondary'}"
+					on:click={() => {
+						item.onClick()
+						open = false
+					}}
+				>
+					<div class="flex flex-row items-center gap-2 min-h-[20px]">
+						{#if item.icon}
+							<svelte:component this={item.icon} size={14} />
+						{/if}
+
+						{#if open}
+							<p class="text-xs text-secondary whitespace-nowrap text-left">
+								{item.label}
+							</p>
+						{/if}
+					</div>
+				</button>
+			{/each}
+		</div>
+	{/if}
+</div>
--- frontend/src/lib/components/schema/AddProperty.svelte
+++ frontend/src/lib/components/schema/AddProperty.svelte
@@ -138,7 +138,9 @@
 			if (Object.keys(modifiedProperties).includes(argName)) {
 				delete modifiedProperties[argName]
 
-				modifiedObject.required = schema.required.filter((arg) => arg !== argName)
+				if (modifiedObject.required) {
+					modifiedObject.required = schema.required.filter((arg) => arg !== argName)
+				}
 				if (modifiedObject.order) {
 					modifiedObject.order = modifiedObject.order.filter((arg) => arg !== argName)
 				}
