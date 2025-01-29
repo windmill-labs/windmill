9ae415bc6c31fe8c7f444bd5f5627edd3ecfad2f
Guilhem <guilhemlemouel@gmail.com>
2025-01-29 11:10:36 +0000

    nit

--- frontend/src/lib/components/FlowPreviewContent.svelte
+++ frontend/src/lib/components/FlowPreviewContent.svelte
@@ -177,7 +177,7 @@
 
 <svelte:window on:keydown={onKeyDown} />
 
-<div class="flex flex-col space-y-2 h-screen bg-surface px-8 py-4 w-full" id="flow-preview-content">
+<div class="flex flex-col space-y-2 h-screen bg-surface px-4 py-2 w-full" id="flow-preview-content">
 	<div class="flex flex-row w-full items-center gap-x-2">
 		<div class="w-8">
 			<Button
