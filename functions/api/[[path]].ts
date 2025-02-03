2ce0b117435dcb1f5b00e6b25479f2c05da510e2
Ruben Fiszel <ruben@windmill.dev>
2025-02-03 14:55:27 +0100

    functions preview

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -24,11 +24,19 @@ export async function onRequest(context) {
     newResponse.headers.set(
       "set-cookie",
       "token" +
-        newResponse.headers
-          .get("set-cookie")
-          ?.replace("Domain=windmill.dev;", "")
-          ?.split("token")
-          .pop() ?? ""
+      newResponse.headers
+        .get("set-cookie")
+        ?.replace("Domain=windmill.dev;", "")
+        ?.split("token")
+        .pop() ?? ""
+    );
+    newResponse.headers.set(
+      "Cross-Origin-Opener-Policy",
+      "same-origin"
+    );
+    newResponse.headers.set(
+      "Cross-Origin-Embedder-Policy",
+      "require-corp"
     );
     return newResponse;
   } catch (e) {
