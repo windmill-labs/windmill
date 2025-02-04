a6123200872a2570be6afd2b0c872b30ef732f41
Guilhem <guilhemlemouel@gmail.com>
2025-02-04 11:50:31 +0000

    use melt menu for operator menu

--- frontend/src/lib/components/sidebar/OperatorMenu.svelte
+++ frontend/src/lib/components/sidebar/OperatorMenu.svelte
@@ -15,19 +15,17 @@
 	} from 'lucide-svelte'
 	import { base } from '$lib/base'
 
-	import Menu from '../common/menu/MenuV2.svelte'
-
 	import MultiplayerMenu from './MultiplayerMenu.svelte'
 	import { enterpriseLicense, superadmin, userWorkspaces, workspaceStore } from '$lib/stores'
-	import MenuButton from './MenuButton.svelte'
-	import { MenuItem } from '@rgossiaux/svelte-headlessui'
-	import MenuLink from './MenuLink.svelte'
 	import { twMerge } from 'tailwind-merge'
 	import { USER_SETTINGS_HASH } from './settings'
 	import { logout } from '$lib/logout'
 	import DarkModeObserver from '../DarkModeObserver.svelte'
 	import BarsStaggered from '../icons/BarsStaggered.svelte'
-
+	import Menu from '$lib/components/meltComponents/Menu.svelte'
+	import Menubar from '$lib/components/meltComponents/Menubar.svelte'
+	import MenuButtonMelt from './MenuButtonMelt.svelte'
+	import MenuLinkMelt from './MenuLink.svelte'
 	let darkMode: boolean = false
 
 	export let isCollapsed: boolean = false
@@ -44,7 +42,10 @@
 	].filter(
 		(link) =>
 			link.id === 'home' ||
-			($userWorkspaces && $workspaceStore && $userWorkspaces.find((_) => _.id === $workspaceStore)?.operator_settings?.[link.id] === true)
+			($userWorkspaces &&
+				$workspaceStore &&
+				$userWorkspaces.find((_) => _.id === $workspaceStore)?.operator_settings?.[link.id] ===
+					true)
 	)
 
 	$: secondMenuLinks = [
@@ -104,21 +105,37 @@
 			href: `${base}/workers`
 		}
 	].filter((link) => {
-		if (!$userWorkspaces || !$workspaceStore) return false;
-		return $userWorkspaces.find((_) => _.id === $workspaceStore)?.operator_settings?.[link.id] === true
+		if (!$userWorkspaces || !$workspaceStore) return false
+		return (
+			$userWorkspaces.find((_) => _.id === $workspaceStore)?.operator_settings?.[link.id] === true
+		)
 	})
 
 	let moreOpen = false
 </script>
 
-<Menu>
-	<div slot="trigger">
-		<MenuButton class="!text-xs" icon={MenuIcon} {isCollapsed} lightMode label={undefined} />
-	</div>
-	<div class="w-full max-w-full">
-		{#each favoriteLinks ?? [] as favorite (favorite.href)}
-			<MenuItem href={favorite.href}>
-				<span class="w-full inline-flex flex-row px-2 py-2 hover:bg-surface-hover">
+<Menubar let:createMenu>
+	<Menu {createMenu} let:item>
+		<svelte:fragment slot="trigger" let:trigger>
+			<MenuButtonMelt
+				class="!text-xs"
+				icon={MenuIcon}
+				{isCollapsed}
+				lightMode
+				label={undefined}
+				{trigger}
+			/>
+		</svelte:fragment>
+		<div class="w-full max-w-full">
+			{#each favoriteLinks ?? [] as favorite (favorite.href)}
+				<a
+					href={favorite.href}
+					use:item
+					class={twMerge(
+						'w-full inline-flex flex-row px-2 py-2 hover:bg-surface-hover',
+						'data-[highlighted]:bg-surface-hover'
+					)}
+				>
 					<span class="center-center">
 						{#if favorite.kind == 'script'}
 							<Code2 size={16} />
@@ -131,120 +148,130 @@
 					<span class="text-primary ml-2 grow min-w-0 text-xs truncate">
 						{favorite.label}
 					</span>
-				</span>
-			</MenuItem>
-		{/each}
-	</div>
-
-	{#each mainMenuLinks as menuLink (menuLink.href ?? menuLink.label)}
-		<MenuLink class="!text-xs" {...menuLink} {isCollapsed} lightMode />
-	{/each}
-
-	<div class="divide-y" role="none">
-		<div role="none">
-			<MenuItem
-				href={USER_SETTINGS_HASH}
-				class={twMerge(
-					'flex flex-row gap-3.5 items-center px-2 py-2 ',
-					'text-secondary text-xs',
-					'hover:bg-surface-hover hover:text-primary cursor-pointer'
-				)}
-			>
-				<Settings size={14} />
-				Account settings
-			</MenuItem>
+				</a>
+			{/each}
 		</div>
 
-		<div role="none">
-			<button
-				on:click={() => {
-					if (!document.documentElement.classList.contains('dark')) {
-						document.documentElement.classList.add('dark')
-						window.localStorage.setItem('dark-mode', 'dark')
-					} else {
-						document.documentElement.classList.remove('dark')
-						window.localStorage.setItem('dark-mode', 'light')
-					}
-				}}
-				class={twMerge(
-					'text-secondary block text-left px-2 py-2 font-normal text-xs hover:bg-surface-hover hover:text-primary w-full',
-					'flex flex-row items-center gap-3.5 '
-				)}
-				role="menuitem"
-				tabindex="-1"
-			>
-				{#if darkMode}
-					<Sun size={14} />
-				{:else}
-					<Moon size={14} />
-				{/if}
-				Switch theme
-			</button>
-			<a
-				href="{base}/user/workspaces"
-				on:click={() => {
-					localStorage.removeItem('workspace')
-				}}
-				class="text-primary flex gap-3.5 px-2 py-2 text-xs hover:bg-surface-hover hover:text-primary"
-				role="menuitem"
-				tabindex="-1"
-			>
-				<Building size={14} />
-				All workspaces
-			</a>
+		{#each mainMenuLinks as menuLink (menuLink.href ?? menuLink.label)}
+			<MenuLinkMelt class="!text-xs" {...menuLink} {isCollapsed} lightMode {item} />
+		{/each}
 
-			{#if $superadmin}
-				<MenuItem
-					href="#superadmin-settings"
+		<div class="divide-y" role="none">
+			<div role="none">
+				<a
+					href={USER_SETTINGS_HASH}
 					class={twMerge(
 						'flex flex-row gap-3.5 items-center px-2 py-2 ',
 						'text-secondary text-xs',
-						'hover:bg-surface-hover hover:text-primary cursor-pointer'
+						'hover:bg-surface-hover hover:text-primary cursor-pointer',
+						'data-[highlighted]:bg-surface-hover data-[highlighted]:text-primary'
 					)}
 				>
-					<ServerCog size={14} />
-					Instance settings
-				</MenuItem>
-			{/if}
+					<Settings size={14} />
+					Account settings
+				</a>
+			</div>
 
-			<MenuItem
-				href="#"
-				on:click={() => logout()}
-				class={twMerge(
-					'flex flex-row gap-3.5  items-center px-2 py-2 ',
-					'text-secondary text-xs',
-					'hover:bg-surface-hover hover:text-primary cursor-pointer'
-				)}
+			<div role="none">
+				<button
+					on:click={() => {
+						if (!document.documentElement.classList.contains('dark')) {
+							document.documentElement.classList.add('dark')
+							window.localStorage.setItem('dark-mode', 'dark')
+						} else {
+							document.documentElement.classList.remove('dark')
+							window.localStorage.setItem('dark-mode', 'light')
+						}
+					}}
+					class={twMerge(
+						'text-secondary block text-left px-2 py-2 font-normal text-xs hover:bg-surface-hover hover:text-primary w-full',
+						'flex flex-row items-center gap-3.5 ',
+						'data-[highlighted]:bg-surface-hover data-[highlighted]:text-primary'
+					)}
+					role="menuitem"
+					tabindex="-1"
+					use:item
+				>
+					{#if darkMode}
+						<Sun size={14} />
+					{:else}
+						<Moon size={14} />
+					{/if}
+					Switch theme
+				</button>
+				<a
+					href="{base}/user/workspaces"
+					use:item
+					on:click={() => {
+						localStorage.removeItem('workspace')
+					}}
+					class={twMerge(
+						'text-primary flex gap-3.5 px-2 py-2 text-xs hover:bg-surface-hover hover:text-primary',
+						'data-[highlighted]:bg-surface-hover data-[highlighted]:text-primary'
+					)}
+					role="menuitem"
+					tabindex="-1"
+				>
+					<Building size={14} />
+					All workspaces
+				</a>
+
+				{#if $superadmin}
+					<a
+						href="#superadmin-settings"
+						class={twMerge(
+							'flex flex-row gap-3.5 items-center px-2 py-2 ',
+							'text-secondary text-xs',
+							'hover:bg-surface-hover hover:text-primary cursor-pointer',
+							'data-[highlighted]:bg-surface-hover data-[highlighted]:text-primary'
+						)}
+						use:item
+					>
+						<ServerCog size={14} />
+						Instance settings
+					</a>
+				{/if}
+
+				<button
+					on:click={() => logout()}
+					class={twMerge(
+						'flex flex-row gap-3.5  items-center px-2 py-2 ',
+						'text-secondary text-xs',
+						'hover:bg-surface-hover hover:text-primary cursor-pointer',
+						'data-[highlighted]:bg-surface-hover data-[highlighted]:text-primary'
+					)}
+					use:item
+				>
+					<LogOut size={14} />
+					Sign out
+				</button>
+			</div>
+			<div
+				on:mouseenter={() => (moreOpen = true)}
+				on:mouseleave={() => (moreOpen = false)}
+				class="divide-y"
+				role="none"
 			>
-				<LogOut size={14} />
-				Sign out
-			</MenuItem>
+				{#if moreOpen == false && secondMenuLinks.length > 0}
+					<div class="px-2 text-tertiary text-2xs">More...</div>
+				{:else}
+					{#each secondMenuLinks as menuLink (menuLink.href ?? menuLink.label)}
+						<div>
+							<a
+								href={menuLink.href}
+								class="flex flex-row gap-3.5 items-center px-2 py-0.5 text-secondary text-2xs hover:bg-surface-hover hover:text-primary cursor-pointer"
+							>
+								{menuLink.label}
+							</a>
+						</div>
+					{/each}
+				{/if}
+			</div>
 		</div>
-		<div
-			on:mouseenter={() => (moreOpen = true)}
-			on:mouseleave={() => (moreOpen = false)}
-			class="divide-y"
-			role="none"
-		>
-			{#if moreOpen == false && secondMenuLinks.length > 0}
-				<div class="px-2 text-tertiary text-2xs">More...</div>
-			{:else}
-				{#each secondMenuLinks as menuLink (menuLink.href ?? menuLink.label)}
-					<div>
-						<a
-							href={menuLink.href}
-							class="flex flex-row gap-3.5 items-center px-2 py-0.5 text-secondary text-2xs hover:bg-surface-hover hover:text-primary cursor-pointer"
-						>
-							{menuLink.label}
-						</a>
-					</div>
-				{/each}
-			{/if}
-		</div>
-	</div>
-	{#if $enterpriseLicense}
-		<MultiplayerMenu />
-	{/if}
-</Menu>
+		{#if $enterpriseLicense}
+			<MultiplayerMenu />
+		{/if}
+	</Menu>
+</Menubar>
 
 <DarkModeObserver bind:darkMode />
