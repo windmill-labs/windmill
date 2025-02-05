7573285a5947cb797a56c28a9fa9af65514b743b
Ruben Fiszel <ruben@windmill.dev>
2025-02-05 12:56:47 +0100

    kill servers only after workers exited

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -386,17 +386,11 @@ async fn windmill_main() -> anyhow::Result<()> {
             tracing::info!("SKIP_MIGRATION set, skipping db migration...")
         }
     }
-    let worker_mode = num_workers > 0;
 
     let (killpill_tx, mut killpill_rx) = tokio::sync::broadcast::channel::<()>(2);
     let mut monitor_killpill_rx = killpill_tx.subscribe();
     let (killpill_phase2_tx, _killpill_phase2_rx) = tokio::sync::broadcast::channel::<()>(2);
-
-    let server_killpill_rx = if worker_mode {
-        killpill_phase2_tx.subscribe()
-    } else {
-        killpill_tx.subscribe()
-    };
+    let server_killpill_rx = killpill_phase2_tx.subscribe();
 
     let shutdown_signal =
         windmill_common::shutdown_signal(killpill_tx.clone(), killpill_tx.subscribe());
@@ -455,6 +449,8 @@ Windmill Community Edition {GIT_VERSION}
             }
         }
     }
+
+    let worker_mode = num_workers > 0;
 
     if server_mode || worker_mode || indexer_mode {
         let port_var = std::env::var("PORT").ok().and_then(|x| x.parse().ok());
@@ -650,9 +646,13 @@ Windmill Community Edition {GIT_VERSION}
                 }
             }
             if killpill_phase2_tx.receiver_count() > 0 {
-                tracing::info!("Starting phase 2 of shutdown");
+                if worker_mode {
+                    tracing::info!("Starting phase 2 of shutdown");
+                }
                 killpill_phase2_tx.send(())?;
-                tracing::info!("Phase 2 of shutdown completed");
+                if worker_mode {
+                    tracing::info!("Phase 2 of shutdown completed");
+                }
             }
             Ok(())
         };
