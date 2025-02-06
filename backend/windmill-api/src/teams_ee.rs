c49cafae95642b0237e423db83e045e47d1d10b5
Alex Petric <petric.al@gmail.com>
2025-02-06 11:57:23 -0500

    ce

--- backend/windmill-api/src/teams_ee.rs
+++ backend/windmill-api/src/teams_ee.rs
@@ -3,18 +3,31 @@ use windmill_common::error::Error;
 
 pub async fn edit_teams_command() -> Result<StatusCode, Error> {
     return Err(Error::BadRequest(
-        "Deploy to is only available on enterprise".to_string(),
+        "Teams only available on enterprise".to_string(),
     ));
 }
 
 pub async fn workspaces_list_available_teams_ids() -> Result<StatusCode, Error> {
     return Err(Error::BadRequest(
-        "Deploy to is only available on enterprise".to_string(),
+        "Teams only available on enterprise".to_string(),
     ));
 }
 
 pub async fn connect_teams() -> Result<StatusCode, Error> {
     return Err(Error::BadRequest(
-        "Deploy to is only available on enterprise".to_string(),
+        "Teams only available on enterprise".to_string(),
     ));
 }
+
+pub async fn run_teams_message_test_job() -> Result<StatusCode, Error> {
+    return Err(Error::BadRequest(
+        "Teams only available on enterprise".to_string(),
+    ));
+}
+
+pub async fn workspaces_list_available_teams_channels() -> Result<StatusCode, Error> {
+    return Err(Error::BadRequest(
+        "Teams only available on enterprise".to_string(),
+    ));
+}
+
