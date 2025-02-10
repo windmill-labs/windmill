8244777d83c8e0c3a93a1117c83932f2f3f3c1bb
dieriba <t.dieriba@gmail.com>
2025-02-10 23:50:16 +0100

    fix: ci build and error compilation

--- backend/windmill-api/src/postgres_triggers/mod.rs
+++ backend/windmill-api/src/postgres_triggers/mod.rs
@@ -15,7 +15,7 @@ use handler::{
     create_template_script, delete_postgres_trigger, delete_publication, drop_slot_name,
     exists_postgres_trigger, get_postgres_trigger, get_publication_info, get_template_script,
     is_database_in_logical_level, list_database_publication, list_postgres_triggers,
-    list_slot_name, set_enabled, update_postgres_trigger, PostgresTrigger,
+    list_slot_name, set_enabled, update_postgres_trigger,
 };
 use windmill_common::{db::UserDB, utils::StripPath};
 use windmill_queue::PushArgsOwned;
