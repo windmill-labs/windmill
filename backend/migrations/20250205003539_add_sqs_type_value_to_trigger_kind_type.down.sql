2679e576e30fb2398543c0faa51a3178666f922e
dieriba <t.dieriba@gmail.com>
2025-02-11 00:16:55 +0100

    update migration type sqs

--- backend/migrations/20250205003539_add_sqs_type_value_to_trigger_kind_type.down.sql
+++ backend/migrations/20250205003539_add_sqs_type_value_to_trigger_kind_type.down.sql
@@ -1,3 +1,1 @@
 -- Add down migration script here
-DROP TYPE TRIGGER_KIND;
-CREATE TYPE TRIGGER_KIND AS ENUM ('webhook', 'http', 'websocket', 'kafka', 'email');
--- backend/migrations/20250205003539_add_sqs_type_value_to_trigger_kind_type.up.sql
+++ backend/migrations/20250205003539_add_sqs_type_value_to_trigger_kind_type.up.sql
@@ -1,2 +1,2 @@
 -- Add up migration script here
-ALTER TYPE TRIGGER_KIND ADD VALUE 'sqs';
+ALTER TYPE TRIGGER_KIND ADD VALUE IF NOT EXISTS 'sqs';
