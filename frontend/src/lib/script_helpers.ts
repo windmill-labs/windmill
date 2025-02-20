482fbdd8ae494318f7b5218770b45014cf2dd04e
pyranota <pyra@duck.com>
2025-02-20 20:28:19 +0300

    add init script

--- frontend/src/lib/script_helpers.ts
+++ frontend/src/lib/script_helpers.ts
@@ -406,10 +406,54 @@ class Script
 }
 `
 
-const NU_INIT_CODE = `
-	def main [] {
-		print "Hello Windmill!"
-	}
+const NU_INIT_CODE = `use std assert
+
+# Nushell
+# A new type of shell
+def main [
+    no_default: string,
+    name = "Nicolas Bourbaki",
+    age: int = 42,
+    date_of_birth?: datetime,
+    obj: record = {records: "included"},
+    l: list<string> = ["or", "lists!"],
+    tables: table<a: string, b: string, c: number> = [
+      { a: "table",   b: "is just a", c: 100 },
+      { a: "list of", b: "records",   c: 999 }
+    ]
+    work_with_bytes: binary = 0x[ffff ffff],
+    enable_kill_mode?: bool = true,
+] {
+    # Test
+    # https://www.nushell.sh/book/testing.html
+		assert ($age == 42)
+
+    print $"Hello World and a warm welcome especially to ($name)"
+    print "and its acolytes.." $age $obj $l
+    print $tables
+
+    let secret = try { 
+      get_variable f/examples/secret
+    } catch { 
+      'No secret yet at f/examples/secret !' 
+    };
+
+    print $"The variable at \`f/examples/secret\`: ($secret)"
+    # fetch context variables
+    let user = $env.WM_USERNAME
+
+    # Nu pipelines
+    ls | where size > 1kb | sort-by modified | print "ls:" $in
+
+    # Nu works with existing data
+    # Nu speaks JSON, YAML, SQLite, Excel, and more out of the box. 
+    # It's easy to bring data into a Nu pipeline whether it's in a file, a database, or a web API:
+    let nu_license = http get https://api.github.com/repos/nushell/nushell | get license
+
+    return { splitted: ($name | split words), user: $user, nu_license: $nu_license}
+    # Interested in learning more?
+    # https://www.nushell.sh/book/getting_started.html
+}
 `
 
 const FETCH_INIT_CODE = `export async function main(
