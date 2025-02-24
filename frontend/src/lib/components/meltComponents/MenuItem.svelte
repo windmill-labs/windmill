d2a18fdb3498c256fc82a9dce81219310813252c
Guilhem <guilhemlemouel@gmail.com>
2025-02-24 14:45:44 +0000

    add local rooting for MenuItem

--- frontend/src/lib/components/meltComponents/MenuItem.svelte
+++ frontend/src/lib/components/meltComponents/MenuItem.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
 	import { melt } from '@melt-ui/svelte'
 	import type { MenubarMenuElements } from '@melt-ui/svelte'
+	import { goto } from '$app/navigation'
+
 	export let href: string | undefined = undefined
 	export let disabled: boolean = false
 	export let target: string | undefined = undefined
 	export let item: MenubarMenuElements['item']
+
+	function handleClick(e: MouseEvent) {
+		if (href && !target) {
+			e.preventDefault()
+			goto(href)
+		}
+	}
 </script>
 
 {#if href}
@@ -16,6 +25,7 @@
 		aria-disabled={disabled}
 		tabindex={disabled ? -1 : undefined}
 		{target}
+		on:click={handleClick}
 		on:m-focusin
 		on:m-focusout
 	>
