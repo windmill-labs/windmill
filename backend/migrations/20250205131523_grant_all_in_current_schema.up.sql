9ae4829295d606399c0ed09b16204b99d1809ce8
Ruben Fiszel <ruben@windmill.dev>
2025-02-27 14:35:32 +0100

    all

--- backend/migrations/20250205131523_grant_all_in_current_schema.up.sql
+++ backend/migrations/20250205131523_grant_all_in_current_schema.up.sql
@@ -9,6 +9,12 @@ BEGIN
     -- Lock the roles table to prevent race conditions
     LOCK TABLE pg_catalog.pg_roles;
 
+
+
+    EXECUTE format('GRANT USAGE ON SCHEMA %I TO windmill_user', current_schema_name);
+    EXECUTE format('GRANT USAGE ON SCHEMA %I TO windmill_admin', current_schema_name);
+
+
     -- Grant privileges dynamically to the current schema
     EXECUTE format('GRANT ALL ON ALL TABLES IN SCHEMA %I TO windmill_user', current_schema_name);
     EXECUTE format('GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA %I TO windmill_user', current_schema_name);
