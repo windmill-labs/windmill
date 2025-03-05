1f69104d1e3cacf7f5eaf5062a40bcf895e94f9e
pyranota <pyra@duck.com>
2025-03-05 13:49:10 +0300

    update ENV_SETTINGS

--- backend/windmill-common/src/global_settings.rs
+++ backend/windmill-common/src/global_settings.rs
@@ -40,7 +40,7 @@ pub const JWT_SECRET_SETTING: &str = "jwt_secret";
 pub const EMAIL_DOMAIN_SETTING: &str = "email_domain";
 pub const OTEL_SETTING: &str = "otel";
 
-pub const ENV_SETTINGS: [&str; 57] = [
+pub const ENV_SETTINGS: [&str; 58] = [
     "DISABLE_NSJAIL",
     "MODE",
     "NUM_WORKERS",
