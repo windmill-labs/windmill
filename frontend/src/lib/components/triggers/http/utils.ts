ade72834ecabb38dcd47437997b523ee2b835c49
dieriba <t.dieriba@gmail.com>
2025-03-13 10:40:26 +0100

    fix: undefined route path

--- frontend/src/lib/components/triggers/http/utils.ts
+++ frontend/src/lib/components/triggers/http/utils.ts
@@ -4,5 +4,5 @@ import { isCloudHosted } from "$lib/cloud";
 export function getHttpRoute(route_path: string, workspaced_route: boolean, workspace_id: string) {
     return `${location.origin}${base}/api/r/${
         isCloudHosted() || workspaced_route ? workspace_id + '/' : ''
-    }${route_path}`
+    }${route_path ?? ''}`
 }
