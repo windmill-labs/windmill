bc351aec38abab901c0d51682257aec24110829f
Diego Imbert <diegoimbert@protonmail.com>
2025-03-19 11:23:32 +0100

    Revert "create new workspace_runnable_dependencies instead of renaming old table"
    This reverts commit 015c38ca8f9fbd8b4a4e817f1ad105434bfd68c5.

--- backend/migrations/20250317145509_more_generic_workspace_runnable_usage.up.sql
+++ backend/migrations/20250317145509_more_generic_workspace_runnable_usage.up.sql
@@ -1,16 +1,13 @@
 -- flow_workspace_runnables only stored runnable usages by
 -- flows although apps can also use runnables
 
-CREATE TABLE workspace_runnable_dependencies (
-    flow_path VARCHAR(255) NULL,
-    app_path VARCHAR(255) NULL,
-    runnable_path VARCHAR(255) NOT NULL,
-    script_hash BIGINT NULL,
-    runnable_is_flow BOOLEAN NOT NULL,
-    workspace_id VARCHAR(50) NOT NULL,
-	FOREIGN KEY (workspace_id, flow_path) REFERENCES flow (workspace_id, path) ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY (workspace_id, app_path) REFERENCES app (workspace_id, path) ON DELETE CASCADE ON UPDATE CASCADE
-);
+ALTER TABLE flow_workspace_runnables
+RENAME TO workspace_runnable_dependencies;
+
+ALTER TABLE workspace_runnable_dependencies ALTER flow_path DROP NOT NULL;
+
+ALTER TABLE workspace_runnable_dependencies 
+ADD COLUMN app_path VARCHAR(255);
 
 ALTER TABLE workspace_runnable_dependencies
 ADD CONSTRAINT workspace_runnable_dependencies_path_exclusive CHECK (
@@ -18,17 +15,22 @@ ADD CONSTRAINT workspace_runnable_dependencies_path_exclusive CHECK (
   (flow_path IS NULL AND app_path IS NOT NULL)
 );
 
-DROP INDEX IF EXISTS workspace_runnable_dependencies_runnable_path_is_flow_idx;
-CREATE INDEX workspace_runnable_dependencies_runnable_path_is_flow_idx ON workspace_runnable_dependencies (runnable_path, runnable_is_flow, workspace_id);
+ALTER TABLE workspace_runnable_dependencies
+ADD CONSTRAINT fk_workspace_runnable_dependencies_app_path
+FOREIGN KEY (app_path, workspace_id) REFERENCES app (path, workspace_id) 
+ON DELETE CASCADE 
+ON UPDATE CASCADE;
 
--- Prevent duplicate dependencies from same flow
-DROP INDEX IF EXISTS workspace_runnable_dependencies_without_hash_unique_idx;
-DROP INDEX IF EXISTS workspace_runnable_dependencies_with_hash_unique_idx;
-CREATE UNIQUE INDEX workspace_runnable_dependencies_without_hash_unique_idx ON workspace_runnable_dependencies (flow_path, runnable_path, runnable_is_flow, workspace_id) WHERE script_hash IS NULL;
-CREATE UNIQUE INDEX workspace_runnable_dependencies_with_hash_unique_idx ON workspace_runnable_dependencies (flow_path, runnable_path, script_hash, runnable_is_flow, workspace_id) WHERE script_hash IS NOT NULL;
 
--- Prevent duplicate dependencies from same app
-DROP INDEX IF EXISTS workspace_runnable_dependencies_without_hash_unique_idx;
-DROP INDEX IF EXISTS workspace_runnable_dependencies_with_hash_unique_idx;
-CREATE UNIQUE INDEX workspace_runnable_dependencies_without_hash_unique_idx ON workspace_runnable_dependencies (app_path, runnable_path, runnable_is_flow, workspace_id) WHERE script_hash IS NULL;
-CREATE UNIQUE INDEX workspace_runnable_dependencies_with_hash_unique_idx ON workspace_runnable_dependencies (app_path, runnable_path, script_hash, runnable_is_flow, workspace_id) WHERE script_hash IS NOT NULL;
+ALTER TABLE workspace_runnable_dependencies
+DROP CONSTRAINT flow_workspace_runnables_workspace_id_flow_path_fkey;
+
+ALTER TABLE workspace_runnable_dependencies
+ADD CONSTRAINT flow_workspace_runnables_workspace_id_flow_path_fkey
+FOREIGN KEY (flow_path, workspace_id) REFERENCES flow (path, workspace_id) 
+ON DELETE CASCADE 
+ON UPDATE CASCADE;
+
+
+CREATE UNIQUE INDEX app_workspace_without_hash_unique_idx ON workspace_runnable_dependencies (app_path, runnable_path, runnable_is_flow, workspace_id) WHERE script_hash IS NULL;
+CREATE UNIQUE INDEX app_workspace_with_hash_unique_idx ON workspace_runnable_dependencies (app_path, runnable_path, script_hash, runnable_is_flow, workspace_id) WHERE script_hash IS NOT NULL;
