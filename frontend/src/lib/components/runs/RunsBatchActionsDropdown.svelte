c192872bfacf3333f66930f0ca53952bb88e7e2b
Diego Imbert <diegoimbert@protonmail.com>
2025-03-19 17:50:48 +0100

    started BatchReRunOptionsPane

--- frontend/src/lib/components/runs/RunsBatchActionsDropdown.svelte
+++ frontend/src/lib/components/runs/RunsBatchActionsDropdown.svelte
@@ -13,6 +13,8 @@
 	export let onSetSelectionMode: (mode: RunsSelectionMode | false) => void
 	export let onCancelSelectedJobs: () => void
 	export let onCancelFilteredJobs: () => void
+	export let onReRunSelectedJobs: () => void
+	export let onReRunFilteredJobs: () => void
 
 	function jobCountString(count: number) {
 		return `${count} ${count == 1 ? 'job' : 'jobs'}`
@@ -28,35 +30,31 @@
 			variant="contained"
 			on:click={() => onSetSelectionMode(false)}
 		/>
-		<Button
-			disabled={selectionCount == 0}
-			startIcon={{ icon: Check }}
-			size="xs"
-			color="red"
-			variant="contained"
-			on:click={onCancelSelectedJobs}
-		>
-			Cancel {jobCountString(selectionCount)}
-		</Button>
-	</div>
-{:else if !$userStore?.is_admin && !$superadmin}
-	<DropdownV2
-		items={[
-			{
-				displayName: 'Select jobs to cancel',
-				action: () => onSetSelectionMode('cancel')
-			}
-		]}
-	>
-		<svelte:fragment slot="buttonReplacement">
-			<div
-				class="mt-1 p-2 h-8 flex flex-row items-center hover:bg-surface-hover cursor-pointer rounded-md"
+		{#if selectionMode == 'cancel'}
+			<Button
+				disabled={selectionCount == 0}
+				startIcon={{ icon: Check }}
+				size="xs"
+				color="red"
+				variant="contained"
+				on:click={onCancelSelectedJobs}
 			>
-				<span class="text-xs min-w-[5rem]">Cancel jobs</span>
-				<ChevronDown class="w-5 h-5" />
-			</div>
-		</svelte:fragment>
-	</DropdownV2>
+				Cancel {jobCountString(selectionCount)}
+			</Button>
+		{/if}
+		{#if selectionMode == 're-run'}
+			<Button
+				disabled={selectionCount == 0}
+				startIcon={{ icon: Check }}
+				size="xs"
+				color="red"
+				variant="contained"
+				on:click={onReRunSelectedJobs}
+			>
+				Re-run {jobCountString(selectionCount)}
+			</Button>
+		{/if}
+	</div>
 {:else}
 	<DropdownV2
 		items={[
@@ -64,14 +62,23 @@
 				displayName: 'Select jobs to cancel',
 				action: () => onSetSelectionMode('cancel')
 			},
-			{ displayName: 'Cancel all jobs matching filters', action: onCancelFilteredJobs }
+			...($userStore?.is_admin || $superadmin
+				? [{ displayName: 'Cancel all jobs matching filters', action: onCancelFilteredJobs }]
+				: []),
+			{
+				displayName: 'Select jobs to re-run',
+				action: () => onSetSelectionMode('re-run')
+			},
+			...($userStore?.is_admin || $superadmin
+				? [{ displayName: 'Re-run all jobs matching filters', action: onReRunFilteredJobs }]
+				: [])
 		]}
 	>
 		<svelte:fragment slot="buttonReplacement">
 			<div
 				class="mt-1 p-2 h-8 flex flex-row items-center hover:bg-surface-hover cursor-pointer rounded-md"
 			>
-				<span class="text-xs min-w-[5rem]">Cancel jobs</span>
+				<span class="text-xs min-w-[5rem]">Batch actions</span>
 				<ChevronDown class="w-5 h-5" />
 			</div>
 		</svelte:fragment>
