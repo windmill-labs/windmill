6efe43aa004996b3240ce2b4491521bff0eae935
Guilhem <guilhemlemouel@gmail.com>
2025-03-31 01:30:24 +0100

    Revert unused change

--- frontend/src/lib/components/FlowHistoryJobPicker.svelte
+++ frontend/src/lib/components/FlowHistoryJobPicker.svelte
@@ -5,15 +5,10 @@
 	import HistoricInputs from './HistoricInputs.svelte'
 	import { workspaceStore } from '$lib/stores'
 	import { JobService } from '$lib/gen'
-	import Button from '$lib/components/common/button/Button.svelte'
 
 	export let path: string
 	export let selected: string | undefined = undefined
 	const dispatch = createEventDispatcher()
-	export let floatingConfig: any | undefined = undefined
-	export let height: number | undefined = undefined
-	export let contentClasses: string | undefined = undefined
-
 
 	async function loadInitial() {
 		let jobs = await JobService.listJobs({
@@ -33,23 +28,12 @@
 	$: $workspaceStore && loadInitial()
 </script>
 
-<PopoverV2 closeButton={false} {floatingConfig} usePointerDownOutside {contentClasses}>
+<PopoverV2 closeButton={false}>
 	<svelte:fragment slot="trigger">
-		<Button
-			color="light"
-			size="xs2"
-			variant="contained"
-			btnClasses="bg-transparent"
-			iconOnly
-			nonCaptureEvent
-			startIcon={{ icon: HistoryIcon }}
-		/>
+		<HistoryIcon size={14} />
 	</svelte:fragment>
 	<svelte:fragment slot="content">
-		<div
-			class="p-2 {height ? '' : 'h-[400px]'} overflow-hidden w-80 shadow-sm"
-			style={`height: ${height}px`}
-		>
+		<div class="p-2 h-[400px] overflow-hidden w-80 border shadow-sm">
 			<HistoricInputs
 				on:select={(e) => {
 					if (e.detail) {
