ef4472cfa0afa38258ece7d754189a710b48a91b
Guilhem <guilhemlemouel@gmail.com>
2025-03-31 17:07:20 +0100

    add Scroll component

---
+++ frontend/src/lib/components/Scroll.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { twMerge } from 'tailwind-merge'
+</script>
+
+<div class={twMerge('h-full w-full overflow-y-auto', $$props.class)}>
+	<slot />
+</div>
+
+<style>
+	/* Custom scrollbar styles */
+	div::-webkit-scrollbar {
+		width: 12px;
+	}
+
+	/* For Firefox */
+	div {
+		/* Prevent layout shift from scrollbar appearance */
+		scrollbar-gutter: stable;
+		/* Prevent horizontal scrollbar */
+		overflow-x: hidden;
+	}
+</style>
