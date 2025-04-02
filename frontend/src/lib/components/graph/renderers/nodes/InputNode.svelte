de16663201a6f0b1e5a76f8f5a0264aa2b9c39e0
Guilhem <guilhemlemouel@gmail.com>
2025-04-02 23:03:49 +0100

    fix flow input not updating properly

--- frontend/src/lib/components/graph/renderers/nodes/InputNode.svelte
+++ frontend/src/lib/components/graph/renderers/nodes/InputNode.svelte
@@ -7,7 +7,6 @@
 	import { getContext } from 'svelte'
 	import type { Writable } from 'svelte/store'
 	import InsertModuleButton from '$lib/components/flows/map/InsertModuleButton.svelte'
-	import type { PropPickerContext } from '$lib/components/prop_picker'
 	import { schemaToObject } from '$lib/schema'
 	import type { Schema } from '$lib/common'
 	import type { FlowEditorContext } from '$lib/components/flows/types'
@@ -33,21 +32,10 @@
 	const { previewArgs, flowStore } =
 		getContext<FlowEditorContext | undefined>('FlowEditorContext') || {}
 
-	const propPickerContext = getContext<PropPickerContext>('PropPickerContext')
-	const pickablePropertiesFiltered = propPickerContext?.pickablePropertiesFiltered
-	const topFlowInput = schemaToObject($flowStore?.schema as Schema, $previewArgs ?? {})
-
-	function filterIterFromInput(inputJson: Record<string, any> | undefined): Record<string, any> {
-		if (!inputJson || typeof inputJson !== 'object') return {}
-
-		const newJson = { ...inputJson }
-		delete newJson.iter
-		delete newJson.iter_parent
-
-		return newJson
-	}
-
-	$: filteredInput = filterIterFromInput($pickablePropertiesFiltered?.flow_input)
+	$: topFlowInput =
+		flowStore && previewArgs && $flowStore?.schema
+			? schemaToObject($flowStore.schema as Schema, $previewArgs || {})
+			: undefined
 </script>
 
 <NodeWrapper let:darkMode>
@@ -91,7 +79,7 @@
 		on:select={(e) => {
 			data.eventHandlers?.select(e.detail)
 		}}
-		inputJson={filteredInput ?? topFlowInput}
+		inputJson={topFlowInput}
 		prefix="flow_input"
 		alwaysPluggable
 		cache={data.cache}
