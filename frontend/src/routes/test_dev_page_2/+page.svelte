42cf713ddc92563c6d1955d403a3b59a932f4a12
Guilhem <guilhemlemouel@gmail.com>
2025-04-02 16:50:00 +0100

    revert test page

--- frontend/src/routes/test_dev_page_2/+page.svelte
+++ frontend/src/routes/test_dev_page_2/+page.svelte
@@ -1,9 +1,4 @@
 <script lang="ts">
 	import { initializeMode } from 'monaco-graphql/esm/initializeMode.js'
-	import DynamicContainer from '$lib/components/DynamicContainer.svelte'
 	initializeMode()
 </script>
-
-<div class="p-8">
-	<DynamicContainer />
-</div>
