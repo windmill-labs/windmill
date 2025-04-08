42ccd0f884bc2cd7c8453de9a985e8d813b665bf
Diego Imbert <diegoimbert@protonmail.com>
2025-04-08 10:44:01 +0200

    fix height

--- frontend/src/lib/components/DBTable.svelte
+++ frontend/src/lib/components/DBTable.svelte
@@ -155,76 +155,77 @@
 
 <DarkModeObserver bind:darkMode />
 
-<div class="flex py-2 h-12 justify-between gap-4">
-	<DebouncedInput
-		class="w-full max-w-[300px]"
-		type="text"
-		bind:value={quicksearch}
-		placeholder="Search..."
-	/>
-	<div class="flex flex-row gap-2">
-		<Button
-			startIcon={{ icon: Plus }}
-			color="dark"
-			size="xs2"
-			on:click={() => {
-				// args = {}
-				// insertDrawer?.openDrawer()
-			}}
-		>
-			Insert
-		</Button>
-	</div>
-</div>
-
-<div
-	class={'flex flex-col h-full component-wrapper divide-y wm-aggrid-container'}
-	bind:clientHeight
-	bind:clientWidth
->
-	<div
-		style:height="{clientHeight}px"
-		style:width="{clientWidth}px"
-		class="ag-theme-alpine"
-		class:ag-theme-alpine-dark={darkMode}
-	>
-		<!-- svelte-ignore a11y_no_static_element_interactions -->
-		<div
-			bind:this={eGui}
-			style:height="100%"
-			onkeydown={(e) => {
-				if ((e.ctrlKey || e.metaKey) && e.key === 'c') {
-					const selectedCell = api?.getFocusedCell()
-					if (selectedCell) {
-						const rowIndex = selectedCell.rowIndex
-						const colId = selectedCell.column?.getId()
-						const rowNode = api?.getDisplayedRowAtIndex(rowIndex)
-						const selectedValue = rowNode?.data?.[colId]
-						navigator.clipboard.writeText(selectedValue)
-						sendUserToast('Copied cell value to clipboard', false)
-					}
-				}
-			}}
-		></div>
-	</div>
-
-	<div class="flex gap-1 w-full justify-between items-center text-xs text-primary p-2">
-		<div>
-			<Popover>
-				<svelte:fragment slot="text">Download</svelte:fragment>
-				<Button
-					startIcon={{ icon: Download }}
-					color="light"
-					size="xs2"
-					on:click={() => api?.exportDataAsCsv()}
-					iconOnly
-				/>
-			</Popover>
+<div class="h-full relative flex flex-col">
+	<div class="flex py-2 h-12 justify-between gap-4">
+		<DebouncedInput
+			class="w-full max-w-[300px]"
+			type="text"
+			bind:value={quicksearch}
+			placeholder="Search..."
+		/>
+		<div class="flex flex-row gap-2">
+			<Button
+				startIcon={{ icon: Plus }}
+				color="dark"
+				size="xs2"
+				on:click={() => {
+					// args = {}
+					// insertDrawer?.openDrawer()
+				}}
+			>
+				Insert
+			</Button>
 		</div>
-		{#if rowCount}
-			{firstRow}{'->'}{lastRow + 1} of {rowCount} rows
-		{:else}
-			{firstRow}{'->'}{lastRow + 1}
-		{/if}
+	</div>
+	<div
+		class={'flex flex-col flex-1 component-wrapper divide-y wm-aggrid-container'}
+		bind:clientHeight
+		bind:clientWidth
+	>
+		<div
+			style:height="{clientHeight}px"
+			style:width="{clientWidth}px"
+			class="ag-theme-alpine"
+			class:ag-theme-alpine-dark={darkMode}
+		>
+			<!-- svelte-ignore a11y_no_static_element_interactions -->
+			<div
+				bind:this={eGui}
+				style:height="100%"
+				onkeydown={(e) => {
+					if ((e.ctrlKey || e.metaKey) && e.key === 'c') {
+						const selectedCell = api?.getFocusedCell()
+						if (selectedCell) {
+							const rowIndex = selectedCell.rowIndex
+							const colId = selectedCell.column?.getId()
+							const rowNode = api?.getDisplayedRowAtIndex(rowIndex)
+							const selectedValue = rowNode?.data?.[colId]
+							navigator.clipboard.writeText(selectedValue)
+							sendUserToast('Copied cell value to clipboard', false)
+						}
+					}
+				}}
+			></div>
+		</div>
+
+		<div class="flex gap-1 w-full justify-between items-center text-xs text-primary p-2">
+			<div>
+				<Popover>
+					<svelte:fragment slot="text">Download</svelte:fragment>
+					<Button
+						startIcon={{ icon: Download }}
+						color="light"
+						size="xs2"
+						on:click={() => api?.exportDataAsCsv()}
+						iconOnly
+					/>
+				</Popover>
+			</div>
+			{#if rowCount}
+				{firstRow}{'->'}{lastRow + 1} of {rowCount} rows
+			{:else}
+				{firstRow}{'->'}{lastRow + 1}
+			{/if}
+		</div>
 	</div>
 </div>
