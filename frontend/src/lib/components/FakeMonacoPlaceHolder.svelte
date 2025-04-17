2183c15267d6b7c3e67f54374838ae2de34217cb
Diego Imbert <diego@windmill.dev>
2025-04-17 14:53:02 +0200

    os dependant constants in monaco

--- frontend/src/lib/components/FakeMonacoPlaceHolder.svelte
+++ frontend/src/lib/components/FakeMonacoPlaceHolder.svelte
@@ -1,6 +1,8 @@
 <!-- Used to avoid height jitter when loading monaco asynchronously -->
 
 <script lang="ts">
+	import { getOS } from '$lib/utils'
+
 	type Props = {
 		code?: string
 		autoheight?: boolean
@@ -19,10 +21,26 @@
 		fontSize = 14
 	}: Props = $props()
 
+	// https://github.com/microsoft/vscode/blob/baa2dad3cdacd97ac02eff0604984faf1167ff1e/src/vs/editor/common/config/editorOptions.ts#L5421
+	const DEFAULT_WINDOWS_FONT_FAMILY = "Consolas, 'Courier New', monospace"
+	const DEFAULT_MAC_FONT_FAMILY = "Menlo, Monaco, 'Courier New', monospace"
+	const DEFAULT_LINUX_FONT_FAMILY = "'Droid Sans Mono', 'monospace', monospace"
+	const fontFamily =
+		getOS() === 'Windows'
+			? DEFAULT_WINDOWS_FONT_FAMILY
+			: getOS() === 'macOS'
+				? DEFAULT_MAC_FONT_FAMILY
+				: DEFAULT_LINUX_FONT_FAMILY
+
+	// https://github.com/microsoft/vscode/blob/baa2dad3cdacd97ac02eff0604984faf1167ff1e/src/vs/editor/common/config/fontInfo.ts#L14
+	const GOLDEN_LINE_HEIGHT_RATIO = getOS() == 'macOS' ? 1.5 : 1.35
+
 	let lines = $derived(code?.split('\n') ?? [])
 
 	const charWidth = 9 // try to match as closely as possible to monaco editor
-	const lineHeight = fontSize * 1.5
+
+	const lineHeight = fontSize * GOLDEN_LINE_HEIGHT_RATIO
+
 	let [clientWidth, clientHeight] = $state([0, 0])
 	let showHorizontalScrollbar = $derived(
 		lines.some((line) => line.length * charWidth > clientWidth - 40)
@@ -40,7 +58,7 @@
 	bind:clientWidth
 	bind:clientHeight
 	class="h-full w-full relative editor dark:bg-[#272D38] {className}"
-	style="--vscode-editorCodeLens-lineHeight: 18px; --vscode-editorCodeLens-fontSize: 12px; --vscode-editorCodeLens-fontFeatureSettings: 'liga' off, 'calt' off; --code-editorInlayHintsFontFamily: Menlo, Monaco, 'Courier New', monospace;"
+	style="--vscode-editorCodeLens-lineHeight: 18px; --vscode-editorCodeLens-fontSize: 12px; --vscode-editorCodeLens-fontFeatureSettings: 'liga' off, 'calt' off; --code-editorInlayHintsFontFamily: {fontFamily};"
 >
 	<div
 		class="monaco-editor no-user-select mac standalone showUnused showDeprecated vs-dark"
@@ -57,7 +75,7 @@
 					class="margin-view-overlays"
 					role="presentation"
 					aria-hidden="true"
-					style="position: absolute; font-family: Menlo, Monaco, 'Courier New', monospace; font-weight: normal; font-size: {fontSize}px; font-feature-settings: 'liga' 0, 'calt' 0; font-variation-settings: normal; line-height: {lineHeight}px; letter-spacing: 0px; width: {lineNumbersWidth}px; height: 4893px;"
+					style="position: absolute; font-family: {fontFamily}; font-weight: normal; font-size: {fontSize}px; font-feature-settings: 'liga' 0, 'calt' 0; font-variation-settings: normal; line-height: {lineHeight}px; letter-spacing: 0px; width: {lineNumbersWidth}px; height: 4893px;"
 				>
 					{#each lines as _, i}
 						<div style="top:{lineHeight * i}px;height:{lineHeight}px;">
@@ -76,7 +94,7 @@
 				>
 					<div
 						class="view-lines monaco-mouse-cursor-text text-tertiary/60"
-						style="line-height: {lineHeight}px; position: absolute; font-family: Menlo, Monaco, 'Courier New', monospace; font-weight: normal; font-size: {fontSize}px; font-feature-settings: 'liga' 0, 'calt' 0; font-variation-settings: normal; line-height: {lineHeight}px; letter-spacing: 0px; width: 1143px; height: 789px;"
+						style="line-height: {lineHeight}px; position: absolute; font-family: {fontFamily}; font-weight: normal; font-size: {fontSize}px; font-feature-settings: 'liga' 0, 'calt' 0; font-variation-settings: normal; line-height: {lineHeight}px; letter-spacing: 0px; width: 1143px; height: 789px;"
 					>
 						{#each lines as line, i}
 							<div
