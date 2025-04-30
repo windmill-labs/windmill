7c4c838043959efb6604f8ab2763b4b814ddf609
Guilhem <guilhemlemouel@gmail.com>
2025-04-30 08:57:39 +0100

    use trigger label for trigger badge dropdown

--- frontend/src/lib/components/graph/renderers/triggers/TriggersBadgeV2.svelte
+++ frontend/src/lib/components/graph/renderers/triggers/TriggersBadgeV2.svelte
@@ -12,6 +12,7 @@
 	import { Menu, Menubar, MeltButton, MenuItem, Tooltip } from '$lib/components/meltComponents'
 	import { twMerge } from 'tailwind-merge'
 	import SchedulePollIcon from '$lib/components/icons/SchedulePollIcon.svelte'
+	import TriggerLabel from '$lib/components/triggers/TriggerLabel.svelte'
 
 	const { selectedTriggerV2, triggersCount } = getContext<TriggerContext>('TriggerContext')
 
@@ -244,24 +245,6 @@
 			dispatch('select', trigger)
 		}}
 	>
-		<span class={trigger.isDraft ? 'text-frost-400 italic' : ''}>
-			{trigger.isDraft ? `New ${trigger.type.replace(/s$/, '')} trigger` : trigger.path}
-		</span>
-
-		{#if trigger.isPrimary}
-			<span
-				class="ml-2 bg-blue-50 dark:bg-blue-900/40 px-1.5 py-0.5 rounded text-xs text-blue-700 dark:text-blue-100"
-			>
-				Primary
-			</span>
-		{/if}
-
-		{#if trigger.isDraft}
-			<span
-				class="ml-2 text-2xs bg-frost-100 dark:bg-frost-900 text-frost-800 dark:text-frost-100 px-1.5 py-0.5 rounded"
-			>
-				Draft
-			</span>
-		{/if}
+		<TriggerLabel {trigger} />
 	</MenuItem>
 {/snippet}
