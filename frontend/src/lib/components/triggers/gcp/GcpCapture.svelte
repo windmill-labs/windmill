bac5b842ddb0c58356f9c63ebbfb4db56d917826
Guilhem <guilhemlemouel@gmail.com>
2025-05-12 13:45:35 +0200

    fix gcp capture

--- frontend/src/lib/components/triggers/gcp/GcpCapture.svelte
+++ frontend/src/lib/components/triggers/gcp/GcpCapture.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import type { CaptureInfo } from '../CaptureSection.svelte'
 	import CaptureSection from '../CaptureSection.svelte'
+	import { base } from '$lib/base'
+	import { workspaceStore } from '$lib/stores'
+	import { Url } from '$lib/components/common'
 
 	export let captureInfo: CaptureInfo | undefined = undefined
 	export let isValid: boolean | undefined = undefined
 	export let hasPreprocessor: boolean = false
 	export let isFlow: boolean = false
+	export let deliveryType: 'push' | 'pull' = 'pull'
+
+	function getCaptureUrl(captureInfo: CaptureInfo | undefined) {
+		if (!captureInfo) {
+			return
+		}
+		return `${window.location.origin}${base}/api/w/${$workspaceStore}/capture_u/gcp/${
+			captureInfo?.isFlow ? 'flow' : 'script'
+		}${captureInfo?.path} `
+	}
 </script>
 
 {#if captureInfo}
@@ -28,5 +41,10 @@
 				Start capturing to listen to GCP Pub/Sub events.
 			{/if}
 		</svelte:fragment>
+
+		{#if deliveryType === 'push'}
+			{@const captureUrl = getCaptureUrl(captureInfo)}
+			<Url url={captureUrl} label="Test URL" />
+		{/if}
 	</CaptureSection>
 {/if}
