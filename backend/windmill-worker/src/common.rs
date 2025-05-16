6899dff147d8650da9839e2f366c8287524457aa
Diego Imbert <diego@windmill.dev>
2025-05-16 18:14:43 +0200

    fix upload_s3_file not checking response

--- backend/windmill-worker/src/common.rs
+++ backend/windmill-worker/src/common.rs
@@ -1591,7 +1591,7 @@ pub struct S3ModeWorkerData {
 }
 
 impl S3ModeWorkerData {
-    pub async fn upload<S>(&self, stream: S) -> error::Result<reqwest::Response>
+    pub async fn upload<S>(&self, stream: S) -> error::Result<()>
     where
         S: futures::stream::TryStream + Send + 'static,
         S::Error: Into<Box<dyn std::error::Error + Send + Sync>>,
