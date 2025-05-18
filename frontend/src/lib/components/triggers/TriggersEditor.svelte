c4d36b3ed40d0b78d257fad770af934647fc914d
Guilhem <guilhemlemouel@gmail.com>
2025-05-18 22:12:58 +0200

    migrate TriggersEditor to svelte 5

--- frontend/src/lib/components/triggers/TriggersEditor.svelte
+++ frontend/src/lib/components/triggers/TriggersEditor.svelte
@@ -24,38 +24,54 @@
 		fetchSqsTriggers,
 		fetchMqttTriggers,
 		addDraftTrigger,
-		type Trigger,
 		triggerTypeToCaptureKind,
 		deleteTrigger,
 		type TriggerType
 	} from './utils'
-	import SchedulePanel from '../SchedulePanel.svelte'
 
-	export let noEditor: boolean
-	export let newItem = false
-	export let currentPath: string
-	export let fakeInitialPath: string = ''
-	export let hash: string | undefined = undefined
-	export let args: Record<string, any> = {}
-	export let initialPath: string
-	export let isFlow: boolean
-	export let canHavePreprocessor: boolean = false
-	export let hasPreprocessor: boolean = false
-	export let isDeployed: boolean = false
-	export let schema: Record<string, any> | undefined = undefined
-	export let noCapture: boolean = false
-	export let isEditor: boolean = true
+	interface Props {
+		noEditor: boolean
+		newItem?: boolean
+		currentPath: string
+		fakeInitialPath?: string
+		hash?: string | undefined
+		args?: Record<string, any>
+		initialPath: string
+		isFlow: boolean
+		canHavePreprocessor?: boolean
+		hasPreprocessor?: boolean
+		isDeployed?: boolean
+		schema?: Record<string, any> | undefined
+		noCapture?: boolean
+		isEditor?: boolean
+	}
 
-	let config: Record<string, any> = {}
-	let editTrigger: Trigger | undefined = undefined
-	let useVerticalTriggerBar = true
-	let width = 0
-	let emailDomain: string | undefined = undefined
-	let isValid = false
-	let renderCount = 0
+	let {
+		noEditor,
+		newItem = false,
+		currentPath,
+		fakeInitialPath = '',
+		hash = undefined,
+		args = {},
+		initialPath,
+		isFlow,
+		canHavePreprocessor = false,
+		hasPreprocessor = false,
+		isDeployed = false,
+		schema = undefined,
+		noCapture = false,
+		isEditor = true
+	}: Props = $props()
+
+	let config: Record<string, any> = $state({})
+	let width = $state(0)
+	let emailDomain: string | undefined = $state(undefined)
+	let isValid = $state(false)
+	let renderCount = $state(0)
+
+	const useVerticalTriggerBar = $derived(width < 1000)
 
 	const {
-		simplifiedPoll,
 		selectedTrigger: selectedTrigger,
 		triggers,
 		triggersCount
@@ -86,12 +102,13 @@
 	}
 
 	async function handleUpdate(trigger: number | undefined, path: string) {
-		if (!trigger) {
+		if (!trigger || trigger === -1) {
 			return
 		}
 
 		const triggerType = $triggers[trigger].type
 		//delete the trigger from the store
+		$selectedTrigger = undefined
 		deleteTrigger(triggers, triggersCount, trigger)
 
 		if (triggerType === 'schedule') {
@@ -173,9 +190,13 @@
 		$selectedTrigger = $triggers.findIndex((t) => t.path === path && t.type === triggerType)
 	}
 
-	function handleUpdateDraftConfig(newConfig: Record<string, any>, canSave: boolean) {
-		if ($selectedTrigger && newConfig) {
-			$triggers[$selectedTrigger].draftConfig = { ...newConfig, canSave }
+	function handleUpdateDraftConfig(
+		triggerIndex: number | undefined,
+		newConfig: Record<string, any>,
+		saveDisabled: boolean
+	) {
+		if (triggerIndex && triggerIndex !== -1 && newConfig) {
+			$triggers[triggerIndex].draftConfig = { ...newConfig, canSave: !saveDisabled }
 		}
 	}
 
@@ -196,157 +217,128 @@
 		)
 		$selectedTrigger = newTrigger
 	}
-
-	$: useVerticalTriggerBar = width < 1000
 </script>
 
 <FlowCard {noEditor} noHeader bind:width>
-	{#if !$simplifiedPoll}
-		<Splitpanes horizontal>
-			<Pane>
-				<div class="flex flex-row h-full">
-					<!-- Left Pane - Triggers List -->
-					{#if !useVerticalTriggerBar}
-						<div class="w-[350px] flex-shrink-0 overflow-auto pr-2 pl-4 pt-2 pb-2">
-							<TriggersTable
-								selectedTrigger={$selectedTrigger}
-								{onSelect}
-								triggers={$triggers}
-								{isEditor}
-								onAddDraftTrigger={handleAddTrigger}
-								onDeleteDraft={deleteDraftTrigger}
-								onReset={handleResetDraft}
-							/>
-						</div>
-					{:else}
-						<div class="p-2 flex flex-col gap-2 border-r">
-							<AddTriggersButton
-								onAddDraftTrigger={handleAddTrigger}
-								class="w-fit h-fit"
-								placement="right-start"
-							>
-								<Button size="xs" nonCaptureEvent btnClasses="p-2 w-fit" wrapperClasses="p-0">
-									<Plus size="14" />
-								</Button>
-							</AddTriggersButton>
-							<TriggersBadge
-								showOnlyWithCount={false}
-								path={initialPath || fakeInitialPath}
-								{newItem}
-								isFlow
-								selected={true}
-								small={false}
-								vertical
-								onSelect={(triggerIndex: number) => ($selectedTrigger = triggerIndex)}
-							/>
-						</div>
-					{/if}
-
-					<div
-						class={twMerge(
-							'flex-grow overflow-auto pl-2 pr-4 pb-4 pt-2',
-							useVerticalTriggerBar ? 'pl-4 pt-2' : ''
-						)}
-						style="scrollbar-gutter: stable"
-					>
-						{#if $selectedTrigger}
-							{@const selected = $triggers[$selectedTrigger]}
-							{#key [selected.id || selected.path, renderCount].join('-')}
-								<div in:fade={{ duration: 100, delay: 100 }} out:fade={{ duration: 100 }}>
-									<TriggersWrapperV2
-										selectedTrigger={selected}
-										{isFlow}
-										{initialPath}
-										{fakeInitialPath}
-										{currentPath}
-										{hash}
-										{isDeployed}
-										small={useVerticalTriggerBar}
-										{args}
-										{newItem}
-										{schema}
-										{isEditor}
-										onDelete={() => {
-											deleteDraftTrigger($selectedTrigger)
-										}}
-										onUpdate={(path) => {
-											handleUpdate($selectedTrigger, path)
-										}}
-										onConfigChange={(cfg, canSave, updated) => {
-											if (updated) {
-												handleUpdateDraftConfig(cfg, canSave)
-											}
-										}}
-										onCaptureConfigChange={(cfg, isValidConfig) => {
-											config = cfg
-											isValid = isValidConfig
-										}}
-										onReset={() => {
-											handleResetDraft($selectedTrigger)
-										}}
-										on:email-domain={({ detail }) => {
-											emailDomain = detail
-										}}
-									/>
-								</div>
-							{/key}
-						{:else}
-							<span class="text-sm text-tertiary text-center mx-auto mt-2"
-								>{`Select a trigger from the ${useVerticalTriggerBar ? 'left toolbar' : 'table'} or a create a new one`}</span
-							>
-						{/if}
-					</div>
-				</div>
-			</Pane>
-			{#if $selectedTrigger && $triggers[$selectedTrigger].type && $triggers[$selectedTrigger].type !== 'schedule' && $triggers[$selectedTrigger].type != 'poll' && !noCapture}
-				{@const captureKind = triggerTypeToCaptureKind($triggers[$selectedTrigger].type)}
-				{#key captureKind}
-					<Pane minSize={20} size={40}>
-						<CaptureWrapper
-							path={initialPath || fakeInitialPath}
-							{isFlow}
-							captureType={captureKind}
-							{hasPreprocessor}
-							{canHavePreprocessor}
-							args={config}
-							data={{ args, hash, emailDomain }}
-							{isValid}
-							on:applyArgs
-							on:updateSchema
-							on:addPreprocessor
-							on:testWithArgs
+	<Splitpanes horizontal>
+		<Pane>
+			<div class="flex flex-row h-full">
+				<!-- Left Pane - Triggers List -->
+				{#if !useVerticalTriggerBar}
+					<div class="w-[350px] flex-shrink-0 overflow-auto pr-2 pl-4 pt-2 pb-2">
+						<TriggersTable
+							selectedTrigger={$selectedTrigger}
+							{onSelect}
+							triggers={$triggers}
+							{isEditor}
+							onAddDraftTrigger={handleAddTrigger}
+							onDeleteDraft={deleteDraftTrigger}
+							onReset={handleResetDraft}
 						/>
-					</Pane>
-				{/key}
-			{/if}
-		</Splitpanes>
-	{:else}
-		{@const selected = $triggers.findIndex((t) => t.isPrimary)}
-		<div class="px-4 py-2">
-			{#if selected}
-				<SchedulePanel
-					{isFlow}
-					path={initialPath || fakeInitialPath}
-					selectedTrigger={selected}
-					{isDeployed}
-					defaultValues={$triggers[selected].draftConfig ??
-						$triggers[selected].captureConfig ??
-						undefined}
-					newDraft={$triggers[selected].draftConfig === undefined}
-					edit={editTrigger === selected}
-					{schema}
-					{isEditor}
-					onDelete={() => {
-						deleteDraftTrigger(selected)
-					}}
-					onUpdate={({ detail }) => {
-						handleUpdate(selected, detail)
-					}}
-					onReset={() => {
-						handleResetDraft(selected)
-					}}
-				/>
-			{/if}
-		</div>
-	{/if}
+					</div>
+				{:else}
+					<div class="p-2 flex flex-col gap-2 border-r">
+						<AddTriggersButton
+							onAddDraftTrigger={handleAddTrigger}
+							class="w-fit h-fit"
+							placement="right-start"
+						>
+							<Button size="xs" nonCaptureEvent btnClasses="p-2 w-fit" wrapperClasses="p-0">
+								<Plus size="14" />
+							</Button>
+						</AddTriggersButton>
+						<TriggersBadge
+							showOnlyWithCount={false}
+							path={initialPath || fakeInitialPath}
+							{newItem}
+							isFlow
+							selected={true}
+							small={false}
+							vertical
+							onSelect={(triggerIndex: number) => ($selectedTrigger = triggerIndex)}
+						/>
+					</div>
+				{/if}
+
+				<div
+					class={twMerge(
+						'flex-grow overflow-auto pl-2 pr-4 pb-4 pt-2',
+						useVerticalTriggerBar ? 'pl-4 pt-2' : ''
+					)}
+					style="scrollbar-gutter: stable"
+				>
+					{#if $selectedTrigger}
+						{#each $triggers as trigger, index}
+							{#key renderCount}
+								{#if $selectedTrigger === index}
+									<div in:fade={{ duration: 100, delay: 100 }} out:fade={{ duration: 100 }}>
+										<TriggersWrapperV2
+											selectedTrigger={trigger}
+											{isFlow}
+											{initialPath}
+											{fakeInitialPath}
+											{currentPath}
+											{hash}
+											{isDeployed}
+											small={useVerticalTriggerBar}
+											{args}
+											{newItem}
+											{schema}
+											{isEditor}
+											onDelete={() => {
+												deleteDraftTrigger($selectedTrigger)
+											}}
+											onUpdate={(path) => {
+												handleUpdate($selectedTrigger, path)
+											}}
+											onConfigChange={(cfg, canSave, updated) => {
+												if (updated) {
+													handleUpdateDraftConfig($selectedTrigger, cfg, canSave)
+												}
+											}}
+											onCaptureConfigChange={(cfg, isValidConfig) => {
+												config = cfg
+												isValid = isValidConfig
+											}}
+											onReset={() => {
+												handleResetDraft($selectedTrigger)
+											}}
+											on:email-domain={({ detail }) => {
+												emailDomain = detail
+											}}
+										/>
+									</div>
+								{/if}
+							{/key}
+						{/each}
+					{:else}
+						<span class="text-sm text-tertiary text-center mx-auto mt-2"
+							>{`Select a trigger from the ${useVerticalTriggerBar ? 'left toolbar' : 'table'} or a create a new one`}</span
+						>
+					{/if}
+				</div>
+			</div>
+		</Pane>
+		{#if $selectedTrigger && $triggers[$selectedTrigger].type && $triggers[$selectedTrigger].type !== 'schedule' && $triggers[$selectedTrigger].type != 'poll' && !noCapture}
+			{@const captureKind = triggerTypeToCaptureKind($triggers[$selectedTrigger].type)}
+			{#key captureKind}
+				<Pane minSize={20} size={40}>
+					<CaptureWrapper
+						path={initialPath || fakeInitialPath}
+						{isFlow}
+						captureType={captureKind}
+						{hasPreprocessor}
+						{canHavePreprocessor}
+						args={config}
+						data={{ args, hash, emailDomain }}
+						{isValid}
+						on:applyArgs
+						on:updateSchema
+						on:addPreprocessor
+						on:testWithArgs
+					/>
+				</Pane>
+			{/key}
+		{/if}
+	</Splitpanes>
 </FlowCard>
