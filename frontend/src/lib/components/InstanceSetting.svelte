36507a2048219fac0e5f8a83d2f5523a8f485b41
pyranota <92104930+pyranota@users.noreply.github.com>
2025-05-23 12:10:42 +0200

    Update InstanceSetting.svelte

--- frontend/src/lib/components/InstanceSetting.svelte
+++ frontend/src/lib/components/InstanceSetting.svelte
@@ -26,7 +26,7 @@
 	} from '$lib/gen'
 	import { Button, SecondsInput, Skeleton } from './common'
 	import Password from './Password.svelte'
-	import { classNames, sleep } from '$lib/utils'
+	import { classNames } from '$lib/utils'
 	import Popover from './Popover.svelte'
 	import PopoverMelt from './meltComponents/Popover.svelte'
 	import Toggle from './Toggle.svelte'
