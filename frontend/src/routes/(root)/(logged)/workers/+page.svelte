81bd1dc8f9af37ea12c5a6d5c6c92982fafde054
dieriba <dieriba.pro@gmail.com>
2025-05-24 15:32:13 +0200

    feat(frontend): add repl to interact with worker and missing packages

--- frontend/src/routes/(root)/(logged)/workers/+page.svelte
+++ frontend/src/routes/(root)/(logged)/workers/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import AssignableTags from '$lib/components/AssignableTags.svelte'
 	import CenteredPage from '$lib/components/CenteredPage.svelte'
-	import { Button, Skeleton, Tab, Tabs } from '$lib/components/common'
+	import { Alert, Button, Skeleton, Tab, Tabs } from '$lib/components/common'
 	import Popover from '$lib/components/meltComponents/Popover.svelte'
 	import Badge from '$lib/components/common/badge/Badge.svelte'
 	import Drawer from '$lib/components/common/drawer/Drawer.svelte'
@@ -24,8 +24,16 @@
 		userWorkspaces
 	} from '$lib/stores'
 	import { sendUserToast } from '$lib/toast'
-	import { displayDate, groupBy, pluralize, truncate } from '$lib/utils'
-	import { AlertTriangle, LineChart, List, Plus, Search } from 'lucide-svelte'
+	import {
+		AGENT_WORKER_NAME_PREFIX,
+		AgentKnownSuffix,
+		checkIfWorkerHasSpecificSuffix,
+		displayDate,
+		groupBy,
+		pluralize,
+		truncate
+	} from '$lib/utils'
+	import { AlertTriangle, LineChart, List, Plus, Search, Terminal } from 'lucide-svelte'
 	import { getContext, onDestroy, onMount } from 'svelte'
 	import AutoComplete from 'simple-svelte-autocomplete'
 
@@ -33,15 +41,14 @@
 	import { DEFAULT_TAGS_WORKSPACES_SETTING } from '$lib/consts'
 	import AutoscalingEvents from '$lib/components/AutoscalingEvents.svelte'
 	import HttpAgentWorkerDrawer from '$lib/components/HttpAgentWorkerDrawer.svelte'
+	import WorkerRepl from '$lib/components/WorkerRepl.svelte'
 
 	let workers: WorkerPing[] | undefined = undefined
 	let workerGroups: Record<string, any> | undefined = undefined
 	let groupedWorkers: [string, [string, WorkerPing[]][]][] = []
 	let intervalId: NodeJS.Timeout | undefined
-
 	const splitter = '_%%%_'
 	let customTags: string[] | undefined = undefined
-
 	$: groupedWorkers = groupWorkers(workers, workerGroups)
 
 	function groupWorkers(
@@ -145,7 +152,7 @@
 
 	let importConfigDrawer: Drawer | undefined = undefined
 	let importConfigCode = ''
-
+	let tag: string = ''
 	async function importSingleWorkerConfig(c: any) {
 		if (typeof c === 'object' && c !== null) {
 			if (!c.name || typeof c.name !== 'string') {
@@ -257,8 +264,8 @@
 
 		return Math.ceil(occupancy_rate * 100) + '%'
 	}
-
 	let newHttpAgentWorkerDrawer: Drawer | undefined = undefined
+	let replForWorkerDrawer: Drawer | undefined = undefined
 </script>
 
 {#if $superadmin}
@@ -288,6 +295,24 @@
 		on:close={() => newHttpAgentWorkerDrawer?.toggleDrawer?.()}
 	>
 		<HttpAgentWorkerDrawer {customTags} />
+	</DrawerContent>
+</Drawer>
+
+<Drawer bind:this={replForWorkerDrawer} size="1000px">
+	<DrawerContent
+		title="Repl"
+		on:close={() => {
+			tag = ''
+			replForWorkerDrawer?.closeDrawer?.()
+		}}
+	>
+		<div class="flex flex-col gap-2">
+			<Alert title="Info" type="info" size="xs">
+				If no command has been run in the past 2 minutes, the next one may take up to 15 seconds to
+				start.
+			</Alert>
+			<WorkerRepl {tag} />
+		</div>
 	</DrawerContent>
 </Drawer>
 
@@ -561,11 +586,20 @@
 									<Cell head>Memory usage<br />(Windmill)</Cell>
 									<Cell head>Limits</Cell>
 									<Cell head>Version</Cell>
-									<Cell head last>Liveness</Cell>
+									<Cell head>Liveness</Cell>
+									<Cell head last
+										>Live Shell <Tooltip>
+											<p class="text-sm">
+												Open a live shell to execute bash commands on the machine where the worker
+												runs — useful for quick access, inspection, and real-time debugging
+											</p>
+										</Tooltip></Cell
+									>
 								</tr>
 							</Head>
 							<tbody class="divide-y">
 								{#each worker_group[1] as [section, workers]}
+									{@const hostname = section?.split(splitter)?.[0]}
 									<tr class="border-t">
 										<Cell
 											first
@@ -578,7 +612,7 @@
 											<div class="flex flex-row w-full">
 												<div class="min-w-64">
 													Host:
-													<span class="font-semibold">{section?.split(splitter)?.[0]}</span>
+													<span class="font-semibold">{hostname}</span>
 												</div>
 												<span class="ml-4">IP: </span>
 												<span class="font-semibold">{workers[0].ip}</span>
@@ -593,7 +627,15 @@
 									{#if workers}
 										{#each workers as { worker, custom_tags, last_ping, started_at, jobs_executed, last_job_id, last_job_workspace_id, occupancy_rate_15s, occupancy_rate_5m, occupancy_rate_30m, occupancy_rate, wm_version, vcpus, memory, memory_usage, wm_memory_usage }}
 											<tr>
-												<Cell first>{worker}</Cell>
+												<Cell first>
+													{@const underscorePos = worker.search('_')}
+													{#if underscorePos === -1}
+														{worker}
+													{:else}
+														{truncate(worker, underscorePos)}
+														<Tooltip>{worker}</Tooltip>
+													{/if}
+												</Cell>
 												<Cell>
 													{#if custom_tags && custom_tags?.length > 2}
 														{truncate(custom_tags?.join(', ') ?? '', 10)}
@@ -652,7 +694,7 @@
 														{wm_version.split('-')[0]}<Tooltip>{wm_version}</Tooltip>
 													</div>
 												</Cell>
-												<Cell last>
+												<Cell>
 													<Badge
 														color={last_ping != undefined
 															? last_ping < 60
@@ -666,6 +708,32 @@
 																: 'Dead'
 															: 'Unknown'}
 													</Badge>
+												</Cell>
+												<Cell last>
+													<Button
+														size="xs"
+														color="light"
+														on:click={() => {
+															if (
+																worker.startsWith(AGENT_WORKER_NAME_PREFIX) &&
+																!checkIfWorkerHasSpecificSuffix(
+																	AgentKnownSuffix.ENABLE_LIVE_SHELL,
+																	worker
+																)
+															) {
+																sendUserToast(
+																	`Worker ${worker} has no right to open live shell with his host machine, please create a new token and enable live shell to be able to use this feature`,
+																	true
+																)
+																return
+															}
+															tag = hostname
+															replForWorkerDrawer?.openDrawer?.()
+														}}
+														startIcon={{ icon: Terminal }}
+													>
+														Command
+													</Button>
 												</Cell>
 											</tr>
 										{/each}
