f6ed2d64c81c1544272c27a0dcd989731e1349f5
Ruben Fiszel <ruben@windmill.dev>
2025-05-25 12:59:43 +0200

    revert

--- backend/src/ee.rs
+++ backend/src/ee.rs
@@ -1,1 +1,8 @@
-/home/rubenfiszel/windmill/../windmill-ee-private/src/ee.rs
+pub async fn set_license_key(_license_key: String) -> () {
+    // Implementation is not open source
+}
+
+#[cfg(feature = "enterprise")]
+pub async fn verify_license_key() -> () {
+    // Implementation is not open source
+}
--- backend/windmill-api/src/agent_workers_ee.rs
+++ backend/windmill-api/src/agent_workers_ee.rs
@@ -1,1 +1,52 @@
-/home/rubenfiszel/windmill/../windmill-ee-private/windmill-api/src/agent_workers_ee.rs
+/*
+ * Author: Ruben Fiszel
+ * Copyright: Windmill Labs, Inc 2042
+ * This file and its contents are licensed under the AGPLv3 License.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-AGPL for a copy of the license.
+ */
+
+use crate::db::DB;
+
+use axum::Router;
+
+use serde::{Deserialize, Serialize};
+
+pub fn global_service() -> Router {
+    Router::new()
+}
+
+pub fn workspaced_service(
+    db: DB,
+    _base_internal_url: String,
+) -> (
+    Router,
+    Vec<tokio::task::JoinHandle<()>>,
+    Option<windmill_worker::JobCompletedSender>,
+) {
+    use windmill_common::worker::Connection;
+    use windmill_worker::JobCompletedSender;
+
+    let (job_completed_tx, _job_completed_rx) =
+        JobCompletedSender::new(&Connection::Sql(db.clone()), 10);
+
+    let router = Router::new();
+
+    (router, vec![], Some(job_completed_tx))
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct AgentAuth {
+    pub worker_group: String,
+    pub suffix: Option<String>,
+    pub tags: Vec<String>,
+    pub exp: Option<usize>,
+}
+
+pub struct AgentCache {}
+
+impl AgentCache {
+    pub fn new() -> Self {
+        AgentCache {}
+    }
+}
