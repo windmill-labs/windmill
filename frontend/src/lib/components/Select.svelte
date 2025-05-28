61aece9ed069edf88bc5e861753c7240a7f7b08f
Diego Imbert <diego@windmill.dev>
2025-05-28 15:08:56 +0200

    select with popover

--- frontend/src/lib/components/Select.svelte
+++ frontend/src/lib/components/Select.svelte
@@ -1,7 +1,7 @@
 <script lang="ts" generics="Item extends { label: string; value: any; }">
-	import { clickOutside } from '$lib/utils'
 	import { twMerge } from 'tailwind-merge'
 	import CloseButton from './common/CloseButton.svelte'
+	import Popover from './meltComponents/Popover.svelte'
 
 	type Value = Item['value']
 
@@ -37,25 +37,11 @@
 	})
 
 	let processedItems: (Item & { __select_group?: string; label: string })[] = $derived.by(() => {
-		let items2 =
-			items?.map((item) => ({
-				...item,
-				label: getLabel(item)
-			})) ?? []
-		if (search) {
+		let items2 = items?.map((item) => ({ ...item, label: getLabel(item) })) ?? []
+		if (search)
 			items2 = items2.filter((item) => item.label.toLowerCase().includes(search.toLowerCase()))
-			console.log('search', items2)
-		}
-		if (groupBy) {
-			items2 =
-				items2?.map((item) => ({
-					...item,
-					__select_group: groupBy(item)
-				})) ?? []
-		}
-		if (sortBy) {
-			items2 = items2?.sort(sortBy)
-		}
+		if (groupBy) items2 = items2?.map((item) => ({ ...item, __select_group: groupBy(item) })) ?? []
+		if (sortBy) items2 = items2?.sort(sortBy)
 		return items2
 	})
 	let valueEntry = $derived(value && processedItems?.find((item) => item.value === value))
@@ -84,10 +70,10 @@
 
 <svelte:window
 	on:keydown={(e) => {
-		console.log('keypress', e.key)
-		if (e.key === 'ArrowUp' && keyArrowPos !== undefined && processedItems?.length > 0) {
+		if (!open || !processedItems?.length) return
+		if (e.key === 'ArrowUp' && keyArrowPos !== undefined) {
 			keyArrowPos = keyArrowPos <= 0 ? undefined : keyArrowPos - 1
-		} else if (e.key === 'ArrowDown' && processedItems?.length > 0) {
+		} else if (e.key === 'ArrowDown') {
 			if (keyArrowPos === undefined) {
 				keyArrowPos = 0
 			} else {
@@ -103,50 +89,57 @@
 	}}
 />
 
-<div class={`relative ${className}`} use:clickOutside={{ onClickOutside: () => (open = false) }}>
-	{#if clearable && value !== undefined}
-		<div class="absolute z-10 right-2 h-full flex items-center">
-			<CloseButton noBg small on:close={clearValue} />
+<Popover
+	placement="bottom-start"
+	closeOnOtherPopoverOpen
+	bind:isOpen={open}
+	forceContentToTriggerWidth
+	class={twMerge(className, valueEntry ? '!placeholder-primary' : '')}
+	usePointerDownOutside
+>
+	<svelte:fragment slot="trigger">
+		<div class={`relative`}>
+			{#if clearable && value !== undefined}
+				<div class="absolute z-10 right-2 h-full flex items-center">
+					<CloseButton noBg small on:close={clearValue} />
+				</div>
+			{/if}
+			<input
+				type="text"
+				bind:value={() => search, (v) => (search = v)}
+				placeholder={valueEntry?.label ?? placeholder}
+				autocomplete="off"
+			/>
 		</div>
-	{/if}
-	<input
-		type="text"
-		bind:value={() => search, (v) => (search = v)}
-		placeholder={valueEntry?.label ?? placeholder}
-		class={twMerge(open ? '' : 'cursor-pointer', valueEntry ? '!placeholder-primary' : '')}
-		autocomplete="off"
-		onpointerdown={() => (open = true)}
-	/>
-	<div class="relative w-full">
-		{#if open}
-			<div
-				class="absolute z-10 max-h-64 overflow-y-scroll w-full bg-surface text-tertiary text-sm select-none border rounded-lg"
-			>
-				{#each processedItems ?? [] as item, itemIndex}
-					{#if (item.__select_group && itemIndex === 0) || processedItems?.[itemIndex - 1].__select_group !== item.__select_group}
-						<div
-							class={twMerge(
-								'mx-4 pb-1 mb-2 text-xs font-semibold text-secondary border-b',
-								itemIndex === 0 ? 'mt-3' : 'mt-6'
-							)}
-						>
-							{item.__select_group}
-						</div>
-					{/if}
-					<button
+	</svelte:fragment>
+	<svelte:fragment slot="content">
+		<div
+			class="flex flex-col max-h-64 overflow-y-scroll w-full bg-surface text-tertiary text-sm select-none rounded-lg"
+		>
+			{#each processedItems ?? [] as item, itemIndex}
+				{#if (item.__select_group && itemIndex === 0) || processedItems?.[itemIndex - 1]?.__select_group !== item.__select_group}
+					<div
 						class={twMerge(
-							'py-2 px-4 w-full font-normal text-left',
-							itemIndex === keyArrowPos ? 'bg-surface-hover' : '',
-							item.value === value
-								? 'bg-surface-selected-inverse text-primary-inverse'
-								: 'hover:bg-surface-hover'
+							'mx-4 pb-1 mb-2 text-xs font-semibold text-secondary border-b',
+							itemIndex === 0 ? 'mt-3' : 'mt-6'
 						)}
-						onclick={() => setValue(item)}
 					>
-						{item.label}
-					</button>
-				{/each}
-			</div>
-		{/if}
-	</div>
-</div>
+						{item.__select_group}
+					</div>
+				{/if}
+				<button
+					class={twMerge(
+						'py-2 px-4 w-full font-normal text-left',
+						itemIndex === keyArrowPos ? 'bg-surface-hover' : '',
+						item.value === value
+							? 'bg-surface-selected-inverse text-primary-inverse'
+							: 'hover:bg-surface-hover'
+					)}
+					onclick={() => setValue(item)}
+				>
+					{item.label}
+				</button>
+			{/each}
+		</div>
+	</svelte:fragment>
+</Popover>
