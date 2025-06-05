d6bf229bd5da90e8a3f46ab9627d3e7dcc31d7a7
Ruben Fiszel <ruben@windmill.dev>
2025-06-05 19:22:46 +0200

    Update FlowEditor.svelte

--- frontend/src/lib/components/flows/FlowEditor.svelte
+++ frontend/src/lib/components/flows/FlowEditor.svelte
@@ -39,14 +39,6 @@
 		pickablePropertiesFiltered: writable<PickableProperties | undefined>(undefined)
 	})
 
-	export function addSelectedLinesToAiChat(lines: string, startLine: number, endLine: number) {
-		aiChatManager.addSelectedLinesToContext(lines, startLine, endLine)
-		if (!aiChatManager.open) {
-			aiChatManager.openChat()
-			aiChatManager.changeMode(AIMode.SCRIPT)
-		}
-	}
-
 	onMount(() => {
 		aiChatManager.changeMode(AIMode.FLOW)
 	})
