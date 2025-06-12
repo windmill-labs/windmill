e2363a811a08d8cf4f217445b452f6dca992f551
pyranota <pyra@duck.com>
2025-06-12 18:18:31 +0200

    remove dbg!

--- backend/windmill-worker/src/go_executor.rs
+++ backend/windmill-worker/src/go_executor.rs
@@ -367,7 +367,7 @@ pub async fn install_go_dependencies(
     occupation_metrics: &mut OccupancyMetrics,
 ) -> error::Result<String> {
     if raw_deps {
-        fs::write(format!("{job_dir}/go.mod"), dbg!(code)).await?;
+        fs::write(format!("{job_dir}/go.mod"), code).await?;
     }
     if !raw_deps && !skip_go_mod {
         gen_go_mymod(code, job_dir).await?;
