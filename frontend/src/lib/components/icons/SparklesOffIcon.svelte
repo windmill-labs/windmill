cac7b1cff3ae5f6b452ee9ffcc24f610b2e1a1a4
Ruben Fiszel <ruben@rubenfiszel.com>
2025-06-13 08:08:38 +0200

    icons

--- frontend/src/lib/components/icons/SparklesOffIcon.svelte
+++ frontend/src/lib/components/icons/SparklesOffIcon.svelte
@@ -1,3 +1,4 @@
+<!-- @migration-task Error while migrating Svelte code: $$props is used together with named props in a way that cannot be automatically migrated. -->
 <script lang="ts">
 	export let size = '24'
 </script>
--- frontend/src/lib/components/icons/WindmillIcon.svelte
+++ frontend/src/lib/components/icons/WindmillIcon.svelte
@@ -1,3 +1,4 @@
+<!-- @migration-task Error while migrating Svelte code: $$props is used together with named props in a way that cannot be automatically migrated. -->
 <script lang="ts">
 	import { customIcon } from './store'
 
