108ff955de9c2c8236f3d7ecb48790a21fe0ed72
Ruben Fiszel <ruben@windmill.dev>
2025-06-30 00:07:35 +0000

    lazy load nits

--- frontend/src/lib/components/resourceTypesStore.ts
+++ frontend/src/lib/components/resourceTypesStore.ts
@@ -4,19 +4,40 @@ import { get, writable } from "svelte/store";
 
 export const resourceTypesStore = writable<string[] | undefined>(undefined)
 
+let loading = false
+
 export async function getResourceTypes() {
     const rts = get(resourceTypesStore)
     if (rts) {
         return rts
     } else {
+        if (loading) {
+            let counter = 0
+            while (loading) {
+                await new Promise(resolve => setTimeout(resolve, 50))
+                counter++
+                if (counter > 10) {
+                    console.error('Resource types loading timeout')
+                    break
+                }
+            }
+            if (!loading) {
+                return get(resourceTypesStore)
+            }
+
+        }
+        loading = true
+
         let workspace = get(workspaceStore)
-        if (workspace){
+        if (workspace) {
             try {
                 const nrts = await ResourceService.listResourceTypeNames({ workspace: workspace })
                 resourceTypesStore.set(nrts)
+                loading = false
                 return nrts
             } catch (e) {
-                return ["error_fetching_names"]        
+                loading = false
+                return ["error_fetching_names"]
             }
         } else {
             return ['workspace_is_undefined']
