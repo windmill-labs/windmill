4f7a5688b5e9621f0e2b350a72f52d5fc240b084
wendrul <dethomassin.etienne@gmail.com>
2025-06-30 06:37:09 +0200

    use varchar 255 for email as in other tables

--- backend/migrations/20250605150135_add_email_and_span_to_audit.up.sql
+++ backend/migrations/20250605150135_add_email_and_span_to_audit.up.sql
@@ -1,3 +1,3 @@
 -- Add email and span columns to audit table
-ALTER TABLE audit ADD COLUMN email VARCHAR(50);
+ALTER TABLE audit ADD COLUMN email VARCHAR(255);
 ALTER TABLE audit ADD COLUMN span VARCHAR(255);
