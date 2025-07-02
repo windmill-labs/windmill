9816e5efdecdbfdd6fd3fb90ab51f30fef50b38b
Guilhem <guilhemlemouel@gmail.com>
2025-07-02 18:50:37 +0100

    fix flow preview rendering

--- frontend/src/lib/components/FlowStatusViewer.svelte
+++ frontend/src/lib/components/FlowStatusViewer.svelte
@@ -26,7 +26,7 @@
 	export let localModuleStates: Writable<Record<string, GraphModuleState>> = writable({})
 	export let localDurationStatuses: Writable<Record<string, DurationStatus>> = writable({})
 	export let job: Job | undefined = undefined
-	export let render = false
+	export let render = true
 
 	let lastJobId: string = jobId
 
