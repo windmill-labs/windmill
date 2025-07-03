aa5575da79c494877b263c2b193b709c7bca87b3
Diego Imbert <diego@windmill.dev>
2025-07-04 00:40:17 +0200

    parse_assets match case in rust

--- backend/windmill-common/src/assets.rs
+++ backend/windmill-common/src/assets.rs
@@ -1,4 +1,7 @@
 use serde::{Deserialize, Serialize};
+use windmill_parser::asset_parser::ParseAssetsResult;
+
+use crate::scripts::ScriptLang;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Copy, Clone, Hash, Eq, sqlx::Type)]
 #[sqlx(type_name = "ASSET_KIND", rename_all = "lowercase")]
@@ -34,4 +37,20 @@ pub struct AssetUsage {
     pub path: String,
     pub kind: AssetUsageKind,
     pub access_type: AssetUsageAccessType,
+}
+
+pub fn parse_assets<'a>(
+    input: &'a str,
+    lang: ScriptLang,
+    paths_storage: &'a mut Vec<String>,
+) -> anyhow::Result<Option<Vec<ParseAssetsResult<'a>>>> {
+    let r = match lang {
+        ScriptLang::Python3 => windmill_parser_py::parse_assets(input, paths_storage),
+        ScriptLang::DuckDb => windmill_parser_sql::parse_assets(input),
+        ScriptLang::Deno | ScriptLang::Bun | ScriptLang::Nativets => {
+            windmill_parser_ts::parse_assets(input, paths_storage)
+        }
+        _ => return Ok(None),
+    };
+    return r.map(Some);
 }
