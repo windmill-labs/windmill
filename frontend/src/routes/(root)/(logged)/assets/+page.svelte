6efd58b5b2b61bdc09fc4a222c241e8e6e83fa25
Diego Imbert <diego@windmill.dev>
2025-07-09 17:23:30 +0200

    move assets usage dropdown to component

--- frontend/src/routes/(root)/(logged)/assets/+page.svelte
+++ frontend/src/routes/(root)/(logged)/assets/+page.svelte
@@ -1,24 +1,19 @@
 <script lang="ts">
-	import { formatAsset, getAssetUsagePageUri } from '$lib/components/assets/lib'
+	import { formatAsset } from '$lib/components/assets/lib'
 	import CenteredPage from '$lib/components/CenteredPage.svelte'
-	import { ClearableInput, DrawerContent } from '$lib/components/common'
-	import Drawer from '$lib/components/common/drawer/Drawer.svelte'
-	import RowIcon from '$lib/components/common/table/RowIcon.svelte'
+	import { ClearableInput } from '$lib/components/common'
 	import DbManagerDrawer from '$lib/components/DBManagerDrawer.svelte'
 	import PageHeader from '$lib/components/PageHeader.svelte'
 	import S3FilePicker from '$lib/components/S3FilePicker.svelte'
 	import { Cell, DataTable } from '$lib/components/table'
 	import Head from '$lib/components/table/Head.svelte'
-	import { AssetService, ResourceService, type ListAssetsResponse } from '$lib/gen'
+	import { AssetService, ResourceService } from '$lib/gen'
 	import { userStore, workspaceStore, userWorkspaces } from '$lib/stores'
 	import { usePromise } from '$lib/svelte5Utils.svelte'
 	import { pluralize, truncate } from '$lib/utils'
 	import { untrack } from 'svelte'
 	import ExploreAssetButton, { assetCanBeExplored } from './ExploreAssetButton.svelte'
-	import {
-		assetDisplaysAsInputInFlowGraph,
-		assetDisplaysAsOutputInFlowGraph
-	} from '$lib/components/graph/renderers/nodes/AssetNode.svelte'
+	import AssetsUsageDropdown from '$lib/components/assets/AssetsUsageDropdown.svelte'
 
 	let assets = usePromise(() => AssetService.listAssets({ workspace: $workspaceStore ?? '' }))
 
@@ -42,9 +37,9 @@
 		})
 	})
 
-	let viewOccurences: ListAssetsResponse[number] | undefined = $state()
 	let s3FilePicker: S3FilePicker | undefined = $state()
 	let dbManagerDrawer: DbManagerDrawer | undefined = $state()
+	let assetsUsageDropdown: AssetsUsageDropdown | undefined = $state()
 </script>
 
 {#if $userStore?.operator && $workspaceStore && !$userWorkspaces.find((_) => _.id === $workspaceStore)?.operator_settings?.resources}
@@ -79,7 +74,7 @@
 					<tr class="h-14">
 						<Cell first class="w-[75%]">{truncate(assetUri, 92)}</Cell>
 						<Cell>
-							<a href={`#${assetUri}`} onclick={() => (viewOccurences = asset)}>
+							<a href={`#${assetUri}`} onclick={() => assetsUsageDropdown?.open(asset)}>
 								{pluralize(asset.usages.length, 'occurrence')}
 							</a>
 						</Cell>
@@ -101,38 +96,6 @@
 	</CenteredPage>
 {/if}
 
-<Drawer
-	open={viewOccurences !== undefined}
-	size="900px"
-	on:close={() => (viewOccurences = undefined)}
->
-	<DrawerContent title="Asset occurrences" on:close={() => (viewOccurences = undefined)}>
-		<ul class="flex flex-col border rounded-md divide-y">
-			{#each viewOccurences?.usages ?? [] as u}
-				<li>
-					<a
-						href={getAssetUsagePageUri(u)}
-						aria-label={`${u.kind}/${u.path}`}
-						class="text-sm text-primary flex items-center py-3 px-4 gap-3 hover:bg-surface-hover cursor-pointer"
-					>
-						<RowIcon kind={u.kind} />
-						<div class="flex flex-col justify-center flex-1">
-							<span class="font-semibold">{u.path}</span>
-							<span class="text-xs text-tertiary">{u.kind}</span>
-						</div>
-						<div class="flex gap-2">
-							{#if assetDisplaysAsInputInFlowGraph(u)}
-								<div class="text-xs border text-tertiary max-w-fit p-1 rounded-md">Read</div>
-							{/if}
-							{#if assetDisplaysAsOutputInFlowGraph(u)}
-								<div class="text-xs border text-tertiary max-w-fit p-1 rounded-md">Write</div>
-							{/if}
-						</div>
-					</a>
-				</li>
-			{/each}
-		</ul>
-	</DrawerContent>
-</Drawer>
+<AssetsUsageDropdown bind:this={assetsUsageDropdown} />
 <S3FilePicker bind:this={s3FilePicker} readOnlyMode />
 <DbManagerDrawer bind:this={dbManagerDrawer} />
