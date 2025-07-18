6f29aa405c658be51d63664d48be94f5d21b0e25
Ruben Fiszel <ruben@windmill.dev>
2025-07-18 09:05:43 +0000

    whitelabelling nits

--- backend/windmill-worker/src/worker.rs
+++ backend/windmill-worker/src/worker.rs
@@ -2069,6 +2069,10 @@ async fn do_nativets(
     .await?)
 }
 
+lazy_static::lazy_static! {
+    static ref LOG_TAG_NAME: String = std::env::var("LOG_TAG_NAME").unwrap_or("tag".to_string());
+}
+
 #[derive(Deserialize, Serialize, Default)]
 pub struct PreviousResult<'a> {
     #[serde(borrow)]
@@ -2264,8 +2268,8 @@ pub async fn handle_queued_job(
         // println!("handle queue {:?}",  SystemTime::now());
 
         logs.push_str(&format!(
-            "job={} tag={} worker={} hostname={}\n",
-            &job.id, &job.tag, &worker_name, &hostname
+            "job={} {}={} worker={} hostname={}\n",
+            &job.id, *LOG_TAG_NAME, &job.tag, &worker_name, &hostname
         ));
 
         if *NO_LOGS_AT_ALL {
