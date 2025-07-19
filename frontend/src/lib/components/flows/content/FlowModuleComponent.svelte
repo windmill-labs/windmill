1b6d055ed6c344b44cac8fe4a940b1880eb55a29
Ruben Fiszel <ruben@windmill.dev>
2025-07-19 07:37:56 +0000

    flow opts

--- frontend/src/lib/components/flows/content/FlowModuleComponent.svelte
+++ frontend/src/lib/components/flows/content/FlowModuleComponent.svelte
@@ -192,10 +192,13 @@
 	let editorSettingsPanelSize = $state(100 - untrack(() => editorPanelSize))
 	let stepHistoryLoader = getStepHistoryLoaderContext()
 
+	let lastJobId: string | undefined = undefined
+
 	function onSelectedIdChange() {
 		if (!$flowStateStore?.[$selectedId]?.schema && flowModule) {
 			reload(flowModule)
 		}
+		lastJobId = undefined
 	}
 
 	async function getLastJob() {
@@ -208,11 +211,20 @@
 		) {
 			return
 		}
+
+		if (
+			lastJobId == $flowStateStore[flowModule.id]?.previewJobId ||
+			lastJob?.id == $flowStateStore[flowModule.id]?.previewJobId
+		) {
+			return
+		}
+		lastJobId = $flowStateStore[flowModule.id]?.previewJobId
 		const job = await JobService.getJob({
 			workspace: $flowStateStore[flowModule.id]?.previewWorkspaceId ?? '',
-			id: $flowStateStore[flowModule.id]?.previewJobId ?? ''
+			id: $flowStateStore[flowModule.id]?.previewJobId ?? '',
+			noCode: true
 		})
-		if (job) {
+		if (job && job.type === 'CompletedJob') {
 			lastJob = job
 		}
 	}
