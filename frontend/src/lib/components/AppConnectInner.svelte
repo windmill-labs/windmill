6db634c3fd4c3af8094c944e83b7726a11150b8c
HugoCasa <hugo@casademont.ch>
2025-07-21 13:33:05 +0200

    nit

--- frontend/src/lib/components/AppConnectInner.svelte
+++ frontend/src/lib/components/AppConnectInner.svelte
@@ -92,7 +92,7 @@
 
 	let scopes: string[] = $state([])
 	let extra_params: [string, string][] = []
-	let extraParams: Record<string, string> = $state({})
+	let responseExtra: Record<string, string> = $state({})
 	let path: string = $state('')
 	let description = $state('')
 
@@ -279,7 +279,7 @@
 			resourceType = data.resource_type
 			value = data.res.access_token!
 			valueToken = data.res
-			extraParams = data.extra ?? {}
+			responseExtra = data.extra ?? {}
 			step = 4
 			if (express) {
 				path = `u/${$userStore?.username}/${resourceType}_${new Date().getTime()}`
@@ -413,8 +413,8 @@
 				if (account_identifier) {
 					args['account_identifier'] = account_identifier[1]
 				}
-			} else if (resourceType === 'quickbooks' && extraParams['realmId']) {
-				args['realmId'] = extraParams['realmId']
+			} else if (resourceType === 'quickbooks' && responseExtra['realmId']) {
+				args['realmId'] = responseExtra['realmId']
 			}
 
 			let account: number | undefined = undefined
