596280ab6d29be0e799329947e6917698a1c0932
dieriba <dieriba.pro@gmail.com>
2025-07-22 20:57:39 +0200

    fix: scopes-run-and-webhook-token-display (#6259)
    * fix
    
    * update sqlx
    
    * Update backend/windmill-api/src/scopes.rs
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    * fix perfect match
    
    ---------
    
    Co-authored-by: Ruben Fiszel <ruben@windmill.dev>
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- backend/windmill-api/src/triggers.rs
+++ backend/windmill-api/src/triggers.rs
@@ -199,17 +199,51 @@ pub async fn list_tokens_internal(
 ) -> JsonResult<Vec<TruncatedTokenWithEmail>> {
     let tokens = if is_flow {
         sqlx::query_as!(
-        TruncatedTokenWithEmail,
-        "SELECT label, concat(substring(token for 10)) as token_prefix, expiration, created_at, last_used_at, scopes, email FROM token WHERE workspace_id = $1 AND scopes @> ARRAY['run:flow/' || $2]::text[]",
-        w_id, path).fetch_all(db)
+            TruncatedTokenWithEmail,
+            r#"
+        SELECT label,
+               concat(substring(token for 10)) AS token_prefix,
+               expiration,
+               created_at,
+               last_used_at,
+               scopes,
+               email
+        FROM token
+        WHERE workspace_id = $1
+          AND (
+               scopes @> ARRAY['jobs:run:flows:' || $2]::text[]
+               OR scopes @> ARRAY['run:flows/' || $2]::text[]
+              )
+        "#,
+            w_id,
+            path
+        )
+        .fetch_all(db)
         .await?
     } else {
         sqlx::query_as!(
             TruncatedTokenWithEmail,
-            "SELECT label, concat(substring(token for 10)) as token_prefix, expiration, created_at, last_used_at, scopes, email FROM token WHERE workspace_id = $1 AND scopes @> ARRAY['run:script/' || $2]::text[]",
-            w_id, path)
+            r#"
+        SELECT label,
+               concat(substring(token for 10)) AS token_prefix,
+               expiration,
+               created_at,
+               last_used_at,
+               scopes,
+               email
+        FROM token
+        WHERE workspace_id = $1
+          AND (
+               scopes @> ARRAY['jobs:run:scripts:' || $2]::text[]
+               OR scopes @> ARRAY['run:scripts/' || $2]::text[]
+              )
+        "#,
+            w_id,
+            path
+        )
         .fetch_all(db)
         .await?
     };
+
     Ok(Json(tokens))
 }
