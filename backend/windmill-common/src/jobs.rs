b09b0ff7925ef69af0fc6972e4328058c8b46dbf
iqdecay <victor@berasconsulting.com>
2025-07-24 14:11:29 +0200

    feat: enable workspace exclusion in custom tags (#6263)
    * feat: enable tags that exclude workspaces
    
    * feat: frontend tooltip
    
    * fix: use method to check if tag applies
    
    * refactor: change conversion method
    
    * fix: change operator comparison
    
    * remove redundant tests

--- backend/windmill-common/src/jobs.rs
+++ backend/windmill-common/src/jobs.rs
@@ -663,16 +663,10 @@ pub async fn check_tag_available_for_workspace_internal(
     }
 
     let custom_tags_per_w = CUSTOM_TAGS_PER_WORKSPACE.read().await;
-    if custom_tags_per_w.0.contains(&tag.to_string()) {
+    if custom_tags_per_w.global.contains(&tag.to_string()) {
         is_tag_in_workspace_custom_tags = true;
-    } else if custom_tags_per_w.1.contains_key(tag)
-        && custom_tags_per_w
-            .1
-            .get(tag)
-            .unwrap()
-            .contains(&w_id.to_string())
-    {
-        is_tag_in_workspace_custom_tags = true;
+    } else if let Some(specific_tag) = custom_tags_per_w.specific.get(tag) {
+        is_tag_in_workspace_custom_tags = specific_tag.applies_to_workspace(w_id);
     }
 
     match is_tag_in_scope_tags {
