9729fb3bdf5695825f3d3fcf4288ff2cad9695d7
centdix <farhadg110@gmail.com>
2025-08-01 12:04:04 +0200

    cleaning

--- cli/windmill-utils-internal/gen_openflow_schema.sh
+++ cli/windmill-utils-internal/gen_openflow_schema.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 output_dirpath="${script_dirpath}/src/gen"
 
@@ -7,16 +9,21 @@ npx @redocly/openapi-cli@latest bundle "${script_dirpath}/../../openflow.openapi
 node -e "
 const fs = require('fs');
 const filePath = '${output_dirpath}/openflow.json';
-const schema = JSON.parse(fs.readFileSync(filePath, 'utf8'));
 
-function removeMapping(obj) {
-  if (obj && typeof obj === 'object') {
-    if (obj.discriminator?.mapping) delete obj.discriminator.mapping;
-    for (const v of Object.values(obj)) removeMapping(v);
-  }
+try {
+    const schema = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    function removeMapping(obj) {
+    if (obj && typeof obj === 'object') {
+        if (obj.discriminator?.mapping) delete obj.discriminator.mapping;
+        for (const v of Object.values(obj)) removeMapping(v);
+    }
+    }
+
+    removeMapping(schema);
+    fs.writeFileSync(filePath, JSON.stringify(schema, null, 2));
+    console.log('Removed discriminator mappings from openflow.json');
+} catch (e) {
+    console.error('Error removing discriminator mappings:', e);
 }
-
-removeMapping(schema);
-fs.writeFileSync(filePath, JSON.stringify(schema, null, 2));
-console.log('Removed discriminator mappings from openflow.json');
 "
