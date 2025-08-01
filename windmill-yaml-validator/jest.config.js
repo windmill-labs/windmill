4031382bf79077eff37ec707b8e6f19b41b43728
centdix <40307056+centdix@users.noreply.github.com>
2025-08-01 13:14:39 +0200

    feat(utils): add flow.yaml validation function (#6316)
    * add validateflow function
    
    * cleaner code
    
    * preprocess json
    
    * cleaning
    
    * create specific package
    
    * cleaning
    
    * add tests

---
+++ windmill-yaml-validator/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/gen/**',
+  ],
+};
