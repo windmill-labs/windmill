f5cf66ee998b05c2259e3f85dfe45a7c7320ce9d
Diego Imbert <diego@windmill.dev>
2025-08-05 15:31:14 +0200

    avoid temp password for ducklake_user

--- backend/migrations/20250731132157_ducklake_instance_settings.up.sql
+++ backend/migrations/20250731132157_ducklake_instance_settings.up.sql
@@ -9,28 +9,17 @@ ON CONFLICT DO NOTHING;
 -- And we cannot drop databases in transactions (migrations)
 
 DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 
-    FROM pg_roles 
-    WHERE rolname = 'ducklake_user'
-  ) THEN
-    CREATE USER ducklake_user WITH PASSWORD 'TO_CHANGE';
-  END IF;
-EXCEPTION
-  WHEN duplicate_object THEN
-    RAISE NOTICE 'User ducklake_user already exists, skipping.';
-END
-$$;
-
-DO $$
 DECLARE
     pwd text;
 BEGIN
     SELECT trim(both '"' from value::text) INTO pwd FROM global_settings WHERE name = 'ducklake_user_pg_pwd';
-    EXECUTE format('ALTER USER ducklake_user WITH PASSWORD %L', pwd);
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'ducklake_user') THEN
+        EXECUTE format('CREATE USER ducklake_user WITH PASSWORD %L', pwd);
+    ELSE
+      EXECUTE format('ALTER USER ducklake_user WITH PASSWORD %L', pwd);
+    END IF;
 EXCEPTION
-  WHEN OTHERS THEN
-    RAISE NOTICE 'Couldn''t set ducklake_user password: %', SQLERRM;
+  WHEN others THEN
+    RAISE NOTICE 'ducklake_user migration error, skipping.';
 END
 $$;
