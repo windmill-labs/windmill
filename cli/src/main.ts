cb649b26893d8829aae0ed8cdf896607a3331804
Alexander Petric <alpetric@users.noreply.github.com>
2025-08-06 19:03:03 -0400

    feat: git sync v2 + cli git_branches support (#6327)
    * adding branch support
    
    * gitsync settings refactor
    
    * promotionOverride
    
    * require branches and initialize them
    
    * branches -> git_branches
    
    * profiles
    
    * format
    
    * format
    
    * only warn instead of error when branches: not set in git context
    
    * error handling
    
    * formatting
    
    * modal job handling improvement
    
    * respect --yes
    
    * logging + apply effective settings
    
    * nit
    
    * ui updates
    
    * npm check
    
    * Update frontend/src/lib/components/git_sync/PullWorkspaceModal.svelte
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    
    * hubpaths
    
    ---------
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -8,7 +8,9 @@ import {
 import flow from "./commands/flow/flow.ts";
 import app from "./commands/app/apps.ts";
 import script from "./commands/script/script.ts";
-import workspace, { getActiveWorkspace } from "./commands/workspace/workspace.ts";
+import workspace, {
+  getActiveWorkspace,
+} from "./commands/workspace/workspace.ts";
 import resource from "./commands/resource/resource.ts";
 import resourceType from "./commands/resource-type/resource-type.ts";
 import user from "./commands/user/user.ts";
@@ -167,8 +169,6 @@ const command = new Command()
   )
   .command("completions", new CompletionsCommand());
 
-
-
 async function main() {
   try {
     if (Deno.args.length === 0) {
@@ -205,7 +205,9 @@ async function main() {
     await command.parse(Deno.args);
   } catch (e) {
     if (e && typeof e === "object" && "name" in e && e.name === "ApiError") {
-      console.log("Server failed. " + (e as any).statusText + ": " + (e as any).body);
+      console.log(
+        "Server failed. " + (e as any).statusText + ": " + (e as any).body
+      );
     }
     throw e;
   }
