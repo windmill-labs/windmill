f654e59ec90e3f0cadab54508a709ef734c8a850
Diego Imbert <diego@windmill.dev>
2025-08-07 17:44:18 +0200

    convert object store error to wmill error for correct status code

--- backend/windmill-api/src/job_helpers_oss.rs
+++ backend/windmill-api/src/job_helpers_oss.rs
@@ -139,7 +139,7 @@ pub async fn download_s3_file_internal(
 pub async fn read_object_streamable(
     s3_client: Arc<dyn ObjectStore>,
     file_key: &str,
-) -> anyhow::Result<Response> {
+) -> error::Result<Response> {
     Err(error::Error::internal_err(
         "Not implemented in Windmill's Open Source repository".to_string(),
     ))
