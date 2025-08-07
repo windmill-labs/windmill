0bdf95c3a730a42a1cdc8864ae18bfcbf060ad6b
centdix <farhadg110@gmail.com>
2025-08-07 13:26:53 +0200

    simpler scripts

--- cli/windmill-utils-internal/remove-ts-ext.sh
+++ cli/windmill-utils-internal/remove-ts-ext.sh
@@ -3,26 +3,15 @@
 # Set script to exit on any error
 set -e
 
-# Parse command line arguments
-USE_GSED=false
-while [[ $# -gt 0 ]]; do
-    case $1 in
-        -m)
-            USE_GSED=true
-            shift
-            ;;
-        *)
-            echo "Unknown option: $1"
-            echo "Usage: $0 [-m]"
-            echo "  -m    Use gsed instead of sed"
-            exit 1
-            ;;
-    esac
-done
-
 echo "Removing .ts extensions from imports..."
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Detected macOS - using BSD sed"
+else
+    echo "Detected Linux - using GNU sed"
+fi
 find . -name "*.ts" -type f | while read -r file; do
-    if [ "$USE_GSED" = true ]; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        # macOS BSD sed
         if ! command -v gsed &> /dev/null; then
             echo "Error: gsed not found"
             echo "Run: brew install gnu-sed"
@@ -30,13 +19,8 @@ find . -name "*.ts" -type f | while read -r file; do
         fi
         gsed -E -i 's/(from|import)[[:space:]]+["'\'']([^"'\'']*?)\.ts(["'\''])/\1 "\2\3/g' "$file"
     else
-        if ! sed -E -i '' 's/(from|import)[[:space:]]+["'\'']([^"'\'']*?)\.ts(["'\''])/\1 "\2\3/g' "$file" 2>/dev/null; then
-            echo "Error: sed command failed"
-            echo "This might be due to sed compatibility issues."
-            echo "Try running with the -m flag to use gsed instead:"
-            echo "  $0 -m"
-            exit 1
-        fi
+        # Linux GNU sed
+        sed -E -i 's/(from|import)[[:space:]]+["'\'']([^"'\'']*?)\.ts(["'\''])/\1 "\2\3/g' "$file"
     fi
 done
 echo "✓ All .ts extensions removed from import/export statements"
