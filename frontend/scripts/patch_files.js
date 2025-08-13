c41d68779c0735879154c39ccc38df2a72876ee8
Ruben Fiszel <ruben@windmill.dev>
2025-08-13 01:13:29 +0000

    update monaco

--- frontend/scripts/patch_files.js
+++ frontend/scripts/patch_files.js
@@ -1,57 +1,59 @@
 // patch-editor-worker.mjs
-import { readFile, writeFile } from 'fs/promises'
-import path from 'path'
-import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const filePath = path.join(
-	__dirname,
-	'..',
-	'node_modules',
-	'@codingame',
-	'monaco-vscode-api',
-	'workers',
-	'editor.worker.js'
-)
 
-const originalSnippet = `return requestHandler?.[propKey];`
+// import { readFile, writeFile } from 'fs/promises'
+// import path from 'path'
+// import { fileURLToPath } from 'url'
 
-const patchedCode = `
-import '../vscode/src/vs/editor/common/services/editorWebWorkerMain.js';
-import { start } from '../vscode/src/vs/editor/editor.worker.start.js';
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
+// const filePath = path.join(
+// 	__dirname,
+// 	'..',
+// 	'node_modules',
+// 	'@codingame',
+// 	'monaco-vscode-api',
+// 	'workers',
+// 	'editor.worker.js'
+// )
 
-function initialize(createFn) {
-    let requestHandler;
-    const foreignModule = new Proxy({}, {
-        get(_target, propKey) {
-            if (propKey === '$initialize') {
-                return async (data) => {
-                    if (!requestHandler) {
-                        requestHandler = createFn(context, data);
-                    }
-                };
-            }
-            const value = requestHandler?.[propKey]
-            if (typeof value === 'function') {
-              return value.bind(requestHandler);
-            }
-            return value;
-        }
-    });
-    const context = start(foreignModule);
-}
+// const originalSnippet = `return requestHandler?.[propKey];`
 
-export { initialize };
-`
+// const patchedCode = `
+// import '../vscode/src/vs/editor/common/services/editorWebWorkerMain.js';
+// import { start } from '../vscode/src/vs/editor/editor.worker.start.js';
 
-try {
-	const current = await readFile(filePath, 'utf8')
-	if (current.includes(originalSnippet)) {
-		await writeFile(filePath, patchedCode, 'utf8')
-		console.log('✅ editor.worker.js patched successfully.')
-	} else {
-		console.log('ℹ️ Patch already applied or file has changed.')
-	}
-} catch (err) {
-	console.error('❌ Failed to patch editor.worker.js:', err)
-}
+// function initialize(createFn) {
+//     let requestHandler;
+//     const foreignModule = new Proxy({}, {
+//         get(_target, propKey) {
+//             if (propKey === '$initialize') {
+//                 return async (data) => {
+//                     if (!requestHandler) {
+//                         requestHandler = createFn(context, data);
+//                     }
+//                 };
+//             }
+//             const value = requestHandler?.[propKey]
+//             if (typeof value === 'function') {
+//               return value.bind(requestHandler);
+//             }
+//             return value;
+//         }
+//     });
+//     const context = start(foreignModule);
+// }
+
+// export { initialize };
+// `
+
+// try {
+// 	const current = await readFile(filePath, 'utf8')
+// 	if (current.includes(originalSnippet)) {
+// 		await writeFile(filePath, patchedCode, 'utf8')
+// 		console.log('✅ editor.worker.js patched successfully.')
+// 	} else {
+// 		console.log('ℹ️ Patch already applied or file has changed.')
+// 	}
+// } catch (err) {
+// 	console.error('❌ Failed to patch editor.worker.js:', err)
+// }
