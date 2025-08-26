bf74f162dc82f0384144b279b7f42eb2c9884261
dieriba <dieriba.pro@gmail.com>
2025-08-26 18:06:02 +0200

    fix

--- backend/windmill-api/src/gcp_triggers_oss.rs
+++ backend/windmill-api/src/gcp_triggers_oss.rs
@@ -2,14 +2,71 @@
 #[allow(unused)]
 pub use crate::gcp_triggers_ee::*;
 
-use serde::{Deserialize, Serialize};
-use serde_json::value::RawValue;
-use sqlx::prelude::FromRow;
-use sqlx::types::Json as SqlxJson;
-use std::collections::HashMap;
+#[cfg(not(feature = "private"))]
+use {
+    crate::{db::DB, trigger_helpers::TriggerJobArgs},
+    serde::{Deserialize, Serialize},
+    serde_json::value::RawValue,
+    sqlx::FromRow,
+    std::collections::HashMap,
+    windmill_common::{triggers::TriggerKind, utils::empty_as_none, worker::to_raw_value},
+};
+
+type SqlxJson<T> = sqlx::types::Json<T>;
+
+#[derive(sqlx::Type, Debug, Deserialize, Serialize)]
+#[serde(rename_all(serialize = "lowercase", deserialize = "lowercase"))]
+#[sqlx(type_name = "DELIVERY_MODE", rename_all = "lowercase")]
+#[allow(unused)]
+#[cfg(not(feature = "private"))]
+pub enum DeliveryType {
+    Pull,
+    Push,
+}
 
 #[cfg(not(feature = "private"))]
-use crate::db::DB;
+impl Default for DeliveryType {
+    fn default() -> Self {
+        Self::Pull
+    }
+}
+
+#[derive(FromRow, Deserialize, Serialize, Debug)]
+#[allow(unused)]
+#[cfg(not(feature = "private"))]
+pub struct PushConfig {
+    #[serde(deserialize_with = "empty_as_none")]
+    route_path: Option<String>,
+    #[serde(deserialize_with = "empty_as_none")]
+    audience: Option<String>,
+    authenticate: bool,
+    base_endpoint: String,
+}
+#[derive(Default, Debug, Serialize, Deserialize)]
+#[allow(unused)]
+#[cfg(not(feature = "private"))]
+pub struct CreateUpdateConfig {
+    pub delivery_type: DeliveryType,
+    #[serde(default, deserialize_with = "empty_as_none")]
+    pub subscription_id: Option<String>,
+    pub delivery_config: Option<SqlxJson<PushConfig>>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[cfg(not(feature = "private"))]
+pub struct ExistingGcpSubscription {
+    pub subscription_id: String,
+    pub base_endpoint: String,
+}
+
+#[derive(Debug, Deserialize, Serialize, sqlx::Type)]
+#[serde(rename_all = "snake_case")]
+#[sqlx(type_name = "GCP_SUBSCRIPTION_MODE", rename_all = "snake_case")]
+#[cfg(not(feature = "private"))]
+pub enum SubscriptionMode {
+    Existing,
+    CreateUpdate,
+}
 
 #[cfg(not(feature = "private"))]
 pub fn start_consuming_gcp_pubsub_event(
@@ -19,6 +76,7 @@ pub fn start_consuming_gcp_pubsub_event(
     // implementation is not open source
 }
 
+#[cfg(not(feature = "private"))]
 #[derive(FromRow, Deserialize, Serialize, Debug)]
 pub struct GcpTrigger {
     pub gcp_resource_path: String,
@@ -47,4 +105,15 @@ pub struct GcpTrigger {
     pub retry: Option<SqlxJson<windmill_common::flows::Retry>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub auto_acknowledge_msg: Option<bool>,
+}
+
+#[cfg(not(feature = "private"))]
+impl TriggerJobArgs<String> for GcpTrigger {
+    fn v1_payload_fn(payload: String) -> HashMap<String, Box<RawValue>> {
+        HashMap::from([("payload".to_string(), to_raw_value(&payload))])
+    }
+
+    fn trigger_kind() -> TriggerKind {
+        TriggerKind::Gcp
+    }
 }
