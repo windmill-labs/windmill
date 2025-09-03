f10cac1c4b11b0ddcfd29ec5d88bcbedaf2cd9da
dieriba <dieriba.pro@gmail.com>
2025-09-03 22:24:38 +0200

    feat: conditional retry workflow (#6461)
    * feat retry
    
    * fix
    
    * fix
    
    * fix and nits
    
    * fix query
    
    * fix test
    
    * nits and perf
    
    * update sqlx
    
    * once
    
    * remove
    
    * update sqlx

--- backend/windmill-worker/src/ai_executor.rs
+++ backend/windmill-worker/src/ai_executor.rs
@@ -12,7 +12,7 @@ use windmill_common::{
     db::DB,
     error::{self, to_anyhow, Error},
     flow_status::AgentAction,
-    flows::{FlowModule, FlowModuleValue},
+    flows::{FlowModule, FlowModuleValue, Step},
     get_latest_hash_for_path,
     jobs::JobKind,
     scripts::{get_full_hub_script_by_path, ScriptHash, ScriptLang},
@@ -21,9 +21,8 @@ use windmill_common::{
 };
 use windmill_parser::Typ;
 use windmill_queue::{
-    flow_status::{get_step_of_flow_status, Step},
-    get_mini_pulled_job, push, CanceledBy, JobCompleted, MiniPulledJob, PushArgs,
-    PushIsolationLevel,
+    flow_status::get_step_of_flow_status, get_mini_pulled_job, push, CanceledBy, JobCompleted,
+    MiniPulledJob, PushArgs, PushIsolationLevel,
 };
 
 use crate::{
