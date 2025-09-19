70e9ae14a9541862aaa31b8435abb0185805d4b7
Alexander Petric <alpetric@users.noreply.github.com>
2025-09-19 13:43:16 -0400

    fix: teams api improvements (#6643)
    * init
    
    * adding cache + ui nits
    
    * improve caching
    
    * sqlx
    
    * ee repo ref
    
    * remove useless comments
    
    * ci errors
    
    * pr comments
    
    * ee repo ref

--- backend/windmill-api/src/teams_oss.rs
+++ backend/windmill-api/src/teams_oss.rs
@@ -24,6 +24,13 @@ pub async fn workspaces_list_available_teams_ids() -> Result<StatusCode, Error> 
 }
 
 #[cfg(not(feature = "private"))]
+pub async fn workspaces_list_available_teams_channels() -> Result<StatusCode, Error> {
+    return Err(Error::BadRequest(
+        "Teams only available on enterprise".to_string(),
+    ));
+}
+
+#[cfg(not(feature = "private"))]
 pub async fn connect_teams() -> Result<StatusCode, Error> {
     return Err(Error::BadRequest(
         "Teams only available on enterprise".to_string(),
@@ -37,12 +44,6 @@ pub async fn run_teams_message_test_job() -> Result<StatusCode, Error> {
     ));
 }
 
-#[cfg(not(feature = "private"))]
-pub async fn workspaces_list_available_teams_channels() -> Result<StatusCode, Error> {
-    return Err(Error::BadRequest(
-        "Teams only available on enterprise".to_string(),
-    ));
-}
 
 #[cfg(all(feature = "enterprise", not(feature = "private")))]
 pub fn teams_service() -> Router {
