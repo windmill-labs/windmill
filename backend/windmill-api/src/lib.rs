56cdb69e599e295a51284c58d6ff56498ad22fd7
Ruben Fiszel <ruben@windmill.dev>
2025-09-20 00:22:15 +0000

    fix: add termination handler earlier in lifecycle

--- backend/windmill-api/src/lib.rs
+++ backend/windmill-api/src/lib.rs
@@ -100,10 +100,10 @@ mod inkeep_oss;
 mod inputs;
 mod integration;
 mod live_migrations;
+pub mod openapi;
 #[cfg(all(feature = "private", feature = "parquet"))]
 pub mod s3_proxy_ee;
 mod s3_proxy_oss;
-pub mod openapi;
 
 mod approvals;
 #[cfg(all(feature = "enterprise", feature = "private"))]
@@ -155,10 +155,10 @@ pub mod stripe_ee;
 #[cfg(all(feature = "stripe", feature = "enterprise"))]
 mod stripe_oss;
 #[cfg(feature = "private")]
-pub mod teams_ee;
-#[cfg(feature = "private")]
 pub mod teams_cache_ee;
 mod teams_cache_oss;
+#[cfg(feature = "private")]
+pub mod teams_ee;
 mod teams_oss;
 mod token;
 mod tracing_init;
@@ -696,9 +696,10 @@ pub async fn run_server(
         name.map(|x| format!("name={x}")).unwrap_or_default()
     );
 
-    port_tx
-        .send(format!("http://localhost:{}", port))
-        .expect("Failed to send port");
+    if let Err(e) = port_tx.send(format!("http://localhost:{}", port)) {
+        tracing::error!("Failed to send port: {e:#}");
+        return Err(anyhow::anyhow!("Failed to send port, exiting early: {e:#}"));
+    }
 
     let server = server.with_graceful_shutdown(async move {
         killpill_rx.recv().await.ok();
@@ -794,8 +795,11 @@ async fn openapi_json() -> Response {
         .unwrap()
 }
 
-pub async fn migrate_db(db: &DB) -> anyhow::Result<Option<JoinHandle<()>>> {
-    db::migrate(db)
+pub async fn migrate_db(
+    db: &DB,
+    killpill_rx: tokio::sync::broadcast::Receiver<()>,
+) -> anyhow::Result<Option<JoinHandle<()>>> {
+    db::migrate(db, killpill_rx)
         .await
         .map_err(|e| anyhow::anyhow!("Error migrating db: {e:#}"))
 }
