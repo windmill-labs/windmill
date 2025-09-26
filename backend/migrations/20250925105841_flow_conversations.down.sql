7d70805e40ad572448e2cd6bc77892d17691b7a2
centdix <farhadg110@gmail.com>
2025-09-26 10:28:10 +0000

    better migrations

--- backend/migrations/20250925105841_flow_conversations.down.sql
+++ backend/migrations/20250925105841_flow_conversations.down.sql
@@ -5,7 +5,6 @@ DROP TRIGGER IF EXISTS update_conversation_on_message ON flow_conversation_messa
 DROP FUNCTION IF EXISTS update_conversation_timestamp();
 
 -- Drop indexes
-DROP INDEX IF EXISTS idx_conversation_message_job;
 DROP INDEX IF EXISTS idx_conversation_message_conversation_time;
 DROP INDEX IF EXISTS idx_flow_conversation_workspace_path;
 
