4b694ff7eb3fd5980b5767f11b8d216aed44bb44
centdix <farhadg110@gmail.com>
2025-09-26 10:28:10 +0000

    better migrations

--- backend/migrations/20250925105841_flow_conversations.up.sql
+++ backend/migrations/20250925105841_flow_conversations.up.sql
@@ -23,8 +23,7 @@ CREATE TABLE flow_conversation_message (
 
 -- Basic indexes for performance
 CREATE INDEX idx_flow_conversation_workspace_path ON flow_conversation(workspace_id, flow_path, updated_at DESC);
-CREATE INDEX idx_conversation_message_conversation_time ON flow_conversation_message(conversation_id, created_at);
-CREATE INDEX idx_conversation_message_job ON flow_conversation_message(job_id) WHERE job_id IS NOT NULL;
+CREATE INDEX idx_conversation_message_conversation_time ON flow_conversation_message(conversation_id, created_at DESC);
 
 CREATE OR REPLACE FUNCTION update_conversation_timestamp()
 RETURNS TRIGGER AS $$
