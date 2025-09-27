0672812d005689f56e4bcc6db31a5d8f36a4b2a3
Ruben Fiszel <ruben@windmill.dev>
2025-09-27 21:29:15 +0000

    nit toggle button

--- frontend/src/lib/components/common/toggleButton-v2/ToggleButton.svelte
+++ frontend/src/lib/components/common/toggleButton-v2/ToggleButton.svelte
@@ -4,20 +4,41 @@
 	import { type ToggleGroupElements, type ToggleGroupItemProps, melt } from '@melt-ui/svelte'
 	import { Info } from 'lucide-svelte'
 
-	export let label: string | undefined = undefined
-	export let iconOnly: boolean = false
-	export let tooltip: string | undefined = undefined
-	export let icon: any | undefined = undefined
-	export let disabled: boolean = false
-	export let selectedColor: string | undefined = undefined
-	export let small = false
-	export let light = false
-	export let iconProps: Record<string, any> = {}
-	export let showTooltipIcon: boolean = false
-	export let documentationLink: string | undefined = undefined
-	export let id: string | undefined = undefined
-	export let item: ToggleGroupElements['item']
-	export let value: ToggleGroupItemProps
+	interface Props {
+		label?: string | undefined
+		iconOnly?: boolean
+		tooltip?: string | undefined
+		icon?: any | undefined
+		disabled?: boolean
+		selectedColor?: string | undefined
+		small?: boolean
+		light?: boolean
+		iconProps?: Record<string, any>
+		showTooltipIcon?: boolean
+		documentationLink?: string | undefined
+		id?: string | undefined
+		item: ToggleGroupElements['item']
+		value: ToggleGroupItemProps
+		class?: string
+	}
+
+	let {
+		label = undefined,
+		iconOnly = false,
+		tooltip = undefined,
+		icon = undefined,
+		disabled = false,
+		selectedColor = undefined,
+		small = false,
+		light = false,
+		iconProps = {},
+		showTooltipIcon = false,
+		documentationLink = undefined,
+		id = undefined,
+		item,
+		value,
+		class: className = ''
+	}: Props = $props()
 </script>
 
 <Tooltip
@@ -34,17 +55,17 @@
 			small ? 'px-1.5 py-0.5 text-2xs' : 'px-2 py-1',
 			light ? 'font-medium' : '',
 			'data-[state=on]:bg-surface data-[state=off]:border-transparent data-[state=on]:border-gray-300 dark:data-[state=on]:border-gray-500',
-			'text-hint hover:text-secondary data-[state=on]:text-tertiary',
+			'text-hint hover:text-secondary data-[state=on]:text-secondary',
 			'bg-surface-secondary hover:bg-surface-hover',
 			disabled ? '!shadow-none' : '',
-			$$props.class
+			className
 		)}
 		use:melt={$item(value)}
 		style={selectedColor ? `--selected-color: ${selectedColor}` : ''}
 	>
 		{#if icon}
-			<svelte:component
-				this={icon}
+			{@const SvelteComponent = icon}
+			<SvelteComponent
 				size={small ? 12 : 14}
 				{...iconProps}
 				class={twMerge(
@@ -64,7 +85,7 @@
 		{/if}
 	</button>
 
-	<svelte:fragment slot="text">
+	{#snippet text()}
 		{tooltip}
-	</svelte:fragment>
+	{/snippet}
 </Tooltip>
