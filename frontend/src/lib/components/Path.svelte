de12b77631e68f2c5cc07c61d99fc08c90b9f087
Ruben Fiszel <ruben@windmill.dev>
2025-09-27 21:47:03 +0000

    nit

--- frontend/src/lib/components/Path.svelte
+++ frontend/src/lib/components/Path.svelte
@@ -437,7 +437,7 @@
 				</div>
 			{/if}
 			{#if !hideUser}
-				<div class="text-xl">/</div>
+				<div class="text-xl text-tertiary">/</div>
 			{/if}
 			<div>
 				{#if meta.ownerKind === 'user'}
@@ -459,7 +459,7 @@
 					</label>
 				{/if}
 			</div>
-			<span class="text-xl">/</span>
+			<span class="text-xl text-tertiary">/</span>
 			<label class="block grow w-full max-w-md">
 				<!-- svelte-ignore a11y_autofocus -->
 				<input
