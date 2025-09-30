b59773b97199399851a55cc99319a49f8972563f
Ruben Fiszel <ruben@windmill.dev>
2025-09-30 19:10:12 +0000

    fix toggle design

--- frontend/src/lib/components/Toggle.svelte
+++ frontend/src/lib/components/Toggle.svelte
@@ -110,11 +110,7 @@
 		<span
 			class={twMerge(
 				'ml-2 font-normal duration-50 select-none',
-				bothOptions || textDisabled
-					? checked
-						? 'text-tertiary'
-						: 'text-disabled'
-					: 'text-tertiary',
+				bothOptions || textDisabled ? (checked ? 'text-primary' : 'text-disabled') : 'text-primary',
 				size === 'xs' || size === '2sm' ? 'text-xs' : size === '2xs' ? 'text-xs' : 'text-sm',
 				textClass
 			)}
