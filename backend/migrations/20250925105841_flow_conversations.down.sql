60e55bf8d80fab3dc2ff204136d10a08fc25bd28
centdix <farhadg110@gmail.com>
2025-10-01 09:28:20 +0000

    add missing drop

--- backend/migrations/20250925105841_flow_conversations.down.sql
+++ backend/migrations/20250925105841_flow_conversations.down.sql
@@ -11,3 +11,6 @@ DROP INDEX IF EXISTS idx_flow_conversation_workspace_path;
 -- Drop tables (order matters due to foreign keys)
 DROP TABLE IF EXISTS flow_conversation_message;
 DROP TABLE IF EXISTS flow_conversation;
+
+-- Drop enum
+DROP TYPE IF EXISTS MESSAGE_TYPE;
