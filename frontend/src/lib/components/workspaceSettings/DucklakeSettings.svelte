07b6efce770a51afc4eff71f81a149f9a62567f6
Diego Imbert <70353967+diegoimbert@users.noreply.github.com>
2025-10-02 22:30:52 +0200

    Clarify Ducklake manual setup instructions (#6737)

--- frontend/src/lib/components/workspaceSettings/DucklakeSettings.svelte
+++ frontend/src/lib/components/workspaceSettings/DucklakeSettings.svelte
@@ -208,9 +208,15 @@
 				setup fails in the middle, but in most cases Windmill will handle it for you.
 				<br /><br />
 
-				If the database <code>ducklake_catalog</code> already exists, assume the setup was already
-				done and do nothing. Otherwise, connect to the Windmill PostgreSQL as the default user (the
-				one in your DATABASE_URL, usually 'postgres') and run :
+				If the database <code>ducklake_catalog</code> already exists, Windmill assumes that the
+				setup was successful and does not do anything. However, it is possible that it failed in the
+				middle (in which case you should have seen an error pop up during setup). There is no
+				rollback as the following operations do not work in a transaction.
+				<br />
+				This is what the setup does :
+				<br /><br />
+				Connect to the Windmill PostgreSQL as the default user (the one in your DATABASE_URL, usually
+				'postgres') and run :
 				<br />
 				<code class="block p-2 border rounded-md bg-surface mt-2">
 					CREATE DATABASE <code>ducklake_catalog</code>;<br />
