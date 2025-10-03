7620eecf05daa9d1f8b5f934ed64b29773841777
Diego Imbert <diego@windmill.dev>
2025-10-03 12:11:06 +0200

    improve dark mode readability monaco

--- frontend/src/lib/components/InputTransformForm.svelte
+++ frontend/src/lib/components/InputTransformForm.svelte
@@ -763,7 +763,7 @@
 						</ArgInput>
 					{:else if arg.expr != undefined}
 						<div
-							class={`bg-surface-secondary rounded-md flex flex-col pl-4 ${inputBorderClass({ forceFocus: focused })}`}
+							class={`${focused ? 'bg-surface' : 'bg-surface-secondary'} transition-colors rounded-md flex flex-col pl-4 ${inputBorderClass({ forceFocus: focused })}`}
 						>
 							<SimpleEditor
 								bind:this={monaco}
