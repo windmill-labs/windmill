4e171d0aa643f8afcd479c11232cb913e985f28e
Ruben Fiszel <ruben@windmill.dev>
2025-10-04 15:31:14 +0000

    flow inputs nits

--- frontend/src/lib/components/flows/map/InsertModuleInner.svelte
+++ frontend/src/lib/components/flows/map/InsertModuleInner.svelte
@@ -69,7 +69,7 @@
 		{#if selectedKind != 'preprocessor' && selectedKind != 'flow'}
 			<ToggleHubWorkspaceQuick bind:selected={preFilter} />
 		{/if}
-		<RefreshButton {loading} on:click={() => (refreshCount.val += 1)} />
+		<RefreshButton size="md" light {loading} on:click={() => (refreshCount.val += 1)} />
 	</div>
 
 	<div class="flex flex-row grow min-h-0">
