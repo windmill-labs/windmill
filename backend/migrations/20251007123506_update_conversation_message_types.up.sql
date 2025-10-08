44d32c25346058bf4531d3181be26d17730817c6
centdix <farhadg110@gmail.com>
2025-10-08 16:37:01 +0000

    fix migration

--- backend/migrations/20251007123506_update_conversation_message_types.up.sql
+++ backend/migrations/20251007123506_update_conversation_message_types.up.sql
@@ -1,9 +1,8 @@
 -- Add up migration script here
 
--- Extend MESSAGE_TYPE enum to include 'system' and 'tool'
-ALTER TYPE MESSAGE_TYPE ADD VALUE 'system';
+-- Extend MESSAGE_TYPE enum to include 'tool'
 ALTER TYPE MESSAGE_TYPE ADD VALUE 'tool';
 
--- Add step_name column to flow_conversation_message table
+-- Add step_name and success columns to flow_conversation_message table
 ALTER TABLE flow_conversation_message ADD COLUMN step_name VARCHAR(255);
 ALTER TABLE flow_conversation_message ADD COLUMN success BOOLEAN DEFAULT TRUE NOT NULL;
