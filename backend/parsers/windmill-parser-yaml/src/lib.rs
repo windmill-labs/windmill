e3f7be1dc637d6f0720dd168d0d708e60f3c4f42
wendrul <dethomassin.etienne@gmail.com>
2025-10-09 16:34:25 +0200

    Move tests to lib.rs

--- backend/parsers/windmill-parser-yaml/src/lib.rs
+++ backend/parsers/windmill-parser-yaml/src/lib.rs
@@ -864,3 +864,88 @@ pub fn add_versions_to_requirements_yaml(
 
     Ok((out_str, logs))
 }
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_ansible_assets() {
+        let p = r#"
+---
+inventory:
+  - resource_type: ansible_inventory
+    # You can pin an inventory to this script by hardcoding the resource path:
+    # resource: u/user/your_resource
+# - name: hcloud.yml
+#   resource_type: dynamic_inventory
+
+additional_inventories:
+  - options: ["a", "b", "c"]
+
+options:
+  - verbosity: vvv
+
+delegate_to_git_repo:
+  resource: u/admin/git_reportino
+  playbook: ./playbooks/playbook.yml
+  commit: 7sh7dh73h7dhd299d91hd1hdh3d3hygh4372
+
+
+# File resources will be written in the relative `target` location before
+# running the playbook
+files:
+  - resource: u/user/fabulous_jinja_template
+    target:  ./config_template.j2
+  - variable: u/user/ssh_key
+    target:  ./ssh_key
+    mode: '0600'
+
+# Define the arguments of the windmill script
+extra_vars:
+  world_qualifier:
+    type: string
+
+# If using Ansible Vault:
+# vault_password: u/user/ansible_vault_password
+
+dependencies:
+  galaxy:
+    collections:
+      - name: community.general
+      - name: community.vmware
+    roles:
+  python:
+    - jmespath
+---
+- name: Echo
+  hosts: 127.0.0.1
+  connection: local
+  vars:
+    my_result:
+      a: 2
+      b: true
+      c: "Hello"
+
+  tasks:
+  - name: Print debug message
+    debug:
+      msg: "Hello, {{world_qualifier}} world!"
+  - name: Write variable my_result to result.json
+    delegate_to: localhost
+    copy:
+      content: "{{ my_result | to_json }}"
+      dest: result.json
+"#;
+        let a = parse_assets(p).unwrap();
+        println!("The resulting assets are: {}", a.len());
+
+        let a = parse_ansible_reqs(p).unwrap();
+        println!("The resulting reqs are: {:#?}", a);
+
+        let a = parse_ansible_sig(p).unwrap();
+        println!("The resulting sig is: {:#?}", a);
+
+        let a = parse_delegate_to_git_repo(p).unwrap();
+        println!("The resulting delegate_to_kit_repo is: {:#?}", a);
+    }
+}
