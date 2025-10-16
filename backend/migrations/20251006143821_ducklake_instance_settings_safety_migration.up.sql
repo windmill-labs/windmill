a4a502cf3af3cd288471fc71a12651de6641bd3f
Diego Imbert <70353967+diegoimbert@users.noreply.github.com>
2025-10-16 22:39:11 +0200

    ducklake safety for instance_settings.yaml users (#6844)

---
+++ backend/migrations/20251006143821_ducklake_instance_settings_safety_migration.up.sql
@@ -0,0 +1,27 @@
+-- Copy of 20250731132157_ducklake_instance_settings.up.sql
+-- Pushing a instance_settings.yaml without ducklake_user_pg_pwd will remove it from the global settings
+-- And the next migration will fail because it will try to insert a NULL value
+
+INSERT INTO global_settings (name, value)
+VALUES ('ducklake_user_pg_pwd', ('"' || gen_random_uuid()::text || '"')::jsonb)
+ON CONFLICT DO NOTHING;
+
+-- Cannot simply create the user because Postgres expect a static string for the password
+-- Also we cannot drop the user easily in the down migration because databases will depend on it
+-- And we cannot drop databases in transactions (migrations)
+
+DO $$
+DECLARE
+    pwd text;
+BEGIN
+    SELECT trim(both '"' from value::text) INTO pwd FROM global_settings WHERE name = 'ducklake_user_pg_pwd';
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'ducklake_user') THEN
+        EXECUTE format('CREATE USER ducklake_user WITH PASSWORD %L', pwd);
+    ELSE
+      EXECUTE format('ALTER USER ducklake_user WITH PASSWORD %L', pwd);
+    END IF;
+EXCEPTION
+  WHEN others THEN
+    RAISE NOTICE 'ducklake_user migration error, skipping.';
+END
+$$;
