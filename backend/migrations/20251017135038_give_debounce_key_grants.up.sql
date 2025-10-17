d2ae0533f41b7bd54add40f2e20b1d619514fa7c
Ruben Fiszel <ruben@windmill.dev>
2025-10-17 13:59:55 +0000

    fix select grant

--- backend/migrations/20251017135038_give_debounce_key_grants.up.sql
+++ backend/migrations/20251017135038_give_debounce_key_grants.up.sql
@@ -1,4 +1,4 @@
 -- Add up migration script here
 
-GRANT SELECT ON debounce_key TO windmill_user;
-GRANT SELECT ON debounce_key TO windmill_admin;
+GRANT SELECT, UPDATE ON debounce_key TO windmill_user;
+GRANT SELECT, UPDATE ON debounce_key TO windmill_admin;
