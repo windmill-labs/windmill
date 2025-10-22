89d0b85096cf3a097ee91fb677b91cd673c2355d
Ruben Fiszel <ruben@windmill.dev>
2025-10-22 18:03:09 +0000

    fix: compute dependencies of apps in deploy to UI

--- frontend/src/lib/components/Toggle.svelte
+++ frontend/src/lib/components/Toggle.svelte
@@ -1,4 +1,3 @@
-<!-- @migration-task Error while migrating Svelte code: props is used together with named props in a way that cannot be automatically migrated. -->
 <script lang="ts">
 	import { createBubbler, stopPropagation } from 'svelte/legacy'
 
