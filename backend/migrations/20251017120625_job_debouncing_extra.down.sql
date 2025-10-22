4acc8182285b70da9ab7dc52d4678f92a2f6d9ee
pyranota <pyra@duck.com>
2025-10-22 18:16:12 +0200

    remove index
    Signed-off-by: pyranota <pyra@duck.com>

--- backend/migrations/20251017120625_job_debouncing_extra.down.sql
+++ backend/migrations/20251017120625_job_debouncing_extra.down.sql
@@ -1,5 +1,4 @@
 -- Add down migration script here
-DROP INDEX IF EXISTS idx_script_workspace_debounce_key;
 ALTER TABLE script DROP COLUMN IF EXISTS debounce_key;
 ALTER TABLE script DROP COLUMN IF EXISTS debounce_delay_s;
 
--- backend/migrations/20251017120625_job_debouncing_extra.up.sql
+++ backend/migrations/20251017120625_job_debouncing_extra.up.sql
@@ -1,6 +1,3 @@
 -- Add up migration script here
 ALTER TABLE script ADD COLUMN debounce_key VARCHAR(255);
 ALTER TABLE script ADD COLUMN debounce_delay_s INTEGER;
-
--- Add index on debounce_key for efficient lookups during debounce operations
-CREATE INDEX IF NOT EXISTS idx_script_workspace_debounce_key ON script(workspace_id, debounce_key) WHERE debounce_key IS NOT NULL;
