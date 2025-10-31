8696c7ddbceae41225c3d9c3f15a24efa58d068f
Alexander Petric <alpetric@users.noreply.github.com>
2025-10-31 02:33:59 -0400

    fix: search drop down z index too low (#7017)

--- frontend/src/lib/components/select/SelectDropdown.svelte
+++ frontend/src/lib/components/select/SelectDropdown.svelte
@@ -153,7 +153,7 @@
 		<div
 			class={twMerge(
 				disablePortal ? 'absolute' : 'fixed',
-				'z-[5001] text-primary text-sm select-none',
+				'z-[5002] text-primary text-sm select-none',
 				dropdownPos.isBelow ? '' : 'flex flex-col justify-end',
 				uiState.visible ? '' : 'pointer-events-none',
 				className
