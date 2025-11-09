c1aa0b74b52c1ebf8ce880523e703d8a110337df
Ruben Fiszel <ruben@windmill.dev>
2025-11-09 18:35:50 +0000

    push

--- frontend/src/lib/components/raw_apps/RawAppInlineScriptsPanel.svelte
+++ frontend/src/lib/components/raw_apps/RawAppInlineScriptsPanel.svelte
@@ -13,43 +13,33 @@
 	let { runnables, selectedRunnable = $bindable(), appPath, initRunnablesContent }: Props = $props()
 </script>
 
-<Splitpanes
-	class={twMerge('!overflow-visible')}
-	style={width !== undefined ? `width:${width}px;` : 'width: 100%;'}
->
-	<Pane size={20}>
-		<RawAppInlineScriptPanelList bind:selectedRunnable {runnables} on:hidePanel />
-	</Pane>
-	<Pane size={80}>
-		{#if !selectedRunnable}
-			<div class="text-sm text-secondary text-center py-8 px-2">
-				Select a runnable on the left panel
-			</div>
-		{:else if runnables?.[selectedRunnable]}
-			{#key selectedRunnable}
-				<RawAppInlineScripRunnable
-					{appPath}
-					on:createScriptFromInlineScript={(e) => {
-						createScriptFromInlineScript(
-							selectedRunnable ?? '',
-							e.detail,
-							$workspaceStore ?? '',
-							appPath
-						)
-					}}
-					on:delete={() => {
-						if (selectedRunnable) {
-							delete runnables[selectedRunnable]
-						}
-						selectedRunnable = undefined
-					}}
-					id={selectedRunnable}
-					bind:runnable={runnables[selectedRunnable]}
-				/>{/key}
-		{:else}
-			<div class="text-sm text-primary text-center py-8 px-2">
-				No runnable at id {selectedRunnable}
-			</div>
-		{/if}
-	</Pane>
-</Splitpanes>
+{#if !selectedRunnable}
+	<div class="text-sm text-secondary text-center py-8 px-2">
+		Select a runnable on the left panel
+	</div>
+{:else if runnables?.[selectedRunnable]}
+	{#key selectedRunnable}
+		<RawAppInlineScripRunnable
+			{appPath}
+			on:createScriptFromInlineScript={(e) => {
+				createScriptFromInlineScript(
+					selectedRunnable ?? '',
+					e.detail,
+					$workspaceStore ?? '',
+					appPath
+				)
+			}}
+			on:delete={() => {
+				if (selectedRunnable) {
+					delete runnables[selectedRunnable]
+				}
+				selectedRunnable = undefined
+			}}
+			id={selectedRunnable}
+			bind:runnable={runnables[selectedRunnable]}
+		/>{/key}
+{:else}
+	<div class="text-sm text-primary text-center py-8 px-2">
+		No runnable at id {selectedRunnable}
+	</div>
+{/if}
