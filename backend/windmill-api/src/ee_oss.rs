2b3c7efb5a7923e7d4e91e4863391a91586a78ee
Ruben Fiszel <ruben@windmill.dev>
2025-11-14 11:07:42 +0000

    nit

--- backend/windmill-api/src/ee_oss.rs
+++ backend/windmill-api/src/ee_oss.rs
@@ -13,7 +13,7 @@ use anyhow::anyhow;
 #[cfg(all(feature = "enterprise", not(feature = "private")))]
 use {std::sync::Arc, tokio::sync::RwLock};
 #[cfg(not(feature = "private"))]
-pub async fn validate_license_key(_license_key: String) -> anyhow::Result<(String, bool)> {
+pub async fn validate_license_key(_license_key: String, _db: &DB) -> anyhow::Result<(String, bool)> {
     // Implementation is not open source
     Err(anyhow!("License can't be validated in Windmill CE"))
 }
