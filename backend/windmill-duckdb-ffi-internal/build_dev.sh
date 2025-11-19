3085af4517d00b5342b313c31a83a3187d43026b
Ruben Fiszel <ruben@windmill.dev>
2025-11-19 11:15:31 +0000

    fix test

--- backend/windmill-duckdb-ffi-internal/build_dev.sh
+++ backend/windmill-duckdb-ffi-internal/build_dev.sh
@@ -1,2 +1,2 @@
-CARGO_NET_GIT_FETCH_WITH_CLI=1 cargo build --release -p windmill_duckdb_ffi_internal
+CARGO_NET_GIT_FETCH_WITH_CLI=true cargo build --release -p windmill_duckdb_ffi_internal
 cp target/release/libwindmill_duckdb_ffi_internal.* ../target/debug/
