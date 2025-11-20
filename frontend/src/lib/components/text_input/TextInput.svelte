6e03377d973fa73e5261e9719e9feba4c21d74cf
Diego Imbert <diego@windmill.dev>
2025-11-20 13:12:10 +0100

    Fix alignment issues and TextArea min-height taller

--- frontend/src/lib/components/text_input/TextInput.svelte
+++ frontend/src/lib/components/text_input/TextInput.svelte
@@ -20,6 +20,7 @@
 	export const inputBaseClass =
 		'rounded-md focus:ring-0 no-default-style text-xs text-primary font-normal !bg-surface-input disabled:!bg-surface-disabled disabled:!border-transparent disabled:!text-disabled disabled:cursor-not-allowed shadow-none !placeholder-hint'
 
+	import autosize from '$lib/autosize'
 	import { ButtonType } from '$lib/components/common/button/model'
 
 	export const inputSizeClasses = {
@@ -38,49 +39,69 @@
 	}
 </script>
 
-<script lang="ts">
-	import type { HTMLInputAttributes } from 'svelte/elements'
+<script lang="ts" generics="UnderlyingInputElT extends 'input' | 'textarea' = 'input'">
+	import type { HTMLInputAttributes, HTMLTextareaAttributes } from 'svelte/elements'
 	import { twMerge } from 'tailwind-merge'
 
-	type Props = {
-		inputProps?: HTMLInputAttributes
+	type Props<UnderlyingInputElT extends 'input' | 'textarea'> = {
+		inputProps?: UnderlyingInputElT extends 'input' ? HTMLInputAttributes : HTMLTextareaAttributes
 		value?: string
 		class?: string
 		error?: string | boolean
 		size?: ButtonType.UnifiedSize
 		unifiedHeight?: boolean
+		underlyingInputEl?: UnderlyingInputElT
 	}
 
 	export function focus() {
 		inputEl?.focus()
 	}
 
-	let inputEl: HTMLInputElement | undefined = $state()
+	let inputEl: HTMLInputElement | HTMLTextAreaElement | undefined = $state()
 
 	let {
-		inputProps,
+		inputProps: _inputProps,
 		value = $bindable(),
 		class: className = '',
 		error,
 		size = 'md',
-		unifiedHeight = true
-	}: Props = $props()
+		unifiedHeight = true,
+		underlyingInputEl: _underlyingInputEl
+	}: Props<UnderlyingInputElT> = $props()
+
+	let underlyingInputEl = $derived(_underlyingInputEl ?? ('input' as const))
+	let inputProps = $derived(_inputProps as any)
+
+	let fullClassName = $derived(
+		twMerge(
+			inputBaseClass,
+			inputSizeClasses[size],
+			'[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
+			inputBorderClass({ error: !!error }),
+			unifiedHeight ? ButtonType.UnifiedHeightClasses[size] : '',
+			'w-full',
+			className
+		)
+	)
 </script>
 
-<input
-	{...inputProps}
-	class={twMerge(
-		inputBaseClass,
-		inputSizeClasses[size],
-		'w-full',
-		'[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none',
-		inputBorderClass({ error: !!error }),
-		unifiedHeight ? ButtonType.UnifiedHeightClasses[size] : '',
-		className
-	)}
-	onpointerdown={(e) => {
-		e.stopImmediatePropagation()
-	}}
-	bind:this={inputEl}
-	bind:value
-/>
+{#if underlyingInputEl === 'textarea'}
+	<!-- We hardcode the py value because since textareas can be multiline, we can't rely on
+ 	  	 the fact that it auto-centers -->
+	<textarea
+		class={twMerge('py-[0.45rem]', fullClassName)}
+		{...inputProps}
+		onpointerdown={(e) => e.stopImmediatePropagation()}
+		bind:this={inputEl}
+		bind:value
+		use:autosize
+	></textarea>
+{:else if underlyingInputEl === 'input'}
+	<input
+		{...inputProps}
+		class={fullClassName}
+		onpointerdown={(e) => e.stopImmediatePropagation()}
+		bind:this={inputEl}
+		bind:value
+	/>
+{/if}
