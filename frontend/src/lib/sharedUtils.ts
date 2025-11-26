e2fad205700c6998d5991673d43e9478abdec6eb
Ruben Fiszel <ruben@windmill.dev>
2025-11-26 07:32:51 +0000

    all

--- frontend/src/lib/sharedUtils.ts
+++ frontend/src/lib/sharedUtils.ts
@@ -1,6 +1,8 @@
 export { updatePolicy } from './components/apps/editor/appPolicy'
 export { genWmillTs } from './components/raw_apps/utils'
 export { updateRawAppPolicy } from './components/raw_apps/rawAppPolicy'
+export * as wmillTsRaw from "./wmill.ts?raw";
+
 
 export function capitalize(word: string): string {
 	return word ? word.charAt(0).toUpperCase() + word.slice(1) : ''
