7b3f3ac4b3c1c5bf47860606b192e00836886fa1
Diego Imbert <diego@windmill.dev>
2025-11-27 14:33:38 +0100

    Better typed client

--- typescript-client/sqlUtils.ts
+++ typescript-client/sqlUtils.ts
@@ -1,9 +1,52 @@
 import { getWorkspace, JobService } from "./client";
 
+type ResultCollection =
+  | "last_statement_all_rows"
+  | "last_statement_first_row"
+  | "last_statement_all_rows_scalar"
+  | "last_statement_first_row_scalar"
+  | "all_statements_all_rows"
+  | "all_statements_first_row"
+  | "all_statements_all_rows_scalar"
+  | "all_statements_first_row_scalar"
+  | "legacy";
+
+type FetchParams<ResultCollectionT extends ResultCollection> = {
+  resultCollection?: ResultCollectionT;
+};
+
+type SqlResult<ResultCollectionT extends ResultCollection> =
+  ResultCollectionT extends "last_statement_first_row"
+    ? object
+    : ResultCollectionT extends "all_statements_first_row"
+    ? object[]
+    : ResultCollectionT extends "last_statement_all_rows"
+    ? object[]
+    : ResultCollectionT extends "all_statements_all_rows"
+    ? object[][]
+    : ResultCollectionT extends "last_statement_all_rows_scalar"
+    ? any[]
+    : ResultCollectionT extends "all_statements_all_rows_scalar"
+    ? any[][]
+    : ResultCollectionT extends "last_statement_first_row_scalar"
+    ? any
+    : ResultCollectionT extends "all_statements_first_row_scalar"
+    ? any[]
+    : unknown;
 export type SqlStatement = {
   content: string;
+
   args: Record<string, any>;
-  fetch(): Promise<any>;
+
+  fetch<
+    ResultCollectionT extends ResultCollection = "last_statement_first_row"
+  >(
+    params?: FetchParams<ResultCollectionT | ResultCollection> // The union is for auto-completion
+  ): Promise<SqlResult<ResultCollectionT>>;
+
+  fetchOne(
+    params?: Omit<FetchParams<"last_statement_first_row">, "resultCollection">
+  ): Promise<SqlResult<"last_statement_first_row">>;
 };
 
 export interface SqlTemplateFunction {
@@ -67,16 +110,24 @@ function sqlProviderImpl(
       ducklake: "duckdb" as const,
     }[provider];
 
+    async function fetch<ResultCollectionT extends ResultCollection>({
+      resultCollection,
+    }: FetchParams<ResultCollectionT> = {}) {
+      if (resultCollection)
+        content = `-- result_collection=${resultCollection}\n${content}`;
+      let result = await JobService.runScriptPreviewInline({
+        workspace: getWorkspace(),
+        requestBody: { args, content, language },
+      });
+      return result as SqlResult<ResultCollectionT>;
+    }
+
     return {
       content,
       args,
-      fetch: async () => {
-        let result = await JobService.runScriptPreviewInline({
-          workspace: getWorkspace(),
-          requestBody: { args, content, language },
-        });
-        return result;
-      },
+      fetch,
+      fetchOne: (params) =>
+        fetch({ ...params, resultCollection: "last_statement_first_row" }),
     } satisfies SqlStatement;
   };
   return sql;
