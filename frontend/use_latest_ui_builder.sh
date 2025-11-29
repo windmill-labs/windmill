5dcd5a0979d1d778048a26922483c62dbab81925
Ruben Fiszel <ruben@windmill.dev>
2025-11-29 01:03:24 +0000

    all

--- frontend/use_latest_ui_builder.sh
+++ frontend/use_latest_ui_builder.sh
@@ -2,4 +2,5 @@ cd ../../windmill-code-ui-builder
 HASH=$(git rev-parse --short HEAD)
 HASH=${HASH::-1}
 
+echo "Using UI Builder hash: ${HASH}"
 sed -i "s/ui_builder-[^.]*\.tar\.gz/ui_builder-${HASH}.tar.gz/" ../windmill/frontend/scripts/untar_ui_builder.js
