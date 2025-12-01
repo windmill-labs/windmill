5a9b9ca439b189074b23ee6f824cbcf34b222f19
Ruben Fiszel <ruben@windmill.dev>
2025-12-01 07:42:01 +0000

    all

--- cli/src/commands/app/apps.ts
+++ cli/src/commands/app/apps.ts
@@ -16,6 +16,7 @@ import { ListableApp, Policy } from "../../../gen/types.gen.ts";
 import { GlobalOptions, isSuperset } from "../../types.ts";
 import { readInlinePathSync } from "../../utils/utils.ts";
 import devCommand from "./dev.ts";
+import lintCommand from "./lint.ts";
 
 export interface AppFile {
   value: any;
@@ -205,6 +206,7 @@ const command = new Command()
   .arguments("<file_path:string> <remote_path:string>")
   .action(push as any)
   .command("dev", devCommand)
+  .command("lint", lintCommand)
   .command(
     "generate-locks",
     "re-generate the lockfiles for app runnables inline scripts that have changed"
