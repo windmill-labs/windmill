6ce6e52439bc6a4bda8a9d27b87134ceb7bea8fe
hugocasa <hugo@casademont.ch>
2025-12-23 16:26:23 +0100

    improve windmill-components compat with hub (#7437)

--- frontend/src/lib/components/copilot/chat/sharedChatState.svelte.ts
+++ frontend/src/lib/components/copilot/chat/sharedChatState.svelte.ts
@@ -1,17 +1,21 @@
+import { BROWSER } from 'esm-env'
+
 export const triggerablesByAi = $state<TriggerablesMap>({})
 
 export type TriggerablesMap = Record<
-    string,
-    {
-        description: string
-        onTrigger: ((value?: string) => void) | undefined
-    }
+	string,
+	{
+		description: string
+		onTrigger: ((value?: string) => void) | undefined
+	}
 >
 export const DEFAULT_SIZE = 22
 
 type ChatState = {
-    size: number
+	size: number
 }
+
+// we first check BROWSER before localStorage to avoid SSR issues when using Drawer in a SvelteKit app (chatState is imported in Drawer.svelte)
 export const chatState = $state<ChatState>({
-    size: localStorage.getItem('ai-chat-open') === 'true' ? DEFAULT_SIZE : 0,
+	size: BROWSER && localStorage.getItem('ai-chat-open') === 'true' ? DEFAULT_SIZE : 0
 })
