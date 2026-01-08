5e3425e4334861d88aaa5b4a716d909a1b90b3f0
Diego Imbert <diego@windmill.dev>
2026-01-08 20:36:34 +0100

    bigger timeout

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+	timeout: 60 * 1000,
 	use: {
 		/* Base URL to use in actions like `await page.goto('')`. */
 		baseURL: process.env.BASE_URL || 'http://localhost:3000',
