cf831b2e79d9ab05684e6fd5d10a7b7f7f68be90
Diego Imbert <diego@windmill.dev>
2026-01-09 11:28:27 +0100

    start e2e docker compose

---
+++ e2e-tests/README.md
@@ -0,0 +1,3 @@
+# E2E testing environment
+
+This setups an environment with all necessary dependencies to run the Playwright E2E tests (Necessary databases, S3 providers, languages etc)
--- frontend/e2e/resource-dbmanager.spec.ts
+++ frontend/e2e/resource-dbmanager.spec.ts
@@ -1,82 +1,103 @@
-// import { test, expect, Page } from '@playwright/test'
-// import { runDbManagerSimpleCRUDTest } from './DbManagerPage'
-// import { DbType } from '../src/lib/components/dbTypes'
-// import { Toast } from './utils'
+import { test, expect, Page } from '@playwright/test'
+import { runDbManagerSimpleCRUDTest } from './DbManagerPage'
+import { DbType } from '../src/lib/components/dbTypes'
+import { Toast } from './utils'
 
-// test.describe('DB Manager', () => {
-// 	for (const dbType of ['postgresql', 'mysql', 'bigquery', 'ms_sql_server', 'snowflake'] as const) {
-// 		test(`setup a ${dbType} resource and test simple CRUD with DB Manager`, async ({ page }) => {
-// 			await setupNewResourceAndOpenDbManager(page, dbType)
-// 			await runDbManagerSimpleCRUDTest(page, dbType)
-// 		})
-// 	}
-// })
+test.describe('DB Manager', () => {
+	for (const dbType of ['postgresql', 'mysql', 'bigquery', 'ms_sql_server', 'snowflake'] as const) {
+		test(`setup a ${dbType} resource and test simple CRUD with DB Manager`, async ({ page }) => {
+			await setupNewResourceAndOpenDbManager(page, dbType)
+			await runDbManagerSimpleCRUDTest(page, dbType)
+		})
+	}
+})
 
-// const resourceByDbType = {
-// 	postgresql: {
-// 		host: 'localhost',
-// 		port: 5432,
-// 		dbname: 'testing',
-// 		user: 'postgres',
-// 		password: 'changeme',
-// 		sslmode: 'disable',
-// 		root_certificate_pem: ''
-// 	},
-// 	mysql: {}, // TODO
-// 	bigquery: {}, // TODO
-// 	ms_sql_server: {}, // TODO
-// 	snowflake: {} // TODO
-// } as const
+const resourceByDbType = {
+	postgresql: {
+		host: 'localhost',
+		port: 5432,
+		dbname: 'testing',
+		user: 'postgres',
+		password: 'changeme',
+		sslmode: 'disable'
+	},
+	mysql: {
+		host: 'localhost',
+		port: 3306,
+		user: 'test_user',
+		database: 'test_db',
+		password: 'test_password',
+		ssl: false
+	},
+	oracle: {
+		user: 'test_user',
+		password: 'test_password',
+		database: 'localhost:1521/test_db'
+	},
+	ms_sql_server: {
+		host: 'localhost',
+		user: 'sa',
+		password: 'MsSql_Pass123!',
+		port: 1433,
+		dbname: 'master',
+		instance_name: '',
+		trust_cert: true,
+		ca_cert: '',
+		encrypt: true
+	},
+	bigquery: {}, // TODO
+	snowflake: {} // TODO
+} as const
 
-// async function setupNewResource(
-// 	page: Page,
-// 	resourceType: DbType
-// ): Promise<{ resourceName: string }> {
-// 	// Generate unique ID with timestamp
-// 	const resourceName = `${resourceType}_${process.env.TEST_UNIQUE_ID}`
+async function setupNewResource(
+	page: Page,
+	resourceType: DbType
+): Promise<{ resourceName: string }> {
+	// Generate unique ID with timestamp
+	const resourceName = `${resourceType}_${process.env.TEST_UNIQUE_ID}`
 
-// 	await page.goto('/resources')
+	await page.goto('/resources')
 
-// 	const newDataTableButton = page.locator('button:text-is("Add resource")')
-// 	await newDataTableButton.waitFor({ state: 'visible' })
-// 	await newDataTableButton.click()
+	const newDataTableButton = page.locator('button:text-is("Add resource")')
+	await newDataTableButton.waitFor({ state: 'visible' })
+	await newDataTableButton.click()
 
-// 	const addResourceDrawer = page.locator('#add-resource-drawer')
-// 	await expect(addResourceDrawer).toBeVisible()
+	const addResourceDrawer = page.locator('#add-resource-drawer')
+	await expect(addResourceDrawer).toBeVisible()
 
-// 	const resourceTypeBtn = addResourceDrawer.locator(`button:has-text("${resourceType}")`)
-// 	await resourceTypeBtn.waitFor({ state: 'visible' })
-// 	await resourceTypeBtn.click()
+	const resourceTypeBtn = addResourceDrawer.locator(`button:has-text("${resourceType}")`)
+	await resourceTypeBtn.waitFor({ state: 'visible' })
+	await resourceTypeBtn.click()
 
-// 	const asJsonToggle = addResourceDrawer.locator('label.as-json-toggle')
-// 	await expect(asJsonToggle).toBeVisible()
-// 	await asJsonToggle.check()
+	const asJsonToggle = addResourceDrawer.locator('label.as-json-toggle')
+	await expect(asJsonToggle).toBeVisible()
+	await asJsonToggle.check()
 
-// 	await addResourceDrawer.locator('input#path').fill(resourceName)
+	await addResourceDrawer.locator('input#path').fill(resourceName)
 
-// 	const resourceObj = resourceByDbType[resourceType]
-// 	const jsonEditor = page.locator('.simple-editor .view-lines')
-// 	await expect(jsonEditor).toBeVisible()
-// 	await jsonEditor.click({ clickCount: 4 }) // Select all existing text
-// 	// We do copy paste to avoid issues with monaco helping with autocomplete and messing up the test
-// 	await page.context().grantPermissions(['clipboard-read', 'clipboard-write'])
-// 	await page.evaluate((c) => navigator.clipboard.writeText(c), JSON.stringify(resourceObj))
-// 	await page.keyboard.press('ControlOrMeta+V')
+	const resourceObj = resourceByDbType[resourceType]
+	const jsonEditor = page.locator('.simple-editor .view-lines')
+	await expect(jsonEditor).toBeVisible()
+	await jsonEditor.click({ clickCount: 4 }) // Select all existing text
+	// We do copy paste to avoid issues with monaco helping with autocomplete and messing up the test
+	await page.context().grantPermissions(['clipboard-read', 'clipboard-write'])
+	await page.evaluate((c) => navigator.clipboard.writeText(c), JSON.stringify(resourceObj))
+	await page.keyboard.press('ControlOrMeta+V')
 
-// 	const saveButton = addResourceDrawer.locator('button:has-text("Save")')
-// 	await saveButton.click()
+	const saveButton = addResourceDrawer.locator('button:has-text("Save")')
+	await saveButton.click()
 
-// 	await Toast.expectSuccess(page, 'Saved resource')
+	await Toast.expectSuccess(page, 'Saved resource')
 
-// 	return { resourceName }
-// }
+	return { resourceName }
+}
 
-// async function setupNewResourceAndOpenDbManager(page: Page, dbType: DbType) {
-// 	let { resourceName } = await setupNewResource(page, dbType)
+async function setupNewResourceAndOpenDbManager(page: Page, dbType: DbType) {
+	let { resourceName } = await setupNewResource(page, dbType)
 
-// 	const resourceRow = page.locator(`table tr:has-text("${resourceName}")`)
-// 	await expect(resourceRow).toBeVisible()
+	const resourceRow = page.locator(`table tr:has-text("${resourceName}")`)
+	await expect(resourceRow).toBeVisible()
 
-// 	const manageButton = resourceRow.locator('button:has-text("Manage")')
-// 	await manageButton.click()
-// }
+	const manageButton = resourceRow.locator('button:has-text("Manage")')
+	await manageButton.click()
+}
