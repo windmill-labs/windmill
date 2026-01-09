9f49ab9b680fd40657ae576c6889b4c4e7ce5a8a
Diego Imbert <diego@windmill.dev>
2026-01-09 15:00:48 +0100

    super nit

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1972,7 +1972,7 @@ export function buildReactiveObj<T extends object>(fields: {
 	return obj
 }
 
-export function pick<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
+export function pick<T extends object, K extends keyof T>(obj: T, keys: readonly K[]): Pick<T, K> {
 	const result = {} as Pick<T, K>
 	for (const key of keys) {
 		if (key in obj) {
