b46b3872a9cb38873f5d6dae7007afa4489f7aa4
Diego Imbert <diego@windmill.dev>
2026-01-13 13:33:12 +0100

    nit remove Default Email filter

--- frontend/src/lib/components/runs/RunsFilter.svelte
+++ frontend/src/lib/components/runs/RunsFilter.svelte
@@ -8,7 +8,7 @@
 	import Label from '../Label.svelte'
 	import Section from '../Section.svelte'
 	import { enterpriseLicense, workspaceStore } from '$lib/stores'
-	import { createEventDispatcher, untrack } from 'svelte'
+	import { createEventDispatcher } from 'svelte'
 	import ToggleButtonMore from '../common/toggleButton-v2/ToggleButtonMore.svelte'
 	import Popover from '$lib/components/meltComponents/Popover.svelte'
 	import Select from '../select/Select.svelte'
@@ -976,10 +976,12 @@
 							{`Filter by what kind of trigger started the run.`}
 						</span>
 						<Select
-							items={jobTriggerKinds.map((value) => ({
-								label: triggerDisplayNamesMap[value],
-								value
-							}))}
+							items={jobTriggerKinds
+								.map((value) => ({
+									label: triggerDisplayNamesMap[value],
+									value
+								}))
+								.filter((d) => d.value !== 'default_email')}
 							bind:value={() => jobTriggerKind ?? undefined, (v) => (jobTriggerKind = v ?? null)}
 							clearable
 						/>
