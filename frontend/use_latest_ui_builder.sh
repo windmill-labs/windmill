687175c6a85f47c707bb429008c93ec0981c50e2
Guilhem <guilhemlemouel@gmail.com>
2026-01-20 18:30:52 +0000

    fix(frontend): improve raw app history  (#7625)
    * fix raw app header overflow
    
    * update ui-builder hash
    
    * Make monaco default size match brand guidelines
    
    * nit
    
    * Move run button to test panel
    
    * wip improve history
    
    * add current checkout point
    
    * fix logic to switch wetween history state
    
    * improve history visualisation
    
    * improve animations
    
    * nit
    
    * remove test page
    
    * fix timing issue when selecty history entries
    
    * update ui_builder hash
    
    * remove dev file
    
    * nit
    
    * revert setting editor font to 13.5 px
    
    * update ui builder
    
    ---------
    
    Co-authored-by: Ruben Fiszel <ruben@windmill.dev>

--- frontend/use_latest_ui_builder.sh
+++ frontend/use_latest_ui_builder.sh
@@ -1,6 +1,20 @@
+#!/bin/bash
+
+# Auto-detect operating system
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  IS_MAC=true
+else
+  IS_MAC=false
+fi
+
 cd ../../windmill-code-ui-builder
 HASH=$(git rev-parse --short HEAD)
 HASH=${HASH::-1}
 
 echo "Using UI Builder hash: ${HASH}"
-sed -i "s/ui_builder-[^.]*\.tar\.gz/ui_builder-${HASH}.tar.gz/" ../windmill/frontend/scripts/untar_ui_builder.js
+
+if [ "$IS_MAC" = true ]; then
+  sed -i '' "s/ui_builder-[^.]*\.tar\.gz/ui_builder-${HASH}.tar.gz/" ../windmill/frontend/scripts/untar_ui_builder.js
+else
+  sed -i "s/ui_builder-[^.]*\.tar\.gz/ui_builder-${HASH}.tar.gz/" ../windmill/frontend/scripts/untar_ui_builder.js
+fi
