9bce9f1b23a4eb4ab1b9ebc6214df561a5738931
wendrul <dethomassin.etienne@gmail.com>
2026-01-22 18:52:45 +0100

    fix: Avoid logout when using deploy ui and no access to some deps

--- frontend/src/lib/components/DeployWorkspace.svelte
+++ frontend/src/lib/components/DeployWorkspace.svelte
@@ -34,6 +34,7 @@
 	import type { App } from './apps/types'
 	import { getAllGridItems } from './apps/editor/appUtils'
 	import { isRunnableByPath } from './apps/inputType'
+	import { sendUserToast } from '$lib/toast'
 
 	const dispatch = createEventDispatcher()
 
@@ -53,7 +54,8 @@
 		hideButton = false
 	}: Props = $props()
 
-	let seeTarget: boolean | undefined = $state(undefined)
+	let canSeeTarget: 'yes' | 'cant-deploy-to-workspace' | 'cant-see-all-deps' | undefined =
+		$state(undefined)
 
 	let dependencies: { kind: Kind; path: string; include: boolean }[] | undefined = $state(undefined)
 
@@ -68,13 +70,20 @@
 			if (!$superadmin) {
 				await UserService.whoami({ workspace: workspaceToDeployTo! })
 			}
-			seeTarget = true
+			canSeeTarget = 'yes'
 		} catch {
-			seeTarget = false
+			canSeeTarget = 'cant-deploy-to-workspace'
 			return
 		}
 
-		const allDeps = await getDependencies(kind, path)
+		let allDeps
+		try {
+			allDeps = await getDependencies(kind, path)
+		} catch {
+			canSeeTarget = 'cant-see-all-deps'
+			return
+		}
+
 		let sortedSet: { kind: Kind; path: string }[] = []
 		allDeps.forEach((x) => {
 			if (!sortedSet.find((y) => y.kind == x.kind && y.path == x.path)) {
@@ -115,6 +124,7 @@
 				}
 			} else if (kind == 'flow') {
 				const flow = await FlowService.getFlowByPath({ workspace: $workspaceStore!, path })
+				console.log('getting modules')
 				return getAllModules(flow.value.modules, flow.value.failure_module).flatMap((x) => {
 					let result: { kind: Kind; path: string }[] = []
 					if (x.value.type == 'script' || x.value.type == 'rawscript' || x.value.type == 'flow') {
@@ -634,10 +644,10 @@
 	>
 	<input class="max-w-xs" type="text" disabled value={workspaceToDeployTo} />
 
-	{#if seeTarget == undefined}
+	{#if canSeeTarget == undefined}
 		<div class="mt-6"></div>
 		<Loader2 class="animate-spin" />
-	{:else if seeTarget == true}
+	{:else if canSeeTarget == 'yes'}
 		<h3 class="mb-6 mt-16">All related deployable items</h3>
 
 		<DiffDrawer bind:this={diffDrawer} {isFlow} />
@@ -714,6 +724,13 @@
 				><Button on:click={deployAll}>Deploy all toggled</Button></div
 			>
 		{/if}
+	{:else if canSeeTarget == 'cant-see-all-deps'}
+		<div class="my-2"></div>
+		<Alert type="error" title="User doesn't have visibility over all dependencies"
+			>You do not have visibility over some of the dependencies of this item. Ask a permissioned
+			user to deploy this item using the shareable link or get the proper permissions on the
+			dependencies</Alert
+		>
 	{:else}
 		<div class="my-2"></div>
 		<Alert type="error" title="User not allowed to deploy to this workspace"
