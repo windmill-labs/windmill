6965516bafe38ec745723abc9f627d6174d01d74
Diego Imbert <diego@windmill.dev>
2026-01-28 19:52:11 +0100

    better UI

--- frontend/src/lib/components/assets/AssetsUsageDrawer.svelte
+++ frontend/src/lib/components/assets/AssetsUsageDrawer.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
-	import type { AssetKind, AssetUsageAccessType, AssetUsageKind } from '$lib/gen'
+	import type { AssetKind } from '$lib/gen'
 	import { Drawer, DrawerContent } from '../common'
 	import RowIcon from '../common/table/RowIcon.svelte'
-	import { formatAssetAccessType, getAssetUsagePageUri } from './lib'
+	import { formatAssetAccessType, getAssetUsagePageUri, type AssetUsage } from './lib'
 	import Tooltip from '../meltComponents/Tooltip.svelte'
 	import { twMerge } from 'tailwind-merge'
-	import { workspaceStore } from '$lib/stores'
 	import { displayDate } from '$lib/utils'
 	import Alert from '../common/alert/Alert.svelte'
 
@@ -13,12 +12,7 @@
 		| {
 				path: string
 				kind: AssetKind
-				usages: {
-					path: string
-					kind: AssetUsageKind
-					access_type?: AssetUsageAccessType
-					created_at?: string
-				}[]
+				usages: AssetUsage[]
 		  }
 		| undefined = $state()
 
@@ -47,26 +41,7 @@
 			<!-- Static/Declared Usages -->
 			<section>
 				<h3 class="text-sm font-semibold mb-2">Scripts & Flows</h3>
-				<ul class="flex flex-col border rounded-md divide-y">
-					{#each staticUsages as u}
-						{@const accessType = formatAssetAccessType(u.access_type)}
-						<li>
-							<a
-								href={getAssetUsagePageUri(u)}
-								aria-label={`${u.kind}/${u.path}`}
-								class="text-xs text-primary font-normal flex items-center py-3 px-4 gap-2 hover:bg-surface-hover cursor-pointer"
-							>
-								<RowIcon kind={u.kind as Exclude<typeof u.kind, 'job'>} />
-								<div class="flex flex-col justify-center flex-1">
-									<span class="font-semibold text-emphasis">{u.path}</span>
-									<span class="text-2xs text-secondary">{u.kind}</span>
-								</div>
-								{@render badge(displayDate(u.created_at), 'Asset detection time')}
-								{@render badge(accessType)}
-							</a>
-						</li>
-					{/each}
-				</ul>
+				{@render list(staticUsages)}
 			</section>
 
 			<!-- Runtime Job Usage -->
@@ -82,26 +57,7 @@
 					It may take a few minutes for jobs to show up here.
 				</Alert>
 
-				<div
-					class={twMerge(
-						'flex flex-col border rounded-md divide-y',
-						!runtimeUsages.length ? 'hidden' : ''
-					)}
-				>
-					{#each runtimeUsages as u}
-						{@const accessType = formatAssetAccessType(u.access_type)}
-						<a
-							href={`/run/${u.path}?workspace=${$workspaceStore}`}
-							class="text-xs text-primary font-normal flex items-center py-3 px-4 gap-2 hover:bg-surface-hover cursor-pointer"
-						>
-							<div class="flex flex-col justify-center flex-1">
-								<span class="text-emphasis">{u.path}</span>
-							</div>
-							{@render badge(displayDate(u.created_at), 'Asset detection time')}
-							{@render badge(accessType)}
-						</a>
-					{/each}
-				</div>
+				{@render list(runtimeUsages)}
 			</section>
 		</div>
 	</DrawerContent>
@@ -124,4 +80,45 @@
 			</svelte:fragment>
 		</Tooltip>
 	{/if}
+{/snippet}
+
+{#snippet list(items: AssetUsage[])}
+	<ul class="flex flex-col border rounded-md divide-y">
+		{#each items as u}
+			{@const accessType = formatAssetAccessType(u.access_type)}
+
+			<li>
+				<a
+					href={getAssetUsagePageUri(u)}
+					aria-label={`${u.kind}/${u.path}`}
+					class="text-xs text-primary font-normal flex items-center py-3 px-4 gap-2 hover:bg-surface-hover cursor-pointer"
+				>
+					<RowIcon
+						kind={!u.metadata?.job_kind
+							? u.kind != 'job'
+								? u.kind
+								: 'script'
+							: ((
+									{
+										script: 'script',
+										preview: 'script',
+										flow: 'flow',
+										flowpreview: 'flow',
+										singlestepflow: 'flow',
+										flownode: 'flow'
+									} as const
+								)[u.metadata.job_kind] ?? 'script')}
+					/>
+					<div class="flex flex-col justify-center flex-1">
+						<span class="font-semibold text-emphasis">
+							{u.kind == 'job' ? (u.metadata?.runnable_path ?? 'Unknown job') : u.path}
+						</span>
+						<span class="text-2xs text-secondary">{u.kind == 'job' ? u.path : u.kind}</span>
+					</div>
+					{@render badge(displayDate(u.created_at), 'Asset detection time')}
+					{@render badge(accessType)}
+				</a>
+			</li>
+		{/each}
+	</ul>
 {/snippet}
