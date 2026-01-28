31f387d3e03cd6ab93879a88e6b9b3570a312160
Diego Imbert <diego@windmill.dev>
2026-01-28 20:18:55 +0100

    sql index

--- backend/migrations/20260122134517_runtime_assets.down.sql
+++ backend/migrations/20260122134517_runtime_assets.down.sql
@@ -1,3 +1,7 @@
+-- Drop indexes added in the up migration
+DROP INDEX IF EXISTS idx_asset_job_pruning;
+DROP INDEX IF EXISTS idx_asset_workspace_created_id;
+
 ALTER TABLE asset
   DROP COLUMN IF EXISTS created_at,
   DROP COLUMN IF EXISTS id;
