72dad39ad694394a65cf198ea96961022f6a57bd
Diego Imbert <diego@windmill.dev>
2026-02-03 18:22:07 +0100

    stash

--- frontend/src/routes/(root)/(logged)/assets/+page.svelte
+++ frontend/src/routes/(root)/(logged)/assets/+page.svelte
@@ -6,22 +6,32 @@
 	import S3FilePicker from '$lib/components/S3FilePicker.svelte'
 	import { Cell, DataTable } from '$lib/components/table'
 	import Head from '$lib/components/table/Head.svelte'
-	import { AssetService, type AssetKind, type ListAssetsResponse } from '$lib/gen'
+	import {
+		AssetService,
+		SettingService,
+		WorkspaceService,
+		type AssetKind,
+		type ListAssetsResponse
+	} from '$lib/gen'
 	import { userStore, workspaceStore, userWorkspaces } from '$lib/stores'
-	import { pluralize, truncate } from '$lib/utils'
+	import { capitalize, pluralize, truncate } from '$lib/utils'
 	import ExploreAssetButton, {
 		assetCanBeExplored
 	} from '../../../../lib/components/ExploreAssetButton.svelte'
 	import AssetsUsageDrawer from '$lib/components/assets/AssetsUsageDrawer.svelte'
 	import AssetGenericIcon from '$lib/components/icons/AssetGenericIcon.svelte'
 	import { Tooltip } from '$lib/components/meltComponents'
-	import { AlertTriangle, Loader2 } from 'lucide-svelte'
+	import { AlertTriangle, ArrowRight, Loader2, SettingsIcon } from 'lucide-svelte'
 	import { StaleWhileLoading, useInfiniteQuery, useScrollToBottom } from '$lib/svelte5Utils.svelte'
-	import { Debounced, watch } from 'runed'
+	import { Debounced, resource, watch } from 'runed'
 	import Label from '$lib/components/Label.svelte'
 	import MultiSelect from '$lib/components/select/MultiSelect.svelte'
 	import TextInput from '$lib/components/text_input/TextInput.svelte'
 	import RefreshButton from '$lib/components/common/button/RefreshButton.svelte'
+	import Section from '$lib/components/Section.svelte'
+	import Tabs from '$lib/components/common/tabs/Tabs.svelte'
+	import Tab from '$lib/components/common/tabs/Tab.svelte'
+	import Button from '$lib/components/common/button/Button.svelte'
 
 	interface AssetCursor {
 		created_at?: string
@@ -76,6 +86,43 @@
 	let s3FilePicker: S3FilePicker | undefined = $state()
 	let dbManagerDrawer: DbManagerDrawer | undefined = $state()
 	let assetsUsageDropdown: AssetsUsageDrawer | undefined = $state()
+
+	let labels = {
+		datatable: 'Data Table',
+		ducklake: 'Ducklake',
+		s3: 'Workspace Object Storage',
+		resource: 'Resource'
+	}
+	let settingsHrefs = {
+		datatable: '/workspace_settings?tab=windmill_data_tables',
+		ducklake: '/workspace_settings?tab=windmill_lfs',
+		s3: '/workspace_settings?tab=windmill_lfs',
+		resource: '/resources'
+	}
+	let selectedTab: keyof typeof labels = $state('datatable')
+
+	let allS3Storages = resource(
+		() => $workspaceStore,
+		() =>
+			SettingService.getSecondaryStorageNames({
+				workspace: $workspaceStore!,
+				includeDefault: true
+			})
+	)
+	let allDucklakes = resource(
+		() => $workspaceStore,
+		() => WorkspaceService.listDucklakes({ workspace: $workspaceStore! })
+	)
+	let allDataTables = resource(
+		() => $workspaceStore,
+		() => WorkspaceService.listDataTables({ workspace: $workspaceStore! })
+	)
+	let selectedTabAllData: string[] = $derived.by(() => {
+		if (selectedTab === 's3') return allS3Storages.current ?? []
+		if (selectedTab === 'ducklake') return allDucklakes.current ?? []
+		if (selectedTab === 'datatable') return allDataTables.current ?? []
+		return []
+	})
 </script>
 
 {#if $userStore?.operator && $workspaceStore && !$userWorkspaces.find((_) => _.id === $workspaceStore)?.operator_settings?.assets}
@@ -90,101 +137,132 @@
 			tooltip="Assets show up here whenever you use them in Windmill."
 			documentationLink="https://www.windmill.dev/docs/core_concepts/assets"
 		/>
-		<div class="flex gap-2 mb-4 items-end justify-between">
-			<div class="flex gap-2">
-				<Label class="lg:min-w-[16rem] max-w-[30rem]" label="Asset path">
-					<TextInput bind:value={assetPathFilter} />
-				</Label>
-				<Label class="lg:min-w-[16rem] max-w-[30rem]" label="Usage path">
-					<TextInput bind:value={usagePathFilter} />
-				</Label>
-				<Label class="lg:min-w-[8rem] max-w-[30rem]" label="Asset kinds">
-					<MultiSelect
-						hideMainClearBtn
-						bind:value={assetKindsFilter}
-						items={[
-							{ label: 'S3 Object', value: 's3object' },
-							{ label: 'Resource', value: 'resource' },
-							{ label: 'Ducklake', value: 'ducklake' },
-							{ label: 'Data Table', value: 'datatable' }
-						]}
-					/>
-				</Label>
-			</div>
-			<RefreshButton onClick={() => assetsQuery.reset()} loading={assetsQuery.isLoading} />
+		<Tabs bind:selected={selectedTab}>
+			{#each Object.entries(labels) as [kind, label]}
+				<Tab value={kind} {label} />
+			{/each}
+		</Tabs>
+		<div class="border-b mb-2 pb-8 pt-2">
+			{#if selectedTab === 'resource'}
+				<Button
+					href="/resources"
+					endIcon={{ icon: ArrowRight }}
+					variant="accent"
+					wrapperClasses="w-fit"
+				>
+					Go to resources
+				</Button>
+			{:else}
+				{JSON.stringify(selectedTabAllData)}
+				<Button
+					href={settingsHrefs[selectedTab]}
+					endIcon={{ icon: SettingsIcon }}
+					wrapperClasses="w-fit"
+				>
+					{capitalize(labels[selectedTab].toLowerCase())} settings
+				</Button>
+			{/if}
 		</div>
-		<DataTable>
-			<Head>
-				<tr>
-					<Cell head first class="w-16"></Cell>
-					<Cell head>Asset name</Cell>
-					<Cell head></Cell>
-					<Cell head></Cell>
-				</tr>
-			</Head>
-			<tbody class="divide-y bg-surface">
-				{#if assets != undefined && assets.length === 0}
-					<tr class="h-14">
-						<Cell></Cell>
-						<Cell colspan="3">No assets found</Cell>
-					</tr>
-				{/if}
-				{#each assets as asset}
-					{@const assetUri = formatAsset(asset)}
-					<tr class="h-14">
-						<Cell first class="w-16">
-							<Tooltip>
-								<AssetGenericIcon assetKind={asset.kind} size="16px" class="!text-secondary" />
-								<svelte:fragment slot="text">{formatAssetKind(asset)}</svelte:fragment>
-							</Tooltip>
-						</Cell>
-						<Cell class="flex flex-col">
-							<span>{truncate(asset.path, 92)}</span>
-							<span class="text-2xs text-secondary">{formatAssetKind(asset)}</span>
-						</Cell>
-						<Cell>
-							<a href={`#${assetUri}`} onclick={() => assetsUsageDropdown?.open(asset)}>
-								{pluralize(asset.usages.length, 'usage')}
-							</a>
-						</Cell>
-						<Cell class="w-24">
-							{#if assetCanBeExplored(asset, asset.metadata) && !$userStore?.operator}
-								<ExploreAssetButton
-									{asset}
-									{s3FilePicker}
-									{dbManagerDrawer}
-									_resourceMetadata={asset.metadata}
-								/>
-							{/if}
-							{#if asset.kind === 'resource' && asset.metadata === undefined}
-								<Tooltip class={'w-24 flex items-center justify-center'}>
-									<AlertTriangle size={20} class="text-orange-600 dark:text-orange-500" />
-									<svelte:fragment slot="text">Could not find resource</svelte:fragment>
-								</Tooltip>
-							{/if}
-						</Cell>
-					</tr>
-				{/each}
-				{#if assets == undefined && assetsQuery.isLoading}
-					<tr class="h-14">
-						<Cell colspan="4" class="text-center">
-							<div class="flex items-center justify-center gap-2">
-								<Loader2 class="animate-spin" size={16} />
-								<span>Loading assets...</span>
-							</div>
-						</Cell>
-					</tr>
-				{/if}
-			</tbody>
-		</DataTable>
-		{#if assetsQuery.isFetchingNextPage}
-			<Loader2 size={32} class="mx-auto my-4 text-primary animate-spin" />
-		{:else if assets?.length && !assetsQuery.hasNextPage}
-			<div class="text-center text-2xs text-secondary my-4"> No more assets to load </div>
-		{/if}
+		<Section label="Latest assets used">
+			<div class="flex gap-2 mb-4 items-end justify-between">
+				<div class="flex gap-2">
+					<Label class="lg:min-w-[16rem] max-w-[30rem]" label="Asset path">
+						<TextInput bind:value={assetPathFilter} />
+					</Label>
+					<Label class="lg:min-w-[16rem] max-w-[30rem]" label="Usage path">
+						<TextInput bind:value={usagePathFilter} />
+					</Label>
+					<Label class="lg:min-w-[8rem] max-w-[30rem]" label="Asset kinds">
+						<MultiSelect
+							hideMainClearBtn
+							bind:value={assetKindsFilter}
+							items={[
+								{ label: 'S3 Object', value: 's3object' },
+								{ label: 'Ducklake', value: 'ducklake' },
+								{ label: 'Data Table', value: 'datatable' }
+							]}
+						/>
+					</Label>
+				</div>
+				<RefreshButton onClick={() => assetsQuery.reset()} loading={assetsQuery.isLoading} />
+			</div>
+			{@render table()}
+			{#if assetsQuery.isFetchingNextPage}
+				<Loader2 size={32} class="mx-auto my-4 text-primary animate-spin" />
+			{:else if assets?.length && !assetsQuery.hasNextPage}
+				<div class="text-center text-2xs text-secondary my-4"> No more assets to load </div>
+			{/if}
+		</Section>
 	</CenteredPage>
 {/if}
 
 <AssetsUsageDrawer bind:this={assetsUsageDropdown} />
 <S3FilePicker bind:this={s3FilePicker} readOnlyMode />
 <DbManagerDrawer bind:this={dbManagerDrawer} />
+
+{#snippet table()}
+	<DataTable>
+		<Head>
+			<tr>
+				<Cell head first class="w-16"></Cell>
+				<Cell head>Asset name</Cell>
+				<Cell head></Cell>
+				<Cell head></Cell>
+			</tr>
+		</Head>
+		<tbody class="divide-y bg-surface">
+			{#if assets != undefined && assets.length === 0}
+				<tr class="h-14">
+					<Cell></Cell>
+					<Cell colspan="3">No assets found</Cell>
+				</tr>
+			{/if}
+			{#each assets as asset}
+				{@const assetUri = formatAsset(asset)}
+				<tr class="h-12">
+					<Cell first class="w-16">
+						<Tooltip>
+							<AssetGenericIcon assetKind={asset.kind} size="16px" class="!text-secondary" />
+							<svelte:fragment slot="text">{formatAssetKind(asset)}</svelte:fragment>
+						</Tooltip>
+					</Cell>
+					<Cell class="flex flex-col">
+						<span>{truncate(asset.path, 92)}</span>
+						<span class="text-2xs text-secondary">{formatAssetKind(asset)}</span>
+					</Cell>
+					<Cell>
+						<a href={`#${assetUri}`} onclick={() => assetsUsageDropdown?.open(asset)}>
+							{pluralize(asset.usages.length, 'usage')}
+						</a>
+					</Cell>
+					<Cell class="w-24">
+						{#if assetCanBeExplored(asset, asset.metadata) && !$userStore?.operator}
+							<ExploreAssetButton
+								{asset}
+								{s3FilePicker}
+								{dbManagerDrawer}
+								_resourceMetadata={asset.metadata}
+							/>
+						{/if}
+						{#if asset.kind === 'resource' && asset.metadata === undefined}
+							<Tooltip class={'w-24 flex items-center justify-center'}>
+								<AlertTriangle size={20} class="text-orange-600 dark:text-orange-500" />
+								<svelte:fragment slot="text">Could not find resource</svelte:fragment>
+							</Tooltip>
+						{/if}
+					</Cell>
+				</tr>
+			{/each}
+			{#if assets == undefined && assetsQuery.isLoading}
+				<tr class="h-14">
+					<Cell colspan="4" class="text-center">
+						<div class="flex items-center justify-center gap-2">
+							<Loader2 class="animate-spin" size={16} />
+							<span>Loading assets...</span>
+						</div>
+					</Cell>
+				</tr>
+			{/if}
+		</tbody>
+	</DataTable>
+{/snippet}
