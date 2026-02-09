963b1d936b602182b6b3af42f19c7aacb9aaab31
Ruben Fiszel <ruben@windmill.dev>
2026-02-09 15:06:53 +0000

    all

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -682,7 +682,6 @@ export function setInputCat(
 	enum_: any,
 	contentEncoding: string | undefined
 ): InputCat {
-	console.log(type, format, itemsType)
 	if (type === 'number' || type === 'integer') {
 		return 'number'
 	} else if (type === 'boolean') {
