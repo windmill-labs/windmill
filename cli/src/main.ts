a2d44ffbd0539999d9ad29cee2a6cede0382a087
rubenfiszel <275584+rubenfiszel@users.noreply.github.com>
2026-02-12 18:48:52 +0000

    Apply automatic changes

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -77,7 +77,7 @@ export {
 //   }
 // });
 
-export const VERSION = "1.633.1";
+export const VERSION = "1.634.0";
 
 // Re-exported from constants.ts to maintain backwards compatibility
 export { WM_FORK_PREFIX } from "./core/constants.ts";
