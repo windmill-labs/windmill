f582d0847f6e2c1625ef5c3fb3045ce0a9a16c3b
Ruben Fiszel <ruben@windmill.dev>
2026-02-13 01:05:50 +0100

    chore(main): release 1.634.1 (#7931)
    * chore(main): release 1.634.1
    
    * Apply automatic changes
    
    ---------
    
    Co-authored-by: rubenfiszel <275584+rubenfiszel@users.noreply.github.com>

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -77,7 +77,7 @@ export {
 //   }
 // });
 
-export const VERSION = "1.634.0";
+export const VERSION = "1.634.1";
 
 // Re-exported from constants.ts to maintain backwards compatibility
 export { WM_FORK_PREFIX } from "./core/constants.ts";
