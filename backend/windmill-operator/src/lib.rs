3b0f5f51ba3ab99bfe902f501f90818127466ba4
Ruben Fiszel <ruben@windmill.dev>
2026-02-16 17:00:35 +0000

    fix sqlx

--- backend/windmill-operator/src/lib.rs
+++ backend/windmill-operator/src/lib.rs
@@ -3,6 +3,8 @@ pub mod crd_ee;
 #[cfg(feature = "private")]
 pub mod db_sync_ee;
 #[cfg(feature = "private")]
+pub use db_sync_ee as db_sync;
+#[cfg(feature = "private")]
 pub mod reconciler_ee;
 #[cfg(feature = "private")]
 pub mod resolve_ee;
