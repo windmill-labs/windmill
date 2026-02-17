9461201842443edf74ed0c9e380c0eb1a190820d
rubenfiszel <275584+rubenfiszel@users.noreply.github.com>
2026-02-17 08:42:06 +0000

    Apply automatic changes

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -79,7 +79,7 @@ export {
 //   }
 // });
 
-export const VERSION = "1.638.0";
+export const VERSION = "1.638.1";
 
 // Re-exported from constants.ts to maintain backwards compatibility
 export { WM_FORK_PREFIX } from "./core/constants.ts";
--- powershell-client/WindmillClient/WindmillClient.psd1
+++ powershell-client/WindmillClient/WindmillClient.psd1
@@ -12,7 +12,7 @@
     RootModule        = 'WindmillClient.psm1'
 
     # Version number of this module.
-    ModuleVersion     = '1.638.0'
+    ModuleVersion     = '1.638.1'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
