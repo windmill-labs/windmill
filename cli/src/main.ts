8d03102883432b9d0c13a3d8d3ea4b29e92ed849
rubenfiszel <275584+rubenfiszel@users.noreply.github.com>
2026-02-26 18:36:41 +0000

    Apply automatic changes

--- cli/src/main.ts
+++ cli/src/main.ts
@@ -67,7 +67,7 @@ export {
   workspaceAdd,
 };
 
-export const VERSION = "1.645.0";
+export const VERSION = "1.646.0";
 
 // Re-exported from constants.ts to maintain backwards compatibility
 export { WM_FORK_PREFIX } from "./core/constants.ts";
