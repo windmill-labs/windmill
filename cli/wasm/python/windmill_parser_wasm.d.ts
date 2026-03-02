d51aa4bf76aa7c286a6054e161d44be234a88917
HugoCasa <hugo@casademont.ch>
2026-03-02 14:53:10 +0100

    remove unused wasms + fix build

--- cli/wasm/python/windmill_parser_wasm.d.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export function parse_python(code: string, main_override?: string | null): string;
-export function parse_assets_py(code: string): string;
