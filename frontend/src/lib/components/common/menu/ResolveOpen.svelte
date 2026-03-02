c2092653a0d6a8c480e57eded87c9be23166884a
Diego Imbert <diego@windmill.dev>
2026-03-02 16:42:31 +0100

    nit typing

--- frontend/src/lib/components/common/menu/ResolveOpen.svelte
+++ frontend/src/lib/components/common/menu/ResolveOpen.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
+	import { run } from 'svelte/legacy'
 
 	import { createDispatcherIfMounted } from '$lib/createDispatcherIfMounted'
 	import { createEventDispatcher } from 'svelte'
 
-	const dispatch = createEventDispatcher()
+	const dispatch = createEventDispatcher<{ open: void; close: void }>()
 	const dispatchIfMounted = createDispatcherIfMounted(dispatch)
 	interface Props {
-		open?: boolean;
+		open?: boolean
 	}
 
-	let { open = false }: Props = $props();
+	let { open = false }: Props = $props()
 
 	run(() => {
 		dispatchIfMounted(open ? 'open' : 'close')
-	});
+	})
 </script>
