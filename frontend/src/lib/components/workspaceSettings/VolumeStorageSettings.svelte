229a009b6c9573e715cce42edea7210de4dd53c4
Diego Imbert <diego@windmill.dev>
2026-03-12 16:28:48 +0100

    nit

--- frontend/src/lib/components/workspaceSettings/VolumeStorageSettings.svelte
+++ frontend/src/lib/components/workspaceSettings/VolumeStorageSettings.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+	import { goto } from '$lib/navigation'
+	import { page } from '$app/stores'
 	import { workspaceStore } from '$lib/stores'
 	import { emptyString, sendUserToast } from '$lib/utils'
 	import SettingsPageHeader from '../settings/SettingsPageHeader.svelte'
 	import SettingsFooter from './SettingsFooter.svelte'
 	import Select from '../select/Select.svelte'
-	import {
-		convertFrontendToBackendSetting,
-		type S3ResourceSettings
-	} from '$lib/workspace_settings'
+	import { Button } from '../common'
+	import Alert from '../common/alert/Alert.svelte'
+	import { convertFrontendToBackendSetting, type S3ResourceSettings } from '$lib/workspace_settings'
 	import { WorkspaceService } from '$lib/gen'
 
 	let {
@@ -50,6 +51,14 @@
 		}
 		return items
 	})
+
+	let hasAvailableStorage = $derived(volumeStorageItems.length > 1)
+
+	function goToStorageSettings() {
+		const params = new URLSearchParams($page.url.searchParams)
+		params.set('tab', 'windmill_lfs')
+		goto(`?${params.toString()}`)
+	}
 </script>
 
 <SettingsPageHeader
@@ -58,24 +67,33 @@
 	link="https://www.windmill.dev/docs/core_concepts/persistent_storage/volumes"
 />
 {#if s3ResourceSettings}
-	<div class="max-w-sm mt-2">
-		<Select
-			items={volumeStorageItems}
-			bind:value={
-				() => s3ResourceSettings.volumeStorage ?? '',
-				(v) => {
-					s3ResourceSettings.volumeStorage = v || undefined
+	{#if hasAvailableStorage}
+		<div class="max-w-sm mt-2">
+			<Select
+				items={volumeStorageItems}
+				bind:value={
+					() => s3ResourceSettings.volumeStorage ?? '',
+					(v) => {
+						s3ResourceSettings.volumeStorage = v || undefined
+					}
 				}
-			}
-		/>
-	</div>
+			/>
+		</div>
 
-	<SettingsFooter
-		class="mt-5 mb-5"
-		inline
-		{hasUnsavedChanges}
-		onSave={saveVolumeStorageSettings}
-		onDiscard={() => onDiscard?.()}
-		saveLabel="Save volume storage settings"
-	/>
+		<SettingsFooter
+			class="mt-5 mb-5"
+			inline
+			{hasUnsavedChanges}
+			onSave={saveVolumeStorageSettings}
+			onDiscard={() => onDiscard?.()}
+			saveLabel="Save volume storage settings"
+		/>
+	{:else}
+		<Alert type="info" title="No workspace storage configured" class="mt-4">
+			You need to configure a workspace object storage before you can use volumes.
+		</Alert>
+		<Button wrapperClasses="mt-2" variant="default" size="sm" on:click={goToStorageSettings}>
+			Go to Object storage settings
+		</Button>
+	{/if}
 {/if}
