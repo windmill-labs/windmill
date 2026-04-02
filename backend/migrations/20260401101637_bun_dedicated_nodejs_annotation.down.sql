ee5420e401ab9b63d5623119c78edd4b023081c2
hugocasa <hugo@casademont.ch>
2026-04-03 01:08:10 +0200

    Revert "feat: restore bun for dedicated workers, fix dispatch & serialization, cross-workspace deps (#8645)"
    This reverts commit 619ebb65ce8dce8264add31c3147919802a8286a.

--- backend/migrations/20260401101637_bun_dedicated_nodejs_annotation.down.sql
+++
@@ -1,8 +0,0 @@
--- Remove the //nodejs annotation that was prepended by the up migration.
--- Only removes it if it's at the very start of the content.
-UPDATE script
-SET content = REPLACE(content, '//nodejs
-// dedicated workers were previously running in nodejs mode by default, remove this annotation to use bun
-', '')
-WHERE language = 'bun'
-  AND dedicated_worker = true;
