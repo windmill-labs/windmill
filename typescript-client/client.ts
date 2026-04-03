ff0035ed8373cf6a13c916de7d5f3670ea16894d
Diego Imbert <diego@windmill.dev>
2026-04-03 16:42:12 +0200

    typescript uses 127.0.0.1

--- typescript-client/client.ts
+++ typescript-client/client.ts
@@ -44,7 +44,7 @@ export const SHARED_FOLDER = "/shared";
 let mockedApi: MockedApi | undefined = undefined;
 
 export function workerHasInternalServer(): boolean {
-  return /^(https?:\/\/)?localhost/.test(OpenAPI.BASE ?? "");
+  return /^https?:\/\/127\.0\.0\.1(:|\/|$)/.test(OpenAPI.BASE ?? "");
 }
 
 /**
