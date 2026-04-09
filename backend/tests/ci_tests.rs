5dbb5a57f5690668ab717baea549c3d528d99f62
hugocasa <hugo@casademont.ch>
2026-04-09 15:21:43 +0200

    fix build/sqlx

--- backend/tests/ci_tests.rs
+++ backend/tests/ci_tests.rs
@@ -29,7 +29,7 @@ mod ci_tests {
             schema: HashMap::new(),
             ws_error_handler_muted: Some(false),
             priority: None,
-            delete_after_use: None,
+            delete_after_secs: None,
             timeout: None,
             restart_unless_cancelled: None,
             deployment_message: None,
